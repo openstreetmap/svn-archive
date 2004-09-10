f697d68d7229b56934e465b3a9486ed22127dacb
Steve Coast <steve@asklater.com>
2004-09-10 14:37:50 +0000

    b

--- src/osmApplet.java
+++ src/osmApplet.java
@@ -11,7 +11,6 @@ public class osmApplet extends JApplet {
 
     
     public void init() {
-        JLabel label = new JLabel("blah");
 
         //Add border.  Should use createLineBorder, but then the bottom
         //and left lines don't appear -- seems to be an off-by-one error.

b962e508467d57c23ffccf7a072c8d4b8985cd49
Steve Coast <steve@asklater.com>
2004-09-14 12:56:53 +0000

    making the applet also behave as an app

--- src/osmApplet2.java
+++ src/osmApplet2.java
@@ -1,60 +1,51 @@
+import java.util.*;
+import java.lang.*;
 import javax.swing.*;
 import java.awt.*;
-
-import com.bbn.openmap.MapBean;
-import com.bbn.openmap.layer.shape.ShapeLayer;
-
-import java.util.Properties;
-
 
 
 
 public class osmApplet2 extends JApplet {
-    JLabel label = new JLabel("OpenStreetMap pre-pre-pre alpha. (control-)Mouse drag to do things");
     
-    public osmApplet2() {
-        getRootPane().putClientProperty("defeatSystemEventQueueCheck",
-                                        Boolean.TRUE);
-    } // osmApplet
+    //public osmApplet2() {
+    //    getRootPane().putClientProperty("defeatSystemEventQueueCheck",
+    //                                    Boolean.TRUE);
+    //} // osmApplet
 
     
     public void init() {
 
-        //Add border.  Should use createLineBorder, but then the bottom
-        //and left lines don't appear -- seems to be an off-by-one error.
 
-        MapBean mapBean = new MapBean();
-        getContentPane().add(label, BorderLayout.SOUTH);
-        
-        osmPointsLayer shapeLayer = new osmPointsLayer();
-        Properties shapeLayerProps = new Properties();
-        
-        shapeLayerProps.put("prettyName", "Political Solid");
-        shapeLayerProps.put("lineColor", "000000");
-        shapeLayerProps.put("fillColor", "BDDE83");
-        shapeLayer.setProperties(shapeLayerProps);
-
-        // Add the political layer to the map
-        mapBean.add(shapeLayer);
-
-        // Add the map to the frame
-        getContentPane().add(mapBean);
-
-       
+      new osmDisplay( getContentPane() );       
 
         
         
-       // getContentPane().add(somepane, BorderLayout.CENTER);
-
         
     } // init
 
     
-    public void setStatusLabel(String s)
+    public static void main(String[] args)
     {
 
-      label.setText(s);
+      new osmApplet2().go();
 
-      label.repaint();
+    } // main
+
+    public osmApplet2()
+    {
+
+    } // osmApplet2
+
+    public void go()
+    {
+      JFrame jf = new JFrame("blah");
+      
+      jf.setSize(600,600);
+      jf.show();
+      
+      new osmDisplay(jf.getContentPane());
+ 
+      jf.pack();
+      
     } 
 }
---
+++ src/osmDisplay.java
@@ -0,0 +1,41 @@
+import java.lang.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
+import com.bbn.openmap.MapBean;
+import com.bbn.openmap.layer.shape.ShapeLayer;
+
+import java.util.Properties;
+
+
+
+public class osmDisplay
+{
+    JLabel label = new JLabel("OpenStreetMap pre-pre-pre alpha. (control-)Mouse drag to do things");
+
+    public osmDisplay(Container cp)
+    {
+
+        MapBean mapBean = new MapBean();
+        cp.add(label, BorderLayout.SOUTH);
+        
+        osmPointsLayer shapeLayer = new osmPointsLayer();
+        Properties shapeLayerProps = new Properties();
+        
+        shapeLayerProps.put("prettyName", "Political Solid");
+        shapeLayerProps.put("lineColor", "000000");
+        shapeLayerProps.put("fillColor", "BDDE83");
+        shapeLayer.setProperties(shapeLayerProps);
+
+        // Add the political layer to the map
+        mapBean.add(shapeLayer);
+
+        // Add the map to the frame
+        cp.add(mapBean);
+
+       
+        mapBean.repaint();
+    }
+ 
+} // osmDisplay

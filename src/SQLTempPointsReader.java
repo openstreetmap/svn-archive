8765010dcea71824153544108105abb5dabd58fa
Steve Coast <steve@asklater.com>
2004-09-14 16:48:51 +0000

    license stuff

--- src/SQLTempPointsReader.java
+++ src/SQLTempPointsReader.java
@@ -1,5 +1,5 @@
 /*
-Copyright (C) Stephen Coast (steve@fractalus.com)
+Copyright (C) 2004 Stephen Coast (steve@fractalus.com)
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License

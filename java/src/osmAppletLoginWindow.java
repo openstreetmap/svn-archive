a47d772198f52023b5915426b54e15ad87ed1ea8
Steve Coast <steve@asklater.com>
2004-12-06 00:49:30 +0000

    cool

---
+++ java/src/osmAppletLoginWindow.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.util.*;
+import java.lang.*;
+
+public class osmAppletLoginWindow extends JFrame
+{
+  
+  void osmAppletLoginWindow()
+  {
+
+
+
+  } // osmAppletLoginWindow
+
+
+
+
+} // osmAppletLoginWindow

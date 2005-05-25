2f290c86febe96d2034562435207eb3311ae414a
ben <ben@svn.openstreetmap.org>
2005-05-25 17:29:12 +0000

    grr, don't stomp the log

--- utils/ny_import/import_tiger.bash
+++ utils/ny_import/import_tiger.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./import_tiger.rb ny_county/TGR36061.RT1 ny_county/TGR36061.RT2 $1 >import_tiger.log 2>&1 </dev/null &
+./import_tiger.rb ny_county/TGR36061.RT1 ny_county/TGR36061.RT2 $1 >>import_tiger.log 2>&1 </dev/null &
 

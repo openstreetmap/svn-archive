fbe16c8c9abe3df1d63e1304767bd7bd97f0127f
ben <ben@svn.openstreetmap.org>
2005-06-18 11:22:23 +0000

    final tweaks to drop the CGI wrappings

--- utils/ny_import/t2dxf/dxf.rb
+++ utils/ny_import/t2dxf/dxf.rb
@@ -18,7 +18,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 =end
 
-require "cgi"
 require "net/http"
 require "tempfile"
 require "uri"
@@ -27,13 +26,7 @@ require "geometry"
 require "tiger"
 require "utm"
 
-SCRIPT = "http://80.68.92.194/cgi-bin/ben/dxf"
 DEFAULT_SOURCE = "http://www2.census.gov/geo/tiger/tiger2004fe/NY/tgr36061.zip"
-CODE_BASE = "http://80.68.92.194/ben/"
-
-def content_type
-  return "Content-type: text/plain\r\n\r\n"
-end
 
 def header(min, max)
   return <<__EOF__
@@ -136,84 +129,23 @@ def wget(url)
   end
 end
 
-as_cgi = ! (ENV['SCRIPT_NAME'].nil? || ENV['SCRIPT_NAME'].empty?)
+raise "Pass a URL to a TIGER county .ZIP as an argument" unless ARGV.length == 1
+tiger_url = ARGV.first
+zip = wget(tiger_url)
+tmp = Tempfile.new("t2dxf")
+tmp.print(zip)
+tmp.close
+rt1 = `/usr/bin/env unzip -qq -p #{tmp.path} *.RT1`
+raise "Could not extract .RT1 file" unless $?.to_i.zero?
+rt2 = `/usr/bin/env unzip -qq -p #{tmp.path} *.RT2`
+raise "Could not extract .RT2 file" unless $?.to_i.zero?
+tmp.unlink
 
-if as_cgi
-  cgi = CGI.new("html3")
-
-  if cgi['source'].nil? || cgi['source'].empty?
-    cgi.out {
-      cgi.head { cgi.title { "T2DXF" } } +
-      cgi.body { cgi.form("GET") {
-        cgi.h3 { "T2DXF" } +
-        cgi.p {
-          "Enter a TIGER source URL:" + cgi.br +
-          cgi.text_field("source", DEFAULT_SOURCE, 60) + "&nbsp;" +
-          cgi.submit("View as DXF")
-        }
-      } }
-    }
-    exit
-  end
-
-  unless cgi.params.keys.member?("dxf")
-    dxf_url = "#{SCRIPT}?source=#{CGI.escape(cgi['source'])}&dxf"
-    cgi.out {
-      cgi.head { cgi.title { "T2DXF Viewer" } } +
-      cgi.body {
-        cgi.p { <<__EOF__
-<applet codebase="#{CODE_BASE}" archive="dxfviewer.jar" code="de.escape.quincunx.dxf.DxfViewer" width="640" height="480" name="DXF_Viewer"><param name="file" value="#{dxf_url}"><param name="framed" value="false"></applet>
-__EOF__
-        } +
-        cgi.p {
-          cgi.a(dxf_url) { "DXF Source" }
-        }
-      }
-    }
-    exit
-  end
-end
-
-begin
-  if as_cgi
-    tiger_url = cgi['source']
-  else
-    raise "Pass a URL to a TIGER county .ZIP as an argument" unless ARGV.length == 1
-    tiger_url = ARGV.first
-  end
-  zip = wget(tiger_url)
-  tmp = Tempfile.new("t2dxf")
-  tmp.print(zip)
-  tmp.close
-  rt1 = `/usr/bin/env unzip -qq -p #{tmp.path} *.RT1`
-  raise "Could not extract .RT1 file" unless $?.to_i.zero?
-  rt2 = `/usr/bin/env unzip -qq -p #{tmp.path} *.RT2`
-  raise "Could not extract .RT2 file" unless $?.to_i.zero?
-  tmp.unlink
-
-  tiger = Tiger.import(rt1, rt2)
-  lines_dxf, min, max = lines(tiger)
-  buffer = ''
-  buffer += content_type
-  buffer += header(min, max)
-  buffer += lines_dxf
-  buffer += closer
-  $stdout.print buffer
-
-rescue => ex
-  if as_cgi
-    cgi.out {
-      cgi.head { cgi.title { "T2DXF" } } +
-      cgi.body {
-        cgi.h3 { "<font color=\"red\">T2DXF Error</font>" } +
-        cgi.p {
-          ex.message + cgi.br +
-          ex.backtrace.join(cgi.br)
-        }
-      }
-    }
-  else
-    raise
-  end
-end
+tiger = Tiger.import(rt1, rt2)
+lines_dxf, min, max = lines(tiger)
+buffer = ''
+buffer += header(min, max)
+buffer += lines_dxf
+buffer += closer
+$stdout.print buffer
 

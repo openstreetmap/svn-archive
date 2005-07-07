bd36e8d3600f64ce3c65197ec91cd468bb458c36
Steve Coast <steve@asklater.com>
2005-07-07 17:08:01 +0000

    obfuscate the email address

--- ruby/rss/gpx_files.rb
+++ ruby/rss/gpx_files.rb
@@ -38,7 +38,7 @@ begin
     item = RSS::Rss::Channel::Item.new
     item.title = row['name']
     item.link = "http://www.openstreetmap.org/edit/applet.jsp?lat=#{row['latitude']}&lon=#{row['longitude']}&scale=10404.917"
-    item.description = "GPX file made by #{row['user']} with #{row['count']} points."
+    item.description = "GPX file made by #{row['user'].gsub('.',' dot ').gsub('@',' at ')} with #{row['count']} points."
     item.date = Time.at( row["timestamp"].to_i / 1000 )
     chan.items << item 
 

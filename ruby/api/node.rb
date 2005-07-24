abbcd23d1802ed99578aec4ea7977b88126ce11b
Steve Coast <steve@asklater.com>
2005-07-24 18:55:42 +0000

    fix node to call a singleton

--- ruby/api/node.rb
+++ ruby/api/node.rb
@@ -16,7 +16,7 @@ nodeid = cgi['nodeid'].to_i
 
 
 if nodeid != 0
-  dao = OSM::Dao.new
+  dao = OSM::Dao.instance
   gpx = OSM::Gpx.new
 
   node = dao.getnode(nodeid)
@@ -24,31 +24,4 @@ if nodeid != 0
   gpx.addnode(node)
   puts gpx
 end
-
-
-
-#if token != 'ERROR'
-#  login = Element.new 'login', {'a' => 'b'}
-#  puts login
-#  puts 'hello'
-#else
-#  puts 'hrm'
-#  exit(Apache::HTTP_UNAUTHORIZED)
-#end
-
-
-
-
-#el1 = Element.new "myelement" 
-#el1.text = "Hello world!" 
-# -> <myelement>Hello world!</myelement> 
-#el1.add_text "Hello dolly" 
-# -> <myelement>Hello world!Hello dolly</element> 
-#el1.add Text.new("Goodbye") 
-# -> <myelement>Hello world!Hello dollyGoodbye</element> 
-#el1 << Text.new(" cruel world") 
-# -> <myelement>Hello world!Hello dollyGoodbye cruel world</element>
-
-#doc.add el1
-#puts doc
 

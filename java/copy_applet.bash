9c467a510d7ac80291d142e5538139d91eec6cc0
Steve Coast <steve@asklater.com>
2005-09-14 16:12:18 +0000

    getting rid of jsp

---
+++ java/copy_applet.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd dist
+cp OSMApplet.jar xmlrpc-2.0-beta.jar commons-codec-1.3.jar core.jar /var/www/openstreetmap/edit/
--- java/jsp/include/loginMessage.jsp
+++
@@ -1,1 +0,0 @@
-You need to log in. Please go <a href="/edit/login.jsp">here</a> to do so!<br>

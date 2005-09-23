2e1999d83a59f5bbebcb9dd5e5da81aa25b1339f
Nick Whitelegg <nick_whitelegg@yahoo.co.uk>
2005-09-23 14:25:31 +0000

    added RouteMetaDataHandler.cpp - missing from last commit

---
+++ osm-editor/RouteMetaDataHandler.cpp
@@ -0,0 +1,56 @@
+#include "RouteMetaDataHandler.h"
+
+#include <iostream>
+using std::cout;
+using std::cerr;
+using std::endl;
+
+namespace OpenStreetMap
+{
+
+// route classes (provisional):
+// 1=unsurfaced path; 2=unsurfaced track; 3=surfaced (town) path; 
+// 4=country lane; 5=residential road; 
+// 6=unclassified town road; 7=secondary road; 8=primary road; 9=motorway;
+// 16=railway.
+//
+// Permissions can be "yes", "no" or "unofficial"
+
+RouteMetaDataHandler::RouteMetaDataHandler()
+{
+	rData["footpath"] = RouteMetaData ("yes","no", "no", "no",   "path");
+	rData["permissive footpath"]=RouteMetaData ("unofficial", "no", "no", "no",
+												  "path");
+	rData["bridleway"] = RouteMetaData ("yes", "yes", "yes", "no",  "path"); 
+	rData["permissive bridleway"] = RouteMetaData ("unofficial", "unofficial", 
+												 "no", "no",  "path");
+	rData["cycle path"] = RouteMetaData ("unofficial", "no", "unofficial",  
+										"no", "path");
+	rData["byway"] = RouteMetaData ("yes", "yes", "yes", "yes",  "unsurfaced");
+	rData["minor road"] = RouteMetaData ("yes", "yes", "yes", "yes",  "minor");
+	rData["B road"] = RouteMetaData ("yes", "yes", "yes", "yes",  "secondary");
+	rData["A road"] = RouteMetaData ("yes", "yes", "yes", "yes",  "primary");
+	rData["motorway"] = RouteMetaData ("no", "no", "no", "yes",  "motorway");
+	rData["railway"] = RouteMetaData ("no", "no", "no", "no",  "railway");
+}
+
+RouteMetaData RouteMetaDataHandler::getMetaData(const QString& type) 
+							throw (QString)
+{
+	std::map<QString,RouteMetaData>::iterator i = rData.find(type);
+	if (i==rData.end())
+		throw QString("Unknown segment type: " + type); 
+	return i->second;
+}
+
+QString RouteMetaDataHandler::getRouteType(const RouteMetaData &rd) 
+{
+	for(std::map<QString,RouteMetaData>::iterator i=rData.begin();
+		i!=rData.end(); i++)
+	{
+		if (i->second==rd)
+			return i->first;
+	}
+	return "track";
+}
+}

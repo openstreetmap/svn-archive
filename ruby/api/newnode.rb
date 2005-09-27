401802375d672f6df9ba3e6a7183c0165743978c
Steve Coast <steve@asklater.com>
2005-09-27 15:20:51 +0000

    whoops, newnode ruby code

---
+++ ruby/api/newnode.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/ruby -w
+
+
+require 'cgi'
+load 'osm/dao.rb'
+require 'osm/gpx'
+require 'rexml/document'
+
+include Apache
+include REXML
+
+r = Apache.request
+
+dao = OSM::Dao.instance
+
+if r.request_method == "PUT"
+  r.setup_cgi_env
+  userid = dao.useruidfromcreds(r.user, r.get_basic_auth_pw)
+  doc = Document.new $stdin.read
+  doc.elements.each('gpx/wpt') do |pt|
+    lat = pt.attributes['lat'].to_f
+    lon = pt.attributes['lon'].to_f
+
+    if userid > 0
+      #FIXME: need to check the node hasn't moved too much
+      new_node_uid = dao.create_node(lat, lon, userid)
+         
+      if new_node_uid
+        print new_node_uid
+      else
+        exit HTTP_INTERNAL_SERVER_ERROR
+      end
+    else
+      exit AUTH_REQUIRED
+    end
+  end
+end

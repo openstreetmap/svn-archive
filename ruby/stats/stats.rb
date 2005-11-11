cb6a94d36f3b5480ca438299af1fff154276da81
Steve Coast <steve@asklater.com>
2005-11-11 11:33:39 +0000

    better sql, gets the number of segments with names too

--- ruby/stats/stats.rb
+++ ruby/stats/stats.rb
@@ -18,23 +18,24 @@ begin
 
   #connect to the MySQL server
   dbh = Mysql.real_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASS, MYSQL_DATABASE)
-
-  res = dbh.query('select * from (select count(*) as users from user where active = true) as h, (select count(*) as gpspoints from tempPoints) as j, (select count(*) as nodes from node_meta_table) as k, (select count(*) as segments from street_segment_meta_table) as l;')
-
+  q = 'select * from (select count(*) as users from user where active = true) as h, (select count(*) as gpspoints from tempPoints) as j, (select count(*) as nodes from node_meta_table) as k, (select count(*) as segments from street_segment_meta_table) as l, (select count(*) as seg_names from street_segments where tags != \'\') as m;'
+#  puts q
+  res = dbh.query(q)
   res.each_hash do |row|
     puts "<table>
       <tr><td>Number of users</td><td>#{row['users']}</td></tr>
       <tr><td>Number of uploaded GPS points</td><td>#{row['gpspoints']}</td></tr>
       <tr><td>Number of nodes</td><td>#{row['nodes']}</td></tr>
       <tr><td>Number of line segments</td><td>#{row['segments']}</td></tr>
+      <tr><td>Number of segments with tags</td><td>#{row['seg_names']}</td></tr>
       </table><br>"
   end
 
   puts '<h2>Top 10 users for uploads of gps data</h2><table><tr><td><b>User</b></td><td><b>No. of points</b></td></tr>'
-  res = dbh.query('select user, count from (select uid, count(*) as count from tempPoints group by uid) as a, (select * from user) as b where a.uid = b.uid order by count desc limit 10;')
+  res = dbh.query('select sum(size) as size, user from points_meta_table, user where user_uid = user.uid group by user_uid order by size desc limit 10;')
 
   res.each_hash do |row|
-    puts "<tr><td>#{row['user']}</td><td>#{row['count']}</td></tr>"
+    puts "<tr><td>#{row['user']}</td><td>#{row['size']}</td></tr>"
   end
   
   puts '</table>'

42db2e71438bf80eb3b2987e804bf1baebfba70d
Steve Coast <steve@asklater.com>
2006-01-30 19:27:06 +0000

    Applying David Oesterhaus <david82@gmx.net>'s patch to make everything go in one jar

--- java/runapplet.sh
+++ java/runapplet.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 echo run me with user and pass
-java -cp dist/osm.jar:dist/OSMApplet.jar:dist/core.jar:dist/commons-logging.jar:dist/MinML2.jar:dist/commons-codec-1.3.jar:dist/commons-httpclient-3.0-rc3.jar processing.core.PApplet org.openstreetmap.processing.OSMApplet $1 $2
+java -cp dist/OSMApplet.jar processing.core.PApplet org.openstreetmap.processing.OSMApplet $1 $2

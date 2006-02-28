247a358df782c1501bd6cfe749b6954016021dc8
Steve Coast <steve@asklater.com>
2006-02-28 01:31:44 +0000

    upgrading to eruby...

--- other/freethepostcode.org/ruby/rss.rb
+++ other/freethepostcode.org/ruby/rss.rb
@@ -1,53 +1,33 @@
 #!/usr/bin/ruby
 require 'cgi'
-require "mysql"
 require 'rss/2.0'
+require 'dao.rb'
 
-
-MYSQL_SERVER = "127.0.0.1"
-MYSQL_USER = "postcode"
-MYSQL_PASS = "kc8dFusmw"
-MYSQL_DATABASE = "postcode"
-
+dao = OSM::Dao.instance
 cgi = CGI.new
 
 latitude = cgi['latitude'].to_f
 longitude = cgi['longitude'].to_f
 
-begin
+res = dao.call_sql { "select part1, part2, lat, lon, date from codes where confirmed = 1 order by date desc limit 10;" }
+rss = RSS::Rss.new("2.0")
+chan = RSS::Rss::Channel.new
+chan.title = "Free the postcode!"
+chan.description = "Latest postcodes submitted to freethepostcode.org"
+chan.link = 'http://www.freethepostcode.org/'
+rss.channel = chan
 
-  #connect to the MySQL server
-  dbh = Mysql.real_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASS, MYSQL_DATABASE)
-  # get server version string and display it
+res.each_hash do |row|
 
-  res = dbh.query("select part1, part2, lat, lon, date from codes where confirmed = 1 order by date desc limit 10;")
-  rss = RSS::Rss.new("2.0")
-  chan = RSS::Rss::Channel.new
-  chan.title = "Free the postcode!"
-  chan.description = "Latest postcodes submitted to freethepostcode.org"
-  chan.link = 'http://www.freethepostcode.org/'
-  rss.channel = chan
+  item = RSS::Rss::Channel::Item.new
 
-  res.each_hash do |row|
-    
-    item = RSS::Rss::Channel::Item.new
+  item.title = row['part1'] + ' ' + row['part2']
+  item.link = "http://www.freethepostcode.org/currentlist"
 
-    item.title = row['part1'] + ' ' + row['part2']
-    item.link = "http://www.freethepostcode.org/currentlist"
-    
-    item.description = row['lat'] + '/' + row['lon']
-    item.date = Time.parse(row['date'])
-    chan.items << item 
+  item.description = row['lat'] + '/' + row['lon']
+  item.date = Time.parse(row['date'])
+  chan.items << item 
 
-  end
-
-  puts rss.to_s
-
-rescue MysqlError => e
-  print "Error code: ", e.errno, "\n"
-  print "Error message: ", e.error, "\n"
-
-ensure
-  # disconnect from server
-  dbh.close if dbh
 end
+
+puts rss.to_s

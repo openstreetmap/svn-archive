647b19d1fec46b58277a90a89693cdbb7406b3cc
Steve Coast <steve@asklater.com>
2006-03-14 00:54:19 +0000

    commify function

--- ruby/api/osm/dao.rb
+++ ruby/api/osm/dao.rb
@@ -995,6 +995,15 @@ module OSM
       return multis
     end
 
+    def commify(number)
+      c = { :value => "", :length => 0 }
+      r = number.to_s.reverse.split("").inject(c) do |t, e|  
+        iv, il = t[:value], t[:length]
+        iv += ',' if il % 3 == 0 && il != 0    
+        { :value => iv + e, :length => il + 1 }
+      end
+      r[:value].reverse!
+    end
   end
 end
 

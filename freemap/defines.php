87a18d299e8bd6c9ed290aedf2d4397de6aee3f9
Steve Coast <steve@asklater.com>
2006-07-03 10:26:59 +0000

    move wms stuff

--- freemap/defines.php
+++ freemap/defines.php
@@ -1,13 +1,13 @@
 <?php
 
-define ('DB_HOST','localhost');
-define ('DB_DBASE','nick');
-define ('DB_USERNAME','nick');
-define ('DB_PASSWORD','??????????????????????');
+define ('DB_HOST','db.openstreetmap.org');
+define ('DB_DBASE','openstreetmap');
+define ('DB_USERNAME','osm-select');
+define ('DB_PASSWORD','g8HnskCs');
 define ('BRUSH_SUPPORTED', 1);
 define ('MAP_WIDTH',400);
 define ('MAP_HEIGHT',320);
-define ('ADMIN_MAIL','nick@hogweed.org');
+define ('ADMIN_MAIL','steve@asklater.com');
 define ('TRUETYPE_FONT','/var/www/data/luxisr.ttf');
 define ('FONT_MARGIN',2);
 define ('CLICK_MODE_HELP', 0);

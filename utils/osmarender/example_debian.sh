c75276f9011eec36d7caa38782310c577f0f198d
Jörg Ostertag <openstreetmap@ostertag.name>
2006-07-19 08:28:31 +0000

    automagically get planet.osm and render a default region

--- utils/osmarender/example_debian.sh
+++ utils/osmarender/example_debian.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
-sudo apt-get install xalan
+
+# check if xalan is installed
+xalan -v || sudo apt-get install xalan
+
+# check if librsvg2-bin is installed
+rsvg-convert -v || sudo apt-get install librsvg2-bin
+
+# get current planet.osm
+export planet_file=planet-2006-07-a.osm.bz2
+export planet_dir=~/.gpsdrive/MIRROR/osm
+
+mkdir -p $planet_dir
+
+echo "Check $planet_file"
+wget -nd -P "$planet_dir" -nv  --mirror http://www.ostertag.name/osm/planet/$planet_file
+
+ln -s "$planet_dir/$planet_file" data.osm
+
+
+# Start rendering
 xalan -in osm-map-features.xml -out data.svg
+
+# svg --> png
+rsvg-convert data.svg >data.png
+

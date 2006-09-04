c1bb9b9feeae4f9abf6eb4c61b5e59c55116c8e3
Jörg Ostertag <openstreetmap@ostertag.name>
2006-09-04 07:04:27 +0000

    new tools for managing lwp requests

---
+++ utils/perl/Utils/LWP/Utils.pm
@@ -0,0 +1,67 @@
+##################################################################
+package Utils::LWP::Utils;
+##################################################################
+
+use Exporter;
+@ISA = qw( Exporter );
+use vars qw(@ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $VERSION);
+@EXPORT = qw( mirror_file 
+	      $PROXY
+	      $NO_MIRROR);
+
+use strict;
+use warnings;
+
+use Utils::Debug;
+
+our $PROXY='';
+
+our $NO_MIRROR=0;
+
+sub mirror_file($$){
+    my $url            = shift;
+    my $local_filename = shift;
+
+    my $mirror=1;
+
+
+    return 1 if $NO_MIRROR;
+
+    # LPW::UserAgent initialisieren
+    my $ua = LWP::UserAgent->new;
+
+    # Set Proxy from Environment
+    if (!$PROXY) {
+        $PROXY ||= $ENV{'PROXY'};
+        $PROXY ||= $ENV{'http_proxy'};
+        print "Set Proxy to $PROXY\n" if $PROXY;
+    }
+    if ( $PROXY ){
+        $PROXY = "http://$PROXY" unless $PROXY =~ m,^.?.tp://,;
+        $PROXY = "$PROXY/"       unless $PROXY =~ m,/$,;
+        $ua->proxy(['http','ftp'],$PROXY);
+    }
+    
+    #$ua->level("+trace") if $DEBUG;
+
+    print STDERR "mirror_file($url --> $local_filename)\n" if $DEBUG || $VERBOSE;
+    my $response = $ua->mirror($url,$local_filename);
+#   printf STDERR "success = %d <%s>",$response->is_success,$response->status_line if $DEBUG;
+    
+    if ( ! $response->is_success ) {
+        if ( $response->status_line =~ /^304/ ) {
+            print "mirror_file($url): NOT MODIFIED\n" if $DEBUG ;
+            $mirror=2;
+        } else {
+            print "mirror_file($url): COULD NOT GET\n";
+	    print sprintf("ERROR: %s\n",$response->message)
+                if $DEBUG || $VERBOSE;
+            $mirror=0;
+        }
+    } else {
+        print "mirror_file($url): OK\n" if $DEBUG; 
+    }    
+    return $mirror;
+}
+
+1;

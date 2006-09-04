8163a2a85f8ef61a41d8ef627dd4f6d792a696b3
Jörg Ostertag <openstreetmap@ostertag.name>
2006-09-04 21:16:15 +0000

    eliminate duplicate definition warning for iLWP::debug

--- utils/perl/Utils/LWP/Utils.pm
+++ utils/perl/Utils/LWP/Utils.pm
@@ -23,27 +23,31 @@ our $NO_MIRROR=0;
 our $lwp_last_was_bytes=0;
 our $lwp_bytes=0;
 our $lwp_timer=0;
-sub LWP::Debug::debug {    
-    my $out_string = shift;
-    if ( $out_string =~ m/read (\d+) bytes/) {
-        my $anz = $1;
-        $lwp_bytes += $anz;
-	if ( time() > $lwp_timer+1){
-	    printf STDERR"LWP: got %d Bytes (%.4f MB)\r",$lwp_bytes,$lwp_bytes/1024/1024
-		if $DEBUG || $VERBOSE;
-	    $lwp_last_was_bytes=1;
-	    $lwp_timer = time();
-	}
-    } else {
-        if ( $lwp_last_was_bytes) {
-            $out_string ="\n".$out_string;
-            $lwp_bytes=0;
-        }
-	printf STDERR "LWP: $out_string\n"; 
-        $lwp_last_was_bytes=0;
+{
+    no warnings;
+    eval {
+	sub LWP::Debug::debug {    
+	    my $out_string = shift;
+	    if ( $out_string =~ m/read (\d+) bytes/) {
+		my $anz = $1;
+		$lwp_bytes += $anz;
+		if ( time() > $lwp_timer+1){
+		    printf STDERR"LWP: got %d Bytes (%.4f MB)\r",$lwp_bytes,$lwp_bytes/1024/1024
+			if $DEBUG || $VERBOSE;
+		    $lwp_last_was_bytes=1;
+		    $lwp_timer = time();
+		}
+	    } else {
+		if ( $lwp_last_was_bytes) {
+		    $out_string ="\n".$out_string;
+		    $lwp_bytes=0;
+		}
+		printf STDERR "LWP: $out_string\n"; 
+		$lwp_last_was_bytes=0;
+	    }
+	};
     }
-};
-
+}
 
 sub mirror_file($$){
     my $url            = shift;

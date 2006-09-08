039b02849d4c94283346a99839ebc60844559476
Jörg Ostertag <openstreetmap@ostertag.name>
2006-09-08 18:34:57 +0000

    create all stripe_* csv Files and concatenate them

--- utils/osm2csv/create_all.sh
+++ utils/osm2csv/create_all.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 areas=`./osm2csv.pl --list-areas`
 
+dir=`dirname $0 `
 for a in $areas ; do
+    echo $a | grep 'stripe_' || continue
     echo "=================================================="
     echo "Trying Area $a"
-  ./osm2csv.pl -v --area=$a 
+  $dir/osm2csv.pl -v --area=$a 
 done
+cat ~/osm/planet/csv/stripe_*.csv | sort -u > ~/osm/planet/csv/osm.csv

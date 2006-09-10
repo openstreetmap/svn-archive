cac017e412dc7c5c05594e2a5cccd6859162d882
Jörg Ostertag <openstreetmap@ostertag.name>
2006-09-10 20:00:18 +0000

    add more functions to Utils::Debug resort Exporter

--- utils/perl/Utils/Debug.pm
+++ utils/perl/Utils/Debug.pm
@@ -5,8 +5,11 @@ package Utils::Debug;
 use Exporter;
 @ISA = qw( Exporter );
 use vars qw(@ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $VERSION);
-@EXPORT = qw( $DEBUG $VERBOSE 
-	      print_time);
+@EXPORT = qw( $DEBUG $VERBOSE
+	      mem_usage
+	      time_estimate
+	      print_time
+	      );
 
 our $DEBUG   = 0;
 our $VERBOSE = 0;
@@ -24,6 +27,34 @@ sub print_time($){
 	printf STDERR " in %.0f sec", $time_diff;
     }
     printf STDERR "\n";
+}
+
+# get memory usage from /proc Filesystem
+sub mem_usage(){
+    my $proc_file = "/proc/$$/statm";
+    my $msg = '';
+    if ( -r $proc_file ) {
+	my $statm = `cat $proc_file`;
+	chomp $statm;
+	my @statm = split(/\s+/,$statm);
+	my $vsz = ($statm[0]*4)/1024;
+	my $rss = ($statm[1]*4)/1024;
+	#      printf STDERR " PID: $$ ";
+	$msg .= sprintf( "VSZ: %.0f MB ",$vsz);
+	$msg .= sprintf( "RSS: %.0f MB",$rss);
+    }
+    return $msg;
+}
+
+sub time_estimate($$$){
+    my $start_time = shift;
+    my $elem_no    = shift;
+    my $elem_max   = shift;
+
+    my $time_diff=time()-$start_time;
+    my $time_estimated= $time_diff*$elem_no/$elem_max;
+    my $msg = sprintf( " time %.0f min rest: %.0f min",$time_diff/60,$time_estimated/60);
+    return $msg;
 }
 
 1;

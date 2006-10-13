d103651b59195d9eb46988e0c9fbc3cd29500b1b
Immanuel Scholz <immanuel.scholz@gmx.de>
2006-10-13 21:55:33 +0000

    fixed little-osm sql script to mysql

--- utils/little-osm/planet.sql
+++ utils/little-osm/planet.sql
@@ -1,3 +1,16 @@
-create table data (uid integer primary key, tags text, time timestamp, reference text, minlat float not null, minlon float not null, maxlat float not null, maxlon float not null);
-create index data_bbox on data (minlat,minlon,maxlat,maxlon);
-create index data_id on data (uid);
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `little-osm`;
+USE `little-osm`;
+
+DROP TABLE IF EXISTS `data`;
+CREATE TABLE `data` (
+  `uid` int(10) unsigned NOT NULL default '0',
+  `tags` text,
+  `time` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `reference` text,
+  `minlat` float NOT NULL default '0',
+  `minlon` float NOT NULL default '0',
+  `maxlat` float NOT NULL default '0',
+  `maxlon` float NOT NULL default '0',
+  KEY `Index_1` (`uid`)
+  KEY `Index_2` (`minlat`,`minlon`,`maxlat`,`maxlon`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;

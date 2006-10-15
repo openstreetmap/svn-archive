c6ecbcbc09444d546e1b1ad697922d2bc262a200
Nick Whitelegg <nick_whitelegg@yahoo.co.uk>
2006-10-15 10:32:56 +0000

    Added current osmabrowser to svn

---
+++ websites/osmabrowser/geocoder.php
@@ -0,0 +1,15 @@
+<?php
+################################################################################
+# This file forms part of the Freemap source code.                             #
+# (c) 2004-06 Nick Whitelegg (Hogweed Software)                                #
+# Licenced under the Lesser GNU General Public Licence; see COPYING            #
+# for details.                                                                 #
+################################################################################
+//header("Content-type: text/xml");
+require_once('geocoder_funcs.php');
+
+
+$pos = geocoder($_REQUEST['place'],$_REQUEST['country']);
+header("Location: index.php?lat=$pos[lat]&lon=$pos[long]");
+
+?>

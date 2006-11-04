e5b75a64bb6ed7f29289cf3fc52b75dcbcf668c6
Jörg Ostertag <openstreetmap@ostertag.name>
2006-11-04 22:14:18 +0000

    update Copyright Info

--- utils/planet.osm/perl/planetosm-to-db.pl
+++ utils/planet.osm/perl/planetosm-to-db.pl
@@ -760,6 +760,9 @@ B<planertosm-to-db.pl>
 
 =head1 DESCRIPTION
 
+This script reads osm data (normally planet.osm) and writes
+them into a local database(postgress or mysql.
+
 =head1 SYNOPSIS
 
 B<Common usages:>
@@ -826,3 +829,28 @@ Before running, think about your database type
 (pgsql or mysql), name, host, user and password and 
 provide them at commandline or set the Environment Variables.
 
+=head1 COPYRIGHT
+
+Copyright 2006,
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+=head1 AUTHOR
+
+=head1 SEE ALSO
+
+http://www.openstreetmap.org/
+
+=cut

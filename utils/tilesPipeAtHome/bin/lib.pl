fc50108055232b8612e58ab7328be7c00f5f5b42
Oliver White <ojwlists@googlemail.com>
2006-12-02 13:09:55 +0000

    Preparatory work for version of tiles@home using a rendering pipeline concept requests.pl -> download.pl -> transform.pl -> render.pl -> (tile.pl) -> -> (archive.pl) -> (upload.pl)

---
+++ utils/tilesPipeAtHome/bin/lib.pl
@@ -0,0 +1,93 @@
+use strict;
+#-----------------------------------------------------------------------------
+# OpenStreetMap tiles@home, library module
+# Functions used by multiple modules
+#
+# Contact OJW on the Openstreetmap wiki for help using this program
+#-----------------------------------------------------------------------------
+# Copyright 2006, Oliver White
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Count the number of files in a directory
+#-----------------------------------------------------------------------------
+sub CountFilesInDir(){
+  my $Dir = shift();
+  opendir(my $dp, $Dir) || die("Couldn't read directory $Dir\n");
+  my $Count = 0;
+  while(my $File = readdir($dp)){
+    $Count++ if($File !~ /^\.*$/);
+  }
+  closedir($dp);
+  return($Count);
+}
+#-----------------------------------------------------------------------------
+# Project latitude in degrees to Y coordinates in mercator projection
+#-----------------------------------------------------------------------------
+sub ProjectF($){
+  my $Lat = DegToRad(shift());
+  my $Y = log(tan($Lat) + sec($Lat));
+  return($Y);
+}
+#-----------------------------------------------------------------------------
+# Project Y to latitude bounds
+#-----------------------------------------------------------------------------
+sub Project(){
+  my ($Y, $Zoom) = @_;
+  
+  my $Unit = 1 / (2 ** $Zoom);
+  my $relY1 = $Y * $Unit;
+  my $relY2 = $relY1 + $Unit;
+  
+  
+  my $LimitY = ProjectF(85.0511);
+  my $RangeY = 2 * $LimitY;
+
+  $relY1 = $LimitY - $RangeY * $relY1;
+  $relY2 = $LimitY - $RangeY * $relY2;
+    
+  my $Lat1 = ProjectMercToLat($relY1);
+  my $Lat2 = ProjectMercToLat($relY2);
+  return(($Lat1, $Lat2));  
+}
+
+#-----------------------------------------------------------------------------
+# Convert Y units in mercator projection to latitudes in degrees
+#-----------------------------------------------------------------------------
+sub ProjectMercToLat($){
+  my $MercY = shift();
+  return(RadToDeg(atan(sinh($MercY))));
+}
+
+#-----------------------------------------------------------------------------
+# Project X to longitude bounds
+#-----------------------------------------------------------------------------
+sub ProjectL(){
+  my ($X, $Zoom) = @_;
+  
+  my $Unit = 360 / (2 ** $Zoom);
+  my $Long1 = -180 + $X * $Unit;
+  return(($Long1, $Long1 + $Unit));  
+}
+
+#-----------------------------------------------------------------------------
+# Angle unit-conversions
+#-----------------------------------------------------------------------------
+sub DegToRad($){return pi * shift() / 180;}
+sub RadToDeg($){return 180 * shift() / pi;}
+
+1
---
+++ utils/tilesPipeAtHome/bin/render.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+use LWP::Simple;
+use LWP::UserAgent;
+use Math::Trig;
+use File::Copy;
+use strict;
+#-----------------------------------------------------------------------------
+# OpenStreetMap tiles@home, rendering module
+# Takes a queue of SVG files, and renders them into images using RSVG
+#
+# Contact OJW on the Openstreetmap wiki for help using this program
+#-----------------------------------------------------------------------------
+# Copyright 2006, Oliver White
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#-----------------------------------------------------------------------------
+require("lib.pl");
+my $SvgDir = "../SVG";
+my $TilesDir = "../tiles";
+
+# Use the -f lag to run the program continuously
+RenderContinuously($SvgDir, $TilesDir) if(shift() eq "-f");
+
+Render($SvgDir, $TilesDir);
+
+#-----------------------------------------------------------------------------
+# Continuously download requests, to keep a "queue" of N files in the data
+# directory
+#-----------------------------------------------------------------------------
+sub RenderContinuously(){
+  my ($SvgDir, $TileDir) = @_;
+  while(1){
+    # If the queue of requsts isn't full, then download one
+    Render($SvgDir, $TileDir);
+    sleep(5);
+  }
+}
+
+#-----------------------------------------------------------------------------
+# Download a request for while tileset to render next
+#-----------------------------------------------------------------------------
+sub Render(){
+  my ($SvgDir, $TileDir) = @_;
+  print "looking in $SvgDir\n";
+  opendir(my $dp, $SvgDir) || return;
+  while(my $file = readdir($dp)){
+    if($file =~ /(\d+)_(\d+)_(\d+)\.svg/i){
+      my $filename = "$SvgDir/$file";
+      RenderFile($2, $3, $1, $filename, $TileDir);
+      unlink($filename);
+    }
+  }
+  closedir($dp);
+}
+
+sub RenderFile(){
+  my($X,$Y,$Z,$filename, $Dir) = @_;
+  my $TileFilename = $Dir.sprintf("/%d_%d_%d.png", $Z,$X,$Y);
+  my $TileFilenameTemp = "$TileFilename.part";
+  my $Cmd = "rsvg $filename $TileFilenameTemp -w 500 -h 500 -f png";
+  print "$Cmd\n";
+  `$Cmd`;
+  
+  rename($TileFilenameTemp, $TileFilename);
+}
+

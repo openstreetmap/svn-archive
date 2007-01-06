1c4fffcd930bf6284def6b8a4ab51d59291f2dc5
Jörg Ostertag <openstreetmap@ostertag.name>
2007-01-06 10:57:04 +0000

    add makefile + makefile generator to icons dir

---
+++ map-icons/create_makefile.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Create example for Makefile.am
+makefile="Makefile.am"
+
+echo "" >$makefile
+for theme in square.big square.small classic nickw ; do 
+    find $theme/ -type d | grep -v /.svn | while read dir; do
+	# if no files in dir
+	echo $dir/*.png | grep -q -e '\*' && continue
+
+	name=${dir//-/_}
+	name=${name//\//_}
+	echo "" >>$makefile
+	echo $name'_DATA = '$dir'/*.png' >>$makefile
+	echo $name'dir = $(datadir)/'$dir >>$makefile
+    done
+    echo  >>$makefile
+done
+
+echo  >>$makefile
+echo  >>$makefile
+echo "EXTRA_DIST= \\" >>$makefile
+for theme in square.big square.small classic nickw ; do 
+    find $theme/ -type d | grep -v /.svn | while read dir; do
+	# if no files in dir
+	echo $dir/*.png | grep -q -e '\*' && continue
+
+	name=${dir//-/_}
+	name=${name//\//_}
+	echo '	$('$name'_DATA)' "\\" >>$makefile
+    done
+done
+echo '	create_makefile.sh' >>$makefile

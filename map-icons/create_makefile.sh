9b5454828d1fd7cef76aac437c2b59dd6291e340
Jörg Ostertag <openstreetmap@ostertag.name>
2007-01-14 22:53:41 +0000

    remove another dupplicate / in makefiles

--- map-icons/create_makefile.sh
+++ map-icons/create_makefile.sh
@@ -21,7 +21,7 @@ echo  >>$makefile
 echo  >>$makefile
 echo "EXTRA_DIST= \\" >>$makefile
 for theme in square.big square.small classic nickw ; do 
-    find $theme/ -type d | grep -v /.svn | while read dir; do
+    find $theme -type d | grep -v /.svn | while read dir; do
 	# if no files in dir
 	echo $dir/*.png | grep -q -e '\*' && continue
 

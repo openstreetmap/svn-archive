5eef48987f49f05a58b4ddade88b318648653eef
Nick Whitelegg <nick_whitelegg@yahoo.co.uk>
2007-01-19 20:07:57 +0000

    update to wayclean to match libosm updates

--- utils/wayclean/wayclean.cpp
+++ utils/wayclean/wayclean.cpp
@@ -16,7 +16,6 @@ using std::endl;
 int main(int argc,char* argv[])
 {
 	OSM::Components *compIn = OSM::Parser::parse(std::cin);
-	OSM::Components compOut;
 
 	if (!compIn)
 	{
@@ -24,42 +23,14 @@ int main(int argc,char* argv[])
 		exit(1);
 	}
 
-
-	compIn->rewindWays();
-	while(compIn->hasMoreWays())
+	OSM::Components *compOut = compIn->cleanWays();	
+	if(compOut)
 	{
-		OSM::Way *w = compIn->nextWay();
-		cerr << w->id << endl;
-		if(w)
-		{
-			std::vector<int> nodes = compIn->orderWay(w->id);
-
-			if(nodes.size())
-			{
-				OSM::Way *way = new OSM::Way;
-				way->tags = w->tags;
-				compOut.addWay(way);
-				for(int i=0; i<nodes.size()-1; i++)
-				{
-					int segid=compOut.addSegment
-					(new OSM::Segment(nodes[i],nodes[i+1]));
-					way->addSegment(segid);
-				}
-			}
-		}
+		compOut->toXML(std::cout);
+		delete compOut;
 	}
-
-	compIn->rewindNodes();
-	while(compIn->hasMoreNodes())
-	{
-		OSM::Node *n = new OSM::Node(*(compIn->nextNode()));
-		compOut.addNode(n);
-	}
-
-	compOut.toXML(std::cout);
 
 	delete compIn;
 
 	return 0;
-
 }

377fbc7c611460d85fa32ba524fefb192441f5d3
Jörg Ostertag <openstreetmap@ostertag.name>
2007-01-20 19:42:37 +0000

    add more checks to osm-pdf-atlas create script

--- utils/osm-pdf-atlas/create.sh
+++ utils/osm-pdf-atlas/create.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # ---------------------- GSHHS
 gshhs=gshhs_h
@@ -12,30 +12,73 @@ if [ ~/osm/GHHS/$gshhs.b.gz" -nt ~/osm/GHHS/$gshhs.b" ] ; then
 fi
 
 
-for place_config in Config/places-*.txt; do
+for place_config in \
+    Config/places-germany.txt \
+    Config/places-bavaria*.txt \
+    Config/places-germany*.txt \
+    Config/places-UK.txt \
+    Config/places-france.txt \
+    Config/places-france.txt \
+    Config/places-*.txt; do
+
     csv_area=${place_config#*places-}
     csv_area=${csv_area%.txt}
 
-    echo $csv_area | grep africa && continue
-    echo $csv_area | grep australia && continue
-    echo $csv_area | grep world && continue
-    echo $csv_area | grep england && continue
+    case $csv_area in
+	*germany*)
+	    csv_area="germany"
+	    ;;
+	*bavari*)
+	    csv_area="germany"
+	    ;;
+	*UK*)
+	    csv_area="uk"
+	    ;;
+	*france*)
+	    csv_area="france"
+	    ;;
+	*)
+	    ;;
+    esac
+    osm_csv=$HOME/osm/planet/csv/osm-${csv_area}.csv
 
-#    echo $csv_area | grep germany || continue
 
-    echo $csv_area
-    perl ../osm2csv/osm2csv.pl "$planet_file" --area=${csv_area} -v --update-only
+    echo ""
+    echo "------------------------------------------------------------------"
+    echo "place: '$place_config' with area: '$csv_area'"
+    echo ""
 
-
-    places_name=${place_config/*\places-/}
-    places_name=${places_name%.txt}
-    pdf_name=~/osm/Results/osm_atlas-${places_name}.pdf
-    if [ \( -s "$pdf_name" \) -a \( "$pdf_name" -nt "~/osm/planet/csv/osm-${csv_area}.csv" \) ] ; then
-	echo "Up to Date: " $places_name $pdf_name
+    if  echo $csv_area | grep \
+	-e world  \
+	-e europe \
+	-e africa \
+	-e  australia \
+	-e france \
+	-e england \
+	-e uk
+	then 
+	echo "Ignoring $place_config with $vsc_area"
 	continue
     fi
+
+    if [ `perl ../osm2csv/osm2csv.pl    --list-areas | grep $csv_area ` ]; then
+	echo "--> Update csv File for Area '$csv_area'"
+	perl ../osm2csv/osm2csv.pl "$planet_file" --area=${csv_area}\
+	    -d -d -d -d --update-only -v -v -v 
+    fi
+    if [ ! -s $osm_csv ] ; then
+	osm_csv=~/osm/planet/csv/osm.csv
+    fi
+    
+    places_name=${place_config/*\places-/}
+    places_name=${places_name%.txt}
+    pdf_name=~/osm/pdf-atlas/osm_atlas-${places_name}.pdf
+    #echo "CSV File: $pdf_name $osm_csv"
+    echo "------------------------------------------------------------------"
     echo "Updating: " $places_name $pdf_name
-    time perl osm-pdf-atlas.pl -d  \
+    echo "perl ./osm-pdf-atlas.pl -d --config=Config/config.txt --Places=$place_config"
+    echo "     ..."
+    time perl osm-pdf-atlas.pl -v \
 	--config=Config/config.txt \
 	--Places=$place_config
 done

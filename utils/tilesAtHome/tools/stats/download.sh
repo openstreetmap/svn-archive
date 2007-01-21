1f4b78e646af4e172a9bb5577cabcf3b3faa2192
Oliver White <ojwlists@googlemail.com>
2007-01-21 13:45:39 +0000

    New stats module

--- utils/tilesAtHome/tools/stats/download.sh
+++ utils/tilesAtHome/tools/stats/download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O temp/stats.txt.gz http://dev.openstreetmap.org/~ojw/Stats/Data/stats.txt.gz
+wget http://dev.openstreetmap.org/~ojw/Stats/Data/stats.txt.gz
 gunzip temp/stats.txt.gz

fc668ae89b175db113893d0352af48e86ca416b9
Dirk-Lüder Kreie <osm@deelkar.net>
2007-02-27 16:05:44 +0000

    removed accidental inclusion of merge conflict in final file

--- utils/tilesAtHome/tilesGen.pl
+++ utils/tilesAtHome/tilesGen.pl
@@ -270,11 +270,7 @@ sub GenerateTileset {
       DownloadFile($URL, $DataFile1, 0, "Map data to $DataFile1");
       if(-s $DataFile1 == 0){
         printf("No data here either\n");
-<<<<<<< .mine
         return 0 if ($Main::Mode eq "loop"); # if loop was requested just return (FIXME: tell the server that the job has not been done yet)
-=======
-        return if ($Mode eq "loop"); # if loop was requested just return (FIXME: tell the server that the job has not been done yet)
->>>>>>> .r2180
         exit(1); # or else exit with an error. (to enable wrappers to better handle this situation i.e. tell the server the job hasn't been done yet)
       }
     appendOSMfile($DataFile,$DataFile1);

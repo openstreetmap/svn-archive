baf2968226e9b287935d86d64041dbb4a11b345c
Oliver White <ojwlists@googlemail.com>
2007-03-05 22:06:29 +0000

    Charcoal-grey frontpage design

--- websites/tilesAtHome/index.php
+++ websites/tilesAtHome/index.php
@@ -1,60 +1,59 @@
-<?php 
-  include("lib/gui.inc");
-  navHeader("Tiles@home project page"); 
-?>
+<html>
+<head>
+<title>OpenStreetMap tiles@home</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<div class="all">
+  <h1 class="title"><a href="http://wiki.openstreetmap.org/index.php/Tiles%40home"><img src="Gfx/tah.png" alt="tiles@home" width="600" height="109"></a></h1>
+  <p class="title">A project to render free maps of the whole world.</p>
   
-  <p><b><a href="http://wiki.openstreetmap.org/index.php/Tiles%40home">tiles@home</a></b> is a distributed project which renders OpenStreetMap data of the whole
-  world, and provides free access to the result here, on this website and others.</p>
-  
-  <p>Map data and images can be copied under the Creative Commons CC-BY-SA 2.0 license,
-  and are easy to integrate into your own project. Ask on the wiki if you need help.</p>
-  
-  
-  <p><a href="Browse/">Browse the map</a>, see what tiles are available</p>
-  
-  <p><a href="http://www.openstreetmap.org/index.html">Slippy map</a> (select the "osmarender" layer, using the <span style="background-color:darkblue;color:white;padding:2px"><b>+</b></span> control at the right)</p>
-  
-  <h2>What's happening today?</h2>
+  <hr>
+  <p>Welcome to the world of Creative Commons mapping.  The main page is at
+  <a href="http://www.openstreetmap.org/">openstreetmap.org</a>.  This page
+  provides map images, which you can use on your website, on a handheld device
+  or mobile phone, or anywhere else that you need maps.</p>
 
-  <p><a href="Log/Messages/">Messages</a>, errors, warnings, etc.</p>
+  <p><a href="Browse/">Browse the map</a> tile-by-tile, or use a <a href="http://informationfreeway.org/">slippy map</a>.
+  These maps are also available on the <a href="http://www.openstreetmap.org/index.html">OSM</a> page - you just need
+  to select the &quot;osmarender&quot; layer using the + to the right of the map.
+  </p>
   
-  <p><a href="Stats/">Statistics</a>, 
-  <a href="http://www.openstreetmap.org/munin/openstreetmap/dev.openstreetmap.html">server stats</a>, 
-  <a href="Log/">recent uploads</a>, 
-  <a href="Log/Requests/">render requests</a>, and
-  <a href="Log/graphs.php">upload graphs</a> (shown below)</p>
+  <p><a href="http://www.openstreetmap.org/munin/openstreetmap/dev.openstreetmap.html">Performance graphs</a> are available, to say how the server is coping with all this data.
+  <a href="Log/">recent uploads and messages</a> can be viewed, as can 
+  the <a href="Log/Requests/">areas people are requesting</a> to be rendered.</p>
   
-  <p><img src="Log/graph.png" alt="graph of upload bandwidth over time"></p>
-    
   <?php
-    if(1){ // Optional section: displays some recent tile-uploads
-      include("connect/connect.php");
-      $Result = mysql_query("select * from tiles_misc limit 1;");
-      $Data = mysql_fetch_assoc($Result);
-      
-      $X = $Data["last_12x"];
-      $Y = $Data["last_12y"];
-      printf("<p>Latest interesting upload: <a href=\"Browse/?x=%d&amp;y=%d&amp;z=%d\">%d,%d</a> by %s</p>\n",
-        $X, $Y, 12,
-        $X, $Y,
-        $Data["last_user"]);
-      
-      printf("<table cellspacing=3 cellpadding=0 border=0>");
-      printf("<tr><td>%s</td>\n<td>%s</td></tr>\n", imageOf($X*2,$Y*2,13),imageOf($X*2+1,$Y*2,13));
-      printf("<tr><td>%s</td>\n<td>%s</td></tr>\n", imageOf($X*2,$Y*2+1,13),imageOf($X*2+1,$Y*2+1,13));
-      printf("</table>\n");
+  $Z = 16;
+  $X = 32684;
+  $Y = 21834;
+  
+  print "<table border=\"0\">";
+  for($yi = 0; $yi < 2; $yi++){
+    print "<tr>";
+    for($xi = 0; $xi < 2; $xi++){
+      $Img = sprintf(
+      "http://dev.openstreetmap.org/~ojw/Tiles/tile.php/%d/%d/%d.png", 
+        $Z, 
+        $X + $xi, 
+        $Y + $yi);
+      printf("<td><img src=\"%s\" width=\"256\" height=\"256\" alt=\"map snippet\"></td>\n", $Img);
     }
-    function imageOf($X,$Y,$Z){
-      return(sprintf("<img border=1 width=256 heght=256 src=\"Tiles/tile.php/%d/%d/%d.png\"></div>\n",$Z,$X,$Y));
-      }
+    print "</tr>";
+  }
+  print "</table>";
+  
   ?>
   
-  <h2>Tools</h2>
+  <p>(You can request an area be updated using the <a href="http://wiki.openstreetmap.org/index.php/Tiles%40home/APIs">API</a>, or
+  when <a href="Browse/">viewing</a> the map here)</p>
   
-  <p><a href="Upload/">Upload tiles</a> as ZIP files (password required)</p>
+  <p>Loads more information is available on the 
+  <a href="http://wiki.openstreetmap.org/index.php/Tiles%40home">tiles@home wiki page</a>,
+  including development activity, the people involved, and discussing future directions
+  for the project</p>
   
-  <h2>See also</h2>
   
-  <p>Read the <a href="http://wiki.openstreetmap.org/index.php/Tiles%40home">tiles@home wiki page</a> for details of this project</p>
-
-<?php navFooter() ?>
+</div>
+</body>
+</html>

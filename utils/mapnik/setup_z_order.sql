009850a503ff47eb8aa34b186bad8ee4e6163112
Artem Pavlenko <artem@mapnik.org>
2007-03-13 18:00:42 +0000

    moved drop table out of transaction + clean up layer values

--- utils/mapnik/setup_z_order.sql
+++ utils/mapnik/setup_z_order.sql
@@ -1,7 +1,8 @@
 begin;
 delete from planet_osm_polygon where "natural" = 'coastline';
 update planet_osm_line set layer=0 where layer is null;
-update planet_osm_line set z_order= 10*int4(layer) + 9 where highway='motorway' or highway='motorway_link';
+update planet_osm_line set layer=0 where layer is null or layer !~ '^[0-9+-]+$';
+Update planet_osm_line set z_order= 10*int4(layer) + 9 where highway='motorway' or highway='motorway_link';
 update planet_osm_line set z_order= 10*int4(layer) + 8 where highway='trunk' or highway='trunk_link';
 update planet_osm_line set z_order= 10*int4(layer) + 7 where highway='primary' or highway='primary_link';
 update planet_osm_line set z_order= 10*int4(layer) + 6 where highway='secondary' or highway='secondary_link';
@@ -10,11 +11,11 @@ update planet_osm_line set z_order= 10*int4(layer) + 4 where highway='tertiary' 
 update planet_osm_line set z_order= 10*int4(layer) + 3 where highway='residential' or highway='minor' or highway='unclassified';
 update planet_osm_line set z_order= z_order + 10  where bridge = 'true';
 update planet_osm_line set z_order= z_order + 10  where bridge = 'yes';
+commit;
 
 delete from geometry_columns where f_table_name='planet_osm_roads';
-drop table planet_osm_roads;
+drop table planet_osm_roads ;
 create table planet_osm_roads as select * from planet_osm_line where highway='motorway' or highway='motorway_link' or highway='trunk' or highway='trunk_link' or highway='primary' or highway='primary_link' or char_length(railway) > 0;
 insert into geometry_columns values('','public','planet_osm_roads','way',2,4326,'LINESTRING');
 create index planet_osm_roads_spidx on planet_osm_roads using GIST (way GIST_GEOMETRY_OPS);
-commit;
 vacuum analyze planet_osm_roads;

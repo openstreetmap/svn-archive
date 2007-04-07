e32ab174059599d5d780845c4e16792213ceb35b
Oliver White <ojwlists@googlemail.com>
2007-04-07 12:02:36 +0000

    Program to re-request anything that still hasn't been rendered x hours after the request was taken. (Minimised the effect of failed clients, fake clients, and people viewing /Requests/ in a browser)

---
+++ websites/tilesAtHome/Log/Requests/History/timeout.php
@@ -0,0 +1,47 @@
+<?php
+  /* 
+  ** OJW 2007
+  ** License: GNU GPL v2 or at your option any later version
+  */
+  header("Content-type:text/plain");
+  if(1){
+    if($_SERVER["REMOTE_ADDR"] != $_SERVER["SERVER_ADDR"]){
+      print "This page can only be run by the dev server\n";
+      exit;
+    }
+  }
+
+  include("../../../connect/connect.php");
+  include("../../../lib/requests.inc");
+    
+  // Find anything older than a certain time-limit that's still
+  // in the "being done" queue, and re-request it
+  $AgeLimit = 6; // hours
+  $SQL = sprintf(
+    "select * from `tiles_queue` where `date` < date_sub(now(), INTERVAL %d HOUR) and `status`=%d;", 
+    $AgeLimit,
+    REQUEST_ACTIVE);
+
+  print $SQL . "\n";
+
+  $Result = mysql_query($SQL);
+  $Count = mysql_num_rows($Result);
+  printf("%d items found. %s\n", $Count, mysql_error());
+
+  // For each request that hasn't been done after x hours...
+  while($Data = mysql_fetch_assoc($Result)){
+
+    // ...move it to the "new requests" queue
+    moveRequest($Data["x"], $Data["y"], REQUEST_ACTIVE, REQUEST_NEW);
+
+    // Display some details to the browser
+    printf("\n----\n%d,%d (%d) at %s. %s\n", 
+      $Data["x"], 
+      $Data["y"], 
+      $Data["status"], 
+      $Data["date"], 
+      mysql_error());
+  }
+
+  
+  ?>

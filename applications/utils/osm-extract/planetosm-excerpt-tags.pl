f6644ddbb9ec109701516e8a7b1af358935aa8e3
Hakan Tandogan <hakan@tandogan.com>
2007-04-22 17:39:00 +0000

    perl modules were renamed to perl_lib

--- applications/utils/osm-extract/planetosm-excerpt-tags.pl
+++ applications/utils/osm-extract/planetosm-excerpt-tags.pl
@@ -50,12 +50,12 @@ my @way_sel_tags = (
 BEGIN {
     my $dir = $0;
     $dir =~s,[^/]+/[^/]+$,,;
-    unshift(@INC,"$dir/perl");
+    unshift(@INC,"$dir/perl_lib");
 
-    unshift(@INC,"./perl");
-    unshift(@INC,"../perl");
-    unshift(@INC,"~/svn.openstreetmap.org/utils/perl");
-    unshift(@INC,"$ENV{HOME}/svn.openstreetmap.org/utils/perl");
+    unshift(@INC,"./perl_lib");
+    unshift(@INC,"../perl_lib");
+    unshift(@INC,"~/svn.openstreetmap.org/utils/perl_lib");
+    unshift(@INC,"$ENV{HOME}/svn.openstreetmap.org/utils/perl_lib");
 }
 
 use Getopt::Long;

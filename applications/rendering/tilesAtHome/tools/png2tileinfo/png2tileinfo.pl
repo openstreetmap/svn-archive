6a199d343a237417aabd177fbc59ca3da6500f6c
Hakan Tandogan <hakan@tandogan.com>
2007-05-02 17:40:19 +0000

    Command line pixel setter/getter from Cagri Coltekin <cagri@xs4all.nl>

--- applications/rendering/tilesAtHome/tools/png2tileinfo/png2tileinfo.pl
+++ applications/rendering/tilesAtHome/tools/png2tileinfo/png2tileinfo.pl
@@ -23,17 +23,23 @@ use constant TILETYPE_LAND => 1;
 use constant TILETYPE_SEA => 2;
 use constant TILETYPE_TILE => 3;
 
-my($world_fh,$tileinfo_fh);
 
-open $world_fh, "<oceantiles_12.png" or die;
-open $tileinfo_fh, ">oceantiles_12.dat" or die;
-my $world_im = GD::Image->newFromPng( $world_fh, 1 );
 
+#
+#
+#
+my ($world_fh, $tileinfo_fh);
+our $world_im;
+
+
+#
+#
+#
 sub get_type 
 {
-  my($x,$y) = @_;
+  my($image, $x, $y) = @_;
 
-  my($r,$g,$b) = $world_im->rgb( $world_im->getPixel( $x,$y ) );
+  my($r,$g,$b) = $image->rgb( $image->getPixel( $x,$y ) );
   
   return TILETYPE_LAND if $r == 0 && $g == 255 && $b == 0;
   return TILETYPE_SEA if $r == 0 && $g == 0   && $b == 255;
@@ -43,13 +49,93 @@ sub get_type
   die "Wierd tiletype at [$x,$y]: ($r,$g,$b)\n";
 }
 
+
+#
+#
+#
+sub set_type
+{
+  my($image, $x, $y, $type) = @_;
+  my $color;
+
+  $color = $image->colorAllocate(0,0,255) if($type == TILETYPE_SEA);
+  $color = $image->colorAllocate(0,255,0) if($type == TILETYPE_LAND);
+  $color = $image->colorAllocate(255,255,255) if($type == TILETYPE_TILE);
+  $image->setPixel($x,$y, $color);
+}
+
+
+#
+#
+#
+if ($#ARGV > -1)
+{
+    if($ARGV[0] eq "check")
+    {
+	if ($#ARGV < 2)
+	{
+	    print "Usage: png2tileinfo.pl check <x> <y>\n";
+	}
+	else
+	{
+	    my @typenames = ('unknown', 'land', 'sea', 'mixed');
+	    my ($x, $y) = ($ARGV[1], $ARGV[2]);
+
+	    open $world_fh, "<oceantiles_12.png" or die;
+
+	    $world_im = GD::Image->newFromPng( $world_fh, 1 );
+
+	    my $png_val = get_type($world_im, $x, $y);
+
+	    print "oceantiles_12.png($x, $y) = $png_val ($typenames[$png_val])\n";
+	}
+
+	exit 0;
+    }
+
+    if ($ARGV[0] eq "set")
+    {
+	if ($#ARGV < 3)
+	{
+	    print "Usage: png2tileinfo.pl set <x> <y> [land|sea|mixed]\n";
+	}
+	else
+	{
+	    my ($x, $y) = ($ARGV[1], $ARGV[2]);
+	    my $newtype;
+
+	    open $world_fh, "<oceantiles_12.png" or die;
+	    $world_im = GD::Image->newFromPng( $world_fh, 1 );
+	    close $world_fh;
+    
+	    $newtype = TILETYPE_LAND if ($ARGV[3] eq "land");
+	    $newtype = TILETYPE_SEA  if ($ARGV[3] eq "sea");
+	    $newtype = TILETYPE_TILE if ($ARGV[3] eq "mixed");
+
+	    set_type($world_im, $x, $y, $newtype);
+
+	    open $world_fh, ">oceantiles_12.png" or die;
+	    print $world_fh $world_im->png;
+	    close $world_fh;
+	}
+
+	exit 0;
+    }
+}
+
+
+# Convert the resulting file in any case...
+open $world_fh, "<oceantiles_12.png" or die;
+open $tileinfo_fh, ">oceantiles_12.dat" or die;
+$world_im = GD::Image->newFromPng( $world_fh, 1 );
+
 for my $y (0..4095)
 {
   my $tmp = 0;
   my $str = "";
   for my $x (0 .. 4095)
   {
-    my $type = get_type($x,$y);
+    my $type = get_type($world_im,$x,$y);
     $tmp = ($tmp << 2) | $type;
     
     if( ($x&3) == 3)

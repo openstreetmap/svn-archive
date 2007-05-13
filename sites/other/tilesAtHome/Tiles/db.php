1c20e14baeb994a44732e960f3607510f1ce7c77
Oliver White <ojwlists@googlemail.com>
2007-05-13 12:32:05 +0000

    Look in the tiles_blank database for land/sea areas, if tile not found in filesystem

--- sites/other/tilesAtHome/Tiles/db.php
+++ sites/other/tilesAtHome/Tiles/db.php
@@ -1,86 +1,104 @@
 <?php
-  header("Content-type:image/PNG");
+  // This is the 404 handler script, for tiles not present 
+  // via mod-rewrite as real files.
   
   include("../lib/tilenames.inc");
+  include("../lib/layers.inc");
   
-  if(0){ // Option to turn off tile browsing
-    readfile("tile_maintenance.png");
-    exit;
+  if(0){ // Option to turn off non-standard tiles
+    BlankTile();
     }
-      
+  
   $URL = $_SERVER["REQUEST_URI"];
-  #printf("<p>%s</p>\n", htmlentities($URL));
   
-  if(preg_match("/(\d+)\/(\d+)\/(\d+)\.png(_\w+)?/", $URL, $Matches)){
-    $Z = $Matches[1];
-    $X = $Matches[2];
-    $Y = $Matches[3];
-  
-    if(TileValid($X,$Y,$Z))
-    {
-      SearchFilesystem($X,$Y,$Z);
-      SearchDatabase($X,$Y,$Z);
-    }
-    else
-      readfile("tile_error.png");
-    }
-  else
-  {
-    readfile("tile_error.png");
+  // Look for tile x,y,z (TODO: layer)
+  if(!preg_match("/(\w+)\.php\/(\d+)\/(\d+)\/(\d+)\.png(_\w+)?/", $URL, $Matches)){
+    BlankTile("error");
   }
   
+  $Layer = $Matches[1];
+  $Z = $Matches[2];
+  $X = $Matches[3];
+  $Y = $Matches[4];
+  
+  // Check x,y,z is valid
+  if(!TileValid($X,$Y,$Z)){
+    BlankTile("error");
+    }
+  
+  // Lookup layer, and check is valid
+  $LayerID = checkLayer($Layer);
+  if($LayerID < 0){
+    BlankTile("error");
+    }
+    
+  // SearchFilesystem($X,$Y,$Z);
+  
+  SearchDatabase($X,$Y,$Z);
+  
+  // Look for a file on the filesystem
   function SearchFilesystem($X,$Y,$Z){
     $Filename = TileName($X,$Y,$Z);
     if(file_exists($Filename)){
-    
+      
       readfile($Filename);
       exit;
     }
   }
   
   function SearchDatabase($X,$Y,$Z){
-    if(1){
-      readfile("404.png");
-      exit;
+    if(0){ // option to turn-off database lookups
+      BlankTile();
     }
     
     $NoExitOnDbFail = 1;
     include("../connect/connect.php");
     
     if(!$DbSuccess){
-      readfile("tile_error.png");
-      exit;
+      BlankTile("database_error");
     }
    
-    $SQL = sprintf("select * from tiles where `x`=%d and `y`=%d and `z`=%d limit 1;", $X, $Y, $Z);
+    $SQL = sprintf("select * from tiles_blank where `x`=%d and `y`=%d and `z`=%d and `layer`=%d limit 1;", 
+      $X, $Y, $Z, $LayerID);
     #printf("<p>%s</p>\n", htmlentities($SQL));
     
     $Result = mysql_query($SQL);
     if(mysql_error()){
-      readfile("tile_error.png");
-      exit;
+      BlankTile("error");
     }
     
     if(mysql_num_rows($Result) == 0){
-      readfile("404.png");
-      exit;
+      BlankTile("assumed_sea");// political decision: make unknown areas blue
     }
   
     $Data = mysql_fetch_assoc($Result);
     mysql_close();
-    
-    if($Data["exists"] == 0){
-      readfile("404.png");
-      exit;
-    }
+
     
     $CacheDays = 14;
+    switch($Data["type"]){
+      case 1:
+        BlankTile("sea");
+        break;
+      case 2:
+        BlankTile("land");
+        break;
+      default:
+        BlankTile("unknwown"); // probably shouldn't reach this line
+        break;
+      }
+  }
+  
+  function BlankTile($Type="404"){
+    $Filename = "Gfx/$Type.png";
+    
+    header("Content-type:image/PNG");
     header('Last-Modified: ' . gmdate('D, j M Y H:i:s T',  $Data["date"]));
     header('Cache-Control: ' . $CacheDays * 86400);
     header('Expires: ' . gmdate('D, j M Y H:i:s T',  $Data["date"] + $CacheDays * 86400));
-    
-    print $Data["tile"]; 
+
+    readfile($Filename);
+    exit;
   }
-  
-  
+
 ?>

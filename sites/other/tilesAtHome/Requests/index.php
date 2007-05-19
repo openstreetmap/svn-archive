6355fa3e761606595da5c16c0ce3d7488da4d539
Robert (Jamie) Munro <rjmunro@arjam.net>
2007-05-19 20:43:19 +0000

    I am an idiot. I forgot to add the date field to the requested fields.

--- sites/other/tilesAtHome/Requests/index.php
+++ sites/other/tilesAtHome/Requests/index.php
@@ -42,8 +42,8 @@
   //printf("OK|3|%d|%d|%d|dev_random",$x,$y,12);
 
 function CheckForRequest(){
-  $SQL =  "select `x`,`y`,`status`,`priority` from `tiles_queue` where `status`=0 union ";
-  $SQL .= "select `x`,`y`,`status`,`priority` from `tiles_queue` where `status`=1 ";
+  $SQL =  "select `x`,`y`,`status`,`priority`,`date` from `tiles_queue` where `status`=0 union ";
+  $SQL .= "select `x`,`y`,`status`,`priority`,`date` from `tiles_queue` where `status`=1 ";
   $SQL .= "order by `priority`,`date` limit 1;";
 
 //  print "$SQL\n";return;

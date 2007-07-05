147309a0963d4083c6380233935c7a4f14adaf98
Frederik Ramm <frederik@remote.org>
2007-07-05 13:21:44 +0000

    new

---
+++ applications/rendering/history/hqrun.pl
@@ -0,0 +1,404 @@
+#!/usr/bin/perl
+
+use LWP;
+use Geo::Proj4;
+use GD;
+use DBI;
+use Data::Dumper;
+use strict;
+
+my $ua = LWP::UserAgent->new();
+my $dsn="DBI:mysql:host=localhost:database=osmhistory";
+my $dbh = DBI->connect($dsn) or die;
+my $destpath = "/var/www/webspace/gryph.de/openstreetmap.gryph.de/history/images/";
+my $thumbpath = "/var/www/webspace/gryph.de/openstreetmap.gryph.de/history/thumbnails/";
+
+my $colormap = { 
+    "0" => [ 255, 0, 0 ], # red
+    "1" => [ 255, 255, 0 ], # yellow
+    "2" => [ 255, 255, 255 ], # white
+    "3" => [ 0, 0, 255 ], # blue
+};
+
+my $query = <<EOF;
+select * 
+from     jobs 
+where    status='waiting'
+order by date_entered asc
+limit    1
+EOF
+
+my $sth = $dbh->prepare($query) or die;
+my $sth_planet = $dbh->prepare("select filedate from planets") or die;
+
+my $sth_begin = $dbh->prepare(<<EOF) or die;
+update jobs 
+set status='processing', 
+date_started=? 
+where id=?
+EOF
+
+my $sth_failure = $dbh->prepare(<<EOF) or die;
+update jobs 
+set status='failed', 
+errmsg=?, 
+date_finished=? 
+where id=?
+EOF
+
+my $sth_success = $dbh->prepare(<<EOF) or die;
+update jobs 
+set status='finished', 
+date_finished=?,
+width=?,
+height=?,
+max_nodes=?,
+filename=?,
+filesize=?,
+num_frames=?
+where id=?
+EOF
+
+while(1)
+{
+    if (-f "/tmp/hqrun.terminate")
+    {
+        print "/tmp/hqrun.terminate exists, aborting\n";
+        exit;
+    }
+    $sth->execute();
+    if (my $row = $sth->fetchrow_hashref)
+    {
+        my $input = {};
+        foreach my $key(keys(%$row))
+        {
+            $input->{$key} = $row->{$key};
+        }
+
+        my @filedates = get_filedates();
+        my @usedates;
+        my $used = {};
+
+        my $lastdate = defined($input->{"todate"}) ? $input->{"todate"} : $filedates[$#filedates];
+        my $firstdate = defined($input->{"fromdate"}) ? $input->{"fromdate"} : $filedates[0];
+        $lastdate = $lastdate + $input->{"frequency"};
+        while(1)
+        {
+            my $want = todays($lastdate) - $input->{"frequency"};
+            my $bestdiff = 9999999;
+            my $thisdate;
+            foreach my $date(@filedates)
+            {
+                next if ($used->{$date});
+                next if ($date < $firstdate);
+                next if ($date > $lastdate);
+                my $diff = abs($want-todays($date));
+                if ($diff<$bestdiff)
+                {
+                    $bestdiff = $diff;
+                    $thisdate = $date;
+                }
+            }
+            last if ($bestdiff == 9999999);
+            unshift(@usedates, $thisdate);
+            $used->{$thisdate} = 1;
+            $lastdate = $thisdate;
+        }
+
+        $input->{"filedates"} = \@usedates;
+        $sth_begin->execute(time(), $input->{"id"});
+        my $output = doit($input);
+        if (ref($output) ne "HASH")
+        {
+            # print "fail: $output\n";
+            $sth_failure->execute($output, time(), $input->{"id"});
+        }
+        else
+        {
+            my $filename = $destpath . $input->{"id"} . ".gif";
+            system "mv ".$output->{"filename"}." ".$filename;
+            my $filesize = -s $filename;
+            system "mv ".$output->{"thumbname"}." ".$thumbpath.$input->{"id"} . ".gif";
+            $sth_success->execute(time(), $output->{"width"}, $output->{"height"}, $output->{"max_nodes"}, $input->{"id"}.".gif", $filesize, scalar(@usedates)+1, $input->{"id"});
+        }
+    }
+    else
+    {
+        sleep 30;
+    }
+}
+
+my $t=<<'EOF';
+my $inp = {
+    "projection" => 2,
+    "minlat" => 48.925,
+    "minlon" => 8.32,
+    "maxlat" => 49.05,
+    "maxlon" => 8.47,
+    "width" => 600,
+    "color" => 0,
+    "bgimage" => 2, 
+    "height" => undef,
+    "delay" => 100,
+    "filedates" => [ 60818, 70307, 70620 ]
+};
+EOF
+
+sub doit
+{
+    my $input = shift;
+    my $output = {};
+    my @tmpfiles;
+    my $outfile = "/tmp/hqrun.out.$$.gif";
+    my $thumbfile = "/tmp/hqrun.thumb.$$.gif";
+
+    my $proj;
+    if ($input->{"projection"} == 1)
+    {
+        $proj = Geo::Proj4->new(proj => "latlong")
+            or return "parameter error: ".Geo::Proj4->error;
+    }
+    elsif ($input->{"projection"} == 2)
+    {
+        $proj = Geo::Proj4->new(proj => "merc", ellps => "WGS84")
+            or return "parameter error: ".Geo::Proj4->error;
+    }
+    else
+    {
+        return "invalid 'projection' parameter: ".$input->{"projection"};
+    }
+
+    return "minlat > maxlat" if ($input->{'minlat'} > $input->{'maxlat'});
+    return "minlon > maxlon" if ($input->{'minlon'} > $input->{'maxlon'});
+    return "lat range too large (max 90deg)" if ($input->{'maxlat'} - $input->{'minlat'} > 90);
+    return "lon range too large (max 120deg)" if ($input->{'maxlon'} - $input->{'minlon'} > 120);
+
+    return "bad minlat" if ($input->{'minlat'} > 90 or $input->{"minlat"} < -90);
+    return "bad maxlat" if ($input->{'maxlat'} > 90 or $input->{"maxlat"} < -90);
+    return "bad minlon" if ($input->{'minlon'} > 180 or $input->{"minlon"} < -180);
+    return "bad maxlon" if ($input->{'maxlon'} > 180 or $input->{"maxlon"} < -180);
+
+    return "width too large (max 1600)" if ($input->{'width'} > 1600);
+    return "height too large (max 1200)" if ($input->{'height'} > 1200);
+
+    my ($min_e, $min_n) = $proj->forward($input->{'minlat'}, $input->{'minlon'});
+    my ($max_e, $max_n) = $proj->forward($input->{'maxlat'}, $input->{'maxlon'});
+
+    my ($width_e, $height_n) = ($max_e - $min_e, $max_n - $min_n);
+
+    return "width is 0" if ($width_e == 0);
+    return "height is 0" if ($height_n == 0);
+
+    my $scale;
+    my $height;
+    my $width;
+
+    if ($input->{"width"} >0)
+    {
+        $width = $input->{"width"};
+        $scale = $width_e / $width; 
+        $height = int($height_n / $scale + .5);
+    }
+    else
+    {
+        $height = $input->{"height"};
+        $scale = $height_n / $height; 
+        $width = int($width_e / $scale + .5);
+    }
+
+    $output->{"width"} = $width;
+
+    my $statusheight = 35;
+    $output->{"height"} = $height + $statusheight;
+
+    # initialize images
+    my $images = {};
+    $images->{0}->{"img"} = new GD::Image($width, $height + $statusheight, 0);
+    for (my $i = 0; $i < scalar(@{$input->{"filedates"}}); $i++)
+    {
+        my $key = $input->{"filedates"}->[$i];
+        $images->{$key}->{"img"} = new GD::Image($width, $height + $statusheight, 0);
+        my ($r, $g, $b) = @{$colormap->{$input->{"color"}}};
+        $images->{$key}->{"col"} = $images->{$key}->{"img"}->colorAllocate($r, $g, $b);
+        $images->{$key}->{"txt"} = $images->{$key}->{"img"}->colorAllocate(255,255,255);
+        $images->{$key}->{"bgr"} = $images->{$key}->{"img"}->colorAllocate(100,100,100);
+    }
+
+    my @tmp = sort keys(%$images);
+    my $minfd = $tmp[1]; 
+    my $maxfd = $tmp[$#tmp];
+    my $request;
+
+    if ($input->{"bgimage"} == 1)
+    {
+        $request = "http://labs.metacarta.com/wms/vmap0?service=WMS&request=GetMap&srs=EPSG:4326&format=image/jpeg&version=1.1.1&layers=basic";
+    }
+    elsif ($input->{"bgimage"} == 2)
+    {
+        $request = "http://onearth.jpl.nasa.gov/wms.cgi?request=GetMap&layers=global_mosaic&styles=&srs=EPSG:4326&format=image/jpeg";
+    }
+    my $image;
+
+    if ($request ne "")
+    {
+        $request = $request . sprintf("&bbox=%f,%f,%f,%f&width=%d&height=%d", 
+                $input->{"minlon"}, $input->{"minlat"}, 
+                $input->{"maxlon"}, $input->{"maxlat"}, $width, $height);
+
+        # print "GET $request\n";
+        my $response = $ua->get($request);
+        # print "response: ".$response->status_line."\n";
+        if (!$response->is_success)
+        {
+            return "WMS server error: ".$response->status_line;
+        }
+        unless ($image = GD::Image->newFromJpegData($response->content, 0))
+        {
+            return "WMS server error: not returned a valid image";
+        }
+    }
+    else
+    {
+        $image = GD::Image->new($width, $height + $statusheight, 0);
+        my $bgr = $image->colorAllocate(100,100,100);
+        $image->filledRectangle(0, 0, $width, $height + $statusheight, $bgr);
+    }
+
+    foreach my $key(keys %$images)
+    {
+        $images->{$key}->{"img"}->copy($image, 0, 0, 0, 0, $width, $height);
+    }
+
+    my @buckets;
+
+    for (my $lat = int($input->{"minlat"}*2); $lat <= int($input->{"maxlat"}*2)+1; $lat++)
+    {
+        for (my $lon = int($input->{"minlon"}*2); $lon <= int($input->{"maxlon"}*2)+1; $lon++)
+        {
+            push(@buckets, ($lat+180)*720+$lon+360);
+        }
+    }
+
+    my $buckets = join(",", @buckets);
+    my $mila = int($input->{"minlat"} * 10000);
+    my $milo = int($input->{"minlon"} * 10000);
+    my $mala = int($input->{"maxlat"} * 10000);
+    my $malo = int($input->{"maxlon"} * 10000);
+
+    my $query =<<EOF;
+select lat, lon, fromdate, todate
+from   nodes
+where  bucket in ($buckets)
+and    fromdate <= $maxfd
+and    todate >= $minfd
+and    lat >= $mila
+and    lat <= $mala
+and    lon >= $milo
+and    lon <= $malo;
+EOF
+
+    #print "$query\n";
+    my $sth = $dbh->prepare($query) or return $dbh->errstr;
+    $sth->execute() or return $dbh->errstr;
+
+    my ($x, $y);
+    my $pixel = $input->{"pixel"};
+
+    while (my $row = $sth->fetchrow_arrayref)
+    {
+        my ($x, $y) = $proj->forward($row->[0] / 10000, $row->[1] / 10000);
+        $x = int(($x-$min_e)/$scale);
+        $y = $height - int(($y-$min_n)/$scale);
+        foreach my $key(keys(%$images))
+        {
+            if ($row->[2] <= $key && $row->[3] >= $key)
+            {
+                if ($pixel == 0)
+                {
+                    $images->{$key}->{"img"}->setPixel($x, $y, $images->{$key}->{"col"});
+                }
+                elsif ($pixel == 1)
+                {
+                    $images->{$key}->{"img"}->rectangle($x, $y, $x+1, $y+1, $images->{$key}->{"col"});
+                }
+                elsif ($pixel == 2)
+                {
+                    $images->{$key}->{"img"}->rectangle($x-1, $y-1, $x+1, $y+1, $images->{$key}->{"col"});
+                }
+                else
+                {
+                    $images->{$key}->{"img"}->filledEllipse($x, $y, 5, 5, $images->{$key}->{"col"});
+                }
+                $images->{$key}->{"cnt"}++;
+            }
+        }
+    }
+
+    my $maxnodes = 0;
+    foreach my $key(keys %$images)
+    {
+        $maxnodes = $images->{$key}->{"cnt"} if ($images->{$key}->{"cnt"} > $maxnodes);
+    }
+    $output->{"max_nodes"} = $maxnodes;
+    my $digitspace = 7 * length($maxnodes);
+    my $finalframe; 
+
+    foreach my $key(sort keys %$images)
+    {
+        my $i = $images->{$key}->{"img"};
+        $finalframe = $i;
+        my $d = sprintf "20%02d-%02d-%02d", $key/10000, ($key%10000)/100, $key%100;
+        my $c = $images->{$key}->{"cnt"};
+
+        my $scaleleft = todays($minfd);
+        my $scaleright = todays($maxfd);
+        my $barwidth = ($width - 120 - $digitspace);
+        my $barscale = $barwidth / ($scaleright - $scaleleft);
+        my $thisbar = (todays($key)-$scaleleft)*$barscale;
+#printf "scale left $scaleleft right $scaleright this date %d gives %d\n", todays($key),$thisbar;
+        $i->filledRectangle(0,$height+1,$width,$height+$statusheight,$images->{$key}->{"bgr"});
+        $i->filledRectangle(110+$digitspace,$height+11,$thisbar+110+$digitspace,$height+20, $images->{$key}->{"txt"});
+
+        $i->string(gdMediumBoldFont, 10, $height + 10, "$d ($c)", $images->{$key}->{"txt"});
+#add_frame_data($i);
+#    $gifdata .= $i->gifanimadd(undef, 0, 0, 100, 1, $prev);
+#    $prev = $i;
+        my $tmp = sprintf("/tmp/hqrun.tmp.$$.frame-%06d.gif", $key);
+        open(BLA, ">$tmp") or return $!;
+        print(BLA $i->gif());
+        close(BLA);
+        push(@tmpfiles, $tmp);
+    }
+
+    system "gifsicle --colors 256 --delay ".$input->{"delay"}." -O2 ".join(" ", @tmpfiles)." > $outfile";
+    foreach my $tmp (@tmpfiles) { unlink $tmp; }
+    $output->{"filename"} = $outfile;
+
+    my $tw = 160;
+    my $th = 160 / $width * ($height);
+    my $thumb = GD::Image->new($tw, $th, 0); 
+    $thumb->copyResampled($finalframe, 0, 0, 0, 0, $tw, $th, $width, $height);
+    open(BLA, ">$thumbfile") or return $!;
+    print(BLA $thumb->gif());
+    close(BLA);
+    $output->{"thumbname"} = $thumbfile;
+    
+    return $output;
+}
+
+sub todays
+{
+    my $d = shift;
+    return 365 * int($d/10000)+30*int(($d%10000)/100)+$d%100;
+}
+
+sub get_filedates
+{
+    my @ret;
+    $sth_planet->execute();
+    while(my $row = $sth_planet->fetchrow_arrayref())
+    {
+        push(@ret, $row->[0]);
+    }
+    return @ret;
+}
---
+++ applications/rendering/history/index.cgi
@@ -0,0 +1,385 @@
+#!/usr/bin/perl
+
+use CGI qw(escapeHTML);
+use URI::Escape;
+use DBI;
+use strict;
+
+
+my $cgi = CGI->new();
+my $values = 'label,user,minlat,minlon,maxlat,maxlon,projection,bgimage,width,height,pixel,color,fromdate,todate,frequency,delay';
+
+
+print <<EOF;
+Content-type: text/html
+
+<html><head><title>OSM History</title></head><body>
+EOF
+
+my $bg="#cccccc";
+
+if ((!defined($cgi->param("label")) && !defined($cgi->param("show")) && !defined($cgi->param("retry"))) || defined($cgi->param("form")))
+{
+print <<EOF;
+<h1>OSM History</h1>
+
+<strong>Displaying Historic OpenStreetMap Coverage</strong>
+<p>
+The history service creates animated GIF images that depict how OSM coverage of an area
+has changed over time. It does so by painting a dot on a map for each node, using old planet 
+files for past data. (It doesn't draw lines for segments and ways, which means that zoomed-in
+inner city views look a bit thin sometimes.) You can control some aspects of image generation,
+like the background image to be used or what time range you want depicted.
+</p>
+<p>
+Computation is too time-consuming to offer this as a live service, but you can request images 
+to be created and then view or download them once they are ready (the list below should give
+an indication how many requests are waiting and also how long other requests took to complete).
+</p>
+<p>
+You can browse past results below. If you request a new image, it will show up in the list
+after some time. This service is mainly intended for people to generate images, then 
+download and use them e.g. in presentations. I will probably delete old images from this 
+server from time to time. 
+</p>
+
+<form method="post">
+<table>
+<tr valign="top">
+<td bgcolor="$bg" colspan="3">
+<b>Fill out this form to request an animated GIF image to be rendered:</b><br />&nbsp;
+</td>
+</tr>
+
+<tr valign="top">
+<td bgcolor="$bg">Identification</td>
+<td>&nbsp;</td>
+<td><table cellspacing="0" cellpadding="0">
+    <tr><td>Your Name:&nbsp;</td><td>
+EOF
+    print $cgi->textfield("user", "", 21, 20);
+    print "</td></tr><tr><td>Image Name:&nbsp;</td><td>";
+    print $cgi->textfield("label", "", 51, 50);
+
+print <<EOF;
+    </td></tr></table>
+    <font size="-1"><em>This is just so that you (and others) can recognize the image later. You can 
+    put any user name, it is not checked against a list.</em></font>
+</td></tr>
+
+<tr valign="top">
+<td bgcolor="$bg">Bounding Box</td>
+<td>&nbsp;</td>
+<td><table border="0" cellpadding="0" cellspacing="0">
+<tr>
+EOF
+    print $cgi->td("min. lat &nbsp;").$cgi->td($cgi->textfield("minlat", "", 10, 11));
+    print $cgi->td("min. lon &nbsp;").$cgi->td($cgi->textfield("minlon", "", 10, 11));
+    print "</tr><tr>";
+    print $cgi->td("max. lat &nbsp;").$cgi->td($cgi->textfield("maxlat", "", 10, 11));
+    print $cgi->td("max. lon &nbsp;").$cgi->td($cgi->textfield("maxlon", "", 10, 11));
+print <<EOF;
+    </table>
+</td></tr>
+
+<tr valign="top">
+<td bgcolor="$bg">Projection</td>
+<td>&nbsp;</td>
+<td>
+EOF
+
+print $cgi->popup_menu(
+    "projection", 
+    [1, 2], 
+    1, 
+    { 1=> "lat/lon (EPSG:4326)", 2=>"Mercator" }
+);
+
+print <<EOF;
+    <br />
+    <font size="-1"><em>(using Mercator for large areas doesn't work well with background images)</em></font>
+</td></tr>
+
+<tr valign="top">
+<td bgcolor="$bg">Background</td>
+<td>&nbsp;</td>
+<td>
+EOF
+
+print $cgi->popup_menu(
+    "bgimage", 
+    [0, 1, 2], 
+    2, 
+    { 0 => "empty", 1 => "Metacarta vmap0", 2 => "Landsat" }
+);
+
+print <<EOF;
+</td></tr>
+
+<tr valign="top">
+<td bgcolor="$bg">GIF size</td>
+<td>&nbsp;</td>
+<td>width: 
+EOF
+
+print $cgi->textfield("width", "640", 6, 5);
+print " OR height: ";
+print $cgi->textfield("height", "", 6, 5);
+
+print <<EOF;
+    <br />
+    <font size="-1"><em>(other dimension results from bounding box and projection data)</em></font>
+</td></tr>
+
+<tr valign="top">
+<td bgcolor="$bg">OSM Nodes</td>
+<td>&nbsp;</td>
+<td>
+EOF
+print $cgi->popup_menu(
+    "pixel", 
+    [0, 1, 2, 3], 
+    1, 
+    { 0 => "drawn as pixels", 1 => "drawn as 2x2 rectangles", 2 => "drawn as 3x3 rectangles", 3 => "drawn as 5x5 circles" }
+);
+
+print $cgi->popup_menu(
+    "color", 
+    [0, 1, 2, 3], 
+    0, 
+    { 0 => "in red", 1 => "in yellow", 2 => "in white", 3 => "in blue" }
+);
+
+print <<EOF;
+</td></tr>
+
+<tr valign="top">
+<td bgcolor="$bg">Date Range</td>
+<td>&nbsp;</td>
+<td>from 
+EOF
+
+print $cgi->popup_menu(
+    "fromdate", 
+    [0, 60701, 70101, 70701],
+    0, 
+    { 0 => "earliest", 60701=> "2006-07", 70101=>"2007-01",70701=>"2007-07" }
+);
+
+print " to ";
+
+print $cgi->popup_menu(
+    "todate", 
+    [999999, 60630, 61231, 70630],
+    0, 
+    { 999999 => "latest", 60630=>"2006-06", 61231=>"2006-12",70630=>"2007-06" }
+);
+print <<EOF;
+    <br />
+    <font size="-1"><em>(actual resolution depends on available historic data)</em></font>
+</td></tr>
+
+<tr valign="top">
+<td bgcolor="$bg">Frame Rate</td>
+<td>&nbsp;</td>
+<td>
+EOF
+
+print $cgi->popup_menu(
+    "frequency", 
+    [0, 10, 14, 30, 61, 91, 9999],
+    30, 
+    { 0 => "one frame for every planet file available", 10 => "one frame per 10 days", 
+    14 => "one frame per fortnight", 30 => "one frame per month", 61 => "one frame for every two months",
+    91 => "one frame per quarter", 9999 => "only one frame altogehter" }
+);
+
+print " displayed at ";
+
+print $cgi->popup_menu(
+    "delay", 
+    [50, 100, 200, 500], 
+    100, 
+    { 50 => "2 frames per second", 100 => "1 frame per second", 
+    200 => "2 seconds per frame", 500 => "5 seconds per frame" }
+);
+
+print <<EOF;
+</td></tr>
+
+<tr valign="top">
+<td bgcolor="$bg" colspan="3" align="right">
+<input type="button" name="Clear" value="Clear" onClick="document.location.href='.'" /> &nbsp; &nbsp; &nbsp; 
+<input type="submit" value="Submit" name="Submit" />
+</td>
+</tr>
+</table>
+</form>
+
+<h2>Request Queue and log</h2>
+EOF
+
+    if (my $lastShown = showRequests(undef, 10))
+    {
+        printf "<p><a href=\"?show=%d\">next page (older requests)</a>", $lastShown-1;
+    }
+}
+elsif (defined($cgi->param("show")))
+{
+    print "<h2>Request Queue and log</h2>";
+    if (my $lastShown = showRequests($cgi->param("show"), 25))
+    {
+        printf "<p><a href=\"?show=%d\">next page (older requests)</a>", $lastShown-1;
+    }
+    print "<p><a href=\"".$cgi->url()."\">Return</a>";
+}
+
+else
+{
+    my $dsn="DBI:mysql:host=localhost:database=osmhistory";
+    my $dbh = DBI->connect($dsn);
+    if (!defined($dbh))
+    { 
+        print "Database error - cannot connect: ".escapeHTML($DBI::errstr)."</body></html>"; 
+        exit; 
+    }
+
+    my $query;
+    if (defined($cgi->param("retry")))
+    {
+        $query=sprintf("update jobs set status='waiting' where id=%d", $cgi->param("retry"));
+        $cgi->delete("retry");
+    }
+    else
+    {
+        my $q;
+
+        foreach my $v (split(/,/, $values))
+        {
+            my $p = $cgi->param($v);
+            $q .= defined($p) ? $dbh->quote($p) : "null";
+            $q .= ",";
+        }
+
+        $query = "insert into jobs ($values,date_entered,status) values ($q".time().",'waiting')";
+    }
+
+    if ($dbh->do($query))
+    {
+        print "Your query has been inserted into the queue.";
+        print "<p><a href=\"".$cgi->url(-query=>1)."&form=1\">Return</a>";
+    }
+    else
+    {
+        print "<font color='red'><b>Could not insert query:</b></font><p>";
+        print escapeHTML($dbh->errstr);
+        print "<p><a href=\"".$cgi->url(-query=>1)."&form=1\">Return</a>";
+    }
+}
+
+print <<EOF;
+<hr>
+<em>Scripts written in Perl by Frederik Ramm &lt;frederik\@remote.org&gt;, Public Domain, source available on request.<br>
+All images use OpenStreetMap data which is licensed under <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA 2.0</a>.</em>
+</body></html>
+EOF
+
+sub mytime
+{
+     my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(shift);
+     return sprintf "%04d-%02d-%02d %02d:%02d", 
+        $year+1900,$mon+1,$mday,$hour,$min;
+}
+
+sub showRequests
+{
+    my ($starting, $howmany) = @_;
+
+    my $dsn="DBI:mysql:host=localhost:database=osmhistory";
+    my $dbh = DBI->connect($dsn);
+    if (!defined($dbh))
+    { 
+        print "Database error - cannot connect: ".escapeHTML($DBI::errstr)."</body></html>"; 
+        exit; 
+    }
+    my $query = "select * from jobs ";
+    if (defined($starting))
+    {
+        $query.="where id<=$starting ";
+    }
+    $howmany++;
+    my $last_shown;
+    $query .= "order by id desc limit $howmany";
+    my $sth = $dbh->prepare($query);
+    $sth->execute();
+    print "<table>";
+    while(my $row=$sth->fetchrow_hashref())
+    {
+        $howmany--;
+        last if ($howmany == 0);
+        $last_shown = $row->{"id"};
+        if ($row->{"status"} eq "finished")
+        {
+            print "<tr valign=top><td><a href='images/$row->{id}.gif'><img border=0 src='thumbnails/$row->{id}.gif'></a></td>";
+            print "<td><b>".escapeHTML($row->{"label"})."</b><br>";
+            printf "requested by: ".escapeHTML($row->{"user"}).", id: ".$row->{"id"}."<br>";
+            printf "Image size: %d kB (%d by %d pixels, %d animation frames)<br>", 
+                $row->{"filesize"}/1024, 
+                $row->{"width"}, 
+                $row->{"height"},
+                $row->{"num_frames"};
+            printf "requested %s, processed %s (took %d seconds)<br>", 
+                mytime($row->{"date_entered"}), 
+                mytime($row->{"date_finished"}), 
+                $row->{"date_finished"}-$row->{"date_started"};
+        }
+        else
+        {
+            print "<tr valign=top><td>(no thumbnail)</td>";
+            print "<td><b>".escapeHTML($row->{"label"})."</b><br>";
+            printf "requested by: ".escapeHTML($row->{"user"}).", id: ".$row->{"id"}."<br>";
+            printf "Status: ".$row->{"status"};
+            print "<br>".$row->{"errmsg"} if ($row->{"errmsg"} ne "");
+            printf "<br>requested %s<br>", mytime($row->{"date_entered"});
+        }
+        printf "bbox: %.4f,%.4f,%.4f,%.4f, date range: ",
+            $row->{"minlat"},$row->{"minlon"},$row->{"maxlat"},$row->{"maxlon"};
+        if ($row->{"fromdate"} == 0)
+        {
+            print "earliest to ";
+        }
+        else
+        {
+            printf "20%02d-%02d-%02d to ",
+            $row->{"fromdate"}/10000,
+            $row->{"fromdate"}%10000/100,
+            $row->{"fromdate"}%100;
+        }
+        if ($row->{"todate"} == 999999)
+        {
+            print "latest";
+        }
+        else
+        {
+            printf "20%02d-%02d-%02d",
+            $row->{"todate"}/10000,
+            $row->{"todate"}%10000/100,
+            $row->{"todate"}%100;
+        }
+        print "<br><a href=\"?";
+        foreach my $v (split(/,/, $values))
+        {
+            next if ($v eq "height");
+            print $v."=".uri_escape($row->{$v})."&";
+        }
+        print "form=1\">new request based on these parameters</a>";
+
+        if ($row->{"status"} eq "failed")
+        {
+            printf "&nbsp;/&nbsp;<a href=\"?retry=%d\">re-try this request</a>", $row->{"id"};
+        }
+        print "</td></tr>\n";
+    }
+    print "</table>";
+    return ($howmany==0) ? $last_shown : undef;
+}

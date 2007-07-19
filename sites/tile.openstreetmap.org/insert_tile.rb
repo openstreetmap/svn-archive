49abe46a2a2b6fb92c1db6098525fc7e49ea3557
Jon Burgess <jburgess777@gmail.com>
2007-07-19 21:36:56 +0000

    insert_tile.rb: handle mysql connect errors.

--- sites/tile.openstreetmap.org/insert_tile.rb
+++ sites/tile.openstreetmap.org/insert_tile.rb
@@ -2,20 +2,10 @@
 
 require 'mysql'
 
-
-def get_local_connection
-  begin
-    return Mysql.real_connect('localhost', 'tile', 'tile', 'tile')
-  rescue MysqlError => e
-    mysql_error(e)
-  end
-end
-
-
 def call_local_sql
   dbh = nil
   begin
-    dbh = get_local_connection
+    dbh = Mysql.real_connect('localhost', 'tile', 'tile', 'tile')
     sql = yield
     res = dbh.query(sql)
     if res.nil? then return true else return res end
@@ -35,7 +25,7 @@ filename = ARGV[3]
 
 puts "tile #{x},#{y},#{z} size is #{File.size(filename)}"
 
-if File.size(filename) == 158 # its blank
+if File.size(filename) == 0 #158 # its blank
   call_local_sql { "delete from tiles where x=#{x} and y=#{y} and z=#{z};" }
 else
   puts 'inserting tile'

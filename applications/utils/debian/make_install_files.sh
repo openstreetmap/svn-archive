98eaf5d0c38591aa657c66ef85aaae84f9409c30
Jörg Ostertag <openstreetmap@ostertag.name>
2007-08-14 15:55:33 +0000

    remove outdated Makefile.am adapt openstreetmap-utils to have some C-programms included too

--- applications/utils/debian/make_install_files.sh
+++ applications/utils/debian/make_install_files.sh
@@ -15,13 +15,99 @@ dst_path=${dst_path%/}
 
 perl_path="$dst_path/usr/share/perl5"
 bin_path="$dst_path/usr/bin"
+lib_path="$dst_path/usr/lib"
 share_path="$dst_path/usr/share/$package_name"
 man1_path="$dst_path/usr/man/man1"
 mkdir -p "$perl_path"
 mkdir -p "$bin_path"
+mkdir -p "$lib_path"
 mkdir -p "$share_path"
 mkdir -p "$man1_path"
 
+
+# ------------------------------------------------------------------
+# Utilities written in C
+
+echo ""
+echo "---> import/and2osm"
+(
+    cd import/and2osm/
+    make clean
+    make || exit -1
+    ) || exit -1
+cp import/and2osm/2AND  ${bin_path}/osm2AND
+
+#if false ; then
+# tweety@pack:~/svn.openstreetmap.org/applications/lib/libosm$ make
+# g++ -g -I/usr/local/include -I../ccoord   -c -o Components.o Components.cpp
+# Components.cpp:27:29: error: libshp/shapefil.h: No such file or directory
+# so 
+#    libosm libimg
+# have to be added later to the loop
+for lib in ccoord ; do 
+	(
+	    echo ""
+	    echo "---> lib/$lib"
+	    cd ../lib/$lib
+	#    for a in *.cpp ; do perl -p -i -e 's,libshp/shapefil.h,shapefil.h,g' $a; done
+	    make clean
+	    make || exit -1
+	    ) || exit -1
+
+	cp ../lib/$lib/lib${lib}.a ${lib_path}
+    done
+
+# As soon as libosm compiles here on my debian machine
+if false ; then
+    echo ""
+    echo "---> filter/wayclean"
+    (
+	cd filter/wayclean
+	make clean
+	make || exit -1
+	) || exit -1
+    cp filter/wayclean/wayclean ${bin_path}
+
+    echo ""
+    echo "---> osm2shp"
+    ( 
+	cd export/osm2shp
+	make clean
+	make || exit -1
+	) || exit -1
+    cp export/osm2shp/osm2shp ${bin_path}
+fi
+# of later
+
+echo ""
+echo "---> color255"
+(
+    cd color255
+    make clean
+    make || exit -1
+) || exit -1
+cp color255/color255 ${bin_path}
+
+echo ""
+echo "---> osm2pqsql"
+(
+    cd export/osm2pgsql
+    make clean
+    make || exit -1
+    ) || exit -1 
+cp export/osm2pgsql/osm2pgsql ${bin_path}
+
+echo ""
+echo "---> UTF8Sanitizer"
+(
+    cd planet.osm/C/
+    make clean
+    make || exit -1
+) || exit -1
+cp planet.osm/C/UTF8sanitizer ${bin_path}
+
+
+# ------------------------------------------------------------------
 # Copy Perl libraries
 find perl_lib/ -name "*.pm" | while read src_fn ; do 
     dst_fn="$perl_path/${src_fn#perl_lib/}"
@@ -76,6 +162,8 @@ find ./ -name "*.py" | while read src_fn ; do
 	echo "           First Line: `head -1 "$src_fn"`"
     fi
 done
+
+
 
 
 # XXX

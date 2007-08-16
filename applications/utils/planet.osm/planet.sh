55d83d53bffbbb88c83964892ac55abc64f755c2
Sebastian Spaeth <spaetz@svn.openstreetmap.org>
2007-08-16 11:49:21 +0000

    add updated planet.sh, which includes planet diff generation.

--- applications/utils/planet.osm/planet.sh
+++ applications/utils/planet.osm/planet.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
+cur_date=`date +%y%m%d`
+cur_planet=planet-${cur_date}.osm
+planet_dir=/var/www/planet.openstreetmap.org/
+
+export PATH='/usr/local/bin:/usr/bin:/bin:/usr/bin/X11'
 cd /home/steve/bin
-./planet.rb > planet-`date +%y%m%d`.osm
-bzip2 planet-`date +%y%m%d`.osm
-chmod a+r planet-`date +%y%m%d`.osm.bz2
-mv planet-`date +%y%m%d`.osm.bz2 /var/www/planet.openstreetmap.org/
+./planet.rb > ${cur_planet}
+#7zr a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on  planet-`date +%y%m%d.osm`.7z  ${cur_planet}
+#chmod a+r ${cur_planet}.7z
+#mv ${cur_planet}.7z ${planet_dir}
+bzip2 ${cur_planet}
+mv ${cur_planet}.bz2 ${planet_dir}
+
+cd ${planet_dir}
+
+#link planet latest to the new file
+ln -fs ${cur_planet}.bz2 planet-latest.osm.bz2
+
+#next, create the planet diff from last week to this week
+
+CURPLANETDATE=`echo ${cur_planet}|sed -e 's/\w*-\([0-9]*\).*/\1/'`
+PREVPLANETDATE=`ls *.osm.bz2|grep -B1 ${cur_planet}|head -n1|sed -e 's/\w*-\([0-9]*\).*/\1/'`
+PREVPLANETBZ=planet-${PREVPLANETDATE}.osm.bz2
+TMPFILE=`mktemp`
+
+planetdiff ${PREVPLANETBZ} ${cur_planet}.bz2 | bzip2 > ${TMPFILE}
+chmod +r ${TMPFILE}
+mv ${TMPFILE} ${planet_dir}/planet-${PREVPLANETDATE}-${CURPLANETDATE}.diff.xml.bz2

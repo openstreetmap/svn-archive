77d0298f8949ca76719b552b9d7f44e523e12ec8
Martijn van Oosterhout <kleptog@gmail.com>
2007-08-31 09:00:45 +0000

    Change the cachefile to be in Berkerly DB format. Apart from being easier to manage, it allows us to store meta-information like number of changes found, successes and failures. Includes backward compatability code to transfer old cache files to new file format.
    Additional changes:
    - Better estimates of the time remaining
    - Exit code determined by success of upload
    - Can add tags on the command line (-t flag)
    - Removed -c option (big footgun)
    
    Several of these suggested by Dave Hansen for the TIGER upload.

--- applications/utils/import/bulk_import/bulk_upload.pl
+++ applications/utils/import/bulk_import/bulk_upload.pl
@@ -4,6 +4,7 @@ use strict;
 
 use Getopt::Long;
 use Pod::Usage;
+use DB_File;
 
 BEGIN {
   unshift @INC, "../../perl_lib";
@@ -13,7 +14,7 @@ use Geo::OSM::OsmChangeReader;
 use Geo::OSM::EntitiesV3;
 use Geo::OSM::APIClientV4;
 
-my($input, $username, $password, $api, $cache, $help);
+my($input, $username, $password, $api, $help, %additional_tags);
 my $force = 0;
 my $dry_run = 0;
 my $loop = 0;
@@ -24,23 +25,28 @@ GetOptions (
              'u|username=s'       => \$username,
              'p|password=s'       => \$password,
              'a|api=s'            => \$api,
-             'c|cache=s'          => \$cache,
              'h|help'             => \$help,
              'f|force+'           => \$force,
              'n|dry-run'          => \$dry_run,
              'l|loop'             => \$loop,
+             't|tags=s%'          => \%additional_tags,
              ) or pod2usage(1);
 
 pod2usage(1) if $help;
 
-#$api ||= "http://openstreetmap.gryph.de/api/0.5/";
-$api ||= "http://www.openstreetmap.org/api/0.4";
+#$api ||= "http://www.openstreetmap.org/api/0.4";
+if( not defined $api )
+{
+  die "Must supply the URL of the API (standard is http://www.openstreetmap.org/api/0.4/)\n";
+}
 
 if( not $dry_run and (not defined $username or not defined $password) )
 {
   die "Must supply username and password to upload data\n";
 }
-$cache ||= $input.".cache";
+my $cache = $input.".cache";
+my $dbcache = $input.".dbcache";
+my %db_file;
 
 my $OSM = new Geo::OSM::OsmChangeReader(\&process,\&progress);
 my $start_time = time();
@@ -48,21 +54,37 @@ my $last_time = 0;
 my $uploader = new Geo::OSM::APIClient( api => $api, username => $username, password => $password )
     unless $dry_run;
 
-init_cache($cache);
+init_cache($cache,$dbcache);
 
 #$OSM->load("data/nld-delete.osm");
 my $did_something;
 
+# On the first iteration, we reset the counters
+if( not exists $db_file{loop} or $db_file{loop} eq "0" )
+{
+  $db_file{loop} = 0;
+  $db_file{total} = 0;
+}
+
 do {
   $did_something = 0;
   $OSM->load($input);
-} while($loop and $did_something);
+  $db_file{loop}++;
+} while($loop and $did_something == 3);  # We exit if all failed *OR* all succeeded *OR* did nothing
 
-use Data::Dumper;
+print STDERR  "\n";
+exit(1) if $did_something >= 2;  # Exiting because something failed
+exit(0);  # Otherwise, nothing to do or everything worked
+
 sub process
 {
   my($command, $entity, $attr, $tags, $segs) = @_;
   
+  push @$tags, %additional_tags;
+
+  if( $db_file{loop} == 0 )
+  { $db_file{total}++ }
+    
   my $ent;
   if( $entity eq "node" )
   {
@@ -76,9 +98,6 @@ sub process
   {
     $ent = new Geo::OSM::Way( $attr, $tags, $segs );
   }
-#  print Dumper($ent);
-#  print $ent->xml;
-#  return if $dry_run;
   return if resolve_ids( $ent, $command );
 
   my $id;
@@ -105,25 +124,27 @@ sub process
   if( not defined $id )
   {
     print "Error: ".$uploader->last_error_code()." ".$uploader->last_error_message." ($command ".$ent->type()." ".$ent->id().")\n";
+    $db_file{failed}++;
     # Unless force is on, exit on any error
-    exit if $force == 0;
-    # For force==1, exit on Bad Request, Unauthorized or Interal Server Error
+    exit(1) if $force == 0;
+    # For force==1, exit on Bad Request, Unauthorized or Internal Server Error
     # These shouldn't happen, but tend to keep happening when they do
     # Note: when you can't connect to server you get error 500
     if( $force == 1 )
     {
       my $code = $uploader->last_error_code();
-      exit if $code == 401 or $code == 400 or $code == 500;
+      exit(2) if $code == 401 or $code == 400 or $code == 500;
     }
     # Force==2 is keep on going, no matter what
+    $did_something |= 2;
   }
   else
   {
     mark_done( $ent, $command, $id );
-    $did_something = 1;
+    $did_something |= 1;
+    $db_file{count}++;
   }
-  
-#  my $id = $uploader->upload($ent);
+  return;
 }
 
 sub progress
@@ -133,36 +154,40 @@ sub progress
   my $time = time();
 #  print "$time == $last_time or $last_time == $start_time\n";
   return if $time == $last_time or $time == $start_time;
-  
+  return if $db_file{total} == 0 or $perc == 0 or $db_file{count} == 0; # Any of these causes problems
   $last_time = $time;  
+  
+  if( $db_file{loop} != 0 )
+  {
+    # After the first loop we have the actual count of changes in the file
+    $perc = $db_file{count}/$db_file{total};
+  }
+  else
+  {
+    # During the first loop, we only have the percentage of the file done to go on
+    # Adjust it for work actually done
+    $perc = $perc*$db_file{count}/$db_file{total};
+  }
   my $remain = (1-$perc)*($last_time - $start_time)/$perc;
-  printf STDERR "%10d %7.2f%% %3d:%02d:%02d \r", $count, $perc*100, int($remain)/3600, int($remain/60)%60, int($remain)%60;
+  printf STDERR "Loop: %2d Done:%10d/%10d %7.2f%% %3d:%02d:%02d \r", $db_file{loop},
+       $db_file{count}, $db_file{total}, $perc*100, int($remain)/3600, int($remain/60)%60, int($remain)%60;
 }
 
-#my %resolved;
-my %done;
-my $cache_fh;
+sub key
+{
+  my($key,$command,$id) = @_;
+  return substr($key,0,1).substr($command,0,1).$id;
+}
 
 sub mark_done
 {
   my( $ent, $command, $newid ) = @_;
 
-  my $line = substr($ent->type,0,1).substr($command,0,1)." ".$ent->id()." ";
-  if( not defined $newid )
-  {
-    $line .= "!"
-  }
-  elsif( $ent->id < 0 and $newid > 0 )
-  {
-    $line .= $newid;
-  }
-  else
-  {
-    $line .= "0";
-  }
-  print $cache_fh $line,"\n"    unless $dry_run;
+  return if $dry_run;
   
-  $done{$ent->type}{$command}{$ent->id} = $newid;
+  my $key = key($ent->type(), $command, $ent->id());
+  
+  $db_file{$key} = $newid;
 }
 
 # Cache has the following format
@@ -174,6 +199,14 @@ sub mark_done
 sub init_cache
 {
   my $cache = shift;
+  my $dbcache = shift;
+
+  # Open the cache file, as DB cache
+  tie %db_file, "DB_File", $dbcache, O_CREAT|O_RDWR, 0666, $DB_HASH
+    or die "Could not open dbcachefile '$dbcache' ($!)\n";
+
+  # This is backward compatability code, to transfer from old cache format
+  # to new. There's no way back.
   if( open my $fh, "<", $cache )  # Check readable first
   {
     my %types = qw( n node s segment w way );
@@ -189,7 +222,7 @@ sub init_cache
         
         if( $status ne "!" )
         {
-          $done{$type}{$command}{$id}=$status;
+          $db_file{key($type, $command ,$id)}=$status;
         }
       }
       else
@@ -197,9 +230,8 @@ sub init_cache
         die "Unknown line $. in cache: $_\n";
       }
     }
+    unlink $cache;
   }
-  open $cache_fh, ">>", $cache or die "Couldn't open cache for append ($!)\n";
-  $cache_fh->autoflush(1);
 }
 
 sub resolve_ids
@@ -207,14 +239,15 @@ sub resolve_ids
   my $ent = shift;
   my $command = shift;
   
-  return 1 if $done{$ent->type}{$command}{$ent->id};
+  return 0 if $dry_run;
+  return 1 if $db_file{key($ent->type, $command, $ent->id)};
 
   my $incomplete = 0;
   if( $ent->id < 0 )
   {
-    if( exists $done{$ent->type}{create}{$ent->id} )
+    if( exists $db_file{key($ent->type, 'create', $ent->id)} )
     {
-      $ent->set_id( $done{$ent->type}{create}{$ent->id} );
+      $ent->set_id( $db_file{key($ent->type, 'create', $ent->id)} );
     }
     elsif( $command ne "create" )
     { $incomplete = 1 }
@@ -223,13 +256,13 @@ sub resolve_ids
   {
     my $from = $ent->from;
     my $to = $ent->to;
-    if( $from < 0 and exists $done{node}{create}{$ent->from} )
+    if( $from < 0 and exists $db_file{key('node', 'create', $ent->from)} )
     {
-      $from = $done{node}{create}{$ent->from};
+      $from = $db_file{key('node', 'create', $ent->from)};
     }
-    if( $to < 0 and exists $done{node}{create}{$ent->to} )
+    if( $to < 0 and exists $db_file{key('node', 'create', $ent->to)} )
     {
-      $to = $done{node}{create}{$ent->to};
+      $to = $db_file{key('node', 'create', $ent->to)};
     }
     $ent->set_fromto( $from, $to );
     if( $from < 0 or $to < 0 )
@@ -241,9 +274,9 @@ sub resolve_ids
     my @newsegs;
     for my $seg (@$segs)
     {
-      if( $seg < 0 and exists $done{segment}{create}{$seg} )
+      if( $seg < 0 and exists $db_file{key('segment', 'create', $seg)} )
       {
-        $seg = $done{segment}{create}{$seg};
+        $seg = $db_file{key('segment', 'create', $seg)};
       }
       push @newsegs, $seg;
       if( $seg < 0 )
@@ -268,19 +301,18 @@ B<Common usages:>
 
 B<bulk_upload.pl> -i input.osc -u username -p password [-a http://server/api/version] [-c cachefile]
 
-  -i input.osc                  file read for changes
-  -u username                   username for server
-  -p password                   password for server
-  -a http://server/api/version  server to uplaod to
-                                default: http://www.openstreetmap.org/api/0.4/
-  -c cachefile                  the name of the cachefile
-                                default: <input.osc>.cache
+  -i input.osc                  file read for changes (required)
+  -u username                   username for server (required)
+  -p password                   password for server (required)
+  -a http://server/api/version  server to upload to (required)
+                                OSM is: http://www.openstreetmap.org/api/0.4/
   -f                            continue even if server returns an transient error
                                 (eg connection timeout)
   -ff                           continue no matter what
   -n                            dry-run (just parse, don't do anything)
   -l                            keep redoing file until nothing more can be done
                                 useful if file is not sorted
+  -t key=value                  Add the tag key=value to each uploaded object
     
 The cachefile is a file that tracks the usage of placeholders and what has
 been uploaded already, allowing aborted uploads to continue. Do not use an

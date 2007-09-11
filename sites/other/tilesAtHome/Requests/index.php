8a54f1f34acba8f72cfe668c54d493ed7ce0bb76
Sebastian Spaeth <spaetz@svn.openstreetmap.org>
2007-09-11 12:54:09 +0000

    hand out lowzoom render requests

--- sites/other/tilesAtHome/Requests/index.php
+++ sites/other/tilesAtHome/Requests/index.php
@@ -14,28 +14,30 @@
     exit;
   }
 
-  # Check which layers the client wants to render and refuse if its 'lowzoom'
-  if ($_GET["layers"] == 'lowzoom'){
-    print "XX|3||||we don't hand out lowzoom requests";
-    exit;
-  }
 
   include("../connect/connect.php");
   include("../lib/log.inc");
   include("../lib/requests.inc");
   include("../lib/versions.inc");
 
-  //Rate limiting: never hand out more than 50 active requests at a time
-  if(1){
-    $SQL='SELECT COUNT(*) FROM tiles_queue WHERE status='.REQUEST_ACTIVE.';';
-    $res = mysql_query($SQL);
-    $row=mysql_fetch_row($res);
-    if($row[0] > 50){
-      print 'XX|3||||rate_limiting ('.$row[0].' requests out)';
-      exit;
+  $Z=12;
+
+  # Check which layers the client wants to render
+  if ($_GET["layers"] == 'lowzoom'){
+    $Z=8;
+  } else {
+
+    //Rate limiting: never hand out more than 50 active requests at a time
+    if(1){
+      $SQL='SELECT COUNT(*) FROM tiles_queue WHERE status='.REQUEST_ACTIVE.';';
+      $res = mysql_query($SQL);
+      $row=mysql_fetch_row($res);
+      if($row[0] > 50){
+        print 'XX|3||||rate_limiting ('.$row[0].' requests out)';
+        exit;
+      }
     }
   }
-
 
   # Check whether the client version is allowed to upload
   # (if they can't, there's no point in them taking requests)
@@ -45,16 +47,12 @@
     exit;
   }
 
-  CheckForRequest();
+  CheckForRequest($Z);
   print "XX|3||||nothing_to_do";
-  // Queue empty, return a random tile
-  //$x = rand(0,4095);
-  //$y = rand(0,4095);
-  //printf("OK|3|%d|%d|%d|dev_random",$x,$y,12);
+  // THE END
 
-function CheckForRequest(){
-  $SQL =  "select `x`,`y`,`status`,`priority`,`date` from `tiles_queue` where `status`=0 union ";
-  $SQL .= "select `x`,`y`,`status`,`priority`,`date` from `tiles_queue` where `status`=1 ";
+function CheckForRequest($Z){
+  $SQL =  "select `x`,`y`,`z`,`status`,`priority`,`date` from `tiles_queue` where `status` <= ".REQUEST_NEW;
   $SQL .= "order by `priority`,`date` limit 1;";
 
 //  print "$SQL\n";return;
@@ -72,11 +70,12 @@ function CheckForRequest(){
   printf("OK|3|%d|%d|%d|db",
     $Data["x"],
     $Data["y"],
-    12);
+    $Data["z"]);
 
   moveRequest(
     $Data["x"],
     $Data["y"],
+    $Data["z"],
     $Data["status"],
     REQUEST_ACTIVE);
 

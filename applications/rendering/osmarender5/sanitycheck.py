5ab846489c8acb6c577520ae78560d01d36cb791
Sebastian Spaeth <sebastian@sspaeth.de>
2007-09-13 06:27:26 +0000

    Improve sanitycheck. Patch slightly adapted from cmarqu. thanks

--- applications/rendering/osmarender5/sanitycheck.py
+++ applications/rendering/osmarender5/sanitycheck.py
@@ -1,5 +1,9 @@
 import re, sys
 
+print_debug = 0
+print_warnings = 1
+
+#regexes to find definition and uses: 
 #class="highway tunnel"
 p  = re.compile('class=[\'"]([^\'"]+)[\'"]')
 # .landuse-retail
@@ -20,54 +24,55 @@ for line in f.readlines():
   m = p.search(line)
   m2= p2.match(line)
   m_symb = p_symb.search(line)
-  m_symb2= p_symb2.match(line)
+  m_symb2= p_symb2.search(line)
 
 
   if (m):
     for i in m.group(1).split():
       if i not in dict.keys():
-        #print "Used %s" % i
+        if print_debug: print "D: Used %s" % i
         dict[i]=1
   if (m2):
     if m2.group(1) in dict2.keys():
-      print "Double definition of class %s" % m2.group(1)
+      print "E: Double definition of class %s" % m2.group(1)
     else:
       dict2[m2.group(1)]=1
-      #print "Defined %s" % m2.group(1)
+      if print_debug: print "D: Defined %s" % m2.group(1)
 
   if (m_symb):
     i = m_symb.group(1)
     if i not in symbol_used.keys():
-      print "Used symbol %s" % i
+      if print_debug: print "D: Used symbol %s" % i
       symbol_used[i]=1
   if (m_symb2):
     if m_symb2.group(1) in symbol_def.keys():
-      print "Double definition of symbol %s" % m_symb2.group(1)
+      print "E: Double definition of symbol %s" % m_symb2.group(1)
     else:
       symbol_def[m_symb2.group(1)]=1
-      print "Defined symbol %s" % m_symb2.group(1)
+      if print_debug: print "D: Defined symbol %s" % m_symb2.group(1)
 
 f.close()
 
+# check CSS classes
 for i in dict.keys():
   if i in dict2.keys():
-    #print "Used class %s" % i
-    1
+    if print_debug: print "D: Defined and used class %s" % i
   else:
-    print "Used but undefined class %s" % i
+    print "E: Used but undefined class %s" % i
 
 for i in dict2.keys():
   if not i in dict.keys():
-    print "Defined but unused class %s" % i
+    if print_warnings: print "W: Defined but unused class %s" % i
 
 
+# check symbols
 for i in symbol_used.keys():
   if i in symbol_def.keys():
-    #print "Used symbol %s" % i
+    if print_debug: print "D: Defined and used symbol %s" % i
     1
   else:
-    print "Used but undefined symbol %s" % i
+    print "E: Used but undefined symbol %s" % i
 
 for i in symbol_def.keys():
   if not i in symbol_used.keys():
-    print "Defined but unused symbol %s" % i
+    if print_warnings: print "W: Defined but unused symbol %s" % i

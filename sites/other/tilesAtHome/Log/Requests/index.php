5723c772bb284d92e1f10ec493b2adb5fcd104a0
Sebastian Spaeth <spaetz@svn.openstreetmap.org>
2007-09-24 08:53:20 +0000

    adapt description to reflect reality

--- sites/other/tilesAtHome/Log/Requests/index.php
+++ sites/other/tilesAtHome/Log/Requests/index.php
@@ -32,8 +32,6 @@
 <ul>
 <li><b>Pending</b> requests are created when you ask for an area to be rerendered. (see the <a href="http://wiki.openstreetmap.org/index.php/Tiles%40home/APIs">API</a> if you want to know how to make those requests)</li>
 
-<li><b>New</b> requests are pretty similar to pending ones, in that they get downloaded by tiles@home clients.  In this case, <b>new</b> means that it's been re-requested.</li>
-
 <li><b>Active</b> requests have been taken by tiles@home clients to render</li>
 
 <li><b>Done</b> means the result has been uploaded, and the request is effectively closed.</li>
@@ -43,32 +41,24 @@
 
 <ul>
 
-<li><b>Green goes up</b> - something's been requested</li>
+<li><b>Green (or blue) goes up</b> - something's been requested</li>
 
-<li><b>Green to red</b> - tiles@home client has taken a request</li>
+<li><b>Green (or blue) to red</b> - tiles@home client has taken a request</li>
 
-<li><b>Red to blue</b> - they failed to upload, and it's been put back in the queue</li>
+<li><b>Red to Pink</b> - a successful rendering</li>
 
-<li><b>Blue to red</b> - client has taken a request that someone else failed to do</li>
-
-<li><b>Red to turquoise</b> - a successful rendering</li>
-
-<li><b>Turquoise goes down</b> - requests from two days ago got cleared-out of the done queue</li>
+<li><b>Turquoise</b> - Long-term moving average of fulfilled requests per hour</li>
 
 </ul>
 
-<h3>What's the difference between new and pending?</h3>
-
-<p>Nothing really. There was an idea to use it for some sort of delay mechanism, so that multiple low-priority automated requests for the same area could be grouped together.  That's probably not going to happen though.</p>
-
 <h3>Request timeouts</h3>
 
-<p>After 6 hours in the <b>active</b> queue, requests are moved to the <b>new</b> queue.  This is when someone said that they'd render something but failed to upload the result</p>
+<p>After 1.5 hours in the <b>active</b> queue, requests are moved to the <b>new</b> queue.  This is when someone said that they'd render something but failed to upload the result. Basically the request will be requeued at the very start of the queue.</p>
 
-<p>After 2 &times; 24 hours in the <b>done</b> queue, requests are deleted.</p>
+<p>Timeouts occur 2 &times; after 1.5 hours, then 4 times after 6 hours. If they haven't been finished by then, requests are deleted.</p>
 
 <h3>How many tiles are being uploaded</h3>
-<p></p>The 'done' queue represents the number of <i>tilesets</i> being uploaded per two days</p>
+<p></p>The 'done' queue represents the number of <i>tilesets</i> being uploaded per the last hour.</p>
 	
 </div>
 </body>

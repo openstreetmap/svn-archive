eaab4088a09900405efe0a80efdaea745e5c5531
Rob Aerts <rubke>
2007-09-29 13:58:11 +0000

    v0.1

---
+++ applications/editors/osmtracker/osmtiledownloader/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("OSMTileDownloader")> 
+<Assembly: AssemblyDescription("Openstreetmap tile downloader")> 
+<Assembly: AssemblyCompany("OpenStreetMap")> 
+<Assembly: AssemblyProduct("OSMTileDownloader")> 
+<Assembly: AssemblyCopyright("BSD")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d562422c-e035-4c7b-bc18-eb0c21105675")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("0.1.0.0")> 
+<Assembly: AssemblyFileVersion("0.1.0.0")> 

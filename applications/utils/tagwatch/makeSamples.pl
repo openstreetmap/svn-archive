814af4d65c8149c024c795e63b91559ac7e4f889
Oliver White <ojwlists@googlemail.com>
2007-10-07 11:18:02 +0000

    Directory for output + comments

--- applications/utils/tagwatch/makeSamples.pl
+++ applications/utils/tagwatch/makeSamples.pl
@@ -25,7 +25,7 @@ use strict;
 use LWP::Simple;
 my $DataDir = "Output";
 my $TempDir = "render";
-my $Images = "Samples";
+my $Images = "html/Samples";
 
 my($W,$H) = (150,150);
 mkdir $TempDir if ! -d $TempDir;
@@ -33,9 +33,10 @@ mkdir $Images if ! -d $Images;
 getOsmarender();
 my $SampleData = getDataSample();
 
-#CreateSample("amenity","parking"); die;
+#CreateSample("amenity","parking"); die; # testing
 
-open(REQUESTS, "<sample_requests.txt") || die;
+# Loop through the list of image requests, rendering each one
+open(REQUESTS, "<sample_requests.txt") || die("Must have a sample_requests.txt file as input");
 while(my $Line = <REQUESTS>){
   if($Line =~ m{^(\w+)\s*=\s*(.*?)\s*$}){
     print STDERR "Creating $1 = $2\n";
@@ -44,9 +45,11 @@ while(my $Line = <REQUESTS>){
 }
 close REQUESTS;
 
+# Create a sample rendering of some tag=value pair
 sub CreateSample{
   my ($Key, $Value) = @_;
 
+  # Create an OSM file showing this data
   my $Data = $SampleData;
   $Data =~ s{\[tag\]}{$Key}g;
   $Data =~ s{\[value\]}{$Value}g;
@@ -55,26 +58,28 @@ sub CreateSample{
   print OUT $Data;
   close OUT;
 
+  # Transform to SVG
   my $Cmd1 = "xsltproc $TempDir/osmarender.xsl $TempDir/map_features.xml > $TempDir/output.svg 2>/dev/null";
   `$Cmd1`;
 
+  # Render to PNG
   my $Filename = sprintf("%s/%s_%s.png", $Images, $Key, $Value);
-
   my $SvgArea = "60:57:82:86"; # -D option doesn't seem to work!?! 
-
   my $Cmd2 = sprintf("inkscape --export-area=%s -w %d -h %d --export-png=%s %s 2>/dev/null",
     $SvgArea,
     $W,$H,$Filename, "$TempDir/output.svg");
   `$Cmd2`;
-
 }
 
 sub getOsmarender{
+  # Grab the latest copy of osmarender5 + styles from SVN
   mirror("http://svn.openstreetmap.org/applications/rendering/osmarender5/osmarender.xsl", "$TempDir/osmarender.xsl");
   mirror("http://svn.openstreetmap.org/applications/rendering/osmarender5/osm-map-features-z17.xml", "$TempDir/map_features.xml");
 }
 
 sub getDataSample{
+  # Get a sample OSM data file (choose API0.4 or API0.5 versions
+  # depending on whether osmarender supports segments)
   open(IN, "<data_sample_0_4.osm") || die;  
   my $Sample;
   while(my $Line = <IN>){

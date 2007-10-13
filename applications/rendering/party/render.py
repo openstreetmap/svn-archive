4b9b0ed22acba5985e52c7fa1b00cddf842499a3
Oliver White <ojwlists@googlemail.com>
2007-10-13 21:42:26 +0000

    Version using median absolute deviation (from mean lat/lon) -- don't use: far too slow for more than 3K points

--- applications/rendering/party/render.py
+++ applications/rendering/party/render.py
@@ -14,19 +14,44 @@ from UserDict import UserDict
 from xml.sax import make_parser
 from math import sqrt
 import os
+import random 
 from os.path import join, getsize
 deg2rad = 0.0174532925
 M_PI = 3.1415926535
 
+def median(a,k):
+  num = len(a)
+  for i in range(1,num):
+    minIndex = i
+    minValue = a[i]
+    for j in range(i+1, num):
+      if(a[j] < minValue):
+        minIndex = j
+        minValue = a[j]
+    # swap a[i] and a[minIndex]
+    temp = a[i]
+    a[i] = a[minIndex]
+    a[minIndex] = temp
+  return a[k]
+
+if(0):
+  a=[]
+  for i in range(5000):
+    a.append(random.randint(1,10000))
+  print median(a, 400)
+  sys.exit();
+
 class TracklogInfo(saxutils.DefaultHandler):
   def __init__(self):
     self.count = 0
+    self.countPoints = 0
     self.points = {}
     self.currentFile = ''
   def walkDir(self, directory):
     for root, dirs, files in os.walk(directory):
       for file in files:
         if(file.endswith(".gpx")):
+          print file
           self.currentFile = join(root, file)
           self.points[self.currentFile] = []
           parser = make_parser()
@@ -34,55 +59,60 @@ class TracklogInfo(saxutils.DefaultHandler):
           parser.parse(self.currentFile)
           self.count = self.count + 1
     self.currentFile = ''
+    print "Read %d points in %d files" % (self.countPoints,self.count)
   def startElement(self, name, attrs):
     if(name == 'trkpt'):
       lat = float(attrs.get('lat', None))
       lon = float(attrs.get('lon', None))
       self.points[self.currentFile].append((lat,lon));
-  def calculate(self, numSD):
-    # Calculate mean and standard deviation
-    self.calculateMean()
-    self.calculateSD()
-    # then use that to calculate extents
-    self.N = self.lat + numSD * self.sdLat
-    self.E = self.lon + numSD * self.sdLon
-    self.S = self.lat - numSD * self.sdLat
-    self.W = self.lon - numSD * self.sdLon
-    self.dLat = self.N - self.S
-    self.dLon = self.E - self.W
-    self.ratio = self.dLon / self.dLat
-    self.debug()
+      self.countPoints = self.countPoints + 1
   def valid(self):
     if(self.ratio == 0.0):
       return(0)
     if(self.dLat <= 0.0 or self.dLon <= 0):
       return(0)
     return(1)
+  def calculate(self, amount):
+    # Calculate mean and standard deviation
+    self.calculateMean()
+    self.calculateSD(amount)
+    # then use that to calculate extents
+    self.N = self.lat + self.sdLat
+    self.E = self.lon + self.sdLon
+    self.S = self.lat - self.sdLat
+    self.W = self.lon - self.sdLon
+    self.dLat = self.N - self.S
+    self.dLon = self.E - self.W
+    self.ratio = self.dLon / self.dLat
+    self.debug()
+    #sys.exit()
 
   def calculateMean(self):
     sumLat = 0
     sumLon = 0
-    self.countPoints = 0
     for x in self.points.values():
       for y in x:
-        self.countPoints = self.countPoints + 1
         sumLat = sumLat + y[0]
         sumLon = sumLon + y[1]
     self.lat = sumLat / self.countPoints
     self.lon = sumLon / self.countPoints
-  def calculateSD(self):
-    sumDLatSq = 0;
-    sumDLonSq = 0;
+    
+  def calculateSD(self,amount):
+    diffsLat = []
+    diffsLon = []
     for x in self.points.values():
       for y in x:
-        dLat = y[0] - self.lat
-        dLon = y[1] - self.lon
-        sumDLatSq = sumDLatSq + dLat ** 2
-        sumDLonSq = sumDLonSq + dLon ** 2
-        #print "%f,%f" %(dLat,dLon)
-    self.sdLat = sqrt(sumDLatSq)
-    self.sdLon = sqrt(sumDLonSq)
-    print "Variance = %f,%f\nSD = %f,%f" % (sumDLatSq, sumDLonSq, self.sdLat, self.sdLon)
+        diffsLat.append(abs(y[0] - self.lat))
+        diffsLon.append(abs(y[1] - self.lon))
+    self.sdLat = median(diffsLat, int(self.countPoints * amount))
+    self.sdLon = median(diffsLon, int(self.countPoints * amount))
+    return
+    diffsLat.sort();
+    diffsLon.sort();
+    self.sdLat = diffsLat[int(self.countPoints * amount)]
+    self.sdLon = diffsLon[int(self.countPoints * amount)]
+    print "SD = %f,%f" % (self.sdLat, self.sdLon)
+    pass
   def debug(self):
     print "%d points" % self.countPoints
     print "Pos: %f, %f +- %f, %f" % (self.lat,self.lon, self.sdLat,self.sdLon)
@@ -115,11 +145,11 @@ class TracklogInfo(saxutils.DefaultHandler):
       
       
 Thingy = TracklogInfo()
-directory = "test/"
+directory = "eg/etienne"
 print "Loading data"
 Thingy.walkDir(directory)
 print "Calculating extents"
-Thingy.calculate(1.0)
+Thingy.calculate(0.99)
 if(not Thingy.valid):
   print "Couldn't calculate extents"
   sys.exit()

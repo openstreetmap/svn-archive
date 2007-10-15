a0d49fb69c624f033d8492732b84f52982fa765b
Dirk-Lüder Kreie <osm@deelkar.net>
2007-10-15 14:25:43 +0000

    move projection subs to tahproject.pm

--- applications/rendering/tilesAtHome/close-areas.pl
+++ applications/rendering/tilesAtHome/close-areas.pl
@@ -14,6 +14,7 @@
 
 use Math::Trig;
 use Math::Complex;
+use tahproject;
 use strict;
 
 my $nodecount = 0;
@@ -781,50 +782,6 @@ sub node_is_inside
     return 0 if ($point->{"lon"} < $minlon);
     return 1;
 }
-
-# - - - - - - - - - rest copied verbatim from tilesGen.pl - - - - - - - - - 
-
-#-----------------------------------------------------------------------------
-# Project X to longitude bounds
-#-----------------------------------------------------------------------------
-sub ProjectL {
-  my ($X, $Zoom) = @_;
-  
-  my $Unit = 360 / (2 ** $Zoom);
-  my $Long1 = -180 + $X * $Unit;
-  return(($Long1, $Long1 + $Unit));  
-}
-#-----------------------------------------------------------------------------
-# Project Y to latitude bounds
-#-----------------------------------------------------------------------------
-sub Project {
-  my ($Y, $Zoom) = @_;
-  
-  my $Unit = 1 / (2 ** $Zoom);
-  my $relY1 = $Y * $Unit;
-  my $relY2 = $relY1 + $Unit;
-  
-  $relY1 = $LimitY - $RangeY * $relY1;
-  $relY2 = $LimitY - $RangeY * $relY2;
-    
-  my $Lat1 = ProjectMercToLat($relY1);
-  my $Lat2 = ProjectMercToLat($relY2);
-  return(($Lat1, $Lat2));  
-}
-
-#-----------------------------------------------------------------------------
-# Convert Y units in mercator projection to latitudes in degrees
-#-----------------------------------------------------------------------------
-sub ProjectMercToLat($){
-  my $MercY = shift();
-  return(RadToDeg(atan(sinh($MercY))));
-}
-
-#-----------------------------------------------------------------------------
-# Angle unit-conversions
-#-----------------------------------------------------------------------------
-sub DegToRad($){return pi * shift() / 180;}
-sub RadToDeg($){return 180 * shift() / pi;}
 
 
 sub compute_angle_from_bbox_center

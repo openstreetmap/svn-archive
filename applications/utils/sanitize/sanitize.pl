0fd8af4a800a8d5be6c0d63ad9221db9813fa742
Jörg Ostertag <openstreetmap@ostertag.name>
2007-10-18 23:57:09 +0000

    sanitize.pl: Adapt perl Module Search Path

--- applications/utils/sanitize/sanitize.pl
+++ applications/utils/sanitize/sanitize.pl
@@ -9,11 +9,11 @@ Version 0.01
 BEGIN {
     my $dir = $0;
     $dir =~s,[^/]+/[^/]+$,,;
-    unshift(@INC,"$dir/perl");
+    unshift(@INC,"$dir/perl_lib");
 
-    unshift(@INC,"../perl");
-    unshift(@INC,"~/svn.openstreetmap.org/utils/perl");
-    unshift(@INC,"$ENV{HOME}/svn.openstreetmap.org/utils/perl");
+    unshift(@INC,"../perl_lib");
+    unshift(@INC,"~/svn.openstreetmap.org/utils/perl_lib");
+    unshift(@INC,"$ENV{HOME}/svn.openstreetmap.org/utils/perl_lib");
 }
 
 

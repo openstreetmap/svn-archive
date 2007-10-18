66e9c87a29179e6362855fc3c4d16dbbee11e2f0
Tom Hughes <tom@compton.nu>
2007-10-18 09:52:15 +0000

    Removed

--- sites/rails_port_0.5/app/models/tracetag.rb
+++
@@ -1,7 +0,0 @@
-class Tracetag < ActiveRecord::Base
-  set_table_name 'gpx_file_tags'
-
-  validates_format_of :tag, :with => /^[^\/;.,?]*$/
-
-  belongs_to :trace, :foreign_key => 'gpx_id'
-end
--- sites/rails_port_0.5/app/models/way_node.rb
+++
@@ -1,5 +0,0 @@
-class WayNode < ActiveRecord::Base
-  set_table_name 'current_way_nodes'
-
-  belongs_to :node
-end

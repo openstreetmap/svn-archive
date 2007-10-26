1fb3bd23aa5001e80daf8ea03dce766f8a0a0b7a
Hakan Tandogan <hakan@tandogan.com>
2007-10-26 12:46:09 +0000

    Detabified, extracted main() method

--- applications/utils/import/wdb2osm/wdb2osm.pl
+++ applications/utils/import/wdb2osm/wdb2osm.pl
@@ -52,82 +52,106 @@ See http://www.gnu.org/licenses/gpl.txt
 
 =cut
 
-# Get the filename from argument
-$filename = $ARGV[0];
-if($ARGV[0] eq ""){ print "Usage: perl wdb2osm.pl filename\n"; }
-print "Opening $filename:\n";
 
-# Create directory alongside source file
-$dirName = $filename;
-$dirName =~ s/\.txt//g;
-mkdir "$dirName";
 
-## Open file
-open( FILE, "< $filename" ) or die "Can't open $filename: $!\n";
+sub main()
+{
+    # Get the filename from argument
+    $filename = $ARGV[0];
+    if($ARGV[0] eq "")
+    {
+        print "Usage: perl wdb2osm.pl filename\n";
+    }
 
-$j=0;
-## foreach line in the file (read line)
-while( my $line = <FILE> ) {
-    # Remove tabs
-    $line =~ s/\t//g;
+    print "Opening $filename:\n";
+
+    # Create directory alongside source file
+    $dirName = $filename;
+    $dirName =~ s/\.txt//g;
+    mkdir "$dirName";
+
+    ## Open file
+    open( FILE, "< $filename" ) or die "Can't open $filename: $!\n";
+
+    $j=0;
+    ## foreach line in the file (read line)
+    while( my $line = <FILE> )
+    {
+        # Remove tabs
+        $line =~ s/\t//g;
     
-    # Split line on spaces
-    @lineSplit = split(" ",$line);
+        # Split line on spaces
+        @lineSplit = split(" ",$line);
 
-    if(@lineSplit[0] eq "segment"){
-	# Output way of all segments if we have added any
-	if( $#nodes ){
-	    print OUT "<way id='-1'>\n";
-	    foreach(@nodes){
-		print OUT "  <nd ref='$_' />\n";
-	    }
-	    print OUT "</way>\n";
-	}
-	
-	# Add the closing "</osm>" and close the file
-	print OUT "</osm>";
-	close(OUT);
-	
-	# Get the remaining "special elements"
-	$segmentNumber = @lineSplit[1];
-	$rankNumber = @lineSplit[3];
-	$points = @lineSplit[5];
-	
-	# name the file according to the segment number
-	$newFile = "segment$segmentNumber";
-	# open the file for writing
-	$outfile = "$dirName/$newFile.osm";
-	print "Opening $outfile\n";
-	open OUT, "> $outfile" or die "Can't open $outfile : $!";
+        if(@lineSplit[0] eq "segment")
+        {
+            # Output way of all segments if we have added any
+            if( $#nodes )
+            {
+                print OUT "<way id='-1'>\n";
+                foreach(@nodes)
+                {
+                    print OUT "  <nd ref='$_' />\n";
+                }
+                print OUT "</way>\n";
+            }
+        
+            # Add the closing "</osm>" and close the file
+            print OUT "</osm>";
+            close(OUT);
+        
+            # Get the remaining "special elements"
+            $segmentNumber = @lineSplit[1];
+            $rankNumber = @lineSplit[3];
+            $points = @lineSplit[5];
+        
+            # name the file according to the segment number
+            $newFile = "segment$segmentNumber";
+            # open the file for writing
+            $outfile = "$dirName/$newFile.osm";
+            print "Opening $outfile\n";
+            open OUT, "> $outfile" or die "Can't open $outfile : $!";
 
-	# Initialize IDs.
-	# We start nodes at -1 and segments at -2 then alternate as we
-	# go along.
-	# The first node will actually be -3 and the first segment
-	# -5. The way get ID -1 This makes sure we get unique IDs for
-	# every element.
-	$nodeid = -1; $segmentid = -2;
+            # Initialize IDs.
+            # We start nodes at -1 and segments at -2 then alternate as we
+            # go along.
+            # The first node will actually be -3 and the first segment
+            # -5. The way get ID -1 This makes sure we get unique IDs for
+            # every element.
+            $nodeid = -1; $segmentid = -2;
 
-	# @segments = ();
-	@nodes = ();
+            # @segments = ();
+            @nodes = ();
 
-	# Output header
-	print OUT "<?xml version='1.0' encoding='UTF-8'?>\n";
-	print OUT "<osm version='0.5' generator='wdb2osm.pl'>\n";
-    } elsif( @lineSplit[0] ne "segment" && @lineSplit[0] ne "" ){
-	$nodeid-=2;
-	print OUT "<node id=\"$nodeid\" lat=\"@lineSplit[0]\" lon=\"@lineSplit[1]\"/>\n";
-	push(@nodes, $nodeid);
+            # Output header
+            print OUT "<?xml version='1.0' encoding='UTF-8'?>\n";
+            print OUT "<osm version='0.5' generator='wdb2osm.pl'>\n";
+        }
+        elsif( @lineSplit[0] ne "segment" && @lineSplit[0] ne "" )
+        {
+            $nodeid-=2;
+            print OUT "<node id=\"$nodeid\" lat=\"@lineSplit[0]\" lon=\"@lineSplit[1]\"/>\n";
+            push(@nodes, $nodeid);
+        }
     }
+
+    # Output the way with all the segments, then close the file
+    if( $#segments )
+    {
+        print OUT "<way id='-1'>\n";
+        foreach(@nodes)
+        {
+            print OUT "  <nd ref='$_' />\n";
+        }
+        print OUT "</way>\n";
+    }
+    print OUT "</osm>";
+    close(OUT);
 }
 
-# Output the way with all the segments, then close the file
-if( $#segments ){
-    print OUT "<way id='-1'>\n";
-    foreach(@nodes){
-	print OUT "  <nd ref='$_' />\n";
-    }
-    print OUT "</way>\n";
-}
-print OUT "</osm>";
-close(OUT);
+
+#
+#   Run that script...
+#
+main();
+

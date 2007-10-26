bb92543ccf9c6f5cda0620b5da822c4500db080f
Dirk-Lüder Kreie <osm@deelkar.net>
2007-10-26 20:18:25 +0000

    moved variable

--- applications/rendering/tilesAtHome/tilesGen.pl
+++ applications/rendering/tilesAtHome/tilesGen.pl
@@ -339,7 +339,7 @@ sub ProcessRequestsFromServer
     # such as the list of fields that it's sending out in requests
     # ----------------------------------
 
-    $Request=GetRequestFromServer("GET");
+    my $Request = GetRequestFromServer("GET");
 
     return (0, "Error reading request from server") unless ($Request);
    

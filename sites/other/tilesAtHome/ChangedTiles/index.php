b3f77db524b435d7ef49908984efeb7f7f74e4d9
Dirk-Lüder Kreie <osm@deelkar.net>
2007-10-30 03:00:41 +0000

    add autorequester php

---
+++ sites/other/tilesAtHome/ChangedTiles/index.php
@@ -0,0 +1,74 @@
+<?php
+
+  $handle = fopen("http://www.openstreetmap.org/api/0.5/changes?hours=4", "r");
+  $contents = stream_get_contents($handle);
+  fclose($handle);
+
+  $xmlReader = new XMLReader();
+  $xmlReader->XML($contents);
+
+  include("../connect/connect.php");
+  include("../lib/requests.inc");
+  
+  while($xmlReader->read()){
+    if($xmlReader->name == tile){
+      $X = $xmlReader->getAttribute("x");
+      $Y = $xmlReader->getAttribute("y");
+      $Z = $xmlReader->getAttribute("z");
+      $Changes = $xmlReader->getAttribute("changes");
+
+      print "X=$X Y=$Y Z=$Z numchanges=$Changes -- ";
+
+      if (!$Z) {$Z=12;};
+
+      $P = "2";                       ## TODO: calculate based on num_changes
+      $Src = "deelkar:changedTiles";  ## TODO: Make configurable
+  
+      if($Z != 12 && $Z != 8){
+        print "Invalid Z\n";
+        exit;
+      }
+      $Max = pow(2,$Z);
+      if($X < 0 || $Y < 0 || $X >= $Max || $Y >= $Max){
+        print "Invalid XY\n";
+        exit;
+      }
+  
+      if($P < 1 || $P > 3){
+        print "Invalid P\n";
+        exit;
+      }
+  
+      if (!requestExists($X,$Y,$Z,NULL)){
+         //Check the number of existing requests by that ip address and downgrade if needed
+         $SQL = sprintf ("SELECT COUNT(*) FROM tiles_queue WHERE `status` < %d AND `ip` = '%s'",
+    	REQUEST_DONE,
+    	$_SERVER['REMOTE_ADDR']);
+         $Result = mysql_query($SQL);
+         if ($row = mysql_fetch_row($Result) and $row[0] >= 20) 
+           if ($row[0] >= 100) {$P = 3;} else {$P=($P<2)?2:$P;}
+
+         $SQL = sprintf(
+           "INSERT into tiles_queue (`x`,`y`,`z`,`status`,`src`,`date`,`priority`,`ip`) values (%d,%d,%d,%d,'%s',now(),%s,'%s');", 
+           $X, 
+           $Y,
+           $Z,
+           REQUEST_PENDING,
+           mysql_escape_string($Src),
+           $P,
+           $_SERVER['REMOTE_ADDR']);
+    
+         mysql_query($SQL);
+         if(mysql_error()){
+           printf("Database error %s\n", mysql_error());
+           exit;
+         }
+         print "OK\n";
+      } else {
+         print "Already in queue\n";
+      }
+
+    }
+  }
+
+?>

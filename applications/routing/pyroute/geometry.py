a645bceeef427b4110ee8fd7a5041ef60760c0ad
Oliver White <ojwlists@googlemail.com>
2007-11-13 19:24:06 +0000

    Changes from the weekend

---
+++ applications/routing/pyroute/geometry.py
@@ -0,0 +1,30 @@
+import math
+
+def bearing(a,b):
+    dlat = math.radians(b[0] - a[0])
+    dlon = math.radians(b[1] - a[1])
+
+    dlon = dlon * math.cos(math.radians(a[0]))
+    
+    return(math.degrees(math.atan2(dlon, dlat)))
+
+def distance(a,b):
+    dlat = math.radians(a[0] - b[0])
+    dlon = math.radians(a[1] - b[1])
+
+    dlon = dlon * math.cos(math.radians(a[0]))
+    print "d = %f, %f" % (dlat, dlon)
+    # todo: mercator proj
+    dRad = math.sqrt(dlat * dlat + dlon * dlon)
+
+    print dRad
+    c = 40000 # earth circumference,km
+    
+    return(dRad * c)
+    
+
+if(__name__ == "__main__"):
+    a = (51.477,-0.4856)
+    b = (51.477,-0.4328)
+
+    print bearing(a,b)
---
+++ applications/routing/pyroute/routeOrDirect.py
@@ -0,0 +1,34 @@
+from route import Router
+
+class RouteOrDirect(Router):
+    def __init__(self, data):
+        Router.__init__(self,data)
+        self.route = {'valid':False}
+    
+    def setStartNode(self,node):
+        self.route['startnode'] = node
+        self.route['startpos'] = self.data.nodes[node]
+    def setEndNode(self,node):
+        self.route['endnode'] = node
+        self.route['endpos'] = self.data.nodes[node]
+    def setStartLL(self,lat,lon,transport):
+        self.route['startnode'] = self.data.findNode(lat,lon,transport)
+        self.route['startpos'] = (lat,lon)
+        self.route['transport'] = transport
+    def setEndLL(self,lat,lon,transport):
+        self.route['endnode'] = self.data.findNode(lat,lon,transport)
+        self.route['endpos'] = (lat,lon)
+        self.route['transport'] = transport
+    def update(self):
+        result, newroute = self.doRouteAsLL( \
+            self.route['startnode'],
+            self.route['endnode'],
+            self.route['transport'])
+        if result == 'success':
+            self.route['route'] = newroute
+            self.route['valid'] = True
+        else:
+            self.route['route'] = [self.route['startpos'],self.route['endpos']]
+            self.route['valid'] = True
+    def valid(self):
+        return(self.route['valid'])

bc078808f03b8700d536c7c599ab26c77d71ceac
Christopher Schmidt <crschmidt@crschmidt.net>
2007-11-16 02:48:57 +0000

    Current state of timeout, munin, log code

--- sites/other/tilesAtHome/Log/Requests/History/munin.php
+++ sites/other/tilesAtHome/Log/Requests/History/munin.php
@@ -14,14 +14,9 @@
   include("../../../connect/connect.php");
 
   //Show just high priority pending
-  $Result = mysql_query("select count(*) as count from `tiles_queue` WHERE status <=1 and z=12 and priority=1;");
+  $Result = mysql_query("select count(*) as count from `tiles_queue` WHERE status <=1 and z=12 and priority<3;");
   $row = mysql_fetch_array($Result);
   printf("prio_high_pending.value %d\n", $row['count']);
-
-  //Show just low priority pending
-  $Result = mysql_query("select count(*) as count from `tiles_queue` WHERE status <=1 and z=12 and priority>1;");
-  $row = mysql_fetch_array($Result);
-  printf("prio_low_pending.value %d\n", $row['count']);
 
   $statuses = array('pending','new','active', 'done');
   $Result = mysql_query("select `status`,count(*) as count from `tiles_queue` WHERE z=12 and status > 1 group by `status` order by `status`;");
@@ -33,4 +28,7 @@
       printf("%s.value %d\n", $statuses[$row['status']],$row['count']);
     }
   }
+  $Result = mysql_query("select count(*) as count from `tiles_queue` WHERE status =3 and z=12 and timediff(now(),date)<1;");
+  $row = mysql_fetch_array($Result);
+  printf("_req_processed_last_hour.value %d\n", $row['count']);
 ?>

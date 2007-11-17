c10c270c0573ff05eb2801300e5fa778e0759285
Sebastian Spaeth <sebastian@sspaeth.de>
2007-11-17 19:24:49 +0000

    noop

--- sites/other/tilesAtHome/lib/config.inc.php
+++ sites/other/tilesAtHome/lib/config.inc.php
@@ -1,5 +1,6 @@
 <?php
-///\file contains the central configuration data for the t@h server installation
+/// \file 
+/// contains the central configuration data for the t@h server installation
 
 /// root directory (no ending slash)
 $conf['BASEDIR'] = '/var/www/osm';

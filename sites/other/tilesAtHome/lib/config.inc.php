7c7fa37c9895308318fa21345b3990ea00eaf8b1
Sebastian Spaeth <sebastian@sspaeth.de>
2007-11-17 19:16:23 +0000

    remove unused gui.inc

--- sites/other/tilesAtHome/lib/config.inc.php
+++ sites/other/tilesAtHome/lib/config.inc.php
@@ -1,9 +1,9 @@
 <?php
 ///\file contains the central configuration data for the t@h server installation
 
-/// root directory of the t@h server installation
+/// root directory (no ending slash)
 $conf['BASEDIR'] = '/var/www/osm';
 
-/// log directory
+/// log directory (no ending slash)
 $conf['LOGDIR'] = $conf['BASEDIR'].'/Log/Data';
 ?>

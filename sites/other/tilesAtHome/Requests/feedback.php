fc54d918cb8a59976260ed4b39589dcea4f76795
Sebastian Spaeth <spaetz@svn.openstreetmap.org>
2007-11-17 12:50:10 +0000

    include log.inc from users.inc.

--- sites/other/tilesAtHome/Requests/feedback.php
+++ sites/other/tilesAtHome/Requests/feedback.php
@@ -1,15 +1,16 @@
 <?php
+  /// \file
   /// THIS IS WORK-IN-PROGRESS BY SPAETZ TO HAVE A GENERIC
   /// FEEDBACK API FOR CLIENT TO REPORT ERRORS (OR SUCCESS) OF
   /// RENDERING BACK TO THE SERVER
   ///
-  /// For a list of return codes, see documentation of FeedbacReturn();
+  /// For a list of return codes, see documentation of FeedbackReturn();
 
-  $X = $_POST["x"];
-  $Y = $_POST["y"];
-  $Z = $_POST["z"];
+  $X = $_POST["x"]|0;
+  $Y = $_POST["y"]|0;
+  $Z = $_POST["z"]|12;
   $client = $_POST["user"];
-  $passwd = $_POST["passwd"]
+  $passwd = $_POST["passwd"];
   $layer = $_POST["layer"];
   $result = $_POST["result"];
   $reason = $_POST["reason"];
@@ -17,53 +18,43 @@
   //sanity checks
   if($Z != 12 && $Z != 8){
      FeedbackReturn(400,'Invalid Z');
-     exit;
   }
   $Max = pow(2,$Z);
   if($X < 0 || $Y < 0 || $X >= $Max || $Y >= $Max){
      FeedbackReturn(400,'Invalid XY');
-     exit;
   }
   
   include("../lib/users.inc");
-  if (($uid = checkUser($_POST['user'],$_POST['passwd']) < 1) {
+  if (($UID = checkUser($_POST['user'],$_POST['passwd'])) < 1) {
     FeedbackReturn(401,'Wrong username/passsword');
-    exit;
   }
 
-  include("../connect/connect.php");
+  //include("../connect/connect.php");
   include("../lib/requests.inc");
 
   if (requestExists($X,$Y,$Z,NULL)){
-     //no unfinished request in queue, so we and client
-     //can ignore the error and get on with life.
+     // no unfinished request in queue, so we and client
+     // can ignore the error and get on with life.
      FeedbackReturn(205,'no such unfinished request in queue. ignore error and get next job.');
-     exit;
   } else {
-    $SQL = sprintf(
-       "INSERT into tiles_queue (`x`,`y`,`z`,`status`,`src`,`date`,`priority`,`ip`, `request_date`) values (%d,%d,%d,%d,'%s',now(),%s,'%s',now());", 
-       $X, $Y, $Z, REQUEST_PENDING, mysql_escape_string($Src), $P, $_SERVER['REMOTE_ADDR']);
-
-     mysql_query($SQL);
-     if(mysql_error()){
-       printf("Database error %s\n", mysql_error());
-       exit;
-     }
-  FeedbackReturn();
+    // The request exists and is in unfinished state. Check the error code and act accordingly
+   
+    FeedbackReturn();
   }
 
 
-/** Outputs a reply to client feedback back to the client
- *
- * Return codes are sent as HTTP error codes
- * and as a plaintext line.
- * 200: everything is fine
- * 205: no unfinished request existed
- * 400: bad request. something was invalid.
- */
+/// Outputs a reply to client feedback back to the client
+///
+/// Return codes are sent as HTTP error codes
+/// and as a plaintext line.
+/// 200: everything is fine
+/// 205: no unfinished request existed
+/// 400: bad request. something was invalid.
+///
 function FeedbackReturn($code=200, $reason='OK') {
   header('HTTP/1.1 '.$code.' '.$reason);
   header('Content-type: text/plain');
   printf("%d: %s\n", $code, $reason);
+  exit;
 }
 ?>

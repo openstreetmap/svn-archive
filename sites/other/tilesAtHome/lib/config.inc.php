80c03b0f4af4fee9d289db5e51f32aae13b25e08
Sebastian Spaeth <spaetz@svn.openstreetmap.org>
2007-11-17 17:36:28 +0000

    add beginning of a central configuration. only log.inc uses that for now.

---
+++ sites/other/tilesAtHome/lib/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+///\file contains the central configuration data for the t@h server installation
+
+/// root directory of the t@h server installation
+$conf['BASEDIR'] = '/var/www/osm';
+
+/// log directory
+$conf['LOGDIR'] = $conf['BASEDIR'].'/Log/Data';
+?>

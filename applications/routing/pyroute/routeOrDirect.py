90891e1a7a3c5cf52d9980c8a8ba7a851d6c0b6d
Oliver White <ojwlists@googlemail.com>
2007-11-21 13:23:36 +0000

    direct route

--- applications/routing/pyroute/routeOrDirect.py
+++ applications/routing/pyroute/routeOrDirect.py
@@ -4,7 +4,9 @@ class RouteOrDirect(Router):
     def __init__(self, data):
         Router.__init__(self,data)
         self.route = {'valid':False}
-    
+        self.mode = 'direct';
+    def setMode(self, mode):
+      self.mode = mode
     def setStartNode(self,node):
         self.route['startnode'] = node
         self.route['startpos'] = self.data.nodes[node]
@@ -20,15 +22,22 @@ class RouteOrDirect(Router):
         self.route['endpos'] = (lat,lon)
         self.route['transport'] = transport
     def update(self):
-        result, newroute = self.doRouteAsLL( \
-            self.route['startnode'],
-            self.route['endnode'],
-            self.route['transport'])
-        if result == 'success':
-            self.route['route'] = newroute
-            self.route['valid'] = True
-        else:
-            self.route['route'] = [self.route['startpos'],self.route['endpos']]
-            self.route['valid'] = True
+      if(self.mode == 'route'):
+	      result, newroute = self.doRouteAsLL( \
+	          self.route['startnode'],
+	          self.route['endnode'],
+	          self.route['transport'])
+	      if result == 'success':
+	          self.route['route'] = newroute
+	          self.route['valid'] = True
+	      else:
+	          self.route['route'] = [self.route['startpos'],self.route['endpos']]
+	          self.route['valid'] = True
+      else:
+        self.route['route'] = []
+        self.route['route'].append(self.route['startpos'])
+        self.route['route'].append(self.route['endpos'])
+        self.route['valid'] = True
+        
     def valid(self):
         return(self.route['valid'])

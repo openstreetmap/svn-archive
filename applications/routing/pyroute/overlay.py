4bb9592bafeed9c1f6dcfd086fa87df7dd6b9f82
Oliver White <ojwlists@googlemail.com>
2007-11-23 13:38:49 +0000

    Split overlay module into helper files

--- applications/routing/pyroute/overlay.py
+++ applications/routing/pyroute/overlay.py
@@ -1,153 +1,16 @@
 import cairo
 import os
 from base import pyrouteModule
+from pyrouteMenu import *
+from menuIcons import menuIcons
+from overlayArea import overlayArea
 
-class menuIcons:
-    def __init__(self):
-        self.images = {}
-        self.cantLoad = []
-    def load(self,name):
-        filename = "icons/bitmap/%s.png" % name
-        if(not os.path.exists(filename)):
-            return(0)
-        self.images[name] = cairo.ImageSurface.create_from_png(filename)
-        if(self.images[name]):
-            return(1)
-        return(0)
-    def draw(self,cr,name,x,y,w,h):
-        if not name in self.images.keys():
-            if(name in self.cantLoad):
-                return
-            if(not self.load(name)):
-                self.cantLoad.append(name)
-                return
-        imagesize = 120.0
-        cr.save()
-        cr.translate(x,y)
-        cr.scale(w / imagesize, h / imagesize)
-        cr.set_source_surface(self.images[name],0,0)
-        cr.paint()
-        cr.restore()
-
-class overlayArea:
-    def __init__(self,cr,x,y,dx,dy,modules,iconSet):
-        self.cr = cr
-        self.x1 = x;
-        self.y1 = y;
-        self.x2 = x + dx;
-        self.y2 = y + dy;
-        self.w = dx
-        self.h = dy
-        self.cx = x + 0.5 * dx
-        self.cy = y + 0.5 * dy
-        self.event = None
-        self.modules = modules
-        self.iconSet = iconSet
-    def fill(self,r,g,b,outline=0):
-        self.cr.set_source_rgb(r,g,b)
-        self.cr.rectangle(self.x1,self.y1,self.w,self.h)
-        if(outline):
-            self.cr.stroke()
-        else:
-            self.cr.fill()
-    def mainMenuButton(self):
-        self.cr.set_line_width(2)
-        self.cr.set_dash((2,2,2), 0);
-        self.cr.set_source_rgba(0.4,0,0)
-        self.cr.arc(self.cx,self.cy, 0.5*self.w, 0, 2*3.14)
-        self.cr.stroke()
-        self.setEvent("menu:main")
-    def drawTextSomewhere(self,text,px1,py1,px2,py2):
-        innerBox = self.copyself(px1,py1,px2,py2)
-        innerBox.drawText(text)
-    def drawText(self,text):
-        self.cr.select_font_face('Verdana', cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
-        test_fontsize = 60
-        self.cr.set_font_size(test_fontsize)
-        xbearing, ybearing, textwidth, textheight, xadvance, yadvance = self.cr.text_extents(text)
-        # Resize the font to fit
-        ratiox = textwidth / self.w
-        ratioy = textheight / self.h
-        ratio = max(ratiox,ratioy)
-        
-        self.cr.set_font_size(test_fontsize / ratio)
-        textwidth = textwidth / ratio
-        textheight = textheight / ratio
-
-        marginx = 0.5 * (self.w - textwidth)
-        marginy = 0.5 * (self.h - textheight)
-        # Text
-        self.cr.move_to(self.x1 + marginx, self.y2 - marginy)
-        self.cr.set_source_rgba(0, 0, 0, 0.5)
-        self.cr.show_text(text)
-        
-    def button(self,text=None,event=None,icon=None):
-        # Rectangle
-        if(not icon):
-          if(text):
-            self.iconSet.draw(self.cr,"generic",self.x1,self.y1,self.w,self.h)
-          else:
-            self.iconSet.draw(self.cr,"blank",self.x1,self.y1,self.w,self.h)
-        if(icon):
-            self.iconSet.draw(self.cr,icon,self.x1,self.y1,self.w,self.h)
-        if(text):
-            self.drawTextSomewhere(text,0.2,0.6,0.8,0.8)
-        if(event):
-            self.setEvent(event)
-    def setEvent(self,text):
-        self.event = text
-    def xc(self,p):
-        return(self.x1 + p * self.w)
-    def yc(self,p):
-        return(self.y1 + p * self.h)
-    def copyself(self,px1,py1,px2,py2):
-        x1 = self.xc(px1)
-        y1 = self.yc(py1)
-        return(overlayArea( \
-            self.cr,
-            x1,
-            y1,
-            self.xc(px2) - x1,
-            self.yc(py2) - y1,
-            self.modules,
-            self.iconSet))
-    def xsplit(self,p):
-        a = self.copyself(0,0,p,1)
-        b = self.copyself(p,0,1,1)
-        return(a,b)
-    def ysplit(self,p):
-        a = self.copyself(0,0,1,p)
-        b = self.copyself(0,p,1,1)
-        return(a,b)
-    def xsplitn(self,px1,py1,px2,py2,n):
-        dpx = (px2 - px1) / n
-        cells = []
-        for i in range(0,n-1):
-            px = px1 + i * dpx
-            cells.append(self.copyself(px,py1,px + dpx,py2))
-        return(cells)
-    def ysplitn(self,px1,py1,px2,py2,n):
-        dpy = (py2 - py1) / n
-        cells = []
-        for i in range(0,n):
-            py = py1 + i * dpy
-            cells.append(self.copyself(px1,py,px2,py+dpy))
-        return(cells)
-            
-    def contains(self,x,y):
-        if(x > self.x1 and x < self.x2 and y > self.y1 and y < self.y2):
-            return(1)
-    def handleClick(self,x,y):
-        if(self.event):
-            self.modules['data'].handleEvent(self.event)
-            return(1)
-        return(0)
-    
 class guiOverlay(pyrouteModule):
     def __init__(self, modules):
         pyrouteModule.__init__(self, modules)
         self.modules = modules
         self.icons = menuIcons()
+        self.menus = loadMenus('Menus')
 
     def fullscreen(self):
         """Asks if the menu is fullscreen -- if it is, then the

6649d6f3b4a248fe10e08a6edc15ab762ddb9267
Oliver White <ojwlists@googlemail.com>
2007-11-24 12:24:28 +0000

    Move the map tile code into its own module
    Add headers and usage instructions to a bunch of modules

--- applications/routing/pyroute/dataStore.py
+++ applications/routing/pyroute/dataStore.py
@@ -1,3 +1,37 @@
+#!/usr/bin/python
+#-----------------------------------------------------------------------------
+# Global data table 
+#
+# Usage: 
+#   (library code for pyroute GUI, not for direct use)
+#
+# Types of data stored:
+#   * Options and settings
+#   * Data (e.g. current position)
+#   * Event-related data (e.g. position of last click)
+#
+# Not stored here:
+#   * Routes (internal within the routing module)
+#   * POIs and map data
+#
+# TODO:
+#   * Event handling code needs to be moved into a module
+#-----------------------------------------------------------------------------
+# Copyright 2007, Oliver White
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-----------------------------------------------------------------------------
 from base import pyrouteModule
 
 class DataStore(pyrouteModule):
--- applications/routing/pyroute/overlayArea.py
+++ applications/routing/pyroute/overlayArea.py
@@ -1,3 +1,33 @@
+#!/usr/bin/python
+#-----------------------------------------------------------------------------
+# Graphics library for drawing pyroute GUI menus
+#
+# Usage: 
+#   (library code for pyroute GUI)
+#
+# Features:
+#   Start by initialising a 'rectangle' that covers a certain bit of screen
+#   Then you can
+#     * Draw an icon there
+#     * Draw text in the rectangle
+#     * Make it clickable (so that it triggers an event when clicked)
+#     * Split it into smaller rectangles (to do complex GUI layouts)
+#-----------------------------------------------------------------------------
+# Copyright 2007, Oliver White
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-----------------------------------------------------------------------------
 import cairo
 import os
 

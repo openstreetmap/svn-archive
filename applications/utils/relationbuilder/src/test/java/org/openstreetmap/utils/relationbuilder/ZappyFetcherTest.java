728da696fb1dcbf0b4c991676cb8a1c848882a24
Hakan Tandogan <hakan@tandogan.com>
2007-12-07 15:49:39 +0000

    Zappy fetcher and accompanying Unit Tests

---
+++ applications/utils/relationbuilder/src/test/java/org/openstreetmap/utils/relationbuilder/ZappyFetcherTest.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package org.openstreetmap.utils.relationbuilder;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author ext.tandogan
+ * 
+ */
+public class ZappyFetcherTest
+{
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	{
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception
+	{
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.openstreetmap.utils.relationbuilder.ZappyFetcher#constructURL(java.lang.String, java.lang.String, float, float, float, float)}.
+	 */
+	@Test
+	public void testConstructURL()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+}

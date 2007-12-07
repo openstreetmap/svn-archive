3aea9625dd1ae7bd5191abbcba7a1aef1ec16aab
Hakan Tandogan <hakan@tandogan.com>
2007-12-07 15:17:26 +0000

    Coordinates for Turkey and Usak, keep them around...

--- applications/utils/relationbuilder/src/main/java/org/openstreetmap/utils/relationbuilder/RelationBuilder.java
+++ applications/utils/relationbuilder/src/main/java/org/openstreetmap/utils/relationbuilder/RelationBuilder.java
@@ -2,14 +2,19 @@ package org.openstreetmap.utils.relationbuilder;
 
 public class RelationBuilder
 {
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		float turkey_min_lat = 35.5f;
+		float turkey_max_lat = 42.2f;
+		float turkey_min_lon = 26.0f;
+		float turkey_max_lon = 45.0f;
 
+		float usak_min_lon = 29.4f;
+		float usak_min_lat = 38.6f;
+		float usak_max_lon = 29.5f;
+		float usak_max_lat = 38.7f;
 	}
-
 }

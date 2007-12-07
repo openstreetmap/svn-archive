e1790dc9ded39a81660d6db395467d95c19364d2
Hakan Tandogan <hakan@tandogan.com>
2007-12-07 15:49:39 +0000

    Zappy fetcher and accompanying Unit Tests

---
+++ applications/utils/relationbuilder/src/main/java/org/openstreetmap/utils/relationbuilder/ZappyFetcher.java
@@ -0,0 +1,50 @@
+package org.openstreetmap.utils.relationbuilder;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ZappyFetcher
+{
+	// private static String DATA_DIR = "data";
+
+	private static String URLBASE = "http://www.informationfreeway.org/api/0.5/";
+
+	public void fetchWithBB(String type, String predicate, float east,
+			float west, float south, float north)
+	{
+		try
+		{
+			URL u = constructURL(type, predicate, east, west, south, north);
+			System.err.println(u);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace(System.err);
+		}
+	}
+
+	URL constructURL(String type, String predicate, float east, float west,
+			float south, float north) throws MalformedURLException
+	{
+		StringBuffer urlBuffer = new StringBuffer(URLBASE);
+
+		if (type != null)
+		{
+			urlBuffer.append(type);
+		}
+
+		if (predicate != null && (!("".equals(predicate))))
+		{
+			urlBuffer.append("[").append(predicate).append("]");
+		}
+
+		urlBuffer.append("[bbox=");
+		urlBuffer.append(east).append(",");
+		urlBuffer.append(south).append(",");
+		urlBuffer.append(west).append(",");
+		urlBuffer.append(north);
+		urlBuffer.append("]");
+
+		return new URL(urlBuffer.toString());
+	}
+}

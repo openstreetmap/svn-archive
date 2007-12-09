f270fed485d70277b4167929df95558cae3eab71
Tom Hughes <tom@compton.nu>
2007-12-09 15:23:28 +0000

    Enhance trace list generator - new features:
      - Handle gzip and bzip2 compressed traces
      - Include file leafname in XML record
      - Take a public/private flag on the command line and record it
      - Remove directory prefix when generating descriptions

--- applications/utils/misc/search_tracklogs/listTraces.pl
+++ applications/utils/misc/search_tracklogs/listTraces.pl
@@ -5,18 +5,30 @@
 #
 # Usage: listTraces.pl YourOsmUsername DirectoryToSearch
 #--------------------------------------------------------------------
+use strict;
+use warnings;
+
+use File::Basename;
 use File::Find;
 use Digest::MD5 qw(md5_hex);
 
 # Get command-line parameters
 my $Username = shift();
+my $Public = shift();
 my $Dir = shift();
 my $OutputFile = "tracelist.xml";
-die("Usage: $0 [your name] [directory to search]\n") if(! -d $Dir);
+die("Usage: $0 [your name] [make public?] [directory to search]\n") unless $Public =~ /^(0|1)$/;
+die("Usage: $0 [your name] [make public?] [directory to search]\n") if(! -d $Dir);
 
+# Escape username
+$Username =~ s{&}{&amp;};
+$Username =~ s{<}{&lt;};
+$Username =~ s{>}{&gt;};
+$Username =~ s{"}{&quot;};
+  
 # Add all files found to an XML file
 open(XML, '>', $OutputFile) || die("I'd like to write to $OutputFile...?\n");
-print XML "<gpx_list owner='$Username'>\n";
+print XML "<gpx_list owner='$Username' public='$Public'>\n";
 find({wanted=>\&wanted,no_chdir=>1}, $Dir);
 print XML "</gpx_list>\n";
 close XML;
@@ -29,15 +41,16 @@ print STDERR "Done\nNow check $OutputFile using a text editor, and remove any tr
 sub wanted
 {
   my $Name = $File::Find::name;
+  my $Leafname = basename($Name);
   
   # GPX extension only
-  return if(lc(substr($Name,-4)) ne '.gpx');
+  return unless $Name =~ /\.gpx(?:\.gz|\.bz2)?$/;
   
   # Identify the file
   my $Hash = fileHash($Name);
   
   # Get date
-  @Stats = stat($Name);
+  my @Stats = stat($Name);
   my $Time = $Stats[9];
   
   # Think of a description
@@ -49,6 +62,7 @@ sub wanted
   
   # Add to the output
   print XML "<record ";
+  print XML "filename='$Leafname' ";
   print XML "hash='$Hash' ";
   print XML "timet='$Time' ";
   print XML "description='$Description' ";
@@ -66,8 +80,10 @@ sub describeGPX
 {
   my $Filename = shift();
   
-  # Remove GPX and just return as string
-  my $Description = substr($Filename,length($Dir),-4);
+  # Remove extension and just return as string
+  my $Description = $Filename;
+  $Description =~ s/^\Q$Dir\E[\\\/]//;
+  $Description =~ s/\.gpx(?:\.gz|\.bz2)?$//;
   $Description =~ s{\W+}{ - }g;
   
   return($Description);
@@ -81,9 +97,23 @@ sub fileHash
   local $/;
   my $filename = shift();
   die("No such $filename\n") if(! -f $filename);
-  
-  open(my $fp, '<', $filename) or die("Can't open $filename\n");
-  my $Data = <$fp>;
-  close $fp;
+
+  my $Data;
+
+  if ($filename =~ /\.gz$/) 
+  {
+      $Data = `zcat $filename`;
+  }
+  elsif ($filename =~ /\.bz2$/) 
+  {
+      $Data = `bzcat $filename`;
+  }
+  else
+  {
+      open(my $fp, '<', $filename) or die("Can't open $filename\n");
+      $Data = <$fp>;
+      close $fp;
+  }
+
   return(md5_hex($Data));
 }

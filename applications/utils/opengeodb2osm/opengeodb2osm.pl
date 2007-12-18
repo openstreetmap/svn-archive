163107d8c98485bc5aab47f2c6743682f3e1fa6c
Sven Anders <sven@anders-hamburg.de>
2007-12-18 15:30:43 +0000

    opengeodb2osm: utf8 support

--- applications/utils/opengeodb2osm/opengeodb2osm.pl
+++ applications/utils/opengeodb2osm/opengeodb2osm.pl
@@ -3,8 +3,9 @@
 # Author: Sven Anders <sven@anders-hamburg.de>
 # License GPL 2.0
 #
-my $version="0.0.1";
+my $version="0.0.2";
 
+use utf8;
 use DBI;
 my $progname="opengeodb2osm";
 my %osmOGDB;
@@ -19,7 +20,7 @@ $mysqluser="root";
 
 $mysqlpw="";
 
-print "<?xml version='1.0' encoding='iso-8859-1'?>
+print "<?xml version='1.0' encoding='UTF-8'?>
 <osm version='0.5' generator='$progname'>\n";
 if (-f "opengeodb2osmSettings.pm") {
     require opengeodb2osmSettings;
@@ -27,12 +28,12 @@ if (-f "opengeodb2osmSettings.pm") {
 
 $osmOGDB{"400100000"}="opengeodb:is_in";
 $osmOGDB{"400200000"}="opengeodb:layer";
-$osmOGDB{"400300000"}="opengeodb:Typ";
+$osmOGDB{"400300000"}="opengeodb:typ";
 $osmOGDB{"500300000"}="opengeodb:postal_codes";
-$osmOGDB{"500600000"}="opengeodb:Amtlicher-Gemeindeschl";
-$osmOGDB{"500100002"}="opengeodb:sortName";
-$osmOGDB{"500500000"}="opengeodb:CarCode";
-$osmOGDB{"500400000"}="opengeodb:TelephoneCode";
+$osmOGDB{"500600000"}="opengeodb:community_identification_number";
+$osmOGDB{"500100002"}="opengeodb:sort_name";
+$osmOGDB{"500500000"}="opengeodb:car_code";
+$osmOGDB{"500400000"}="opengeodb:telephone_area_code";
 $osmOGDB{"500100000"}="name";
 
 
@@ -49,7 +50,7 @@ if ($uselat==1) {
 my $sth = $dbh->prepare( 'SELECT loc_id,lon,lat,valid_since,date_type_since,valid_until,date_type_until FROM geodb_coordinates '.$where ) ||
      die "Kann Statement nicht vorbereiten: $DBI::errstr\n";
 
-my $sthText = $dbh->prepare('select text_type,text_val from geodb_textdata where loc_id=?');
+my $sthText = $dbh->prepare('select text_type,text_val,text_locale,is_native_lang,is_default_name from geodb_textdata where loc_id=?');
 my $sthLoc = $dbh->prepare('select loc_type from geodb_locations where loc_id=?');
 my $sthPop = $dbh->prepare('select int_val from geodb_intdata where loc_id=? and int_type=600700000');
 
@@ -85,20 +86,29 @@ while ( my @ergebnis = $sth->fetchrow_array() ){
     my %textval;
     while ( my @texterg = $sthText->fetchrow_array() ){
 	my $key=$texterg[0];
-	if (defined $textval{"$texterg[0]"}) {
-	    $textval{$texterg[0]}.=",".$texterg[1];
+	utf8::encode($texterg[1]);
+	my $locale=$texterg[2];
+	my $nativelang=$texterg[3];
+	my $defaultname=$texterg[4];
+	if (defined($osmOGDB{$key})) {
+	    $key=$osmOGDB{$key};
 	} else {
-	    $textval{$texterg[0]}=$texterg[1];
+	    $key='opengeodb:'.$key;
+	}
+	if (defined($defaultname) and ($defaultname eq "0")) {
+	    $key.=":".$locale;
+	}
+	if (defined $textval{"$key"}) {
+	    $textval{$key}.=",".$texterg[1];
+	} else {
+	    $textval{$key}=$texterg[1];
 	}
 
     }
     for my $key ( keys %textval ) {
         my $value = $textval{$key};
-	if (defined($osmOGDB{$key})) {
-	    $tag.=' <tag k="'.$osmOGDB{$key}.'" v="'.$value.'" />'."\n";
-	} else {
-	    $tag.=' <tag k="opengeodb:'.$key.'" v="'.$value.'" />'."\n";
-	}
+	$tag.=' <tag k="'.$key.'" v="'.$value.'" />'."\n";
+	
     }
     my $population="";
     while ( my @poperg = $sthPop->fetchrow_array() ){
@@ -130,17 +140,20 @@ while ( my @ergebnis = $sth->fetchrow_array() ){
 	
     }
     if ($place =~ /^opengeodb:/) {
-	my $typ=$textval{400300000};
+
+	my $typ=$textval{$osmOGDB{"400300000"}};
 	if (!defined($typ)) {
 	} elsif ($typ=~/Stadtteil/) {
 	    $place="suburb";
 	} elsif ($typ=~/stadt/i) {
-	    if ($population>100000) {
+	    if ($population eq "") {
+		$place="town";
+	    } elsif ($population>100000) {
 		$place="city";
 	    } else {
 		$place="town";
 	    }
-	} elsif ($typ=~/gemeinde/) {
+	} elsif ($typ=~/[Gg]emeinde/i) {
 	    $place="village";
 	} elsif ($population eq "") {
 	    # do nothing

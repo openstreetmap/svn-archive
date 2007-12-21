47092ba24290eb17de8d215cadb13ee0327ea107
Martijn van Oosterhout <kleptog@gmail.com>
2007-12-21 20:37:27 +0000

    Commit Dave Hansons ressurect patch.

--- applications/utils/perl_lib/Geo/OSM/APIClientV5.pm
+++ applications/utils/perl_lib/Geo/OSM/APIClientV5.pm
@@ -18,6 +18,8 @@ use HTTP::Request;
 use Carp;
 use Encode;
 use POSIX qw(sigprocmask);
+use URI;
+use Socket qw(inet_ntoa);
 
 sub new
 {
@@ -32,7 +34,7 @@ sub new
   }
 
   $url =~ s,/$,,;   # Strip trailing slash
-  $obj->{url} = $url;
+  $obj->{url} = URI->new($url);
   $obj->{client} = new LWP::UserAgent(agent => 'Geo::OSM::APIClientV5');
   
   if( defined $attr{username} and defined $attr{password} )
@@ -41,6 +43,21 @@ sub new
     $obj->{client}->default_header( "Authorization", "Basic $encoded" );
   }
   
+  # We had the problem of the client doing a DNS lookup each request. To
+  # solve this we do a gethostbyname now and store that in the URI.
+  {
+    my $addr;
+    (undef, undef, undef, undef, $addr) = gethostbyname($obj->{url}->host);
+    if( defined $addr )
+    {
+      $obj->{client}->default_header( "Host", $obj->{url}->host );
+      $obj->{url}->host( inet_ntoa($addr) );
+      print STDERR "Using address: ".$obj->{url}->as_string()."\n";
+    }
+  }
+  # Hack to avoid protocol lookups each time
+  @LWP::Protocol::http::EXTRA_SOCK_OPTS = ( 'Proto' => 6 );
+      
   $obj->{reader} = init Geo::OSM::OsmReader( sub { _process($obj,@_) } );
   return $obj;
 }
@@ -83,12 +100,16 @@ sub _request
 
 sub last_error_code
 {
-  return shift->{last_error}->code;
+  my $self=shift;
+  croak "No last error" unless defined $self->{last_error};
+  return $self->{last_error}->code;
 }
 
 sub last_error_message
 {
-  return shift->{last_error}->message;
+  my $self=shift;
+  croak "No last error" unless defined $self->{last_error};
+  return $self->{last_error}->message;
 }
 
 sub create($)
@@ -123,7 +144,7 @@ sub modify($)
   my $req = new HTTP::Request PUT => $self->{url}."/".$ent->type()."/".$ent->id();
   $req->content($content);
   
-#  print $req->as_string;
+  print $req->as_string;
   
   my $res = $self->_request($req);
   
@@ -153,8 +174,11 @@ sub get($$)
   my $self = shift;
   my $type = shift;
   my $id = shift;
+  my $extra = shift;
   
-  my $req = new HTTP::Request GET => $self->{url}."/$type/$id";
+  $extra = "/".$extra if (defined $extra);
+
+  my $req = new HTTP::Request GET => $self->{url}."/$type/$id$extra";
   
   my $res = $self->_request($req);
 
@@ -168,12 +192,57 @@ sub get($$)
   $self->{buffer} = \@res;
   $self->{reader}->parse($res->content);
   undef $self->{buffer};
-  if( scalar(@res) != 1 )
+  if($extra =~ /history/)
+  {
+    return @res;
+  }
+  if(scalar(@res) != 1 )
   {
     die "Unexpected response for get_$type [".$res->content()."]\n";
   }
-  
+
   return $res[0];
+}
+
+sub resurrect($$)
+{
+  my $self = shift;
+  my $type = shift;
+  my $id = shift;
+
+  my $ret = $self->get($type, $id);
+  if (defined $ret || !defined $self->{last_error} || ($self->{last_error}->code != 410)) {
+    return $ret;
+  }
+  
+  my @ents = $self->get($type, $id, 'history');
+  # we want the last _visible_ one
+  my $ent = $ents[-2];
+  if ($ent->type eq 'way') {
+  	printf("resurrecting way, checking all member nodes...\n");
+	foreach my $node_id (@{$ent->nodes()}) {
+		printf("checking node: $node_id...");
+		my $node_ent = $self->get('node', $node_id);
+		if (defined $node_ent) {
+			printf("good\n");
+			next;
+		}
+		printf("attempting to resurrect node: $node_id...");
+		$node_ent = $self->resurrect('node', $node_id);
+		if (!defined $node_ent) {
+			die "failed";
+		}
+		printf("success!\n");
+	}
+  	printf("all way nodes are OK, ");
+  }
+  printf("attempting to resurrect %s...", $ent->type);
+  $ret = $self->modify($ent);
+  if ($ret == $ent->id) {
+	printf("ok\n");
+	return $ret;
+  }
+  die sprintf("unable to resurrect $type $id: %s\n", $self->last_error_message);
 }
 
 sub get_node($)
@@ -194,6 +263,42 @@ sub get_relation($)
   return $self->get("relation",shift);
 }
 
+sub get_subtype($$)
+{
+  my $self = shift;
+  my $type = shift;
+  my $id = shift;
+  my $subtype = shift;
+  
+  my $req = new HTTP::Request GET => $self->{url}."/$type/$id/$subtype";
+  
+  my $res = $self->_request($req);
+
+  if( $res->code != 200 )
+  {
+    $self->{last_error} = $res;
+    return undef;
+  }
+  
+  my @res;
+  $self->{buffer} = \@res;
+  $self->{reader}->parse($res->content);
+  undef $self->{buffer};
+  if( scalar(@res) < 1 )
+  {
+    die "Unexpected response for get_subtype($type,$id,$subtype) [".$res->content()."]\n";
+  }
+  
+  return \@res;
+}
+
+sub get_node_ways($)
+{
+  my $self = shift;
+  my $id = shift;
+  
+  return $self->get_subtype("node",$id,"ways");
+}
 
 sub map($$$$)
 {

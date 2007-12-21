c1a472c4ae2407098a844baaec650477bed07932
Dirk-Lüder Kreie <osm@deelkar.net>
2007-12-21 13:12:55 +0000

    add support for single location from command line (bbox must be configured already)

--- applications/utils/export/osmgoogleearth/osmgoogleearth.sh
+++ applications/utils/export/osmgoogleearth/osmgoogleearth.sh
@@ -19,11 +19,18 @@ fi
 
 echo "configured fetch url: $url"
 
+if [ -n "$1" ]; then
+  locations="$*"
+fi
+
 for place in $locations; do
   bbox=$(eval echo \$${place}_bbox)
+  if [ -z "$bbox" ]; then 
+    echo "warning: no data for bbox at $place";
+  fi
   echo  "Fetching: $place, $bbox"
-  rm -f map.kmz data.osm
   wget -q -O ${place}.osm "${url}${bbox}"
+  rm -f map.kmz data.osm
   perl conv05/05to04.pl < ${place}.osm > data.osm
   make 
   mv map.kmz ${place}.kmz

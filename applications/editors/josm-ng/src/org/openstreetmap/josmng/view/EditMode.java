91fcb3f60f8c5ebd9c9bad464670873f77a55320
Petr Nejedly <nenik@svn.openstreetmap.org>
2008-02-19 23:45:44 +0000

    Provide some utility methods that might be useful to all edit modes.
    Track current active state and block the mode if no editable data set is selected.

--- applications/editors/josm-ng/src/org/openstreetmap/josmng/view/EditMode.java
+++ applications/editors/josm-ng/src/org/openstreetmap/josmng/view/EditMode.java
@@ -20,11 +20,16 @@
 
 package org.openstreetmap.josmng.view;
 
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import javax.swing.AbstractAction;
+import org.openstreetmap.josmng.osm.Coordinate;
+import org.openstreetmap.josmng.osm.DataSet;
+import org.openstreetmap.josmng.osm.OsmPrimitive;
+import org.openstreetmap.josmng.view.osm.OsmLayer;
 
 /**
  * An abstracion of an editation mode.
@@ -33,36 +38,77 @@ import javax.swing.AbstractAction;
  */
 public abstract class EditMode extends AbstractAction implements MouseListener, MouseMotionListener {
 
-    private MapView parent;
+    private final Listener listener = new Listener();
+    
+    protected final MapView mapView;
+    private OsmLayer layer;
+    private Projection projection;
     
     public EditMode(String name, MapView view) {
         super(name);
-        parent = view;
+        mapView = view;
     }
 
     public final void actionPerformed(ActionEvent e) {
-        parent.setEditMode(this);
+        mapView.setEditMode(this);
     }
 
     public final void enter() {
-        parent.addMouseListener(this);
-        parent.addMouseMotionListener(this);
+        mapView.addMouseListener(listener);
+        mapView.addMouseMotionListener(listener);
+        update();
         entered();
     }
     
     public final void exit() {
         exited();
-        parent.removeMouseMotionListener(this);
-        parent.removeMouseListener(this);
+        mapView.removeMouseMotionListener(listener);
+        mapView.removeMouseListener(listener);
     }
 
     protected abstract void exited();
     protected abstract void entered();
 
     protected MapView getMapView() {
-        return parent;
+        return mapView;
     }
     
+    private boolean update() {
+        EditableLayer edit = mapView.getCurrentEditLayer();
+        if (edit instanceof OsmLayer) {
+            layer = (OsmLayer)edit;
+            projection = mapView.getProjection();
+            return true;
+        }
+        return false;
+    }
+    
+    protected final DataSet getData() {
+        return layer.getDataSet();
+    }
+    
+    protected final OsmPrimitive getNearestPrimitive(Point at, int[] idx) {
+        return layer.getNearestPrimitive(at, idx);
+    }
+    
+    protected final ViewCoords pointToView(Point p) {
+        return mapView.getPoint(p);
+    }
+    
+    protected final ViewCoords coordToView(Coordinate c) {
+        return projection.coordToView(c);
+    }
+    
+    protected final Coordinate viewToCoord(ViewCoords vc) {
+        return projection.viewToCoord(vc);
+    }
+    
+    protected final Coordinate moveOnScreen(Coordinate c, Point from, Point to) {
+        ViewCoords start = pointToView(from);
+        ViewCoords end = pointToView(to);
+        return viewToCoord(coordToView(c).movedByDelta(pointToView(to), pointToView(from)));
+    }
+
     public void mouseClicked(MouseEvent e) {}
     public void mouseEntered(MouseEvent e) {}
     public void mouseExited(MouseEvent e) {}
@@ -70,4 +116,32 @@ public abstract class EditMode extends AbstractAction implements MouseListener, 
     public void mouseReleased(MouseEvent e) {}
     public void mouseDragged(MouseEvent e) {}
     public void mouseMoved(MouseEvent e) {}
+
+    private class Listener implements MouseListener, MouseMotionListener {
+        public void mouseClicked(MouseEvent e) {
+            if (update()) EditMode.this.mouseClicked(e);
+        }
+        
+        public void mouseEntered(MouseEvent e) {
+            if (update()) EditMode.this.mouseEntered(e);
+        }
+        
+        public void mouseExited(MouseEvent e) {
+            if (update()) EditMode.this.mouseExited(e);
+        }
+        
+        public void mousePressed(MouseEvent e) {
+            if (update()) EditMode.this.mousePressed(e);
+        }
+        
+        public void mouseReleased(MouseEvent e) {
+            if (update()) EditMode.this.mouseReleased(e);
+        }
+        public void mouseDragged(MouseEvent e) {
+            if (update()) EditMode.this.mouseDragged(e);
+        }
+        public void mouseMoved(MouseEvent e) {
+            if (update()) EditMode.this.mouseMoved(e);
+        }
+    }
 }

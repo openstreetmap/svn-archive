f9d9b63606e91b198a916775ecdec95c23ed0670
Hakan Tandogan <hakan@tandogan.com>
2008-02-21 21:41:01 +0000

    Example for authentification data

---
+++ applications/utils/sync-wiki/myWikiCredentials_example.pm
@@ -0,0 +1,11 @@
+# Edit this to add an account for uploading to wiki,
+# then rename it to myWikiCredentials.pm
+#
+# The same user/pass combination should exist on wikipedia,
+# and on wikimedia commons.
+sub getWikiUserPass
+{
+  return("My username", "My password");
+}
+
+1

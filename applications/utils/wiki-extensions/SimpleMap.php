96465bc46e44b1bfca68b298098e4e71c665d78a
Harry Wood <github@onlineanimals.co.uk>
2008-03-30 12:04:27 +0000

    improved paramater validation (trim spaces + slightly more helpful error messages)

--- applications/utils/wiki-extensions/SimpleMap.php
+++ applications/utils/wiki-extensions/SimpleMap.php
@@ -36,6 +36,7 @@ function map( $input ) {
 	//Parse pipe separated name value pairs (e.g. 'aaa=bbb|ccc=ddd')
 	$paramStrings=explode('|',$input);
 	foreach ($paramStrings as $paramString) {
+		$paramString = trim($paramString);
 		$eqPos = strpos($paramString,"=");
 		if ($eqPos===false) {
 			$params[$paramString] = "true";
@@ -64,52 +65,61 @@ function map( $input ) {
 	if (substr($width,-2)=="px")  $width = substr($width,0,-2);
 	if (substr($height,-2)=="px") $height = substr($height,0,-2);
 
-	//Validation of parameters values
+	
+	//Check required parameters values are provided
+	if ($lat=='') $error .= "Missing lat value (for the lattitude). ";
+	if ($lon=='') $error .= "Missing lon value (for the longitude). ";
+	if ($zoom=='') $error .= "Missing z value (for the zoom level). ";
+	if ($params['long']!='') $error .= "Please use 'lon' instead of 'long' (parameter was renamed). ";
 
-	if ($zoom=="") {
-		$error = "missing z value (for the zoom level)";
-	} else if ($lat=="") {
-		$error = "missing lat value (for the lattitude)";
-	} else if ($lon=="") {
-		$error = "missing lon value (for the lattitude)";
-		if ($params["long"]!="") $error = "Please use 'lon' instead of 'long' (parameter was renamed)";
-	} else if (!is_numeric($width)) {
-		$error = "width (w) value '$width' is not a valid integer";
-	} else if (!is_numeric($height)) {
-		$error = "height (h) value '$height' is not a valid integer";
-	} else if (!is_numeric($zoom)) {
-		$error = "zoom (z) value '$zoom' is not a valid integer";
-	} else if (!is_numeric($lat)) {
-		$error = "lattitude (lat) value '$lat' is not a valid number";
-	} else if (!is_numeric($lon)) {
-		$error = "logiditude (lon) value '$lon' is not a valid number";
-	} else if ($width>1000) {
-		$error = "width (w)) value cannot be greater than 1000";
-	} else if ($width<50) {
-		$error = "width (w)) value cannot be less than 50";
-	} else if ($height>1000) {
-		$error = "height (h)) value cannot be greater than 1000";
-	} else if ($height<50) {
-		$error = "height (h)) value cannot be less than 50";
-	} else if ($lat>90) {
-		$error = "lattitude (lat) value cannot be greater than 90";
-	} else if ($lon<-90) {
-		$error = "lattitude (lat) value cannot be less than -90";
-	} else if ($lon>180) {
-		$error = "longitude (lon) value cannot be greater than 180";
-	} else if ($lon<-180) {
-		$error = "longitude (lon) value cannot be less than -180";
-	} else if ($zoom<0) {
-		$error = "zoom (z) value cannot be less than zero";
-	} else if ($zoom==18) {
-		$error =	"zoom (z) value cannot be greater than 17. ".
-					"Note that this mediawiki extension hooks into the OpenStreetMap 'osmarender' layer ".
-					"which does not go beyond zoom level 17. The Mapnik layer available on ".
-					"openstreetmap.org, goes up to zoom level 18";
-	} else if ($zoom>17) {
-		$error = "zoom (z) value cannot be greater than 17.";
+	if ($error=='') {
+		//no errors so far. Now check the values	
+		
+		if ($zoom=="") {
+			$error = "missing z value (for the zoom level)";
+		} else if ($lat=="") {
+			$error = "missing lat value (for the lattitude)";
+		} else if ($lon=="") {
+			$error = "missing lon value (for the lattitude)";
+			if ($params["long"]!="") $error = "Please use 'lon' instead of 'long' (parameter was renamed)";
+		} else if (!is_numeric($width)) {
+			$error = "width (w) value '$width' is not a valid integer";
+		} else if (!is_numeric($height)) {
+			$error = "height (h) value '$height' is not a valid integer";
+		} else if (!is_numeric($zoom)) {
+			$error = "zoom (z) value '$zoom' is not a valid integer";
+		} else if (!is_numeric($lat)) {
+			$error = "lattitude (lat) value '$lat' is not a valid number";
+		} else if (!is_numeric($lon)) {
+			$error = "logiditude (lon) value '$lon' is not a valid number";
+		} else if ($width>1000) {
+			$error = "width (w)) value cannot be greater than 1000";
+		} else if ($width<50) {
+			$error = "width (w)) value cannot be less than 50";
+		} else if ($height>1000) {
+			$error = "height (h)) value cannot be greater than 1000";
+		} else if ($height<50) {
+			$error = "height (h)) value cannot be less than 50";
+		} else if ($lat>90) {
+			$error = "lattitude (lat) value cannot be greater than 90";
+		} else if ($lon<-90) {
+			$error = "lattitude (lat) value cannot be less than -90";
+		} else if ($lon>180) {
+			$error = "longitude (lon) value cannot be greater than 180";
+		} else if ($lon<-180) {
+			$error = "longitude (lon) value cannot be less than -180";
+		} else if ($zoom<0) {
+			$error = "zoom (z) value cannot be less than zero";
+		} else if ($zoom==18) {
+			$error =	"zoom (z) value cannot be greater than 17. ".
+						"Note that this mediawiki extension hooks into the OpenStreetMap 'osmarender' layer ".
+						"which does not go beyond zoom level 17. The Mapnik layer available on ".
+						"openstreetmap.org, goes up to zoom level 18";
+		} else if ($zoom>17) {
+			$error = "zoom (z) value cannot be greater than 17.";
+		}
 	}
-
+	
 	if ($error!="") {
 		//Something was wrong. Spew the error message and input text.
 		$output  = "";

0dd6f54b0b54568e27e4f87bb6071a7aa6f9778e
Oliver White <ojwlists@googlemail.com>
2008-04-03 21:07:20 +0000

    add animation images, and fix names with quotes in

--- applications/rendering/misc/lighthouses/test.pl
+++ applications/rendering/misc/lighthouses/test.pl
@@ -3,6 +3,7 @@ use Data::Dumper;
 use parseFlashSequence;
 use renderFlashSequence;
 use flashSequenceToImage;
+use flashSequenceToGifAnim;
 use renderIconLighthouse;
 use XML::Simple;
 
@@ -14,6 +15,7 @@ mkdir $OutDir if ! -d $OutDir;
 # open OUT, ">dumper.txt"; print OUT Dumper($OSM); close OUT;
 
 open HTML, ">$OutDir/index.html";
+print HTML "<html><head><title>Irish lighthouses</title></head><body style='background-color:white'><h1>Irish lighthouses in OSM</h1><p><i>Data is CC-BY-SA 2.0</i></p>\n";
 
 my $Cwd = getcwd;
 while(my($id, $fields) = each(%{$OSM->{node}}))
@@ -24,7 +26,6 @@ while(my($id, $fields) = each(%{$OSM->{node}}))
 		$Tags{$Tag->{k}} = $Tag->{v};
 	}
 
-	printf HTML "<h3>%s</h3>\n", $Tags{name};
 
 	if($Tags{man_made} eq 'lighthouse')
 	{	
@@ -32,19 +33,30 @@ while(my($id, $fields) = each(%{$OSM->{node}}))
 	open(OUT, ">temp.svg"); print OUT $SVG;close OUT;
 	#open(OUT, ">$OutDir/$Tags{name}.txt"); print OUT Dumper(%Tags);close OUT;
 	
-	my $BuildingFilename = "building_$Tags{name}.png";
-	renderSvg("$Cwd/temp.svg", "$Cwd/$OutDir/$BuildingFilename", 200);
+	printf HTML "<h3>%s</h3>\n", $Tags{name};
+	
+	my $Name = $Tags{name};
+	$Name =~ s{\W}{_}g;
+	
+	my $BuildingFilename = "building_$Name.png";
+	renderSvg("$Cwd/temp.svg", "$Cwd/$OutDir/$BuildingFilename", 120);
 	unlink "temp.svg";
 	
-	my $LightFilename = "lighting_$Tags{name}.png";
+	my $LightFilename = "lighting_$Name.png";
 	my $Sequence = parseFlashSequence($Tags{"lighting:sequence"});
 	my $SequenceText = renderFlashSequence($Sequence);
 	flashSequenceToImage($SequenceText, "$OutDir/$LightFilename");
 
+        my $GifFilename = "anim_$Name.gif";
+
+        flashSequenceToGifAnim($SequenceText, "$OutDir/$GifFilename");
+
 	printf HTML "<p><img src='$BuildingFilename'></p>";
 	printf HTML "<p>Description: $Tags{description}</p>\n";
-	printf HTML "<p><img src='$LightFilename'></p>";
 	printf HTML "<p>Sequence: %s</p>\n", $Tags{"lighting:sequence"};
+	
+	printf HTML "<p><a href='$GifFilename'>Animation</a></p>";
+	printf HTML "<p><img src='$LightFilename'></p>";
 	
 	}
 }

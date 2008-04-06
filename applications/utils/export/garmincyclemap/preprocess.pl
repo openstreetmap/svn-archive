68274b3cbb343098aee4a199dfbd9389a699ab35
Richard Fairhurst <richard@systemeD.net>
2008-04-06 23:57:01 +0000

    new relations-capable, current-planet-friendly version

--- applications/utils/export/garmincyclemap/preprocess.pl
+++ applications/utils/export/garmincyclemap/preprocess.pl
@@ -5,7 +5,7 @@
 	# 2. calls osmcut to split into smaller .osm files
 	# 3. calls mkgmap to create map
 
-	# Richard Fairhurst 8-13.1.08
+	# Richard Fairhurst 8-13.1.08, 6.4.08
 	# released under the WTFPL
 
 	# To use:
@@ -17,14 +17,17 @@
 	# ...and the result is a gmapsupp.img file to copy to your Garmin
 
 	# Still to do:
-	# - read bzipped file directly?
 	# - rationalise 'proposed routes' code?
 	# - cycle node handling
-	
+
+	use IO::Uncompress::Bunzip2 qw ($Bunzip2Error);
+	use IO::File;
+
 	# =====	Constants
 
-	use constant NODE => 1;
-	use constant WAY  => 2;
+	use constant NODE     => 1;
+	use constant WAY      => 2;
+	use constant RELATION => 3;
 	
 	use constant NCN  		  => 1;
 	use constant RCN		  => 2;
@@ -50,11 +53,6 @@
 			'service','minor',
 			'living_street','minor');
 	
-	# -----	Other routes to highlight (using route= tag)
-	
-	%routes=('National Byway',NCN,
-			 'Four Castles Cycle Route',RCN);
-	
 	# -----	Read in only the tags we use
 	
 	%usetag=('name',1, 'ref',1, 'route',1,
@@ -68,14 +66,64 @@
 	}
 	close INFILE;
 	
-	# =====	Process file
+	# =====	Open file
 	
 	$fn=shift @ARGV;
-	open (INFILE,$fn) or die "Couldn't open input file: $!\n";
-	open (OUTFILE,">$fn.processed") or die "Couldn't open output file: $!\n";
-	print "Processing $fn\n";
+	$ofn=Open_File($fn);
+	open (OUTFILE,">$ofn") or die "Couldn't open output file: $!\n";
 	
-	while (<INFILE>) {
+	# -----	Read relations
+
+	print "Reading relations\n";
+	# uncomment following line if you've saved relations into a separate file
+	# $fh->close; $fh=new IO::File; $fh->open("relations_only.txt");
+	%wayrefs=();
+	%waytypes=();
+	$in=0;
+	while (<$fh>) {
+		chomp ($t=$_);
+		if ($t=~/^  <relation id="(\d+)".+>$/) {
+			$c=$1; print "relation $c     \r";
+			$in=RELATION; %tags=(); @members=();
+			$tags{'type'}=''; $tags{'route'}='bicycle';
+			$tags{'state'}=''; $tags{'network'}='';
+		} elsif ($t =~/^    <member type="way" ref="(\d+)".*\/>$/) {
+			push @members,$1;
+		} elsif ($t =~/^    <tag k="(.+)" v="(.*)"\s*\/>$/ and $in==RELATION) {
+			$tags{$1}=$2;
+		} elsif ($t eq '  </relation>') {
+			$in=0;
+			next if ($tags{'type'} ne 'route');
+			next if ($tags{'route'} ne 'bicycle');
+			# What network are we in? (default LCN)
+			$cycle=LCN; $prefix=''; $suffix='';
+			if    ($tags{'network'} eq 'ncn') { $cycle=NCN; }
+			elsif ($tags{'network'} eq 'rcn') { $cycle=RCN; $prefix='R'; }
+			elsif ($tags{'network'} eq 'lcn') { $cycle=LCN; $prefix='L'; }
+			if ($tags{'state'} eq 'proposed') { $cycle+=(NCN_PROPOSED)-(NCN); $suffix='*'; }
+			# What's the ref? (fallback to name if none)
+			$n='';
+			if (exists $tags{'ref'}) { $n=$prefix.$tags{'ref'}.$suffix; }
+			elsif (exists $tags{'name'}) { $n=$tags{'name'}.$suffix; }
+			elsif (exists $tags{'network'}) { $n=$tags{'network'}.$suffix; }
+			if ($n ne '') { $n.=' '; }
+			# Set in all members
+			foreach $m (@members) {
+				if   (!exists $waytypes{$m}) { $waytypes{$m}=$cycle; }
+				elsif ($waytypes{$m}>$cycle) { $waytypes{$m}=$cycle; }
+				if (!exists $wayrefs{$m}) { $wayrefs{$m} =$n; }
+									 else { $wayrefs{$m}.=$n; }
+			}
+		}
+	}
+	$fh->close;
+	$in=0;
+	
+	# -----	Read whole file
+	
+	print "Reading tags and ways\n";
+	Open_File($fn);
+	while (<$fh>) {
 		chomp ($t=$_);
 		$t=~s/ timestamp="[^"]+"//;
 		if ($t=~/^  <node id="(\d+)".+">$/) {
@@ -94,35 +142,35 @@
 		} elsif ($t eq '  </way>') {
 			# - End of way element, process tags
 			$in=0;
-			$refnum=''; $highway=''; $cycle=NONE; $cycleref='';
+			$refnum=''; $highway='';
+			$cycle=NONE; if (exists $waytypes{$c}) { $cycle=$waytypes{$c}; }
+			$cycleref=''; if (exists $wayrefs{$c}) { $cycleref=$wayrefs{$c}; }
 			if (exists $tags{'ref'})		{ $refnum=$tags{'ref'}; delete $tags{'ref'}; }
 			if (exists $tags{'highway'})	{ $highway=$tags{'highway'}; delete $tags{'highway'}; }
-			if (exists $tags{'route'})		{ if ($tags{'route'} eq 'ncn'	) { $cycle=NCN; }
-											  elsif ($tags{'route'} eq 'rcn'	) { $cycle=RCN; }
-											  elsif ($tags{'route'} eq 'lcn'	) { $cycle=LCN; }
-											  elsif (exists $routes{$tags{'route'}}) { $cycle=$routes{$tags{'route'}}; 
-											  										   $cycleref.=$tags{'route'}.' '; }
-											  delete $tags{'route'}; }
-			if (exists $tags{'ncn'})		{ if ($tags{'ncn'} eq 'proposed') { $cycle=NCN_PROPOSED; }
-																	     else { $cycle=NCN; } 
+			if (exists $tags{'ncn'})		{ if ($tags{'ncn'} eq 'proposed') { Set_Cycle(NCN_PROPOSED); }
+																	     else { Set_Cycle(NCN); } 
 											  delete $tags{'ncn'}; }
-			elsif (exists $tags{'rcn'})		{ if ($tags{'rcn'} eq 'proposed') { $cycle=NCN_PROPOSED; }
-																	     else { $cycle=RCN; }
+			elsif (exists $tags{'rcn'})		{ if ($tags{'rcn'} eq 'proposed') { Set_Cycle(RCN_PROPOSED); }
+																	     else { Set_Cycle(RCN); }
 											  delete $tags{'rcn'}; }
-			elsif (exists $tags{'lcn'})		{ if ($tags{'lcn'} eq 'proposed') { $cycle=NCN_PROPOSED; }
-																	     else { $cycle=LCN; }
+			elsif (exists $tags{'lcn'})		{ if ($tags{'lcn'} eq 'proposed') { Set_Cycle(LCN_PROPOSED); }
+																	     else { Set_Cycle(LCN); }
 											  delete $tags{'lcn'}; }
+			if (exists $tags{'route'})		{ if    ($tags{'route'} eq 'ncn'	) { Set_Cycle(NCN); }
+											  elsif ($tags{'route'} eq 'rcn'	) { Set_Cycle(RCN); }
+											  elsif ($tags{'route'} eq 'lcn'	) { Set_Cycle(LCN); }
+											  delete $tags{'route'}; }
 
 			#	munge ref tag
-			if (exists $tags{'ncn_ref'})	{ if ($cycle>NCN and $cycle!=NCN_PROPOSED) { $cycle=NCN; }
+			if (exists $tags{'ncn_ref'})	{ if ($cycle!=NCN_PROPOSED) { Set_Cycle(NCN); }
 											  $cycleref.=$tags{'ncn_ref'}; delete $tags{'ncn_ref'};
 											  if ($cycle==NCN_PROPOSED) { $cycleref.='*'; } 
 											  $cycleref.=' '; }
-			if (exists $tags{'rcn_ref'})	{ if ($cycle>RCN and $cycle!=RCN_PROPOSED) { $cycle=RCN; }
+			if (exists $tags{'rcn_ref'})	{ if ($cycle!=RCN_PROPOSED) { Set_Cycle(RCN); }
 											  $cycleref.='R'.$tags{'rcn_ref'}; delete $tags{'rcn_ref'};
 											  if ($cycle==RCN_PROPOSED) { $cycleref.='*'; } 
 											  $cycleref.=' '; }
-			if (exists $tags{'lcn_ref'})	{ if ($cycle>LCN and $cycle!=LCN_PROPOSED) { $cycle=LCN; }
+			if (exists $tags{'lcn_ref'})	{ if ($cycle!=LCN_PROPOSED) { Set_Cycle(LCN); }
 											  $cycleref.='L'.$tags{'lcn_ref'}; delete $tags{'lcn_ref'};
 											  if ($cycle==LCN_PROPOSED) { $cycleref.='*'; }
 											  $cycleref.=' '; }
@@ -149,7 +197,7 @@
 			foreach $k (keys %tags) { print OUTFILE "    <tag k=\"$k\" v=\"$tags{$k}\" />\n"; }
 #			if ($cycle!=NONE or $cycleref) { print "$refnum ($highway)\n"; }
 
-		} elsif ($t =~/^    <tag k="(.+)" v="(.*)" \/>$/) {
+		} elsif ($t =~/^    <tag k="(.+)" v="(.*)"\s*\/>$/) {
 			# - read tag
 			if ($usetag{$1}) { $tags{$1}=$2; }
 			$t="";
@@ -157,7 +205,7 @@
 		if ($t) { $t=~s/^\s+//; print OUTFILE "$t\n"; }
 	}
 
-	close INFILE;
+	$fh->close;
 	close OUTFILE;
 
 	# ===== Call osmcut to split into smaller files
@@ -166,15 +214,33 @@
 	$tempdir='temp';
 	print "Splitting with osmcut\n";
 	system ("mkdir $tempdir");
-	system ("java -Xmx512M -jar osmcut.0.5.jar 0.5 $fn.processed $tempdir");
-	system ("rm $fn.processed");
+	system ("java -Xmx512M -jar osmcut.0.5.jar 0.5 $ofn $tempdir");
+	system ("rm $ofn");
 	
 	# =====	Call mkgmap to create map
 	#		(delete temporary directory afterwards)
 	
 	print "Creating Garmin map with mkgmap\n";
 	system ("java -Xmx512M -jar mkgmap.jar --map-features=resources/cycling-map-features.csv --gmapsupp $tempdir/*");
-	system ("rm -rf $tempdir");
+	print "Deleting temporary files\n";
 	system ("rm 6*.img");
 	system ("rm 6*.tdb");
+	system ("rm -rf $tempdir");
 	print "Finished!\n";
+
+
+	# ===========================
+	# General-purpose subroutines
+	
+	sub Set_Cycle { if ($_[0]<$cycle) { $cycle=$_[0]; } }
+
+	sub Open_File {
+		my $fn=$_[0];
+		if ($fn=~/^(.+)\.bz2$/) {
+			$fh=new IO::Uncompress::Bunzip2 $fn or die "Couldn't open bzipped input file: $Bunzip2Error\n";
+			return "$1.processed";
+		} else {
+			$fh=new IO::File($fn) or die "Couldn't open input file: $!\n";
+			return "$fn.processed";
+		}
+	}

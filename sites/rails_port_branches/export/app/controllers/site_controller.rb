9447520318a4ba21529bd54c90127c57a5402dfe
Tom Hughes <tom@compton.nu>
2008-04-19 13:21:21 +0000

    Remove export branch.

--- sites/rails_port_branches/export/app/controllers/site_controller.rb
+++
@@ -1,8 +0,0 @@
-class SiteController < ApplicationController
-  before_filter :authorize_web
-  before_filter :require_user, :only => [:edit]
-
-  def export
-    render :action => 'index'
-  end
-end
--- sites/rails_port_branches/export/app/helpers/application_helper.rb
+++
@@ -1,5 +0,0 @@
-module ApplicationHelper
-  def htmlize(text)
-    return sanitize(auto_link(simple_format(text), :urls))
-  end
-end

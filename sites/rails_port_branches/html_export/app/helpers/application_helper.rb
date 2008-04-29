120f2c57661c2e2bcb467e05f897fb3cc8134709
Christopher Schmidt <crschmidt@crschmidt.net>
2008-04-29 11:58:12 +0000

    Create branch for HTML export from export tab.

---
+++ sites/rails_port_branches/html_export/app/helpers/application_helper.rb
@@ -0,0 +1,9 @@
+module ApplicationHelper
+  def htmlize(text)
+    return sanitize(auto_link(simple_format(text), :urls))
+  end
+
+  def rss_link_to(*args)
+    return link_to(image_tag("RSS.gif", :size => "16x16", :border => 0), Hash[*args], { :class => "rsssmall" });
+  end
+end

823f2b575b2282dd8b604f8d9959df49673e5016
Jon Burgess <jburgess777@gmail.com>
2008-05-04 10:32:58 +0000

    update weekly planet dump script to include relations and exit if anything fails

--- applications/utils/planet.osm/planet.sh
+++ applications/utils/planet.osm/planet.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
 cur_date=`date +%y%m%d`
 cur_planet=planet-${cur_date}.osm
+cur_relation=relations-${cur_date}.osm
 planet_dir=/var/www/planet.openstreetmap.org/
 
 export PATH='/usr/local/bin:/usr/bin:/bin:/usr/bin/X11'
@@ -8,10 +10,14 @@ export PATH='/usr/local/bin:/usr/bin:/bin:/usr/bin/X11'
 cd ${planet_dir}
 
 planetdump | gzip -9 > .${cur_planet}.gz.new
+planetdump --relations | gzip -9 > .${cur_relation}.gz.new
 mv .${cur_planet}.gz.new ${cur_planet}.gz
+mv .${cur_relation}.gz.new ${cur_relation}.gz
 
 gzip -dc ${cur_planet}.gz | bzip2 -2 > .${cur_planet}.bz2.new
+gzip -dc ${cur_relation}.gz | bzip2 -2 > .${cur_relation}.bz2.new
 mv .${cur_planet}.bz2.new  ${cur_planet}.bz2
+mv .${cur_relation}.bz2.new  ${cur_relation}.bz2
 
 #7zr a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on  planet-`date +%y%m%d.osm`.7z  ${cur_planet}
 #chmod a+r ${cur_planet}.7z
@@ -20,6 +26,7 @@ mv .${cur_planet}.bz2.new  ${cur_planet}.bz2
 
 #link planet latest to the new file
 ln -fs ${cur_planet}.bz2 planet-latest.osm.bz2
+ln -fs ${cur_relation}.bz2 relations-latest.osm.bz2
 
 #next, create the planet diff from last week to this week
 
@@ -33,3 +40,4 @@ chmod +r ${TMPFILE}
 mv ${TMPFILE} ${planet_dir}/planet-${PREVPLANETDATE}-${CURPLANETDATE}.diff.xml.bz2
 
 rm ${cur_planet}.gz
+rm ${cur_relation}.gz

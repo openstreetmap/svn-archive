a0af9ec1e5e528b0ac3a759886d33389caeef164
Jon Burgess <jburgess777@gmail.com>
2008-05-04 10:12:13 +0000

    weekly planet dump script updated not to rely on using planetdump from Steve's directory

--- applications/utils/planet.osm/planet.sh
+++ applications/utils/planet.osm/planet.sh
@@ -4,16 +4,19 @@ cur_planet=planet-${cur_date}.osm
 planet_dir=/var/www/planet.openstreetmap.org/
 
 export PATH='/usr/local/bin:/usr/bin:/bin:/usr/bin/X11'
-cd /home/steve/bin
-./planet.rb | bzip2 > ${cur_planet}.bz2
-mv ${cur_planet}.bz2 ${planet_dir}
+
+cd ${planet_dir}
+
+planetdump | gzip -9 > .${cur_planet}.gz.new
+mv .${cur_planet}.gz.new ${cur_planet}.gz
+
+gzip -dc ${cur_planet}.gz | bzip2 -2 > .${cur_planet}.bz2.new
+mv .${cur_planet}.bz2.new  ${cur_planet}.bz2
 
 #7zr a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on  planet-`date +%y%m%d.osm`.7z  ${cur_planet}
 #chmod a+r ${cur_planet}.7z
 #mv ${cur_planet}.7z ${planet_dir}
 
-
-cd ${planet_dir}
 
 #link planet latest to the new file
 ln -fs ${cur_planet}.bz2 planet-latest.osm.bz2
@@ -25,6 +28,8 @@ PREVPLANETDATE=`ls *.osm.bz2|grep -B1 ${cur_planet}|head -n1|sed -e 's/\w*-\([0-
 PREVPLANETBZ=planet-${PREVPLANETDATE}.osm.bz2
 TMPFILE=`mktemp`
 
-planetdiff ${PREVPLANETBZ} ${cur_planet}.bz2 | bzip2 > ${TMPFILE}
+planetdiff ${PREVPLANETBZ} ${cur_planet}.bz2 | bzip2 -2 > ${TMPFILE}
 chmod +r ${TMPFILE}
 mv ${TMPFILE} ${planet_dir}/planet-${PREVPLANETDATE}-${CURPLANETDATE}.diff.xml.bz2
+
+rm ${cur_planet}.gz

52837293fa9cd79bbc1ddf83b479bafae6506678
Jochen Topf <jochen@remote.org>
2008-05-05 09:02:35 +0000

    German IRC channel Got rid of old web site

--- sites/www.openstreetmap.de/var/www/www.openstreetmap.de-old/javascript/OpenLayers/SingleFile.js
+++
@@ -1,6 +0,0 @@
-/* Copyright (c) 2006 MetaCarta, Inc., published under a modified BSD license.
- * See http://svn.openlayers.org/trunk/openlayers/repository-license.txt 
- * for the full text of the license. */
-
-_OPENLAYERS_SFL_=true;
-

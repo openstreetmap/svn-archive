88a24e46169ae670e74f5f603f6fecb54fed99c1
Oliver White <ojwlists@googlemail.com>
2008-05-12 21:52:47 +0000

    cairo renderer, using OSM tags for colours/widths

--- applications/rendering/pyrender/server.py
+++ applications/rendering/pyrender/server.py
@@ -24,7 +24,7 @@ import re
 import sys
 
 # Choose which rendering engine you want to use here
-import renderer_PIL as RenderModule
+import renderer_default as RenderModule
 
 class tileServer(BaseHTTPRequestHandler):
   def __init__(self, request, client_address, server):

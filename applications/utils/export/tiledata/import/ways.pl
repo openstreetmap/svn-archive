e42f3b7f74befb900a94614aad625e12611ea53a
Oliver White <ojwlists@googlemail.com>
2008-05-27 21:12:41 +0000

    cache of recently-used nodes, to avoid going back to the database too often + try to make it ignore lines faster while it's sitting through the nodes section + add a display of million nodes ignored, to watch while waiting for it to start processing ways

--- applications/utils/export/tiledata/import/ways.pl
+++ applications/utils/export/tiledata/import/ways.pl
@@ -4,7 +4,7 @@ use Data::Dumper;
 use DBI;
 use dbpassword;
 
-print STDERR "way data - ctrl-c to quit in next 10 seconds\n";sleep(10);print STDERR "Running...\n";
+#print STDERR "way data - ctrl-c to quit in next 10 seconds\n";sleep(10);print STDERR "Running...\n";
 
 # Connect to database
 my $db = DBI->connect("dbi:mysql:ojw:localhost:3306", getUser, getPass) or die();
@@ -17,8 +17,15 @@ my $xml = new XML::Parser(Handlers => {
   Default => \&default_handler});
 
 my $Count = 0;
-my $inWay;
+my $inWay = 0;
 my $Way;
+my %NodeCache;
+my $NodeCacheSize = 0;
+my $NodeCount = 0;
+my ($Hit, $Miss) = (0,0);
+my $FoundStart = 0;
+my $LineNum = 0;
+my $TimeSinceWhine = 0;
 
 $db->prepare("delete from wayloc")->execute();
 $db->prepare("delete from waydata")->execute();
@@ -36,13 +43,24 @@ my $GetNodeSQL = $db->prepare('select * from nodepos where id=?');
 
 while(my $Line = <>)
 {
-
-  if($Line =~ m{<way id='(\d+)'})
+  $LineNum++;
+  
+  if($Line =~ m{^\s*<way id='(\d+)'})
   {
     $Way = {id => $1, nodes=>[], tags=>{}, numtags=>0, tiles=>{}};
     $inWay = 1;
+    $FoundStart = 1 if(!$FoundStart);
   }
-  elsif($Line =~ m{<nd ref='(\d+)'})
+  elsif(!$FoundStart)
+  {
+    if(++$TimeSinceWhine == 1000000)
+    {
+      printf "ignored %d million lines\n", $LineNum / 1000000;
+      $TimeSinceWhine = 0;
+    }
+    next;
+  }
+  elsif($Line =~ m{^\s*<nd ref='(\d+)'})
   {
     my $ID = $1;
     
@@ -54,7 +72,7 @@ while(my $Line = <>)
     }
   
   }
-  elsif($inWay && $Line =~ m{<tag k='(.*)' v='(.*)' />})
+  elsif($inWay && $Line =~ m{^\s*<tag k='(.*)' v='(.*)' />})
   {
     if(!$Ignore{$1})
       {
@@ -62,13 +80,12 @@ while(my $Line = <>)
       $Way->{numtags}++;
       }
   }
-  elsif($Line =~ m{</way>})
+  elsif($Line =~ m{^\s*</way>})
   {
     print "$Count ways\n" if(($Count++ % 5000) == 0);
     
     if($Way->{numtags} > 0) # Don't store untagged ways
     {
-      #print Dumper($Way);
       $ID = $Way->{id};
       $WayAsXml = way2xml($Way);
       
@@ -82,7 +99,19 @@ while(my $Line = <>)
       }
     }
   }
+  #elsif($Line =~ m{<node})
+  #{
+  #  if((++$NodeCount % 1000000) == 0)
+  #  {
+  #    printf "%d million nodes\n", $NodeCount / 1000000;
+  #  }
+  #}
+  
 }
+printf "Cache hit %d of %d = %1.2f%%\n", 
+  $Hit, 
+  $Miss, 
+  100.0 * $Hit / ($Hit + $Miss);
 
 sub way2xml
 {
@@ -108,10 +137,42 @@ sub way2xml
 sub getNode
 {
   my $ID = shift();
+  
+  if(exists($NodeCache{$ID}))
+  {
+    $Hit++;
+    return($NodeCache{$ID}->[1]);
+  }
+  
+  $Miss++;
+  $NodeCount++;
+  
+  my $MaxSize = 20000;
+  if($NodeCacheSize > $MaxSize)
+  {
+    my $Limit = $NodeCount - ($MaxSize / 2);
+    $NodeCacheSize = 0;
+    
+    while(my($k,$v) = each(%NodeCache))
+    {
+      if($v->[0] < $Limit)
+        {
+        delete($NodeCache{$k});
+        }
+      else
+        {
+        $NodeCacheSize++;
+        }
+    }
+  }
+  
+  $NodeCacheSize++;
+  
   # Lookup the node
   $GetNodeSQL->execute($ID);
   if(@row = $GetNodeSQL->fetchrow_array())
   {
+    $NodeCache{$ID} = [$NodeCount, [@row]];
     return(@row);
   }   
   

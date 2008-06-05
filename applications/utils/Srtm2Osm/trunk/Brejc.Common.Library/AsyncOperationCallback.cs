78fd6d35baf2df3cc17d9e40f68966829cc0eab3
Igor Brejc <igor.brejc@gmail.com>
2008-06-05 16:52:42 +0000

    Added Brejc.Common.Library sources.
    Added build files for VS 2008.
    Updated to the latest version of NAnt.

---
+++ applications/utils/Srtm2Osm/trunk/Brejc.Common.Library/AsyncOperationCallback.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brejc.Common
+{
+    public delegate bool AsyncOperationCallback (string activityDescription, int progress);
+}

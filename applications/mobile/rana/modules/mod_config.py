9a0952b7d183a15b628a22b28f71b36309897da3
Oliver White <ojwlists@googlemail.com>
2008-06-25 12:30:39 +0000

    put some config options into a module (hardcoded for now)

---
+++ applications/mobile/rana/modules/mod_config.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#----------------------------------------------------------------------------
+# User configuration
+#----------------------------------------------------------------------------
+# Copyright 2007-2008, Oliver White
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#---------------------------------------------------------------------------
+from module_base import ranaModule
+
+def getModule(m,d):
+  return(config(m,d))
+
+class config(ranaModule):
+  """Handle configuration, options, and setup"""
+  def __init__(self, m, d):
+    ranaModule.__init__(self, m, d)
+    self.firstTime = True
+
+  def firstTimeInit(self):
+    # Example: load a GPX replay
+    m = self.m.get('replayGpx', None)
+    if(m != None):
+      m.load('C:/home/OSM/Waddington/2007_05_07_bedfordshire_footpaths.gpx')
+
+  
+  def update(self):
+    if(self.firstTime):
+      self.firstTimeInit()
+    self.firstTime = False

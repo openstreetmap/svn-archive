9b4f7db0477d77dafc8fb2bc888f507d6d699abb
Nic Roets <nroets@gmail.com>
2008-06-28 12:57:46 +0000

    Now runs on SmartPhone 2003 emulator.

--- applications/rendering/gosmore/resource.h
+++ applications/rendering/gosmore/resource.h
@@ -16,15 +16,17 @@
 #define IDC_BICYCLE                     1010
 #define IDC_BIG                         1011
 #define IDC_SQUARE                      1012
+#define IDC_BUTTON1                     1013
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        110
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif

ba50c449abe77dd3d1ce05e2d274ee174f81b54d
Oliver White <ojwlists@googlemail.com>
2008-07-01 18:44:01 +0000

    allow toggling of options

--- applications/mobile/rana/modules/mod_messages.py
+++ applications/mobile/rana/modules/mod_messages.py
@@ -42,6 +42,8 @@ class messageModule(ranaModule):
           if(value == str(i)):
             value = i
         self.set(key, value)
+      if(module == 'toggle'):
+        self.set(text, not self.get(text,0))
       elif(module == "*"):
         for m in self.m.items():
           m.handleMessage(text)

3ec839aadda82d9ff85a3a46b837432be7914be0
Oliver White <ojwlists@googlemail.com>
2008-07-03 20:12:31 +0000

    start allowing drawing routines

--- applications/mobile/rana/modules/listable_menu.py
+++ applications/mobile/rana/modules/listable_menu.py
@@ -1,5 +1,22 @@
 import cairo
 
 class listable_menu:
-  def __init__(self,x,y,w,h):
-    pass
+  def __init__(self,cr,x,y,w,h):
+    self.cr = cr
+    self.x = x
+    self.y = y
+    self.w = w
+    self.h = h
+    self.numItems = 10
+    self.dy = self.h / self.numItems
+  
+  def write(self,pos,text):
+    border = 4
+    self.cr.set_source_rgb(0,0,0)
+    self.cr.set_font_size(45)
+    self.cr.move_to(
+      self.x + border,
+      self.y + (pos+1) * self.dy - border)
+    self.cr.show_text(text)
+        
+    

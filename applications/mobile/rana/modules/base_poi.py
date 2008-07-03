d2c00eb9d87415609f3aa0c0357844c38e2ac85d
Oliver White <ojwlists@googlemail.com>
2008-07-03 20:12:11 +0000

    start making the POI modules use a base class

---
+++ applications/mobile/rana/modules/base_poi.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+#---------------------------------------------------------------------------
+# Base class for any module which displays points of interest
+#---------------------------------------------------------------------------
+# Copyright 2008, Oliver White
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#---------------------------------------------------------------------------
+from module_base import ranaModule
+
+class poiModule(ranaModule):
+  
+  def __init__(self, m, d):
+    ranaModule.__init__(self, m, d)
+    self.poi = {}
+
+  def drawList(self, cr, menuName, listHelper):
+    
+    for i in range(0, listHelper.numItems):
+      listHelper.write(i, "Item %d of %d" % (i+1, listHelper.numItems))
+
+    

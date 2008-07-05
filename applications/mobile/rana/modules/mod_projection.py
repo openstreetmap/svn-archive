4d4ef1aa33087df34c240bb113ffaf0edde2917e
Oliver White <ojwlists@googlemail.com>
2008-07-05 13:04:18 +0000

    rename module_base to base_module, so that m+tab actually autocompletes mod_ and makes it easier to run things in the module directory...

--- applications/mobile/rana/modules/mod_projection.py
+++ applications/mobile/rana/modules/mod_projection.py
@@ -17,8 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #----------------------------------------------------------------------------
+from base_module import ranaModule
 from tilenames import *
-from module_base import ranaModule
 
 def getModule(m,d):
   return(Projection(m,d))

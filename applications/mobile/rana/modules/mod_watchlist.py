ac13dddf8b216f7ac20b8699b1b274ac49b88bb2
Oliver White <ojwlists@googlemail.com>
2008-07-08 19:50:25 +0000

    move "watching for variable changes" to module

--- applications/mobile/rana/modules/mod_watchlist.py
+++ applications/mobile/rana/modules/mod_watchlist.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 #---------------------------------------------------------------------------
-# Handles messages between modules (in response to clicks etc)
+# Allows notification of data-changes
 #---------------------------------------------------------------------------
-# Copyright 2007-2008, Oliver White
+# Copyright 2008, Oliver White
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,39 +18,29 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #---------------------------------------------------------------------------
 from base_module import ranaModule
-import sys
-import os
-import socket
-from time import sleep
-import re
 
 def getModule(m,d):
-  return(messageModule(m,d))
+  return(watchlist(m,d))
 
-class messageModule(ranaModule):
-  """Handles messages"""
+class watchlist(ranaModule):
+  """Allows notification of data-changes"""
   def __init__(self, m, d):
     ranaModule.__init__(self, m, d)
 
-  def routeMessage(self, messages):
-    for message in messages.split('|'):
-      (module, text) = message.split(":", 1)
-      
-      if(module == 'set'):
-        (key,value) = text.split(":", 1)
-        for i in(None, True, False):
-          if(value == str(i)):
-            value = i
-        self.set(key, value)
-        
-      elif(module == 'toggle'):
-        self.set(text, not self.get(text,0))
-      elif(module == "*"):
-        for m in self.m.items():
-          m.handleMessage(text)
-      else:
-        m = self.m.get(module, None)
-        if(m != None):
-          m.handleMessage(text)
-        else:
-          print "Message addressed to %s which isn't loaded" % module
+    # store the last value of some things, so we can detect changes
+    # (todo: why not just send it as param to notify)
+    self.watching = ['pos']  
+    self.last = {}
+    
+  def notify(self,name, value):
+
+    # Position seen for first time, centering
+    if(name == "pos" and not self.last.has_key("pos")):
+      self.set("centreOnce", True)
+    
+    # Menu changed, so redraw
+    if(name == "menu"):
+      self.set("needRedraw", True)
+
+    if(name in self.watching):
+      self.last[name] = value

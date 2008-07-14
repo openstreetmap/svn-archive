8b8970699041294906a83c82dd72f286dbc01d5b
Sebastian Spaeth <spaetz@svn.openstreetmap.org>
2008-07-14 08:43:42 +0000

    apply patch by Kai Krueger to user oceantiles.dat for blankness fallback.

--- sites/other/tilesAtHome_tahngo/tah_intern/Tile.py
+++ sites/other/tilesAtHome_tahngo/tah_intern/Tile.py
@@ -78,7 +78,8 @@ class Tile:
       d_offset = 0
       #next 2 lines are temporary fallback to legacy tiles
       data = self.serve_legacy_tile(layername)
-      if data: return data
+      if data > 3 or data == None: return data # we found a tile or found nothing at all
+      else: d_offset = data    # found a blank tile, use blankness value
     if d_offset > 3:
       f.seek(d_offset)
       data = f.read(d_offset_next-d_offset)
@@ -94,7 +95,7 @@ class Tile:
 
 
   def serve_legacy_tile(self,layername):
-    """ Return the PNG data of a legacy tile """
+    """ Returns: None (nothing found), a blanktile value (0,..,3) or the PNG data of a legacy tile. """
     leg_basetiledir='/mnt/agami/openstreetmap/tah/Tiles'
     fname = os.path.join(leg_basetiledir,layername,"%02d"%(self.z),"%03d"%(self.x//1000),"%03d"%(self.x%1000),"%03d"%(self.y//1000),"%03d.png"%(self.y%1000))
     try:
@@ -103,5 +104,26 @@ class Tile:
       f.close()
       return data
     except:
-      return None
+      #There is no legacy tile either, fall back to blanktile database
+      #Return transparent for hardcoded transparent layers
+      if layername in ['maplint','caption']: return 3
+      if self.z < 12:
+        #We only handle z12 and above this way. Lowzooms are required to have actual tiles
+        return None
+      else:
+        #Lookup in the oceans tile
+        #For z12 and above the file will tell us directly what type it is
+        try:
+          (layern, base_z,base_x,base_y) = self.basetileset()
+          f_oceant = open(os.path.join(leg_basetiledir,"oceantiles_12.dat"),'rb')
+          f_oceant.seek((4096*base_y + base_x) >> 2)
+          data = ord(f_oceant.read(1));
+          #take 2 bits of the char
+          bit_off = 3 - (base_x % 4);
+          type = (data >> 2*bit_off) & 3
+          # map tile type to blankness type used in the server
+          blank = [0,2,1,0]
+          return blank[type]
+	except: pass # do nothing if we don't find oceantiles.dat
+        return None
  

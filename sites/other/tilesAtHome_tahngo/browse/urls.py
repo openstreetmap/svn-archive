2eca49c3480fbe94208d91f772689ca87df2e0bc
Sebastian Spaeth <sebastian@sspaeth.de>
2008-07-14 12:39:17 +0000

    remove hardcoded path from URL

--- sites/other/tilesAtHome_tahngo/browse/urls.py
+++ sites/other/tilesAtHome_tahngo/browse/urls.py
@@ -4,6 +4,7 @@ from tah.browse.views import *
 urlpatterns = patterns('',
     (r'^$', index),
     (r'^slippy/$', slippymap),
+    (r'^details/$', tiledetails, {'layername':'tile','z':0,'x':0,'y':0}),
     (r'^details/(?P<layername>\w+)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+)/$', tiledetails),
     (r'^MapOf/$', show_map_of),
     (r'^Tiles/(?P<layername>\w+)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).png$', serve),

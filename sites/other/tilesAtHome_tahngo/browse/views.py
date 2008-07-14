f98159dd735bc4b73ba0f39ab46dd5e00e3ac550
Sebastian Spaeth <sebastian@sspaeth.de>
2008-07-14 07:05:23 +0000

    Display the user who uploaded the tileset on the details page.

--- sites/other/tilesAtHome_tahngo/browse/views.py
+++ sites/other/tilesAtHome_tahngo/browse/views.py
@@ -1,10 +1,12 @@
 import os
 from stat import *
+from struct import unpack, calcsize
 from datetime import datetime
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from tah.tah_intern.Tile import Tile
 from tah.tah_intern.models import Settings
+from django.contrib.auth.models import User
 
 def index(request):
   layer =  request.GET.get("layer","tile")
@@ -26,6 +28,7 @@ def tiledetails(request,layername,z,x,y):
   z=int(z)
   x=int(x)
   y=int(y)
+  userid=None
   t=Tile(layername,z,x,y)
   (layer, base_z,base_x,base_y) = t.basetileset()
   # basetileset returns (None,None,None,None) if invalid!
@@ -35,10 +38,16 @@ def tiledetails(request,layername,z,x,y):
   try: 
     fstat = os.stat(tilefile)
     (basetile_fsize,basetile_mtime) = (fstat[6], datetime.fromtimestamp(fstat[8]))
+    f=open(tilefile, 'rb')
+    FILEVER, userid = unpack('BI',f.read(calcsize("BI")))
+    f.close()
   except OSError: 
     (basetile_fsize,basetile_mtime) = None, None
 
-  return render_to_response('tile_details.html',{'tile':t,'basetile_fsize':basetile_fsize,'basetile_mtime':basetile_mtime})
+  if userid != None: user = User.objects.get(pk=userid)
+  else: user='Unknown'
+
+  return render_to_response('tile_details.html',{'tile':t,'basetile_fsize':basetile_fsize,'basetile_mtime':basetile_mtime, 'user': user})
 
 def show_map_of(request):
   return HttpResponse("not implemented",mimetype="text/plain")

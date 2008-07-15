3c7afb15a95f1c39ec5df6ca3bc385baa6377f2c
Nic Roets <nroets@gmail.com>
2008-07-15 13:04:25 +0000

    WinCE : Rotate the map WinCE : Easier AddWayOrNode WinCE : QuickOptions
    Better detection of the way we are travelling along by using the motion vector

--- applications/rendering/gosmore/resource.h
+++ applications/rendering/gosmore/resource.h
@@ -7,6 +7,8 @@
 #define IDB_BITMAP1                     108
 #define IDD_SETTINGS                    109
 #define IDD_SETTAGS                     110
+#define IDD_SETTAGS2                    111
+#define IDD_CHOOSEO                     112
 #define IDC_EDIT1                       1000
 #define IDC_LIST1                       1001
 #define IDC_SEARCHGO                    1002
@@ -22,6 +24,7 @@
 #define IDC_NAME                        1017
 #define IDC_ONEWAY                      1018
 #define IDC_BRIDGE                      1019
+#define IDC_LISTO                       1021
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -29,9 +32,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        111
+#define _APS_NEXT_RESOURCE_VALUE        113
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1020
+#define _APS_NEXT_CONTROL_VALUE         1022
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif

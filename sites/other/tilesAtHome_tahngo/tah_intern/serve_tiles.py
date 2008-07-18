bd756e805a450670ec861ccb1a526bdd6e7fa359
Sebastian Spaeth <sebastian@sspaeth.de>
2008-07-18 07:13:06 +0000

    leave mod_python as is now...

--- sites/other/tilesAtHome_tahngo/tah_intern/serve_tiles.py
+++ sites/other/tilesAtHome_tahngo/tah_intern/serve_tiles.py
@@ -15,15 +15,16 @@ def handler(req):
     if data: 
       #req.content_type = 'text/plain'
       req.content_type = 'image/png'
-      #req.mtime = t.mtime
+      #req.update_mtime(t.mtime)
       #req.set_last_modified() #This can be used in newer mod_python versions. Does not work in 3.2.1
       req.set_content_length(len(data))
       req.headers_out['Expires']=(datetime.utcnow()+timedelta(0,0,0,0,0,3)).strftime("%a, %d %b %Y %H:%M:%S GMT")
       req.headers_out['Last-Modified']=datetime.utcfromtimestamp(t.mtime).strftime("%a, %d %b %Y %H:%M:%S GMT")
       status = req.meets_conditions()
-      if status == apache.OK:
-        req.write(data)
-      return status
+      if status != apache.OK:
+        return status
+      req.write(data)
+      return apache.OK
 
     else: return apache.HTTP_NOT_FOUND
   return apache.HTTP_BAD_REQUEST

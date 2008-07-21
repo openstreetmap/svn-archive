a7a6f3e656209b52665af05c1a527988120ef174
Sebastian Spaeth <sebastian@sspaeth.de>
2008-07-21 07:17:33 +0000

    remove superfluous MapOf URL

--- sites/other/tilesAtHome_tahngo/browse/urls.py
+++ sites/other/tilesAtHome_tahngo/browse/urls.py
@@ -6,6 +6,5 @@ urlpatterns = patterns('',
     (r'^slippy/$', slippymap),
     (r'^details/$', tiledetails_base),
     (r'^details/(?P<layername>\w+)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+)/$', tiledetails),
-    (r'^MapOf/$', show_map_of),
     (r'^Tiles/(?P<layername>\w+)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).png$', serve),
 )

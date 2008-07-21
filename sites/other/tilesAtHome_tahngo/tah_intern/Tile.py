0d23460a4ca0e437d5464eed753dba06324ecba8
Sebastian Spaeth <spaetz@svn.openstreetmap.org>
2008-07-21 05:56:01 +0000

    make use of apache.sendfile() to avoid copying all served tile data in memory around. Untested yet

--- sites/other/tilesAtHome_tahngo/tah_intern/Tile.py
+++ sites/other/tilesAtHome_tahngo/tah_intern/Tile.py
@@ -51,10 +51,21 @@ class Tile:
     return (self.layer, base_z, base_x, base_y)
 
 
-  def serve_tile(self, layername):
+  def serve_tile_data(self, layername):
     """ Return the PNG data of a tile """
+    (tilesetfile, offset, datalength) = serve_tile_sendfile(layername)
+    f = open(tilesetfile,'rb')
+    f.seek(offset)
+    data = f.read(datalength)
+    f.close()
+    return data
+
+  def serve_tile_sendfile(self, layername):
+    """ Return the (filename,offset,datalength) of a tile. If a full file should be sent, 
+        offset is 0, and datalength -1. This is geared to apache.sendfile()
+    """
     (layer, base_z,base_x,base_y) = self.basetileset() 
-    # basetilepath could be take from the Settings, hardcode for efficiency 
+    # basetilepath could be taken from the Settings, hardcode for efficiency 
     basetilepath='/mnt/agami/openstreetmap/tah/Tiles'
     self.mtime = 0
     if base_z == None:
@@ -63,7 +74,7 @@ class Tile:
     else:
       tilesetfile = os.path.join(basetilepath,"%s_%s_%d"%(layername,base_z,base_x//1000),str(base_x)+'_'+str(base_y))
     try:
-      self.mtime = os.stat(tilesetfile)[8]
+      self.mtime = os.stat(tilesetfile)[8] #set mtime to be used by 'Last-Modified' header
       f = open(tilesetfile,'rb')
       #calculate file offset
       offset = 8 # skip the header data
@@ -82,39 +93,33 @@ class Tile:
     except OSError, IOError:
       d_offset = 0
       #next 3 lines are temporary fallback to legacy tiles
-      data = self.serve_legacy_tile(layername)
-      if data > 3: return data        # we found a tile
-      else: d_offset = max(data,0)    # return blankness value or 0 if it is None
+      tilesetfile = self.serve_legacy_tile(layername)
+      if isinstance(tilesetfile,str):
+        return (tilesetfile,0,-1)       # we found a tile, send entire file
+      else: d_offset = 0    # return blankness value
     if d_offset > 3:
-      f.seek(d_offset)
-      data = f.read(d_offset_next-d_offset)
+      # we got a regular tilesetfile here
+      return (tilesetfile,d_offset,d_offset_next-d_offset)
     else:
-      # return blankness value
+      # return complete blankness tile
       blankpng = ['unknown.png','sea.png','land.png','transparent.png']
-      f_png = open(os.path.join(basetilepath,blankpng[d_offset]),'rb')
-      data = f_png.read()
-      f_png.close()
-    try: f.close()
-    except: pass
-    return data
+      tilesetfile = os.path.join(basetilepath,blankpng[d_offset])
+      return (tilesetfile, 0, -1)
 
 
   def serve_legacy_tile(self,layername):
-    """ Returns: None (nothing found), a blanktile value (0,..,3) or the PNG data of a legacy tile. """
+    """ Returns: a blanktile value (0,..,3) as 'int' or the file name of the tile file as 'str'. """
     leg_basetiledir='/mnt/agami/openstreetmap/tah/Tiles'
     fname = os.path.join(leg_basetiledir,layername,"%02d"%(self.z),"%03d"%(self.x//1000),"%03d"%(self.x%1000),"%03d"%(self.y//1000),"%03d.png"%(self.y%1000))
-    try:
-      f = open(fname,'rb')
-      data = f.read()
-      f.close()
-      return data
+    if os.path.isfile(fname):
+      return fname
     except:
       #There is no legacy tile either, fall back to blanktile database
       #Return transparent for hardcoded transparent layers
       if layername in ['maplint','caption']: return 3
       if self.z < 12:
         #We only handle z12 and above this way. Lowzooms are required to have actual tiles
-        return None
+        return 0
       else:
         #Lookup in the oceans tile
         #For z12 and above the file will tell us directly what type it is
@@ -129,6 +134,7 @@ class Tile:
           # map tile type to blankness type used in the server
           blank = [0,2,1,2]
           return blank[type]
-	except: pass # do nothing if we don't find oceantiles.dat
-        return None
+	except:
+          # return 0 if we don't find oceantiles.dat
+          return 0
  

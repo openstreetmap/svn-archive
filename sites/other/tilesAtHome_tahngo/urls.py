a3b5154268389d2b1ef761efe5588f6bc164f8e9
Sebastian Spaeth <spaetz@svn.openstreetmap.org>
2008-07-21 09:05:11 +0000

    mv settings.py to settings.py.example, svn:ignore settings.py and adapt INSTALL accordingly to make svn ignore changes in it.

--- sites/other/tilesAtHome_tahngo/urls.py
+++ sites/other/tilesAtHome_tahngo/urls.py
@@ -1,15 +1,17 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
 from django.views.generic.simple import direct_to_template,redirect_to
 from tah.tah_intern.views import show_log
 from tah.browse.MapOf import export_MapOf
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', direct_to_template, {'template': 'homepage.html'}),
     (r'^Request/', include('tah.requests.urls')),
     (r'^User/', include('tah.user.urls')),
-    #(r'^blank/', include('tah.tah_intern.urls_blank')),
     (r'^Browse/', include('tah.browse.urls')),
-    (r'^admin/', include('django.contrib.admin.urls')),
+    (r'^admin/(.*)', admin.site.root),
     (r'^Log/$', show_log),
     (r'^MapOf/', export_MapOf),
     (r'Requests/Version\.php$', redirect_to, {'url': '/tahngo/Request/latestClientVersion/'}), #temporary redirect

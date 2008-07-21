87e77b01c14009decc160e2584766cd4e40211ff
Sebastian Spaeth <spaetz@svn.openstreetmap.org>
2008-07-21 15:09:58 +0000

    use 'GMT' time zone rather than +0000

--- sites/other/tilesAtHome_tahngo/tah_intern/serve_tiles.py
+++ sites/other/tilesAtHome_tahngo/tah_intern/serve_tiles.py
@@ -17,8 +17,8 @@ def handler(req):
       #req.update_mtime(t.mtime)
       #req.set_last_modified() #This can be used in newer mod_python versions. Does not work in 3.2.1 
       if datalength > 0: req.set_content_length(datalength)
-      req.headers_out['Expires']=formatdate(time()+10800) #expire in 3 hours
-      req.headers_out['Last-Modified']=formatdate(t.mtime)
+      req.headers_out['Expires']=formatdate(time()+10800,usegmt=True) #expire in 3 hours
+      req.headers_out['Last-Modified']=formatdate(t.mtime,usegmt=True)
 
       status = req.meets_conditions()
       if status != apache.OK:

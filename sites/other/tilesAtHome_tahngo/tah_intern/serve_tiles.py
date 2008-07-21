2c4ed03adb5cf452a767164d64183568d0ac52a4
Sebastian Spaeth <sebastian@sspaeth.de>
2008-07-21 15:02:14 +0000

    slightly beautify server_tiles

--- sites/other/tilesAtHome_tahngo/tah_intern/serve_tiles.py
+++ sites/other/tilesAtHome_tahngo/tah_intern/serve_tiles.py
@@ -1,8 +1,8 @@
 from tah.tah_intern.Tile import Tile
 import re
-from datetime import datetime, timedelta
-from mod_python import apache #needed for mod_python tile serving
-
+from time import time
+from rfc822 import formatdate
+from mod_python import apache
 
 #mod_python handler for serving tiles
 def handler(req):
@@ -16,11 +16,10 @@ def handler(req):
       req.content_type = 'image/png'
       #req.update_mtime(t.mtime)
       #req.set_last_modified() #This can be used in newer mod_python versions. Does not work in 3.2.1 
-      #maybe need to work around with req.finfo... req.finfo = apache.stat(req.filename, apache.APR_FINFO_MIN)
-      req.finfo = apache.stat(tilefile, apache.APR_FINFO_MIN)
       if datalength > 0: req.set_content_length(datalength)
-      req.headers_out['Expires']=(datetime.utcnow()+timedelta(0,0,0,0,0,3)).strftime("%a, %d %b %Y %H:%M:%S GMT")
-      req.headers_out['Last-Modified']=datetime.utcfromtimestamp(t.mtime).strftime("%a, %d %b %Y %H:%M:%S GMT")
+      req.headers_out['Expires']=formatdate(time()+10800) #expire in 3 hours
+      req.headers_out['Last-Modified']=formatdate(t.mtime)
+
       status = req.meets_conditions()
       if status != apache.OK:
         return status
@@ -29,4 +28,3 @@ def handler(req):
 
     else: return apache.HTTP_NOT_FOUND
   return apache.HTTP_BAD_REQUEST
-

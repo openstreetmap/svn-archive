bba824732ae0bd9e658f6ed9eedd96042ac0a328
Sebastian Spaeth <sebastian@sspaeth.de>
2008-07-22 05:50:41 +0000

    don't use the django 1.0 version of the admin interface.

--- sites/other/tilesAtHome_tahngo/urls.py
+++ sites/other/tilesAtHome_tahngo/urls.py
@@ -4,16 +4,15 @@ from django.views.generic.simple import direct_to_template,redirect_to
 from tah.tah_intern.views import show_log
 from tah.browse.MapOf import export_MapOf
 
-admin.autodiscover()
-
 urlpatterns = patterns('',
     (r'^$', direct_to_template, {'template': 'homepage.html'}),
     (r'^Request/', include('tah.requests.urls')),
     (r'^User/', include('tah.user.urls')),
     (r'^Browse/', include('tah.browse.urls')),
-    (r'^admin/(.*)', admin.site.root),
+    (r'^admin/', include('django.contrib.admin.urls')),
     (r'^Log/$', show_log),
     (r'^MapOf/', export_MapOf),
+    (r'^Log/Requests/Recent/$', redirect_to, {'url': '/Request/show/'}), #temporary redirect
     (r'Requests/Version\.php$', redirect_to, {'url': '/tahngo/Request/latestClientVersion/'}), #temporary redirect
     #(r'/accounts/profile/$', 'redirect_to', {'url': '/user/'}),
 )

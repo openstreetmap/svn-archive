9af4f1089abc0ffbb18eca2b4878dcd79c9db3a3
Raphael Studer <studerap@gmail.com>
2008-07-23 12:13:06 +0000

    first try to make serving tiles faster. patch from ast.

--- sites/other/tilesAtHome_tahngo/tah_intern/serve_tiles.py
+++ sites/other/tilesAtHome_tahngo/tah_intern/serve_tiles.py
@@ -4,22 +4,25 @@ from time import time
 from email.Utils import formatdate
 from mod_python import apache
 
+_URL_PATTERN = re.compile('/(\w+)(\.php)?/(\d+)/(\d+)/(\d+)(\.png)?$')
+_EXPIRES = 3 * 3600     # expire in 3 hours
+
 #mod_python handler for serving tiles
 def handler(req):
-  r = re.compile('/(\w+)(\.php)?/(\d+)/(\d+)/(\d+)(\.png)?$')
-  m = r.search(req.uri)
+  m = _URL_PATTERN.search(req.uri)
   if m:
-    (layername,php,z,x,y,ext) = m.groups()
+    (layername, php, z, x, y, ext) = m.groups()
     t = Tile(None,z,x,y)
     (tilefile, offset, datalength) = t.serve_tile_sendfile(layername)
     if tilefile: 
       req.content_type = 'image/png'
       #req.update_mtime(t.mtime)
       #req.set_last_modified() #This can be used in newer mod_python versions. Does not work in 3.2.1 
-      if datalength > 0: req.set_content_length(datalength)
-      req.headers_out['Cache-Control']='max-age=10800, must-revalidate'
-      req.headers_out['Expires']=formatdate(time()+10800,usegmt=True) #expire in 3 hours
-      req.headers_out['Last-Modified']=formatdate(t.mtime,usegmt=True)
+      if datalength > 0:
+        req.set_content_length(datalength)
+      req.headers_out['Cache-Control'] = 'max-age=10800, must-revalidate'
+      req.headers_out['Expires'] = formatdate(time() + _EXPIRES, usegmt=True) 
+      req.headers_out['Last-Modified'] = formatdate(t.mtime,usegmt=True)
 
       status = req.meets_conditions()
       if status != apache.OK:
@@ -27,5 +30,6 @@ def handler(req):
       req.sendfile(tilefile, offset, datalength)
       return apache.OK
 
-    else: return apache.HTTP_NOT_FOUND
+    else:
+      return apache.HTTP_NOT_FOUND
   return apache.HTTP_BAD_REQUEST

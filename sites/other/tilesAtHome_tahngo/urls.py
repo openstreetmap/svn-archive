a153bc6d984cfb2d90e3b92cc9febf05b63274d1
Sebastian Spaeth <spaetz@svn.openstreetmap.org>
2008-07-28 21:13:19 +0000

    make admin interface work with django SVN version. will break on older djangos. sorry

--- sites/other/tilesAtHome_tahngo/urls.py
+++ sites/other/tilesAtHome_tahngo/urls.py
@@ -4,12 +4,14 @@ from django.views.generic.simple import direct_to_template,redirect_to
 from tah.tah_intern.views import show_log
 from tah.browse.MapOf import export_MapOf
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
     (r'^$', direct_to_template, {'template': 'homepage.html'}),
     (r'^Request/', include('tah.requests.urls')),
     (r'^User/', include('tah.user.urls')),
     (r'^Browse/', include('tah.browse.urls')),
-    (r'^admin/', include('django.contrib.admin.urls')),
+    (r'^admin/(.*)', admin.site.root),
     (r'^Log/$', show_log),
     (r'^MapOf/', export_MapOf),
     (r'^Log/Requests/Recent/$', redirect_to, {'url': '/Request/show/'}), #temporary redirect

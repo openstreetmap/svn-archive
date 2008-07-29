fb723649152673847172af0636203728696e779d
Sebastian Spaeth <sebastian@sspaeth.de>
2008-07-29 10:14:44 +0000

    adapt path in stitch lowzoom. various other minor changes.

--- sites/other/tilesAtHome_tahngo/tah_intern/stitch_lowzoom.py
+++ sites/other/tilesAtHome_tahngo/tah_intern/stitch_lowzoom.py
@@ -27,31 +27,28 @@ class Lowzoom(Tileset):
       for rec_y in range(2*y,2*y+2):
         self.stitch(layer,z+1,rec_x,rec_y)
 
+    # temporarily insert sleeps to no stress the NFS disk so much
+    if ((x+y) % 10 == 0): time.sleep(5)
     #print "tile %s %d %d %d " % (layer,z,x,y)
     pngfilepath = os.path.join(self.tmpdir,"%d_%d_%d.png" % (z,x,y))
-    #print "write to %s" % pngfilepath
     im = Image.new('RGBA', (256,256))
 
     for i in range(0,2):
       for j in range(0,2):
-        retries, error = 0,True
-        while error and retries < 3:
-          if z == self.base_z+5: 
-            imagefile = StringIO.StringIO(Tile(None,z+1,2*x+i,2*y+j).serve_tile('captionless'))
-          else: imagefile = os.path.join(self.tmpdir,"%d_%d_%d.png" % (z+1,2*x+i,2*y+j))
-          try:
-            image = Image.open(imagefile).convert('RGB')
-            error = False
-          except IOError, e:
-            retries += 1
-            print "Try %d: %s at (%d,%d,%d). " % (retries,e,z+1,2*x+i,2*y+j)
-            image = Image.open(StringIO.StringIO(Tile(None,0,0,1).serve_tile('tile')))
+        if z == self.base_z+5: 
+          imagefile = StringIO.StringIO(Tile(None,z+1,2*x+i,2*y+j).serve_tile('captionless'))
+        else: imagefile = os.path.join(self.tmpdir,"%d_%d_%d.png" % (z+1,2*x+i,2*y+j))
+        try:
+          image = Image.open(imagefile).convert('RGB')
+        except IOError, e:
+          print "Try %d: %s at (%d,%d,%d). " % (retries,e,z+1,2*x+i,2*y+j)
+          image = Image.open(StringIO.StringIO(Tile(None,0,0,1).serve_tile('tile')))
 
         if z==self.base_z+5:
           enh = ImageEnhance.Contrast(image)
           image = enh.enhance(1.4)
         enh = ImageEnhance.Sharpness(image)
-        image = enh.enhance(0.3)
+        image = enh.enhance(0.2)
         ##image2 = image.convert('RGBA').filter(ImageFilter.BLUR)
         image = image.resize((128, 128))
         im.paste(image, (128*i,128*j,128*(i+1),128*(j+1)))
@@ -65,7 +62,7 @@ class Lowzoom(Tileset):
 
 def find_old_lowzooms(base_tile_path):
   old_lowzooms = {} # return a dict with (z,x,y) tuples that need an update
-  lz_path = os.path.join(base_tile_path,'tile_6_0')
+  lz_path = os.path.join(base_tile_path,'tile_6')
   reqs = Request.objects.filter(status=2,min_z=12)
   now = datetime.now()
   for r in reqs:
@@ -73,7 +70,7 @@ def find_old_lowzooms(base_tile_path):
     if r.clientping_time + timedelta(2) >= now:
       x,y = r.x>>6, r.y>>6
       try: 
-          lz_file = os.path.join(lz_path,"%d_%d" % (x,y))
+          lz_file = os.path.join(lz_path,"%04d"%x,"%d_%d" % (x,y))
           lz_mtime = os.stat(os.path.join(lz_path,lz_file)).st_mtime
           if r.clientping_time > datetime.fromtimestamp(lz_mtime):
             #print "New tiles in old lowzoom (%d %d ) found" % (x,y)
@@ -90,9 +87,13 @@ if __name__ == '__main__':
   base_tile_path = Settings().getSetting(name='base_tile_path')
   layer=Layer.objects.get(name='tile')
   lz = Lowzoom()
-  for (z,x,y) in find_old_lowzooms(base_tile_path)+[(0,0,0)]:
-    print "sleep 10 seconds then do %d %d %d" % (z,x,y)
+  for i,(z,x,y) in enumerate(find_old_lowzooms(base_tile_path).values()):
+    print "%i) sleep 10 seconds then do %d %d %d" % (i, z,x,y)
     time.sleep(10)
     now = time.time()
-    lz.create(layer,6,x,y,base_tile_path)
-    print "Took %.1f sec." % (time.time()-now)
+    lz.create(layer,z,x,y,base_tile_path)
+    print "(%d,%d,%d)Took %.1f sec." % (z,x,ytime.time()-now)
+  #Finally d
+  now = time.time()
+  lz.create(layer,z,x,y,base_tile_path)
+  print "(0,0,0) took %.1f sec." % (time.time()-now)

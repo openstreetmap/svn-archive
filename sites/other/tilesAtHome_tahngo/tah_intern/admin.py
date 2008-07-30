8dcb4de8401ac893b161889cbc2790066a8c3f7c
Sebastian Spaeth <sebastian@sspaeth.de>
2008-07-30 08:25:18 +0000

    Use new AdminModel classes for the web admin interface, works only with SVN django. 2) Delete unused blank stuff

---
+++ sites/other/tilesAtHome_tahngo/tah_intern/admin.py
@@ -0,0 +1,7 @@
+admin.site.register(Settings, SettingsAdmin)
+class SettingsAdmin(admin.ModelAdmin):
+     pass
+
+admin.site.register(Layer, LayerAdmin)
+class RequestAdmin(admin.ModelAdmin):
+     pass

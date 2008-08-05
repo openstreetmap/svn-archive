1db805d10a42c0374d324f9180384f2924d52569
Sebastian Spaeth <sebastian@sspaeth.de>
2008-08-05 06:15:03 +0000

    make single user view work

--- sites/other/tilesAtHome_tahngo/user/urls.py
+++ sites/other/tilesAtHome_tahngo/user/urls.py
@@ -4,7 +4,8 @@ from tah.user.views import *
 urlpatterns = patterns('tah.requests',
     (r'^$', index),
     (r'^show/$', show_user),
-    (r'^show/(.+)/$', show_single_user),
+    (r'^show/byname/(.+)/$', show_single_user, {'by': 'username'}),
+    (r'^show/byid/(.+)/$', show_single_user, {'by': 'pk'}),
     #(r'^login/$', login),
 )
 

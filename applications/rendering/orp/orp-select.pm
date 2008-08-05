187ab43889054e238566bf70937a219c2b33ac7e
Frederik Ramm <frederik@remote.org>
2008-08-05 21:45:50 +0000

    added proximity filter support

--- applications/rendering/orp/orp-select.pm
+++ applications/rendering/orp/orp-select.pm
@@ -15,6 +15,9 @@ our $index_way_tags;
 our $index_node_tags;
 our $debug;
 
+# for collision avoidance / proximity filter
+my $used_boxes = {};
+
 sub select_elements_without_tags
 {
     my ($oldsel, $e) = @_;
@@ -219,6 +222,63 @@ outer:
         }   
     } 
     
+    return $newsel;
+}
+
+# this implements a very simple proximity selection. it works only for nodes and
+# draws an imaginary box around the node. then it checks all "used" boxes in the
+# same proximity class and unselects the object if a collision is detected. 
+#
+# otherwise, the object remains selected and its box is stored.
+#
+# there are many FIXMEs: 
+# 1. the box is computed based on lat/lon so will have different sizes on the map
+#    at different latitudes.
+# 2. any object that is selected is considered to have "used" its box. this mechanism
+#    only works when the proximity filter is on the last selection rule (which then
+#    only contains drawing code). If subsequent rules further reduce the object set,
+#    then the boxes are "used" nonetheless.
+# 3. the order in which the objects are processed is more or less random (as the 
+#    storage is backed by a perl hash). it will be identical for identical input
+#    data, but as soon as input data varies a bit, the order might change completely.
+
+sub select_proximity
+{
+    my ($oldsel,$hp, $vp, $pc) = @_;
+    my $newsel = Set::Object->new();
+    $pc = "default" if ($pc eq "");
+    foreach ($oldsel->members())
+    {
+        # proximity stuff currently only works for nodes; copy others
+        if (ref($_) ne "node")
+        {
+            $newsel->insert($_);
+            next;
+        }
+        
+        my $bottom = $_->{'lat'} - $hp;
+        my $left = $_->{'lon'} - $vp;
+        my $top = $_->{'lat'} + $hp;
+        my $right = $_->{'lon'} + $vp;
+        my $intersect = 0;
+
+        foreach my $ub(@{$used_boxes->{$pc}})
+        {
+            if ((($ub->[0] > $bottom && $ub->[0] < $top) || ($ub->[2] > $bottom && $ub->[2] < $top) || ($ub->[0] <= $bottom && $ub->[2] >= $top)) &&
+               (($ub->[1] > $left && $ub->[1] < $right) || ($ub->[3] > $left && $ub->[3] < $right) || ($ub->[1] <= $left && $ub->[3] >= $right)))
+            {
+                # intersection detected; skip this object.
+                $intersect = 1;
+                debug("object skipped due to collision in class '$pc'");
+                last;
+            }
+        }
+        next if ($intersect);
+        $newsel->insert($_);
+        debug("object added in class '$pc'");
+        push(@{$used_boxes->{$pc}}, [ $bottom, $left, $top, $right ]);
+    }
+    delete $used_boxes->{$pc} if ($pc eq "default");
     return $newsel;
 }
 

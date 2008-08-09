895b8a27c8c011709f698499bb58959bc0ad8120
Sebastian Spaeth <sebastian@sspaeth.de>
2008-08-09 19:59:59 +0000

    request missing tiles script

--- sites/other/tilesAtHome_tahngo/tah_intern/requestmissingtiles.py
+++ sites/other/tilesAtHome_tahngo/tah_intern/requestmissingtiles.py
@@ -3,9 +3,10 @@ import sys,os
 sys.path.insert(0, os.path.dirname(os.path.dirname(sys.path[0])))
 os.environ['DJANGO_SETTINGS_MODULE'] = "tah.settings"
 from time import time,sleep
-#import urllib
+import urllib
 from tah.tah_intern.models import Layer
-from tah.tah_intern.LegacyTileset import LegacyTileset
+#from tah.tah_intern.LegacyTileset import LegacyTileset
+from tah.tah_intern.Tileset import Tileset
 from django.conf import settings
 
 base_tile_path = settings.TILES_ROOT
@@ -13,13 +14,14 @@ layer=Layer.objects.get(name='tile')
 
 for x in range(750,1000):
  for y in range(0,4096):
-    tilesetfile = os.path.join(base_tile_path,"%s_%d_%d" % (layer.name,12,x//1000),str(x)+'_'+str(y))
+    (tilepath, tilefile) = Tileset(layer,12,x,y).get_filename(base_tile_path)
+    tilesetfile = os.path.join(tilepath, tilefile)
     if not os.path.isfile(tilesetfile):
-      URL = "http://tah.openstreetmap.org/tahngo/Request/create/?x=%s&y=%d&priority=4" %(x,y)
+      URL = "http://tah.openstreetmap.org/Request/create/?x=%s&y=%d&priority=4" %(x,y)
       #print "Would request %s" % tilesetfile
       urllib.urlopen(URL)
       #sleep(1)
-      #now = time()
+      now = time()
       #lt = LegacyTileset()
       #lt.convert(layer,12,x,y,base_tile_path)
       if y%50 == 0:

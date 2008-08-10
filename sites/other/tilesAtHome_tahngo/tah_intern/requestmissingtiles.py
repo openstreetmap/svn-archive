8be4a9baa710f020edb31c6b52660c9e71968109
Sebastian Spaeth <sebastian@sspaeth.de>
2008-08-10 05:45:53 +0000

    this works directly now.

--- sites/other/tilesAtHome_tahngo/tah_intern/requestmissingtiles.py
+++ sites/other/tilesAtHome_tahngo/tah_intern/requestmissingtiles.py
@@ -3,26 +3,33 @@ import sys,os
 sys.path.insert(0, os.path.dirname(os.path.dirname(sys.path[0])))
 os.environ['DJANGO_SETTINGS_MODULE'] = "tah.settings"
 from time import time,sleep
-import urllib
-from tah.tah_intern.models import Layer
-#from tah.tah_intern.LegacyTileset import LegacyTileset
-from tah.tah_intern.Tileset import Tileset
 from django.conf import settings
+from tah.tah_intern.models import Layer
+from tah.tah_intern.Tileset import Tileset
+from tah.requests.forms import CreateForm
+from tah.requests.models import Request
+from tah.requests.views import saveCreateRequestForm
+base_tile_path = settings.TILES_ROOT
+
+class request:
+  META={'REMOTE_ADDR':'127.0.0.1'}
 
 base_tile_path = settings.TILES_ROOT
 layer=Layer.objects.get(name='tile')
+r=request()
 
-for x in range(750,1000):
- for y in range(0,4096):
-    (tilepath, tilefile) = Tileset(layer,12,x,y).get_filename(base_tile_path)
+for x in range(753,1000):
+ print "x=%d" % x
+ for y in range(1000,2000):
+    tilepath, tilefile = Tileset(layer,12,x,y).get_filename(base_tile_path)
     tilesetfile = os.path.join(tilepath, tilefile)
     if not os.path.isfile(tilesetfile):
-      URL = "http://tah.openstreetmap.org/Request/create/?x=%s&y=%d&priority=4" %(x,y)
-      #print "Would request %s" % tilesetfile
-      urllib.urlopen(URL)
-      #sleep(1)
-      now = time()
-      #lt = LegacyTileset()
-      #lt.convert(layer,12,x,y,base_tile_path)
-      if y%50 == 0:
-        print "x=%d y=%d (time %.1f sec)"% (x,y,time()-now)
+      CreateFormClass = CreateForm
+      CreateFormClass.base_fields['max_z'].required = False 
+      CreateFormClass.base_fields['layers'].required = False 
+      CreateFormClass.base_fields['status'].required = False 
+      form = CreateFormClass({'min_z': 12, 'x': x, 'y': y, 'priority': 4, 'layer':[1]})
+      if form.is_valid():
+        req, reason = saveCreateRequestForm(r, form)
+      else:
+        print "form is not valid. %s\n" % form.errors

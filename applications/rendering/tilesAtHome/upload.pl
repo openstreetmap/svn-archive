d4a8a504dae2ecdbd24ff52374f14c4ce3447ebd
matthiasj <matthiasj@svn.openstreetmap.org>
2008-08-19 21:00:28 +0000

    of course, TahConf is in lib...

--- applications/rendering/tilesAtHome/upload.pl
+++ applications/rendering/tilesAtHome/upload.pl
@@ -7,7 +7,7 @@ use Fcntl ':flock'; #import LOCK_* constants
 use English '-no_match_vars';
 use tahconfig;
 use tahlib;
-use TahConf;
+use lib::TahConf;
 
 #-----------------------------------------------------------------------------
 # OpenStreetMap tiles@home, upload module

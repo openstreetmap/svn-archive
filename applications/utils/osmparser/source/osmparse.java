f3fcf1136716ca8448c15905fae200a18f8d71df
josias <josias@svn.openstreetmap.org>
2008-08-22 12:18:37 +0000

    osmparser added

---
+++ applications/utils/osmparser/source/osmparse.java
@@ -0,0 +1,92 @@
+
+import java.io.File;
+
+
+
+/**
+ * Comandozeilen schnittstelle von osmparser
+ * 
+ * @author Josias Polchau
+ * 
+ * Copyright (C) 2008 Josias Polchau
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ * 
+ * */
+
+public class osmparse {
+
+public static void main(String[] args) throws Exception {
+	if ((args.length ==0)||(args[0].equals("--help"))){printHelp();
+	}
+	else{
+		int i=0;
+		String inputfile=null;
+		String outputfile=null;
+		String mapFeatures=null;
+		boolean differentFiles=false;
+		while (i<args.length){
+			switch (args[i].charAt(0)) {
+			case 'x':
+				if (args[i].charAt(1)=='=') inputfile = args[i].substring(2);
+			break;
+			case 'f':
+				if (args[i].charAt(1)=='=') mapFeatures = args[i].substring(2);
+			break;
+			case 'o':
+				if (args[i].charAt(1)=='=') outputfile = args[i].substring(2);
+			break;
+			case 'd':
+				differentFiles=true;
+				args[i]=args[i].substring(1);
+				--i;
+			break;
+			case '-':
+				args[i]=args[i].substring(1);
+				--i;
+			break;
+
+			default:
+				break;
+			} 
+			i++;
+		}
+		if ((outputfile==null)) differentFiles=true;
+		
+		if ((inputfile==null)||(!(new File(inputfile).exists()))) System.out.println("bitte gib ein Inputfile an");
+		else if ((mapFeatures==null)||(!(new File(mapFeatures).exists()))) System.out.println("bitte gib ein MapFeatures-File an");
+		else
+		{
+			osmparser proggi = new osmparser(mapFeatures,inputfile,outputfile,differentFiles);
+			proggi.parse();
+		}
+	}
+}
+
+private static void printHelp() {
+
+	System.out.println("OSMParser");
+	System.out.println("systax: osmparse OPTION\n" +
+			"\n" +
+			"required options:\n" +
+			"-x=[Inputfile]      osm-XML data\n" +
+			"-f=[mapFeatures]    XML file with requested Tags (see readme)\n" +
+			"\n" +
+			"optional:\n" +
+			"-o=[outputfile]     file to write the Openlayer-Text. if not specified saves each Tag to different Openlayer-Text-files\n" +
+			"\n" +
+			"e.g.:      >java osmparse f=mapFeatures.xml x=map.osm \n" +
+			"\n\n" +
+			"OSMParser  Copyright (C) 2008  Josias Polchau\n\n" +
+			"This program comes with ABSOLUTELY NO WARRANTY.\n" +
+			"This is free software, and you are welcome to redistribute it\n" +
+			"under certain conditions; look at the readme-file for details.\n");
+	
+
+}
+
+}
---
+++ applications/utils/osmparser/source/osmparser.java
@@ -0,0 +1,237 @@
+
+/**
+ * OSMPArser wandelt eine osm-xmldatei in eine openlayer text datei um
+ * 
+ * @author Josias Polchau
+ * 
+ * Copyright (C) 2008 Josias Polchau
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ * 
+ * */
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+
+public class osmparser {
+	
+	boolean differentFiles;
+	Document xmlbaum;
+	static final Mercator mercator = new Mercator();
+
+	MapFeatures mapFeatures;
+	String inputfile;
+	String mapFeaturesFile;
+	String Outputfile;
+
+	ArrayList<String[]> knowenTyes;
+	
+	
+	
+	public osmparser(String mapFeaturesFile,String inputfile,String Outputfile,boolean differentFiles) {
+		mapFeatures = new MapFeatures(mapFeaturesFile);
+		knowenTyes = mapFeatures.getKnowenTyes();
+		
+		if ((differentFiles)||(Outputfile==null)){
+			for (String[] element : knowenTyes) {	
+				String filename = mapFeatures.getfilename(element[0], element[1]);
+				resetfile(filename);
+				firstLineInfile(filename);
+			}
+		}
+		else{
+
+			resetfile(Outputfile);
+			firstLineInfile(Outputfile);
+		}
+		
+		this.mapFeaturesFile=mapFeaturesFile;
+		this.inputfile=inputfile;
+		this.Outputfile=Outputfile;
+		this.differentFiles=differentFiles;
+	}
+	
+	 
+
+	public void parse() throws Exception {
+		
+		DocumentBuilderFactory fabrik = DocumentBuilderFactory.newInstance();
+		DocumentBuilder aufbau = fabrik.newDocumentBuilder();
+		xmlbaum = aufbau.parse(inputfile);
+		NodeList tags = xmlbaum.getElementsByTagName("tag");
+		int anzahl = tags.getLength();
+		int oldpercent=0;
+
+		System.out.println("|                                                                                                    |");
+
+		System.out.print("|");
+		for (int i = 0; i < anzahl; i++) {
+			
+
+			if (oldpercent < (int)((double)i/anzahl*100)){
+				oldpercent = (int)((double)i/anzahl*100); 
+
+				System.out.print("#");
+			}
+			Node tag = tags.item(i);
+			double[] latlon = {0.,0.};
+			boolean found = false;
+			String osmTag = "leisure";
+			String osmValue = "playground";
+			int k = 0;
+
+			while ((k < knowenTyes.size()) && (!found)) {
+
+				String[] strings = knowenTyes.get(k);
+				if ((tag.getAttributes().getNamedItem("k").getNodeValue()
+						.equals(strings[0]))
+						&& (tag.getAttributes().getNamedItem("v")
+								.getNodeValue().equals(strings[1]))) {
+					found = true;
+					osmTag = strings[0];
+					osmValue = strings[1];
+					//System.out.println(osmTag + "=" + osmValue);
+				}
+				k++;
+			}
+
+			if (found) {
+				String name = "";
+				Node node = tag.getParentNode();
+				//System.out.println(node.getNodeName());
+				latlon = getLatLon(node);
+
+				NodeList kinder = node.getChildNodes();
+
+				// namen suchen
+				k = 0;
+				found = false;
+				while ((k < kinder.getLength()) && (!found)) {
+					try {
+						if ((kinder.item(k).getAttributes() != null)&&
+								(kinder.item(k).getAttributes().getNamedItem("k")!=null)&& 
+								(kinder.item(k).getAttributes().getNamedItem("k").getNodeValue().equals("name"))) {
+							found = true;
+							name = kinder.item(k).getAttributes().getNamedItem("v").getNodeValue();
+						}
+					} catch (Exception e) {
+						System.out.println("k="+k);
+						e.printStackTrace();
+					}
+					
+					k++;
+				}
+				writeFile(Outputfile, latlon[0], latlon[1], osmTag, osmValue, name);
+			}
+		}
+		System.out.println("#|");
+	}
+
+	private double[] getLatLon(Node knoten){
+		double[] latlon = {0.,0.};
+		int anzahl=0;
+	
+		if (knoten.getNodeName().equals("node")) {
+			latlon[0] = Double.valueOf(knoten.getAttributes().getNamedItem("lat").getNodeValue());
+			latlon[1] = Double.valueOf(knoten.getAttributes().getNamedItem("lon").getNodeValue());
+			anzahl=1;
+		}
+		else if (knoten.getNodeName().equals("way")) 
+		{
+			NodeList kinder = knoten.getChildNodes();
+			for (int j = 0; j < kinder.getLength(); j++) {
+				if (kinder.item(j).getNodeName().equals("nd"))
+				{
+					Node point = getNodePerID(new Integer(kinder.item(j).getAttributes().getNamedItem("ref").getNodeValue()));
+					latlon[0] += Double.valueOf(point.getAttributes().getNamedItem("lat").getNodeValue());
+					latlon[1] += Double.valueOf(point.getAttributes().getNamedItem("lon").getNodeValue());
+					anzahl++;
+				}
+				
+			}
+		}
+		latlon[0]=latlon[0]/anzahl;
+		latlon[1]=latlon[1]/anzahl;		
+		return latlon;
+	}
+	
+	private Node getNodePerID(Integer id) {
+		boolean found =false;
+		Node node=null;
+		NodeList nodes = xmlbaum.getElementsByTagName("node");
+		int anzahl = nodes.getLength();
+		int i=0;
+		while ((i<anzahl)&&(!found))
+		{
+			found = new Integer(nodes.item(i).getAttributes().getNamedItem("id").getNodeValue()).equals(id);
+			if (!found) i++;			
+		}
+		if (found)node= nodes.item(i);
+		return node;
+		
+	}
+	
+	private void resetfile(String dateiName) {
+		File fileVar = new File(dateiName);
+		fileVar.delete();
+	}
+
+	private void firstLineInfile(String dateiName) {
+
+		FileWriter file;
+		try {
+			file = new FileWriter(dateiName, true);
+			file.append("point	title	description	icon	iconSize	iconOffset\n");
+			file.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public boolean writeFile(String dateiName, double lat, double lon,
+			String osmKey, String osmVal, String name) {
+
+		boolean erfolgreich = false;
+		try {FileWriter file;
+			if (differentFiles) 
+			{
+				String filename = mapFeatures.getfilename(osmKey, osmVal);
+				file = new FileWriter(filename, true);
+			}
+			else file = new FileWriter(dateiName, true);
+			Double[] merc = mercator.merc(lon,lat);
+			file.append(merc[1] + "," + merc[0] + "	"
+					+ mapFeatures.getName(osmKey, osmVal) + "	" + name + "	"
+					+ mapFeatures.getImage(osmKey, osmVal)+"	" 
+					+ mapFeatures.getImagesize(osmKey, osmVal)+"	" 
+					+ mapFeatures.getImageoffset(osmKey, osmVal)+"	" 
+					+ "\n");
+
+			erfolgreich = true;
+			file.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Datei konnte nicht erstellt werden");
+			erfolgreich = false;
+		}
+		return erfolgreich;
+	}
+	
+	
+
+}

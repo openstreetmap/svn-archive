7b06344d3a80876df7a4175b7c4c2a9310c526a2
Oliver White <ojwlists@googlemail.com>
2008-08-25 17:14:44 +0000

    try using the same style of menu as the options page, where there are 3 big rows displayed at once, and the top-line is dedicate to big up/down/cancel buttons

--- applications/mobile/rana/modules/base_poi.py
+++ applications/mobile/rana/modules/base_poi.py
@@ -26,7 +26,120 @@ class poiModule(ranaModule):
     ranaModule.__init__(self, m, d)
     self.poi = {}
     self.needUpdate = False
+    self.scroll = 0
 
+  def handleMessage(self, message):
+    if(message == "up"):
+      if(self.scroll > 0):
+        self.scroll -= 1
+    if(message == "down"):
+      self.scroll += 1
+    if(message == "reset"):
+      self.scroll = 0
+    self.set("needRedraw", True)
+
+  def describePlaintext(self, item, category,index):
+    return(item, "", "")
+
+  def describePoi(self, item, category, index):
+    action = "set:menu_poi_category:%s" % category
+    action += "|set:menu_poi_location:%f,%f" % (item['lat'], item['lon'])
+    action += "|set:menu_poi_item:%d" % index
+    action += "|set:menu:poi"
+    
+    return(
+      item['name'],
+      "%1.1f km away" % (item['d']),
+      action)
+  
+  def describeCategory(self, item, category, index):
+    action = "set:menu:%s_cat_%s" % (self.moduleName, item)
+    action += "|%s:reset" % self.moduleName
+    return(item, "", action)
+  
+  def drawList2(self, cr, list, describeFunction, category=None):
+    # Find the screen
+    if not self.d.has_key('viewport'):
+      return
+    (x1,y1,w,h) = self.get('viewport', None)
+
+    dx = w / 3
+    dy = h / 4
+
+    menus = self.m.get("menu",None)
+    if(menus):
+      # Top row:
+      # * parent menu
+      menus.drawButton(cr, x1, y1, dx, dy, "", "up", "set:menu:%s_categories" % self.moduleName)
+      # * scroll up
+      menus.drawButton(cr, x1+dx, y1, dx, dy, "", "up_list", "%s:up" % self.moduleName)
+      # * scroll down
+      menus.drawButton(cr, x1+2*dx, y1, dx, dy, "", "down_list", "%s:down" % self.moduleName)
+
+      # One option per row
+      for row in (0,1,2):
+        index = self.scroll + row
+        numItems = len(list)
+        if(0 <= index < numItems):
+
+          (text1,text2,onClick) = describeFunction(list[index], category, index)
+
+          y = y1 + (row+1) * dy
+          
+          # Draw background and make clickable
+          menus.drawButton(cr,
+            x1,
+            y,
+            w,
+            dy,
+            "",
+            "3h", # background for a 3x1 icon
+            onClick)
+
+          border = 20
+
+          self.showText(cr, text1, x1+border, y+border, w-2*border)
+
+          # 2nd line: current value
+          self.showText(cr, text2, x1 + 0.15 * w, y + 0.6 * dy, w * 0.85 - border)
+
+          # in corner: row number
+          self.showText(cr, "%d/%d" % (index+1, numItems), x1+0.85*w, y+border, w * 0.15 - border, 20)
+
+            
+  def showText(self,cr,text,x,y,widthLimit=None,fontsize=40):
+    if(text):
+      cr.set_font_size(fontsize)
+      stats = cr.text_extents(text)
+      (textwidth, textheight) = stats[2:4]
+
+      if(widthLimit and textwidth > widthLimit):
+        cr.set_font_size(fontsize * widthLimit / textwidth)
+        stats = cr.text_extents(text)
+        (textwidth, textheight) = stats[2:4]
+
+      cr.move_to(x, y+textheight)
+      cr.show_text(text)
+      
+  def drawMenu(self, cr, menuName):
+    l = len(self.moduleName)
+    if(menuName[0:l] != self.moduleName):
+      return
+    menuName = menuName[l:]
+
+    if(menuName == "_categories"):
+      self.drawList2(cr, self.poi.keys(), getattr(self,"describeCategory"))
+
+    elif(menuName[0:5] == "_cat_"):
+      category = menuName[5:]
+
+      items = self.poi[category]
+      
+      items.sort(lambda x, y: int(x.get('d',1E+5)) - int(y.get('d',1E+5)))
+      
+      self.drawList2(cr, items, getattr(self,"describePoi"), category)
+
+    
   def addItem(self, type, name, lat, lon):
     item = {'name':name, 'lat':float(lat),'lon':float(lon)}
     self.poi[type].append(item)
@@ -44,41 +157,11 @@ class poiModule(ranaModule):
     degToKm = 40041.0 / 360.0
     for type,items in self.poi.items():
       for item in items:
-        dx = item['lon'] - ownlon
-        dy = item['lat'] - ownlat
-        # TODO: cos(lat)
-        item['d'] = math.sqrt(dx * dx + dy * dy) * degToKm
+        dlon = item['lon'] - ownlon
+        dlat = item['lat'] - ownlat
+        dlon *= math.cos(math.radians(ownlat))
+
+        item['d'] = math.sqrt(dlon * dlon + dlat * dlat) * degToKm
         
     self.needUpdate = False
-    
-  def drawList(self, cr, menuName, listHelper):
-    m = self.m.get("clickHandler", None)
-    if(m):
-      m.registerDraggableEntireScreen("menu")
-      
-    if(menuName[0:4] == "poi:"):
-      type = menuName[4:]
-      
-      items = self.poi.get(type, [])
-      items.sort(lambda x, y: int(x.get('d',1E+5)) - int(y.get('d',1E+5)))
-      submenu = False
-    else:
-      items = [{'name':a} for a in self.poi.keys()]
-      submenu = True
 
-    #for i in range(max(0,listHelper.first), min(listHelper.last+1, len(items))):
-    for i in range(0, len(items)):
-      item = items[i]
-      listHelper.write(i, "%d: %s"% (i,item['name']))
-      if(submenu):
-        listHelper.makeClickable(i, "set:menu:poi:%s" % item['name'])
-      else:
-        actions = (
-          "set:selected_type:poi",
-          "set:selected_name:%s" % item['name'],
-          "set:selected_pos:%f,%f" % (item['lat'], item['lon']),
-          "set:menu:poi")
-        listHelper.makeClickable(i, "|".join(actions))
-        
-
-    

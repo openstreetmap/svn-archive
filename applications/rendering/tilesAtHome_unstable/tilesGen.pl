6d3e759d39086a92813d5ce7957ed0d38e35d941
Sebastian Spaeth <spaetz@svn.openstreetmap.org>
2008-08-27 17:39:22 +0000

    add use warnings

--- applications/rendering/tilesAtHome_unstable/tilesGen.pl
+++ applications/rendering/tilesAtHome_unstable/tilesGen.pl
@@ -22,6 +22,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #-----------------------------------------------------------------------------
 
+use warnings;
 use strict;
 use File::Copy;
 use File::Path;

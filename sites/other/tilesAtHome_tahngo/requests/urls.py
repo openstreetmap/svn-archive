12cd131ee574d8558965ebc2e839a489b8ea0223
Sebastian Spaeth <sebastian@sspaeth.de>
2008-08-30 23:09:59 +0000

    start of requests RSS feed

--- sites/other/tilesAtHome_tahngo/requests/urls.py
+++ sites/other/tilesAtHome_tahngo/requests/urls.py
@@ -1,11 +1,21 @@
 from django.conf.urls.defaults import *
 from tah.requests.views import *
+from tah.requests.feeds import LatestRequests, LatestRequestsByCategory
+
+# available RSS feeds
+request_feeds = {
+    'latest': LatestRequests,
+    'categories': LatestRequestsByCategory,
+}
 
 urlpatterns = patterns('',
     (r'^$', index),
     (r'^show/$', show_first_page),
     (r'^show/uploads/$', show_uploads_page),
     (r'^show/page(?P<page>[0-9]+)/$', show_requests),
+    (r'^show/ajax$', show_ajax),
+    # an RSS feed for the latest requests
+    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': request_feeds}),
     (r'^create/$', create),
     (r'^create/changedTiles/$', request_changedTiles),
     (r'^take/$', take),

92b65aa3090980393854b75fde518577fc608d43
Sebastian Spaeth <spaetz@svn.openstreetmap.org>
2008-09-23 08:14:07 +0000

    enable python-based tile viewing for a while...

--- sites/other/tilesAtHome_tahngo/urls.py
+++ sites/other/tilesAtHome_tahngo/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.views.generic.simple import redirect_to
-from tah.tah_intern.views import homepage, show_log
+from tah.tah_intern.views import homepage, show_log, show_tile
 from tah.browse.MapOf import export_MapOf
 
 admin.autodiscover()
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
     (r'^Log/$', show_log),
     (r'^MapOf/', export_MapOf),
+    (r'Tiles/(?P<layername>\w+)/(?P<z>\d+)/(?P<x>\d+)/(?P<y>\d+)(.png)?/$',show_tile),
     (r'^Log/Requests/Recent/$', redirect_to, {'url': '/Request/show/'}), #temporary redirect
     (r'Requests/Version\.php$', redirect_to, {'url': '/tahngo/Request/latestClientVersion/'}), #temporary redirect
     #(r'/accounts/profile/$', 'redirect_to', {'url': '/user/'}),

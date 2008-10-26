65029704306c5fd960f8e3f72afdcefb5412f5d0
Shaun McDonald <mail+github@shaunmcdonald.me.uk>
2008-10-26 08:41:22 +0000

    removing the osmbinaryformat branch, as it is now dead

--- sites/rails_port_branches/osmbinaryformat/app/models/diary_entry.rb
+++
@@ -1,9 +0,0 @@
-class DiaryEntry < ActiveRecord::Base
-  belongs_to :user
-  has_many :diary_comments, :order => "id"
-
-  validates_presence_of :title, :body
-  validates_numericality_of :latitude, :allow_nil => true
-  validates_numericality_of :longitude, :allow_nil => true
-  validates_associated :user
-end

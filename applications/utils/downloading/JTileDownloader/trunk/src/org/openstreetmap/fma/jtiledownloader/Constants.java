63ecfbb6717d57aa3fbd3f3ac9aa4d306ec62183
Friedrich Maier <fr.maier@gmx.at>
2008-11-04 00:07:37 +0000

    added constant to support different download configs

--- applications/utils/downloading/JTileDownloader/trunk/src/org/openstreetmap/fma/jtiledownloader/Constants.java
+++ applications/utils/downloading/JTileDownloader/trunk/src/org/openstreetmap/fma/jtiledownloader/Constants.java
@@ -31,4 +31,10 @@ public interface Constants
     public static final double MIN_LON = -85.0511;
     public static final double MAX_LON = 85.0511;
 
+    public static final String[] INPUT_TAB_TYPE = new String[] {"Paste URL (Square)", "Bounding Box (Lat/Lon)", "Bounding Box (XY)" };
+    public static final String[] CONFIG_TYPE = new String[] {"UrlSquare", "BBoxLatLon", "BBoxXY" };
+    public static final int TYPE_URLSQUARE = 0;
+    public static final int TYPE_BOUNDINGBOX_LATLON = 1;
+    public static final int TYPE_BOUNDINGBOX_XY = 2;
+
 }

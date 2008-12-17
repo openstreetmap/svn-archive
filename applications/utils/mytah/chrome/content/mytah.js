9720695484ac77a45772efea40e021b5b11d761e
Mario Ferraro <fadinlight@gmail.com>
2008-12-17 11:21:23 +0000

    MyTaH: added capability to read number of tiles, number of kb, idle time, number of tiles remaining for better ranking and display it in a tooltip

--- applications/utils/mytah/chrome/content/mytah.js
+++ applications/utils/mytah/chrome/content/mytah.js
@@ -1,6 +1,8 @@
 //TODO: Icon
 //TODO: Additional info to retrieve number of tiles (tooltip?), and different orders, details in own homepage
 
+const mytah_version="0.1";
+const mytah_build="20081217";
 
 const USER_BY_ID = 0;
 const USER_BY_NAME = 1;
@@ -10,11 +12,24 @@ var user_type;
 var timeout=60;
 var current_text;
 var current_rank="updating...";
+var number_of_tiles="updating...";
+var number_of_kb="updating...";
+var idle_time="updating...";
+var tiles_to_better_rank="updating...";
+
 var interval_id=null;
 
 function mytah_init() {
-	var current_rank="updating...";
-	document.getElementById("mytah_sbmi").label="My Tah: "+current_rank;
+	current_rank="updating...";
+	number_of_tiles="updating...";
+	number_of_kb="updating...";
+	idle_time="updating...";
+	tiles_to_better_rank="updating...";
+	document.getElementById("mytah_sbmi").label="MyTah: "+current_rank;
+	document.getElementById("mytah_numberoftiles").value=number_of_tiles;
+	document.getElementById("mytah_numberofkb").value=number_of_kb;
+	document.getElementById("mytah_idletime").value=idle_time;
+	document.getElementById("mytah_tilestobetterrank").value=tiles_to_better_rank;
 
 	this.prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
 	if (this.prefs.getPrefType("mytah.user_type") == this.prefs.PREF_INT) {
@@ -62,29 +77,45 @@ function mytah_update_text() {
 }
 
 function mytah_getRanking() {
-	var myRegExp = "";
-	
-	if (user_type==USER_BY_ID) {
-		myRegExp = '(?:(<tr><td>))(.*?)(?=(<\/td><td><a href="\/User\/show\/byid\/'+user+'\/">))';
-	} else if (user_type==USER_BY_NAME) {
-		myRegExp = '(?:(<tr><td>))(.*?)(?=(<\/td><td><a href="\/User\/show\/byid\/\\d*\/">'+user+'))';
+	var regexs = {
+		by_id: {
+			current_rank: '(?:(<tr><td>))(.*?)(?=(<\/td><td><a href="\/User\/show\/byid\/'+user+'\/">))',
+			number_of_tiles: '(?:(\/byid\/'+user+'\/">.*?<\/a><\/td><td>))(.*)(?=(<\/td><td>.*?<\/td><td>.*?<\/td><\/tr>))',
+			number_of_kb: '(?:(\/byid\/'+user+'\/">.*?<\/a><\/td><td>.*?<\/td><td>))(.*)(?=(<\/td><td>))',
+			idle_time: '(?:(\/byid\/'+user+'\/">.*?<\/a><\/td><td>.*?<\/td><td>.*?<\/td><td>))(.*)(?=(<\/td><\/tr>))',
+			tiles_to_better_rank: '(?:(<\/a><\/td><td>))(.*?)(?=(<\/td><td>.*?<\/td><td>.*?<\/td><\/tr>\\s*<tr><td>.*?<\/td><td><a href="\/User\/show\/byid\/'+user+'\/">))'
+		},
+		by_name: {
+			current_rank: '(?:(<tr><td>))(.*?)(?=(<\/td><td><a href="\/User\/show\/byid\/\\d*\/">'+user+'))',
+			number_of_tiles: '(?:('+user+'<\/a><\/td><td>))(.*?)(?=(<\/td><td>))',
+			number_of_kb: '(?:('+user+'<\/a><\/td><td>.*?<\/td><td>)(.*?)(?=(<\/td><td>)))',
+			idle_time: '(?:('+user+'<\/a><\/td><td>.*?<\/td><td>.*?<\/td><td>)(.*?)(?=(<\/td><\/tr>)))',
+			tiles_to_better_rank: '(?:(<\/a><\/td><td>))(.*?)(?=(<\/td><td>.*?<\/td><td>.*?<\/td><\/tr>\\s*<tr><td>.*?<\/td><td><a href="\/User\/show\/byid\/\\d*\/">'+user+'<\/a>))'
+		}
+	};
+	var looper;
+	if (user_type==USER_BY_ID) looper=regexs.by_id; else looper=regexs.by_name;
+	for (var a in looper) {
+		var m = new RegExp(looper[a]).exec(current_text);
+		eval(a+"='"+m[2]+"'");
 	}
-	var re = new RegExp(myRegExp);
-	var m = re.exec(current_text);
-	current_rank = m[2];
+	tiles_to_better_rank=(tiles_to_better_rank-number_of_tiles);
 	document.getElementById("mytah_sbmi").label="MyTaH: "+current_rank;
+	document.getElementById("mytah_numberoftiles").value=number_of_tiles;
+	document.getElementById("mytah_numberofkb").value=number_of_kb;
+	document.getElementById("mytah_idletime").value=idle_time;
+	document.getElementById("mytah_tilestobetterrank").value="-"+tiles_to_better_rank;
 }
 
 function mytah_showPreferences() {
 	var params={out:null};
 	window.openDialog("chrome://MyTaH/content/preferences.xul","","chrome,dialog,modal",params).focus();
-
-	  if (params.out) {
-    current_rank="updating...";
-    mytah_init();
-  }
-  else {
-  }
+	if (params.out) {
+		current_rank="updating...";
+		mytah_init();
+	}
+	else {
+	}
 }
 
 function mytah_showAbout() {

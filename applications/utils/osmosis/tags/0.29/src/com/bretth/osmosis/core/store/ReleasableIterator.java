fdd7f1f0dfd75e1c2f16974a41cc4a33239a41d1
Brett Henderson <brett@bretth.com>
2008-12-18 12:02:50 +0000

    Tagged version 0.29.

---
+++ applications/utils/osmosis/tags/0.29/src/com/bretth/osmosis/core/store/ReleasableIterator.java
@@ -0,0 +1,16 @@
+// License: GPL. Copyright 2007-2008 by Brett Henderson and other contributors.
+package com.bretth.osmosis.core.store;
+
+import java.util.Iterator;
+
+
+/**
+ * Defines an iterator that must be explicitly released after use.
+ * 
+ * @param <T>
+ *            The data type to be iterated.
+ * @author Brett Henderson
+ */
+public interface ReleasableIterator<T> extends Iterator<T>, Releasable {
+	// This combines multiple interfaces but doesn't add methods.
+}
---
+++ applications/utils/osmosis/tags/0.29/src/com/bretth/osmosis/extract/mysql/Configuration.java
@@ -0,0 +1,158 @@
+// License: GPL. Copyright 2007-2008 by Brett Henderson and other contributors.
+package com.bretth.osmosis.extract.mysql;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.bretth.osmosis.core.OsmosisRuntimeException;
+
+
+/**
+ * Loads and exposes the extraction configuration properties.
+ * 
+ * @author Brett Henderson
+ */
+public class Configuration {
+	private static final String KEY_HOST = "host";
+	private static final String KEY_DATABASE = "database";
+	private static final String KEY_USER = "user";
+	private static final String KEY_PASSWORD = "password";
+	private static final String KEY_INTERVAL_LENGTH = "intervalLength";
+	private static final String KEY_LAG_LENGTH = "lagLength";
+	private static final String KEY_CHANGE_FILE_BEGIN_FORMAT = "changeFileBeginFormat";
+	private static final String KEY_CHANGE_FILE_END_FORMAT = "changeFileEndFormat";
+	private static final String KEY_ENABLE_PROD_ENCODING_HACK = "enableProdEncodingHack";
+	
+	
+	private Properties properties;
+	
+	
+	/**
+	 * Creates a new instance.
+	 * 
+	 * @param configFile
+	 *            The configuration file to read from.
+	 */
+	public Configuration(File configFile) {
+		properties = loadProperties(configFile);
+	}
+	
+	
+	private Properties loadProperties(File configFile) {
+		FileInputStream fileInputStream = null;
+		
+		properties = new Properties();
+		
+		try {
+			fileInputStream = new FileInputStream(configFile);
+			
+			properties.load(fileInputStream);
+			
+		} catch (IOException e) {
+			throw new OsmosisRuntimeException("Unable to load properties from config file " + configFile);
+		} finally {
+			if (fileInputStream != null) {
+				try {
+					fileInputStream.close();
+				} catch (IOException e) {
+					// Ignore errors.
+				}
+			}
+		}
+		
+		return properties;
+	}
+	
+	
+	/**
+	 * Returns the database host.
+	 * 
+	 * @return The database host.
+	 */
+	public String getHost() {
+		return properties.getProperty(KEY_HOST);
+	}
+	
+	
+	/**
+	 * Returns the database instance.
+	 * 
+	 * @return The database instance.
+	 */
+	public String getDatabase() {
+		return properties.getProperty(KEY_DATABASE);
+	}
+	
+	
+	/**
+	 * Returns the database user.
+	 * 
+	 * @return The database user.
+	 */
+	public String getUser() {
+		return properties.getProperty(KEY_USER);
+	}
+	
+	
+	/**
+	 * Returns the database password.
+	 * 
+	 * @return The database password.
+	 */
+	public String getPassword() {
+		return properties.getProperty(KEY_PASSWORD);
+	}
+	
+	
+	/**
+	 * Returns the duration of each changeset interval.
+	 * 
+	 * @return The interval length in milliseconds.
+	 */
+	public int getIntervalLength() {
+		return Integer.parseInt(properties.getProperty(KEY_INTERVAL_LENGTH)) * 1000;
+	}
+	
+	
+	/**
+	 * Returns the amount of time the extraction process lags the current time
+	 * to allow the database to stabilise to ensure consistent queries.
+	 * 
+	 * @return The lag length in milliseconds.
+	 */
+	public int getLagLength() {
+		return Integer.parseInt(properties.getProperty(KEY_LAG_LENGTH)) * 1000;
+	}
+	
+	
+	/**
+	 * Returns the begin time portion of the changeset filename.
+	 * 
+	 * @return The format.
+	 */
+	public String getChangeFileBeginFormat() {
+		return properties.getProperty(KEY_CHANGE_FILE_BEGIN_FORMAT);
+	}
+	
+	
+	/**
+	 * Returns the end time portion of the changeset filename.
+	 * 
+	 * @return The format.
+	 */
+	public String getChangeFileEndFormat() {
+		return properties.getProperty(KEY_CHANGE_FILE_END_FORMAT);
+	}
+	
+	
+	/**
+	 * Returns the production encoding hack flag.
+	 * 
+	 * @return The production encoding hack flag.
+	 */
+	public boolean getEnableProductionEncodingHack() {
+		return Boolean.valueOf(properties.getProperty(KEY_ENABLE_PROD_ENCODING_HACK));
+	}
+}

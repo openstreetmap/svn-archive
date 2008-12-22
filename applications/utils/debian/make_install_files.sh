aedba61edcec504e497f7ee19f1be09b8293190a
Jörg Ostertag <openstreetmap@ostertag.name>
2008-12-22 16:33:20 +0000

    utils/debian/debian/make_install_files.sh: Add more things we build; add more Error Checks, Add color to output; reduce debug output

--- applications/utils/debian/make_install_files.sh
+++ applications/utils/debian/make_install_files.sh
@@ -12,6 +12,28 @@ if [ ! -n "$dst_path" ] ; then
     exit -1 
 fi
 
+# define Colors
+ESC=`echo -e "\033"`
+RED="${ESC}[91m"
+GREEN="${ESC}[92m"
+YELLOW="${ESC}[93m"
+BLUE="${ESC}[94m"
+MAGENTA="${ESC}[95m"
+CYAN="${ESC}[96m"
+WHITE="${ESC}[97m"
+BG_RED="${ESC}[41m"
+BG_GREEN="${ESC}[42m"
+BG_YELLOW="${ESC}[43m"
+BG_BLUE="${ESC}[44m"
+BG_MAGENTA="${ESC}[45m"
+BG_CYAN="${ESC}[46m"
+BG_WHITE="${ESC}[47m"
+BRIGHT="${ESC}[01m"
+UNDERLINE="${ESC}[04m"
+BLINK="${ESC}[05m"
+REVERSE="${ESC}[07m"
+NORMAL="${ESC}[0m"
+
 echo "copying Files to '$dst_path'"
 package_name=openstreetmap
 dst_path=${dst_path%/}
@@ -31,94 +53,147 @@ mkdir -p "$man1_path"
 # ------------------------------------------------------------------
 # Utilities written in C
 
-echo ""
-echo "---> import/and2osm"
-(
-    cd import/and2osm/
-    make clean
-    make || exit -1
-    ) || exit -1
-cp import/and2osm/2AND  ${bin_path}/osm2AND
+# ------------------------------------------------------------------
+# Various libs
+for lib in ccoord libosm libimg ; do 
+    echo "${BLUE}----------> lib/$lib${NORMAL}"
+    cd ../lib/$lib
+    # for a in *.cpp ; do perl -p -i -e 's,libshp/shapefil.h,shapefil.h,g' $a; done
+    make clean >build.log 2>build.err
+    make >>build.log 2>>build.err
+    if [ "$?" -ne "0" ] ; then
+	echo "${RED}!!!!!! ERROR compiling $lib ${NORMAL}"
+	exit -1
+    fi
+    cd ..
+    result_lib=$lib/lib${lib}.a
+    result_lib=`find . -name "*${lib}*.a"`
+    test -s "$result_lib" || result_lib="$lib/${lib}.a"
+    if [ ! -s "$result_lib"  ] ; then
+	echo "${RED}!!!!!! ERROR compiling $lib no Resulting '$result_lib'${NORMAL}"
+	exit -1
+    fi
+    cp "$result_lib" "../utils/${lib_path}" || exit -1
+done
+cd ../utils/
 
-#if false ; then
-# tweety@pack:~/svn.openstreetmap.org/applications/lib/libosm$ make
-# g++ -g -I/usr/local/include -I../ccoord   -c -o Components.o Components.cpp
-# Components.cpp:27:29: error: libshp/shapefil.h: No such file or directory
-# so 
-#    libosm libimg
-# have to be added later to the loop
-for lib in ccoord ; do 
-	(
-	    echo ""
-	    echo "---> lib/$lib"
-	    cd ../lib/$lib
-	#    for a in *.cpp ; do perl -p -i -e 's,libshp/shapefil.h,shapefil.h,g' $a; done
-	    make clean
-	    make || exit -1
-	    ) || exit -1
-
-	cp ../lib/$lib/lib${lib}.a ${lib_path}
-    done
 
 # ------------------------------------------------------------------
-# As soon as libosm compiles here on my debian machine
-if false ; then
-    echo ""
-    echo "---> filter/wayclean"
-    (
-	cd filter/wayclean
-	make clean
-	make || exit -1
-	) || exit -1
-    cp filter/wayclean/wayclean ${bin_path}
+# Importer
+for import in `ls import/*/Makefile| sed 's,/Makefile,,;s,import/,,'` ; do 
+    if echo $import | grep -q  and_import ; then
+	echo "Ignored '$import' because it does not compile on my debian machine"
+	continue
+    fi
 
-    echo ""
-    echo "---> osm2shp"
-    ( 
-	cd export/osm2shp
-	make clean
-	make || exit -1
-	) || exit -1
-    cp export/osm2shp/osm2shp ${bin_path}
-fi
-# of later
+    echo "${BLUE}----------> import/$import${NORMAL}"
+    cd import/$import/
+    make clean >build.log 2>build.err
+    make >>build.log 2>>build.err
+    if [ "$?" -ne "0" ] ; then
+	echo "${RED}!!!!!! ERROR compiling  import/${import} ${NORMAL}"
+	exit -1
+    fi
+    cd ../..
+    cp import/$import/2AND  ${bin_path}/osm2AND
+done
 
 # ------------------------------------------------------------------
-echo ""
-echo "---> color255"
-(
-    cd color255
-    make clean
-    make || exit -1
-) || exit -1
-cp color255/color255 ${bin_path}
+# Filter
+# As soon it compiles here on my debian machine
+# i will remove the excludes
+for filter in `ls filter/*/Makefile| sed 's,/Makefile,,;s,filter/,,'` ; do 
+    if echo $filter | grep -q wayclean ; then
+	echo "Ignored '$filter' because it does not compile on my debian machine"
+	continue
+    fi
+
+    echo "${BLUE}----------> filter/${filter}${NORMAL}"
+    cd filter/${filter}  || exit -1
+    make clean >build.log 2>build.err
+    make >>build.log 2>>build.err
+    if [ "$?" -ne "0" ] ; then
+	echo "${RED}!!!!!! ERROR compiling  filter/${filter} ${NORMAL}"
+	exit -1
+    fi
+    cd ../..
+    cp filter/${filter}/${filter} ${bin_path}
+done
+
+# ------------------------------------------------------------------
+# Export
+# As soon it compiles here on my debian machine
+# i will remove the excludes
+for export in `ls export/*/Makefile| sed 's,/Makefile,,;s,export/,,'` ; do 
+    if echo $export | grep -q -e osmgarminmap -e osm2shp -e osmgoogleearth; then
+	echo "Ignored '$export' because it does not compile on my debian machine"
+	continue
+    fi
+
+    echo "${BLUE}----------> export/${export}${NORMAL}"
+    cd export/${export}  || exit -1
+
+    if [ -s "Makefile.$export" ] ;then
+	custom_makefile=" -f Makefile.$export"
+    else
+	custom_makefile=''
+    fi
+
+    make $custom_makefile clean >build.log 2>build.err
+    make $custom_makefile >>build.log 2>>build.err
+    if [ "$?" -ne "0" ] ; then
+	echo "${RED}!!!!!! ERROR compiling  export/${export} ${NORMAL}"
+	exit -1
+    fi
+    cd ../..
+    cp export/${export}/${export} ${bin_path}
+done
 
 # ------------------------------------------------------------------
 if true ; then
-    echo ""
-    echo "---> osm2pqsql"
-    (
-	cd export/osm2pgsql
-	make clean
-	make || exit -1
-	) || exit -1 
+    echo "${BLUE}----------> color255${NORMAL}"
+    cd color255 || exit -1
+    make clean >build.log 2>build.err
+    make >>build.log 2>>build.err
+    if [ "$?" -ne "0" ] ; then
+	echo "${RED}!!!!!! ERROR compiling color255 ${NORMAL}"
+	exit -1
+    fi
+    cd ..
+    cp color255/color255 ${bin_path}
+fi
+
+# ------------------------------------------------------------------
+if true ; then
+    echo "${BLUE}----------> osm2pqsql${NORMAL}"
+    cd export/osm2pgsql  || exit -1
+    make clean >build.log 2>build.err
+    make >>build.log 2>>build.err
+    if [ "$?" -ne "0" ] ; then
+	echo "${RED}!!!!!! ERROR compiling color255 ${NORMAL}"
+	exit -1
+    fi
+    cd ../..
     cp export/osm2pgsql/osm2pgsql ${bin_path}
     cp export/osm2pgsql/default.style ${share_path}/default.style
 fi
 
 # ------------------------------------------------------------------
-echo ""
-echo "---> UTF8Sanitizer"
-(
-    cd planet.osm/C/
-    make clean
-    make || exit -1
-) || exit -1
-cp planet.osm/C/UTF8sanitizer ${bin_path}
-
+if true; then
+    echo "${BLUE}----------> UTF8Sanitizer${NORMAL}${NORMAL}"
+    cd planet.osm/C/  || exit -1
+    make clean >build.log 2>build.err
+    make >>build.log 2>>build.err
+    if [ "$?" -ne "0" ] ; then
+	echo "${RED}!!!!!! ERROR compiling color255 ${NORMAL}"
+	exit -1
+    fi
+    cd ../..
+    cp planet.osm/C/UTF8Sanitizer ${bin_path} || exit -1 
+fi
 
 # ------------------------------------------------------------------
-# Copy Perl libraries
+echo "${BLUE}----------> Copy Perl libraries${NORMAL}${NORMAL}"
 find perl_lib/ -name "*.pm" | while read src_fn ; do 
     dst_fn="$perl_path/${src_fn#perl_lib/}"
     dst_dir=`dirname "$dst_fn"`
@@ -126,7 +201,7 @@ find perl_lib/ -name "*.pm" | while read src_fn ; do
     cp "$src_fn" "$dst_fn"
 done
 
-# Copy Perl Binaries
+echo "${BLUE}----------> Copy Perl Binaries${NORMAL}${NORMAL}"
 find ./ -name "*.pl" | while read src_fn ; do 
     dst_fn="$bin_path/${src_fn##*/}"
     filename="`basename $src_fn`"
@@ -138,9 +213,9 @@ find ./ -name "*.pl" | while read src_fn ; do
     if head -1 "$src_fn" | grep -q -e '^#! */usr/bin/perl' ; then
 	cp "$src_fn" "$dst_fn"
     else
-	echo "WARNING!!! Perl Hash Bang is missing at File '$src_fn'"
+	echo "${RED}WARNING!!! Perl Hash Bang is missing at File '$src_fn'${NORMAL}"
 	echo "           I'm not adding this File to the debian Package"
-	echo "First Line: `head -1 "$src_fn"`"
+	echo "First Line: `head -1 '$src_fn'`"
     fi
 
     # ----- Try to create man Pages
@@ -163,36 +238,38 @@ find ./ -name "*.pl" | while read src_fn ; do
 done
 
 # --------------------------------------------
-# Copy Python Binaries
+echo "${BLUE}----------> Copy Python Binaries${NORMAL}"
 find ./ -name "*.py" | while read src_fn ; do 
     dst_fn="$bin_path/${src_fn##*/}"
     dst_fn="${dst_fn/.py}"
-    if head -1 "$src_fn" | grep -q -e '^#! */usr/bin/python' -e '^#!/usr/bin/env python'; then
+    if head -1 "$src_fn" | grep -q -e '^#! */usr/bin/python' -e '^#!/opt/python-2_5/bin/python' -e '^#!/usr/bin/env python'; then
 	cp "$src_fn" "$dst_fn"
     else
-	echo "WARNING!!! Python Hash Bang is missing at File '$src_fn'"
-	echo "           I'm not adding this File to the debian Package"
-	echo "           First Line: `head -1 "$src_fn"`"
+	if head -1 "$src_fn" | grep -q -e '^#!/opt/python-2_5/bin/python' -e '^#!/usr/bin/env python'; then
+	    (echo '#!/usr/bin/python2.5'; cat "$src_fn") >"$dst_fn"
+	else
+	    echo "${RED}WARNING!!! Python Hash Bang is missing at File '$src_fn'${NORMAL}"
+	    echo "           I'm not adding this File to the debian Package"
+	    echo "           First Line: `head -1 $src_fn`"
+	fi
     fi
 done
-
-
-
-
-# XXX
-# For later:
-# Add java tools, but for these a build.xml with a target jar or similar would be best 
 
 
 # #######################################################
 # Osmosis
 # #######################################################
-cd osmosis
-ant dist_binary
-cd ..
+echo "${BLUE}----------> Osmosis${NORMAL}"
+cd osmosis/trunk
+ant clean >build.log 2>build.err
+ant dist >>build.log 2>>build.err
+if [ "$?" -ne "0" ] ; then
+    echo "${RED}!!!!!! ERROR compiling  Osmosis ${NORMAL}"
+    exit -1
+fi
+cd ../..
 mkdir -p $dst_path/usr/local/share/osmosis/
 cp ./osmosis/dist/result/osmosis.jar $dst_path/usr/local/share/osmosis/
-
 
 cp debian/osmosis.sh "$bin_path/osmosis"
 
@@ -200,4 +277,13 @@ cp debian/osmosis.sh "$bin_path/osmosis"
 #########################################################
 # Mapnik installation tool
 #########################################################
+echo "${BLUE}----------> Mapnik${NORMAL}"
 cp export/osm2pgsql/mapnik-osm-updater.sh "$bin_path"
+cp export/osm2pgsql/900913.sql "$bin_path"
+cp export/osm2pgsql/default.style  "$bin_path"
+
+
+##################################################################
+# XXX
+# For later:
+# Add java tools, but for these a build.xml with a target jar or similar would be best 

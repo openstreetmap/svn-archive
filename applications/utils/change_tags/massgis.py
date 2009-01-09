9c1219ef23f3d8ee9701c99d49be9edf8787fc80
Christopher Schmidt <crschmidt@crschmidt.net>
2009-01-09 12:44:39 +0000

    add more shortname -> longname mappings, fix for playgrounds

--- applications/utils/change_tags/massgis.py
+++ applications/utils/change_tags/massgis.py
@@ -18,6 +18,53 @@ def conservation_organization(tags, type):
 
     return changed    
 
+def playgrounds(tags, type):
+    changed = False
+    
+    if type == "way" and 'name' in tags:
+        name = tags['name']
+    
+        if name.endswith("Playground"):
+            if not 'leisure' in tags or tags['leisure'] != "playground":
+                changed = True
+                tags['leisure'] = 'playground'
+    tags['created_by'] = 'change_tags.py - massgis area_names %s' % __version__
+
+    return changed    
+
+def more_shrt_nms(tags, type):
+    import re
+    map = {
+      " Cons Lnd$": ' Conservation Land', 
+      " Cons Area$": ' Conservation Area', 
+      " Consv Land$": ' Conservation Land', 
+      " Sanc$": ' Sanctuary', 
+      " Consv Prjt$": ' Conservation Project',
+      " Pk$": ' Park',
+      " Conserv\. Land$": " Conservation Land"
+    }
+    remap = {}
+    for key, value in map.items():
+        pat = re.compile(key)
+        remap[pat] = value
+        
+    changed = False
+    
+    if type == "way" and 'name' in tags:
+        name = tags['name']
+        for key, value in remap.items():
+            new_name = key.sub(value, name)
+            if new_name != name:
+                print tags
+                tags['name'] = new_name
+                changed = True
+                break
+         
+    tags['created_by'] = 'change_tags.py - massgis short_names %s' % __version__
+
+    return changed    
+
+
 def area_names(tags, type):
     """Fix for MassGIS OpenSpace areas: do better keying based on name, fix some common typos."""
     

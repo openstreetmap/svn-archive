7448cdb0aee99c82ba30d5aab67367422c377257
Blars Blarson <blarson@svn.openstreetmap.org>
2009-01-09 07:46:08 +0000

    Remove my local directory from trapi.pm

--- sites/other/trapi/trapi.pm
+++ sites/other/trapi/trapi.pm
@@ -7,7 +7,7 @@ use constant MAXOPEN => 500;		# max cached filehandles
 use constant KEEPOPEN => 400;		# drop down to this when MAXOPEN hit
 use constant SPLIT => 4096;		# bytes in node file before splitting
 use constant TRAPIDIR => '/trapi';	# directory trapi data is in
-use constant DBDIR => '/home/blarson/trapidb/';		# directory where the indexes are kept (absolute or relitive to TRAPIDIR) end with / if not empty
+use constant DBDIR => '';		# directory where the indexes are kept (absolute or relitive to TRAPIDIR) end with / if not empty
 use constant IGNORETAGS => '^(?:created_by$|tiger:|gnis:|source$|attribution$|import_uuid$|time$|AND[_:]|massgis:|open[gG]eo[dD][bB]:|converted_by$)';	# regular expression of tags to ignore
 
 use constant TMPDIR => "/tmp/";

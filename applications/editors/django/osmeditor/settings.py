17a1ba79fc55c916b1ff16044d86956ed430ec71
Hugh Barnes <hughb@svn.openstreetmap.org>
2009-01-26 01:26:26 +0000

    Oops - last commit (rev.13387) was one level too low - changes require new settings.py

--- applications/editors/django/osmeditor/settings.py
+++ applications/editors/django/osmeditor/settings.py
@@ -79,4 +79,7 @@ INSTALLED_APPS = (
     'simple'
 )
 
+#OSM_API = "http://main.dev.openstreetmap.org/api/0.5"
 OSM_API = "http://openstreetmap.org/api/0.5"
+OSM_BROWSE = "http://openstreetmap.org/browse/%s/%s" # maybe it's not a good idea to put interpolation points in here
+OSM_USER = "http://openstreetmap.org/user/%s"

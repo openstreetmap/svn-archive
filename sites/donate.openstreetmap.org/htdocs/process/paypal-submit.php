56c15fbb3009e6f91c325588ed7b9cb8106120fb
Grant Slater <git@firefishy.com>
2009-01-26 00:05:07 +0000

    Initial Commit of donate.openstreetmap.org Work-in-Progress, but I need sleep.

---
+++ sites/donate.openstreetmap.org/htdocs/process/paypal-submit.php
@@ -0,0 +1,39 @@
+<?
+/* Paypal Submission 
+
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_donations">
+<input type="hidden" name="business" value="treasurer@openstreetmap.org">
+<input type="hidden" name="lc" value="GB">
+<input type="hidden" name="item_name" value="One-time donation">
+<input type="hidden" name="item_number" value="DONATE">
+<input type="hidden" name="no_shipping" value="1">
+<input type="hidden" name="rm" value="1">
+<input type="hidden" name="return" value="http://example/success">
+<input type="hidden" name="cancel_return" value="http://example/cancel">
+<input type="hidden" name="currency_code" value="GBP">
+<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHosted">
+<input type="hidden" name="abc" value="123">
+<input type="image" src="https://www.paypal.com/en_GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="">
+<img alt="" border="0" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+<p>Submitting to payment processor...</p>
+<form method="post" name="contributiontracking" action="https://www.paypal.com/cgi-bin/webscr">
+<input type="hidden" name="on0" value="contribution_tracking_id" />
+<input type="hidden" name="os0" value="1238662" />
+<input type="hidden" name="business" value="treasurer@openstreetmap.org" />
+<input type="hidden" name="item_name" value="One-time donation" />
+<input type="hidden" name="item_number" value="DONATE" />
+<input type="hidden" name="cmd" value="_xclick" />
+<input type="hidden" name="no_note" value="0" />
+<input type="hidden" name="notify_url" value="https://donate.openstreetmap.org/scripts/paypal-callback.php" />
+<input type="hidden" name="return" value="http://donate.openstreetmap.org/thank_you/" />
+<input type="hidden" name="currency_code" value="GBP" />
+<input type="hidden" name="amount" value="5" />
+<p>If you are not automatically redirected, click the button to complete your donation at PayPal.</p>
+<input type="submit" value="Continue" />
+</form>
+
+*/
+?>

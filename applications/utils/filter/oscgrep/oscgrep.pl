3711187633e54636464cabb3f3d9ba00a3da4b97
Frederik Ramm <frederik@remote.org>
2009-01-28 10:06:50 +0000

    added --help message for oscgrep.pl

--- applications/utils/filter/oscgrep/oscgrep.pl
+++ applications/utils/filter/oscgrep/oscgrep.pl
@@ -1,23 +1,27 @@
 #!/usr/bin/perl
 
-# simple script that helps you find certain objects in .osc files.
-#
-# called like this:
-#
-# perl oscgrep.pl -a action -t type regex file file...
-#
-# where
-#   "action" is either create, modify, or delete;
-#   "type" is either way, node, or relation;
-#   "regex" is any regex
-#   "file" is an .osc file
-#
-# lists all elements from the .osc file that match the given action, type, and
-# regex (regex is applied against full XML content of the object). If -a, -t or
-# regex are omitted, dumps all actions/all types/all content; if file is omitted,
-# reads from stdin.
-#
 # Written by Frederik Ramm <frederik@remote.org>, public domain.
+sub HELP_MESSAGE { print <<EOF;
+$0 is a simple script that helps you find certain objects in .osc files.
+
+It is called like this:
+
+perl $0 [-a action] [-t type] [regex] [file] [file...]
+
+where
+  "action" is either create, modify, or delete;
+  "type" is either way, node, or relation;
+  "regex" is any regular expression 
+  "file" is an .osc file (does not support .osc.gz)
+
+It lists all elements from the .osc file that match the given action, type, and
+regex (regex is applied against full XML content of the object). If -a, -t or
+regex are omitted, dumps all actions/all types/all content; if file is omitted,
+reads from stdin.
+EOF
+exit;
+}
+sub VERSION_MESSAGE {};
 
 use Getopt::Std;
 my $options = {};

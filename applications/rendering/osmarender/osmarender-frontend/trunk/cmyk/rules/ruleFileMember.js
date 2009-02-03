517d539a593422eb5b4d86523023ff3e2d898fc7
Mario Ferraro <fadinlight@gmail.com>
2009-02-03 22:58:19 +0000

    Osmarender Frontend: added support for multiple key/values/selectors in rule tags and made children var public in ruleFileMember for debug purposes

--- applications/rendering/osmarender/osmarender-frontend/trunk/cmyk/rules/ruleFileMember.js
+++ applications/rendering/osmarender/osmarender-frontend/trunk/cmyk/rules/ruleFileMember.js
@@ -15,8 +15,6 @@ dojo.require("cmyk.rules.svgPattern");
 dojo.require("cmyk.rules.svgRect");
 dojo.require("cmyk.rules.svgSymbol");
 dojo.require("cmyk.rules.Symbol");
-dojo.require("cmyk.rules.Symbol");
-dojo.require("cmyk.rules.Symbol");
 dojo.require("cmyk.rules.Tag");
 dojo.require("cmyk.rules.wayMarker");
 dojo.require("cmyk.rules.xmlComment");
@@ -34,7 +32,7 @@ dojo.declare("cmyk.rules.ruleFileMember",null,{
 	*/
 //http://www.ibm.com/developerworks/web/library/wa-aj-dojo/
 	constructor: function(node) {
-		var children = [];
+		this.children = [];
 		var string_debug="unknown type: "+node.nodeType+" name: "+node.nodeName+" value: "+node.nodeValue;
 		//TODO: overload constructor for new node based on text
 
@@ -47,7 +45,7 @@ dojo.declare("cmyk.rules.ruleFileMember",null,{
 			break;
 			case node.COMMENT_NODE:
 				string_debug="";
-				children.push(new cmyk.rules.xmlComment(node.nodeValue));
+				this.children.push(new cmyk.rules.xmlComment(node.nodeValue));
 			break;
 			case node.ELEMENT_NODE:
 				switch (node.nodeName) {
@@ -60,71 +58,71 @@ dojo.declare("cmyk.rules.ruleFileMember",null,{
 						case "rule":
 							string_debug="";
 							if (node.getAttribute("s")==null) {
-								children.push(new cmyk.rules.Rule(node));
+								this.children.push(new cmyk.rules.Rule(node));
 							}
 							else {
-								children.push(new cmyk.rules.selectorRule(node));
+								this.children.push(new cmyk.rules.selectorRule(node));
 							}
 						break;
 							case "area":
 								string_debug="";
-								children.push(new cmyk.rules.Area(node));
+								this.children.push(new cmyk.rules.Area(node));
 							break;
 							case "else":
 								string_debug="";
-								children.push(new cmyk.rules.Else(node));
+								this.children.push(new cmyk.rules.Else(node));
 							break;
 							case "line":
 								string_debug="";
-								children.push(new cmyk.rules.Line(node));
+								this.children.push(new cmyk.rules.Line(node));
 							break;
 							case "caption":
 								string_debug="";
-								children.push(new cmyk.rules.Caption(node));
+								this.children.push(new cmyk.rules.Caption(node));
 							break;
 							case "pathText":
 								string_debug="";
-								children.push(new cmyk.rules.pathText(node));
+								this.children.push(new cmyk.rules.pathText(node));
 							break;
 							case "wayMarker":
 								string_debug="";
-								children.push(new cmyk.rules.wayMarker(node));
+								this.children.push(new cmyk.rules.wayMarker(node));
 							break;
 							case "circle":
 								string_debug="";
-								children.push(new cmyk.rules.Circle(node));
+								this.children.push(new cmyk.rules.Circle(node));
 							break;
 							case "symbol":
 								string_debug="";
-								children.push(new cmyk.rules.Symbol(node));
+								this.children.push(new cmyk.rules.Symbol(node));
 							break;
 							case "tag":
 								string_debug="";
-								children.push(new cmyk.rules.Tag(node));
+								this.children.push(new cmyk.rules.Tag(node));
 							break;
 							case "svg:marker":
 								string_debug="";
-								children.push(new cmyk.rules.svgMarker(node));
+								this.children.push(new cmyk.rules.svgMarker(node));
 							break;
 							case "svg:pattern":
 								string_debug="";
-								children.push(new cmyk.rules.svgPattern(node));
+								this.children.push(new cmyk.rules.svgPattern(node));
 							break;
 							case "svg:symbol":
 								string_debug="";
-								children.push(new cmyk.rules.svgSymbol(node));
+								this.children.push(new cmyk.rules.svgSymbol(node));
 							break;
 								case "svg:path":
 									string_debug="";
-									children.push(new cmyk.rules.svgPath(node));
+									this.children.push(new cmyk.rules.svgPath(node));
 								break;
 								case "svg:rect":
 									string_debug="";
-									children.push(new cmyk.rules.svgRect(node));
+									this.children.push(new cmyk.rules.svgRect(node));
 								break;
 								case "svg:g":
 									string_debug="";
-									children.push(new cmyk.rules.svgRect(node));
+									this.children.push(new cmyk.rules.svgRect(node));
 								break;
 					case "include":
 					break;
@@ -134,15 +132,15 @@ dojo.declare("cmyk.rules.ruleFileMember",null,{
 			break;
 			case node.TEXT_NODE:
 				string_debug="";
-				children.push(new cmyk.rules.xmlText(node.nodeValue));
+				this.children.push(new cmyk.rules.xmlText(node.nodeValue));
 			break;
 			default:
 				throw new Error('unknown node type: '+node.nodeType+' for node: '+node.nodeName+' with value '+node.nodeValue);
 		}
+		var children = this.children;
 		dojo.forEach(node.childNodes,function(node,index,array) {
 			children.push(new cmyk.rules.ruleFileMember(node));
 		});
 		if (string_debug!="") console.debug(string_debug);
-//		console.dir(children);
 	}
 });

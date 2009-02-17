1895e9f16b062dbacc0fc3f2f99d64bfbc460a87
Thomas Wood <grand.edgemaster@gmail.com>
2009-02-17 16:33:34 +0000

    Begin to parse StopPoints into OSM Nodes.

--- applications/utils/import/naptan2osm/parse.py
+++ applications/utils/import/naptan2osm/parse.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
-import sys
+import sys, time
+from osmparser import osmparser     # We eventually want to mod this to stream out the XML to file, bypassing ET.
 import xml.etree.cElementTree as ET
 
 class ParseXML:
     namespace = '{http://www.naptan.org.uk/}'
     tagmap = {}
-    def cleantag(self, elem):
-        """Cleans namespace from element tag
+    featurecounter = 0
+    feature = None      # The OSM feature we're building
+    document = ET.Element("osm", {"version": "0.5"})    # OSM document we're creating
+    
+    def cleannode(self, elem):
+        """Cleans namespace from element node
         eg: elem.tag = '{http://www.naptan.org.uk/}AtcoCode'
             is transformed to 'AtcoCode'
         """
@@ -15,16 +20,26 @@ class ParseXML:
     def node2tag(self, elem):
         """Lookup an OSM tag mapping in tagmap for a given node name"""
         try:
-            key = self.tagmap[self.cleantag(elem)]
+            key = self.tagmap[self.cleannode(elem)]
             value = elem.text
             return (key, value)
         except:
-            pass
+            return None
+            
+    def newfeature(self):
+        self.featurecounter += 1
+        return osmparser.OSMObj(type='node', id='-%d' % self.featurecounter)
+            
+    def getosmxml(self):
+        return ET.tostring(self.document)
 
 class ParseNaptan(ParseXML):
-    tagmap = {'AtcoCode': 'naptan:AtcoCode'}
+    tagmap = {
+        'AtcoCode': 'naptan:AtcoCode'
+    }
     STOPPOINTS = 1
     STOPAREAS = 2
+    
     def __init__(self):
         self.group = 0
         
@@ -32,18 +47,34 @@ class ParseNaptan(ParseXML):
         """Event handler for when the XML parser encounters an opening tag.
         Should be used for maintaining state, data retrieval should be done on
         encountering an endElement"""
-        tag = self.cleantag(elem)
-        if tag == 'StopPoints':
+        node = self.cleannode(elem)
+        if node == 'StopPoints':
             self.group = self.STOPPOINTS
         # The StopAreas node also appears in a StopPoint as well as a grouping node.
-        elif tag == 'StopAreas' and self.group != self.STOPPOINTS:
+        elif node == 'StopAreas' and self.group != self.STOPPOINTS:
             self.group = self.STOPAREAS
-        elif tag == '':
-            pass
+        elif node == 'StopPoint':
+            self.feature = self.newfeature()
     
     def endElement(self, elem):
-        print self.node2tag(elem)
-        
+        node = self.cleannode(elem)
+        if node == 'StopPoints':
+            self.group = 0
+        elif node == 'StopAreas' and self.group != self.STOPPOINTS:
+            self.group = 0
+        elif node == 'StopPoint':
+            self.feature.toxml(parent=self.document, as_string=False, indent=False)
+            self.feature = None
+        elif node == 'Latitude' and self.group == self.STOPPOINTS:
+            self.feature.loc[1] = elem.text
+        elif node == 'Longitude' and self.group == self.STOPPOINTS:
+            self.feature.loc[0] = elem.text
+        else:
+            tag = self.node2tag(elem)
+            if tag:
+                k, v = tag
+                self.feature.tags[k] = v
+    
 class ParseNPTG(ParseXML):
     def __init__(self):
         pass
@@ -65,13 +96,28 @@ def treeparse(f):
     else:
         raise Exception, "Unknown XML type"
     
+    count = 0
     for event, elem in it:
         if event == 'start':
             parser.startElement(elem)
         elif event == 'end':
             parser.endElement(elem)
             elem.clear()
+            
+    return parser.getosmxml()
     
 if __name__ == "__main__":
+    if not len(sys.argv) == 2:
+        raise Exception, "Input file required"
+    filename = sys.argv[1]
+    time1 = time.time()
     f = open(sys.argv[1])
-    treeparse(f)
+    xml = treeparse(f)
+    time2 = time.time()
+    print "Time: %f" % (time2 - time1)
+    
+    osmfile = '%s.osm' % (filename.rpartition('.')[0])
+    
+    outfile = open(osmfile, 'w')
+    outfile.write(xml)
+    outfile.close()

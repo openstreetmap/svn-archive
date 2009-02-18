ebaecc462a274d271b68a761d53f744fc14e24ee
Thomas Wood <grand.edgemaster@gmail.com>
2009-02-18 17:25:25 +0000

    Considerable reworkings, including the XML output, code for Indicator parsing (focused towards london), and more default tags to include.

--- applications/utils/import/naptan2osm/parse.py
+++ applications/utils/import/naptan2osm/parse.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python
-import sys, time
+import re, sys, time
 from osmparser import osmparser     # We eventually want to mod this to stream out the XML to file, bypassing ET.
 import xml.etree.cElementTree as ET
 
 class ParseXML:
     namespace = '{http://www.naptan.org.uk/}'
+    tagprefix = ''
     tagmap = {}         # The basic tag mappings that are a simple one to one mapping
     
     # Tuple containing XML (parent) nodes that are required to determine a child node's meaning
-    interestingparentnodes = ('StopPoints', 'StopAreas')
+    watchnodes = ()
     parentnodes = {}    # Dict noting parent nodes that may need to be checked to determine child's meaning
     
     featurecounter = 0  # OSM feature id incrementor
     feature = None      # The OSM feature we're building
-    document = ET.Element("osm", {"version": "0.5"})    # OSM document we're creating
+    outfile = None
     
-    def __init__(self):
-        self.parentnodes = dict.fromkeys(self.interestingparentnodes, False)
+    def __init__(self, of):
+        self.parentnodes = dict.fromkeys(self.watchnodes, False)
+        self.outfile = of
+        
+    def startElement(self, elem):
+        node = self.cleannode(elem)
+        # Keep our watched parent nodes list updated
+        if node in self.watchnodes:
+            self.parentnodes[node] = True
+            
+    def endElement(self, elem):
+        node = self.cleannode(elem)
+        if node in self.watchnodes:
+            self.parentnodes[node] = False
     
     def cleannode(self, elem):
         """Cleans namespace from element node
@@ -25,39 +38,60 @@ class ParseXML:
         """
         return elem.tag.replace(self.namespace, '')
         
-    def node2tag(self, elem, tagmap=None):
-        """Lookup an OSM tag mapping in tagmap for a given node name"""
+    def node2tag(self, elem, tagmap=None, noset=False):
+        """Lookup (and set) an OSM tag mapping in tagmap for a given node name"""
         if not tagmap:
             tagmap = self.tagmap
         try:
-            key = tagmap[self.cleannode(elem)]
-            value = elem.text
-            return (key, value)
-        except:
-            return None
+            node = self.cleannode(elem)
+            key = tagmap[node]
+            if not key:
+                key = '%s:%s' % (self.tagprefix, node)
+            value = elem.text.strip(' -')
+            if not value:
+                return None
+            
+            if not noset:
+                self.feature.tags[key] = value
+            else:
+                return (key, value)
+            
+        except KeyError:
+            return
             
     def newfeature(self):
         self.featurecounter += 1
         return osmparser.OSMObj(type='node', id='-%d' % self.featurecounter)
-            
-    def getosmxml(self):
-        return ET.tostring(self.document)
+
+
+
 
 class ParseNaptan(ParseXML):
+    tagprefix = 'naptan'
     tagmap = {
-        'AtcoCode': 'naptan:AtcoCode',
-        'StopType': 'naptan:StopType',
-        'BusStopType': 'naptan:BusStopType'
+        'AtcoCode': '',         # Blank entries automatically form tags of tagprefix:nodename
+        'NaptanCode': 'ref',
+        'CommonName': 'name',
+        'ShortCommonName': '',
+        'Landmark': '',
+        'Street': '',
+        'Crossing': '',
+        'Indicator': '',
     }
+    tagmap_altdescriptors = {
+        
+    }
+    watchnodes = ('StopPoints', 'StopAreas', 'AlternativeDescriptors')
         
     def startElement(self, elem):
         """Event handler for when the XML parser encounters an opening tag.
         Should be used for maintaining state, data retrieval should be done on
         encountering an endElement"""
-        node = self.cleannode(elem)
-        if node in self.interestingparentnodes:
-            self.parentnodes[node] = True
 
+        # Updates the parent nodes list
+        ParseXML.startElement(self, elem)
+
+        node = self.cleannode(elem)
         if node == 'StopPoint':
             self.feature = self.newfeature()
     
@@ -65,21 +99,43 @@ class ParseNaptan(ParseXML):
         node = self.cleannode(elem)
         
         if node == 'StopPoint':
-            self.feature.toxml(parent=self.document, as_string=False, indent=False)
+            # That's all for this point, wrap up the feature's xml ready for the next one
+            self.outfile.write(self.feature.toxml(needs_parent=False, indent=False))
             self.feature = None
+        
         elif node == 'Latitude' and self.parentnodes['StopPoints']:
             self.feature.loc[1] = elem.text
         elif node == 'Longitude' and self.parentnodes['StopPoints']:
             self.feature.loc[0] = elem.text
-        else:
-            tag = self.node2tag(elem)
+        elif node == 'Indicator' and not self.parentnodes['AlternativeDescriptors']:
+            indicator = elem.text.upper()
+            matches = re.match('^(?:(?:BAY)|(?:ENTRANCE)|(?:STAND)|(?:STOP) )?([A-Z0-9\-& ]{1,7})', indicator)
+            if matches:
+                v = matches.group(1).strip(' -')
+                if v:
+                    self.feature.tags['ref'] = v
+        elif self.parentnodes['AlternativeDescriptors']:
+            tag = self.node2tag(elem, self.tagmap_altdescriptors, noset=True)
             if tag:
                 k, v = tag
-                self.feature.tags[k] = v
+                if self.feature.tags[k]:
+                    self.feature.tags[k] = '%s;%s' % (self.feature.tags[k], v)
+                else:
+                    self.feature.tags[k] = v
+            else:
+                self.node2tag(elem)
+        else:
+            # Fallthrough for simple tag mappings
+            tag = self.node2tag(elem)
         
-        if node in self.interestingparentnodes:
-            self.parentnodes[node] = False
-    
+        # Keep our parent nodes dict updated.
+        ParseXML.endElement(self, elem)
+
+
+
+
+
+
 class ParseNPTG(ParseXML):
     
     def startElement(self, elem):
@@ -88,14 +144,16 @@ class ParseNPTG(ParseXML):
     def endElement(self, elem):
         pass
 
-def treeparse(f):
+def treeparse(f, of=sys.stdout):
+    of.write('<osm version="0.5">')
+
     it = iter(ET.iterparse(f, events=('start', 'end')))
     event, root = it.next()
     
     if root.tag == ParseNaptan.namespace + 'NaPTAN':
-        parser = ParseNaptan()
+        parser = ParseNaptan(of=of)
     elif root.tag == ParseNPTG.namespace + 'NationalPublicTransportGazetteer':
-        parser = ParseNPTG()
+        parser = ParseNPTG(of=of)
     else:
         raise Exception, "Unknown XML type"
     
@@ -106,21 +164,21 @@ def treeparse(f):
         elif event == 'end':
             parser.endElement(elem)
             elem.clear()
-            
-    return parser.getosmxml()
+
+    of.write('</osm>')
     
 if __name__ == "__main__":
     if not len(sys.argv) == 2:
         raise Exception, "Input file required"
+    
     filename = sys.argv[1]
-    time1 = time.time()
-    f = open(sys.argv[1])
-    xml = treeparse(f)
-    time2 = time.time()
-    print "Time: %f" % (time2 - time1)
-    
+    infile = open(filename)
     osmfile = '%s.osm' % (filename.rpartition('.')[0])
-    
     outfile = open(osmfile, 'w')
-    outfile.write(xml)
+    
+    time1 = time.time()
+    xml = treeparse(infile, outfile)
+    time2 = time.time()
+    print >>sys.stderr, "Time: %f" % (time2 - time1)
+    
     outfile.close()

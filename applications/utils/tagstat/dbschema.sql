d6b52470106786b75ca3b0d046ec3e837e9ca8f9
Patrick Kilian <petschge>
2009-02-24 19:35:31 +0000

    Switch from varchar to text and add table for comments.

--- applications/utils/tagstat/dbschema.sql
+++ applications/utils/tagstat/dbschema.sql
@@ -4,22 +4,44 @@
 -- ------------------------------------------------------
 -- Server version	5.0.67
 
-USE tagstat;
+--
+-- Table structure for table `tagcomments`
+--
+
+DROP TABLE IF EXISTS `tagcomments`;
+CREATE TABLE `tagcomments` (
+  `id` int(11) NOT NULL auto_increment,
+  `tag` text,
+  `comment` text,
+  `score` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `tagpairs`
+--
 
 DROP TABLE IF EXISTS `tagpairs`;
 CREATE TABLE `tagpairs` (
   `id` int(11) NOT NULL auto_increment,
-  `tag` varchar(166) default NULL,
-  `value` varchar(166) default NULL,
+  `tag` text,
+  `value` text,
   `count` int(11) default NULL,
   PRIMARY KEY  (`id`),
-  KEY `tag_idx` (`tag`,`value`)
-) ENGINE=MyISAM DEFAULT CHARSET=UTF8;
+  KEY `tag_idx` (`tag`(12),`value`(12)),
+  KEY `count_ind` (`count`)
+) ENGINE=MyISAM AUTO_INCREMENT=4633729 DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `tags`
+--
 
 DROP TABLE IF EXISTS `tags`;
 CREATE TABLE `tags` (
-  `tag` varchar(256) default NULL,
+  `tag` text NOT NULL,
   `uses` int(11) default NULL,
   `uniq_values` int(11) default NULL,
-  PRIMARY KEY (`tag`)
-) ENGINE=MyISAM DEFAULT CHARSET=UTF8;
+  PRIMARY KEY  (`tag`(255)),
+  KEY `uses_ind` (`uses`),
+  FULLTEXT KEY `tag` (`tag`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;

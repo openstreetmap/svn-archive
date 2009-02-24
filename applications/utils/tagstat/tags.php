519fd8be2f3941c5c904b3bb868e48038c3af870
Patrick Kilian <petschge>
2009-02-24 19:28:27 +0000

    Clean up the code in tags.php a bit.

--- applications/utils/tagstat/tags.php
+++ applications/utils/tagstat/tags.php
@@ -11,18 +11,12 @@
 	include("config.php");
 	include("func.php");
 
-	$limit = getValueFromRequest("limit", 25);
-	if(getValueFromRequest("rev") != "true") {
-		$dir = "DESC";
-	} else {
-		$dir = "ASC";
-	}
+	$limit = getIntFromRequest("limit", 25);
+	$skip = getIntFromRequest("skip", 25);
+	$dir = getValueFromRequest("dir", "DESC", array("ASC", "DESC"));
 
-	if($dir == "DESC") {
-		echo "<H1>Popular tags</H1>\n";
-	} else {
-		echo "<H1>Least popular tags</H1>\n";
-	}
+	$invdir = flipDir($dir);
+	echo "<H1>".getPopWord($dir)." tags</H1>\n";
 
 	$dsn = "mysqli://$user:$passwd@$host/$db";
 	$conn =& DB::connect($dsn);
@@ -31,7 +25,7 @@
 	}
 	$conn->query("SET NAMES utf8");
 
-	$result =& $conn->query("SELECT tag, uses FROM tags ORDER BY uses $dir LIMIT $limit");
+	$result =& $conn->query("SELECT tag, uses FROM tags ORDER BY uses $dir LIMIT $skip, $limit");
 	if (DB::isError($result)) {
 		die ("SELECT failed: " . $result->getMessage() . "\n");
 	}
@@ -49,7 +43,7 @@
 		} else {
 			$style="even";
 		}
-		$rank = get_tag_rank($conn, $row[1]);
+		$rank = getTagRank($conn, $row[1]);
 		printf('<TR class="%1$s"><TD>%4$s</TD><TD><A href="tagdetails.php?tag=%2$s">%5$s</A></TD><TD class="count">%3$s</TD>', $style, $row[0], displayNum($row[1]), $rank, displayTag($row[0]));
 		if($dir == "ASC") {
 			$getsuggestion =& $conn->prepare("SELECT tag, uses FROM tags WHERE SOUNDEX(tag) = SOUNDEX(?) AND tag <> ?ORDER BY uses DESC LIMIT 1");
@@ -71,17 +65,13 @@
 	$result->free();
 	$conn->disconnect();
 
-	if($dir == "DESC") {
-		echo "<A href=\"tags.php?limit=$limit&rev=true\">Rare tags</A>\n";
-	} else {
-		echo "<A href=\"tags.php?limit=$limit\">Popular tags</A>\n";
-	}
-	$limit += 25;
-	if($dir == "DESC") {
-		echo "<A href=\"tags.php?limit=$limit\">More tags</A>\n";
-	} else {
-		echo "<A href=\"tags.php?limit=$limit&rev=true\">More tags</A>\n";
-	}
+	$next = $skip + $limit;
+	echo "<A href=\"tags.php?limit=$limit&skip=$next&dir=$dir\">".getOrdWord($dir)." tags</A>\n";
+	$prev = max($skip - $limit, 0);
+	echo "<A href=\"tags.php?limit=$limit&skip=$prev&dir=$dir\">".getOrdWord($invdir)." tags</A>\n";
+	$more = $limit + 25;
+	echo "<A href=\"tags.php?limit=$more&skip=$skip\">More tags</A>\n";
+	echo "<A href=\"tags.php?limit=$limit&skip=$skip&dir=$invdir\">".getPopWord($invdir)." tags</A>\n";
 	echo "<A href=\"index.php\">Back to index page</A>\n";
 ?>
  </BODY>

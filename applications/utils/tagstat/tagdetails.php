1dbc73bc8b40a1f6f2fd28656fa5571725a147ba
Patrick Kilian <petschge>
2009-02-24 19:45:57 +0000

    Cleanup tagdetails.php and allow sorting by node/way/relation usage.

--- applications/utils/tagstat/tagdetails.php
+++ applications/utils/tagstat/tagdetails.php
@@ -13,18 +13,14 @@
 
 	$tag = getValueFromRequest("tag");
 	$tag_html = htmlspecialchars($tag);
-	$limit = getValueFromRequest("limit", 25);
-	if(getValueFromRequest("rev") != "true") {
-		$dir = "DESC";
-	} else {
-		$dir = "ASC";
-	}
+	$limit = getIntFromRequest("limit", 25);
+	$skip = getIntFromRequest("skip", 0);
+	$dir = getValueFromRequest("dir", "DESC", array("ASC", "DESC"));
+	$order = getValueFromRequest("order", "total", array("node", "way", "relation", "total"));
 
-	if($dir == "DESC") {
-		echo "<H1>Most popular value for tag &quot;$tag_html&quot;</H1>\n";
-	} else {
-		echo "<H1>Least popular value for tag &quot;$tag_html&quot;</H1>\n";
-	}
+	$invdir = flipDir($dir);
+	echo "<H1>".getPopWord($dir)." values for tag &quot;$tag_html&quot;</H1>\n";
+
 	$dsn = "mysqli://$user:$passwd@$host/$db";
 	$conn =& DB::connect($dsn);
 	if (DB::isError($conn)) {
@@ -54,16 +50,25 @@
 
 
 	echo "<H3>Stats</H3>\n";
-	$getcount =& $conn->prepare("SELECT tag, value, count FROM tagpairs WHERE tag = ? ORDER BY count $dir LIMIT $limit");
+	$getcount =& $conn->prepare("SELECT tag, value, c_total, c_node, c_way, c_relation FROM tagpairs WHERE tag = ? ORDER BY c_$order $dir LIMIT $skip, $limit");
 	$result =& $conn->execute($getcount, $tag);
 	if (DB::isError($result)) {
 		die ("SELECT failed: " . $result->getMessage() . "\n");
 	}
+	$dirA = array("total" => $dir, "node" => $dir, "way" => $dir, "relation" => $dir);
+	$dirA[$order] = $invdir;
 	echo "<TABLE>\n";
-	echo "<TR><TH>tag</TH><TH>value</TH><TH>uses</TH>";
+	echo "<TR>";
+	echo "<TH>tag</TH>";
+	echo "<TH>value</TH>";
+	echo "<TH><A href=\"tagdetails.php?tag=$tag&limit=$limit&skip=$skip&dir={$dirA["total"]}&order=total\">uses</A></TH>";
+	echo "<TH><A href=\"tagdetails.php?tag=$tag&limit=$limit&skip=$skip&dir={$dirA["node"]}&order=node\">node</A></TH>";
+	echo "<TH><A href=\"tagdetails.php?tag=$tag&limit=$limit&skip=$skip&dir={$dirA["way"]}&order=way\">way</A></TH>";
+	echo "<TH><A href=\"tagdetails.php?tag=$tag&limit=$limit&skip=$skip&dir={$dirA["relation"]}&order=relation\">relation</A></TH>";
 	if($dir == "ASC") {
 		echo "<TH>suggestion</TH>";
 	}
+	echo "<TH>link</TH>\n";
 	echo "</TR>\n";
 	$i = 1;
 	while ($row =& $result->fetchRow()) {
@@ -72,9 +77,9 @@
 		} else {
 			$style="even";
 		}
-		printf('<TR class="%1$s"><TD>%2$s</TD><TD>%3$s</TD><TD class="count">%4$s</TD>', $style, displayTag($row[0]), displayTag($row[1]), displayNum($row[2]));
+		printf('<TR class="%1$s"><TD>%2$s</TD><TD>%3$s</TD><TD class="count">%4$s</TD><TD class="count">%5$s</TD><TD class="count">%6$s</TD><TD class="count">%7$s</TD>', $style, displayTag($row[0]), displayTag($row[1]), displayNum($row[2]), displayNum($row[3]), displayNum($row[4]), displayNum($row[5]));
 		if($dir == "ASC") {
-			$getsuggestion =& $conn->prepare("SELECT value, count FROM tagpairs WHERE tag = ? AND SOUNDEX(value) = SOUNDEX(?) AND value <> ? ORDER BY count DESC LIMIT 1");
+			$getsuggestion =& $conn->prepare("SELECT value, c_total FROM tagpairs WHERE tag = ? AND SOUNDEX(value) = SOUNDEX(?) AND value <> ? ORDER BY c_total DESC LIMIT 1");
 			$result2 =& $conn->execute($getsuggestion, array($row[0], $row[1], $row[1]));
 			if (!DB::isError($result2) && $result2->numRows() == 1) {
 				$row2 =& $result2->fetchRow();
@@ -84,12 +89,44 @@
 					echo "<TD>&nbsp;</TD>";
 				}
 				$result2->free();
+			} else {
+				echo "<TD>&nbsp;</TD>";
 			}
 		}
+		echo "<TD><A href=\"http://www.informationfreeway.org/api/0.5/*[{$row[0]}={$row[1]}]\">xapi</A></TD>\n";
 		echo "</TR>\n";
 		$i++;
 	}
 	echo "</TABLE>\n";
+	$result->free();
+
+	$getrelated =& $conn->prepare("SELECT tag1, tag2, c_total FROM tagfriends WHERE tag1=? OR tag2=? AND c_total > 0 ORDER BY c_total DESC LIMIT 10");
+	$result =& $conn->execute($getrelated, array($tag, $tag));
+	if (DB::isError($result)) {
+		echo "Sorry, can't retrieve related tags";
+	} else if ($result->numRows() > 0) {
+		echo "<H3>Related Tags</H3>\n";
+		echo "<TABLE>\n";
+		echo "<TR><TH>tag</TH><TH>uses</TH></TR>\n";
+		$i = 1;
+		while($row =& $result->fetchRow()) {
+			if($i % 2) {
+				$style="odd";
+			} else {
+				$style="even";
+			}
+			$tag1 = $row[0];
+			$tag2 = $row[1];
+			if($tag1 != $tag) {
+				$tagother = $tag1;
+			} else {
+				$tagother = $tag2;
+			}
+			printf('<TR class="%1$s"><TD><A href="tagdetails.php?tag=%2$s">%3$s</A></TD><TD class="count">%4$s</TD></TR>', $style, $tagother, displayTag($tagother), displayNum($row[2]));
+			echo "\n";
+		}
+		echo "</TABLE>\n";
+	}
 	$result->free();
 
 	$getfurther =& $conn->prepare("SELECT id, comment FROM tagcomments WHERE tag=? ORDER BY score DESC LIMIT 11");
@@ -109,24 +146,21 @@
 			echo "<DIV>\n";
 			echo "<BR>\n";
 		}
+		echo "<A href=\"viewcomments.php?tag=$tag\">View all comments</A><BR>\n";
+		echo "<BR><BR>\n";
 	}
-	echo "<A href=\"viewcomments.php?tag=$tag\">View all comments</A><BR>\n";
-	echo "<BR><BR>\n";
 	$result->free();
 
 	$conn->disconnect();
 
-	if($dir == "DESC") {
-		echo "<A href=\"tagdetails.php?tag=$tag&limit=$limit&rev=true\">Rare values</A>\n";
-	} else {
-		echo "<A href=\"tagdetails.php?tag=$tag&limit=$limit\">Popular values</A>\n";
-	}
-	$limit += 25;
-	if($dir == "DESC") {
-		echo "<A href=\"tagdetails.php?tag=$tag&limit=$limit\">More values</A>\n";
-	} else {
-		echo "<A href=\"tagdetails.php?tag=$tag&limit=$limit&rev=true\">More values</A>\n";
-	}
+	echo "<BR><BR>\n";
+	$next = $skip + $limit;
+	echo "<A href=\"tagdetails.php?tag=$tag&limit=$limit&skip=$next&dir=$dir&order=$order\">".getOrdWord($dir)." values</A>\n";
+	$prev = max($skip - $limit, 0);
+	echo "<A href=\"tagdetails.php?tag=$tag&limit=$limit&skip=$prev&dir=$dir&order=$order\">".getOrdWord($invdir)." values</A>\n";
+	$more = $limit + 25;
+	echo "<A href=\"tagdetails.php?tag=$tag&limit=$more&skip=$skip&dir=$dir&order=$order\">More values</A>\n";
+	echo "<A href=\"tagdetails.php?tag=$tag&limit=$limit&skip=$skip&dir=$invdir&order=$order\">".getPopWord($invdir)." values</A>\n";
 	echo "<A href=\"index.php\">Back to index page</A>\n";
 ?>
  </BODY>

3f89bffe5f27f2dfa7e9fc6011a1d4c942e1bece
Tom Hughes <tom@compton.nu>
2009-05-08 15:16:14 +0000

    Remove the api06 branch.

--- sites/rails_port_branches/api06/app/helpers/browse_helper.rb
+++
@@ -1,5 +0,0 @@
-module BrowseHelper
-  def link_to_page(page, page_param)
-    return link_to(page, page_param => page)
-  end
-end
--- sites/rails_port_branches/api06/app/models/way_node.rb
+++
@@ -1,8 +0,0 @@
-class WayNode < ActiveRecord::Base
-  set_table_name 'current_way_nodes'
-
-  set_primary_keys :id, :sequence_id
-  belongs_to :node
-
-  belongs_to :way, :foreign_key => :id
-end

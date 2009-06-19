75be194c1f9a1d466bfb0b940d9cf886fedbb9b2
Dirk Stöcker <openstreetmap@dstoecker.de>
2009-06-19 19:55:09 +0000

    updated

--- applications/editors/josm/i18n/specialmessages.java
+++ applications/editors/josm/i18n/specialmessages.java
@@ -7,4 +7,5 @@ tr("You requested too many nodes (limit is 50,000). Either request a smaller are
 tr("Database offline for maintenance"); // OSM server message
 tr("The maximum bbox size is 0.25, and your request was too large. Either request a smaller area, or use planet.osm"); // OSM server message
 tr("could not get audio input stream from input URL"); // Java message loading audio data
+tr("Audio Device Unavailable"); // Java message loading audio data
 tr("You must make your edits public to upload new data"); // use has not made edits public

c51f9a6692563b2d2304f7f9564f8a10d4317d9f
Dirk Stöcker <openstreetmap@dstoecker.de>
2009-07-09 08:39:54 +0000

    added launchpad support

---
+++ applications/editors/josm/i18n/launchpad.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+die "URL not given." if $#ARGV != 0;
+
+mkdir "new";
+die "Could not change into new data dir." if !chdir "new";
+system "wget $ARGV[0]";
+system "tar -xf laun*";
+chdir "..";
+foreach my $name (split("\n", `find new -name "*.po"`))
+{
+  my $a=$name;
+  $a =~ s/.*-//;
+  system "mv -v $name po/$a" if -f "po/$a";
+}
+system "ant";
+my $outdate = `date -u +"%Y-%m-%dT%H_%M_%S"`;
+chomp $outdate;
+mkdir "keys";
+system "cp po/*.po keys";
+system "cp po/josm.pot keys/keys.pot";
+system "tar -czf launchpad_upload_josm_$outdate.tgz keys";
+system "rm -rv keys new";

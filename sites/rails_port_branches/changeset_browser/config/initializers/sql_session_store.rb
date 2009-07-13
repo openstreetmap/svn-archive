1a0193082fe07fa6a54a5ecd14c7bfa10eac2249
Tom Hughes <tom@compton.nu>
2009-07-13 23:53:20 +0000

    Remove changeset browser branch.

--- sites/rails_port_branches/changeset_browser/config/initializers/sql_session_store.rb
+++
@@ -1,7 +0,0 @@
-# Work out which session store adapter to use
-environment = Rails.configuration.environment
-adapter = Rails.configuration.database_configuration[environment]["adapter"]
-session_class = adapter + "_session"
-
-# Configure SqlSessionStore
-SqlSessionStore.session_class = session_class.camelize.constantize

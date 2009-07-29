a2c93b620ea30d22f70fd3ac8d09b1cada3a290f
Thomas Wood <grand.edgemaster@gmail.com>
2009-07-29 22:22:56 +0000

    Upload heavily refactored version of bulk_upload.py

--- applications/utils/import/bulk_upload_06/bulk_upload.py
+++ applications/utils/import/bulk_upload_06/bulk_upload.py
@@ -18,7 +18,8 @@
 # If you change $input.osm between calls to the script (ie different data with the
 # same file name) you should delete $input.osm.db
 #
-# Author: Steve Singer <ssinger_pg@sympatico.ca>
+# Authors: Steve Singer <ssinger_pg@sympatico.ca>
+#          Thomas Wood <grand.edgemaster@gmail.com>
 #
 # COPYRIGHT
 #
@@ -39,86 +40,247 @@
 
 
 import xml.etree.cElementTree as ET
-import sets
-import optparse
 import httplib2
 import pickle
 import os
 
-api_host='http://api.openstreetmap.org'
-#api_host='http://api06.dev.openstreetmap.org'
+#api_host='http://api.openstreetmap.org'
+api_host='http://api06.dev.openstreetmap.org'
 headers = {
     'User-Agent' : 'bulk_upload.py',
 }
 
 
 class XMLException(Exception): pass
+class APIError(Exception): pass
 
 class ImportProcessor:
-    def __init__(self,httpObj,comment,idMap,changesetid=None):
-        self.comment=comment
-        self.addElem=ET.Element('create')
-        self.modifyElem=ET.Element('modify')
-        self.deleteElem=ET.Element('delete')
+    currentChangeset = None
+    relationStore = []
+    idMap = None
+
+    def __init__(self,user,password,idMap,tags={}):
+        self.httpObj = httplib2.Http()
+        self.httpObj.add_credentials(user,password)
         self.idMap = idMap
-        self.httpCon = httpObj
-        if changesetid:
-            self.changesetid = changesetid
+        self.tags = tags
+        self.createChangeset()
+
+
+    def parse(self, infile):
+        osmData=ET.parse(infile)
+        osmRoot = osmData.getroot()
+        if osmRoot.tag != "osm":
+            raise XMLException("Input file must be a .osm XML file (JOSM-style)")
+    
+        for type in ('node','way','relation'):
+            for elem in osmRoot.getiterator(type):
+                # If elem.id is already mapped we can skip this object
+                #
+                id=elem.attrib['id']
+                if self.idMap[type].has_key(id):
+                    continue
+                #
+                # If elem contains nodes, ways or relations as a child
+                # then the ids need to be remapped.
+                if elem.tag=='way':
+                    for child in elem.getiterator('nd'):
+                        if child.attrib.has_key('ref'):
+                            old_id=child.attrib['ref']
+                            if idMap['node'].has_key(old_id):
+                                child.attrib['ref'] = self.idMap['node'][old_id]
+                elif elem.tag=='relation':
+                    for child in elem.getiterator('member'):
+                        if child.attrib.has_key('ref'):
+                            old_id=child.attrib['ref']
+                            old_id_type = child.attrib['type']
+                            if self.idMap[old_id_type].has_key(old_id):
+                                child.attrib['ref'] = self.idMap[old_id_type][old_id]
+                            if old_id_type == "relation" and elem not in self.relationStore:
+                                self.relationStore.append(elem)
+
+                if elem not in self.relationStore:
+                    self.addToChangeset(elem)
+
+        for relation in self.relationStore:
+            self.addToChangeset(relation)
+
+        self.currentChangeset.close() # (uploads any remaining diffset changes)
+
+    def createChangeset(self):
+        self.currentChangeset = Changeset(tags=self.tags, idMap=self.idMap, httpObj=self.httpObj)
+
+    def addToChangeset(self, elem):
+        if elem.attrib.has_key('action'):
+            action = elem.attrib['action']
         else:
-            self.createChangeSet()
-    def createChangeSet(self):
-        createReq=ET.Element('osm',version="0.6")
-        change=ET.Element('changeset')
-        change.append(ET.Element('tag',k='comment',v=self.comment))
-        change.append(ET.Element('tag',k='created_by', v='bulk_upload.py'))
-        createReq.append(change)
-        xml=ET.tostring(createReq)
-        resp,content=self.httpCon.request(api_host +
-                                          '/api/0.6/changeset/create','PUT',xml,headers=headers)
+            action = 'create'
+
+        try:
+            self.currentChangeset.addChange(action, elem)
+        except ChangesetClosed:
+            self.createChangeset()
+            self.currentChangeset.addChange(action, elem)
+
+class IdMap:
+    # Default IdMap class, using a Pickle backend, this can be extended
+    # - if ids in other files need replacing, for example
+    idMap = {'node':{}, 'way':{}, 'relation':{}}
+
+    def __init__(self, filename=''):
+        self.filename = filename
+        self.load()
+
+    def __getitem__(self, item):
+        return self.idMap[item]
+
+    def load(self):
+        try:
+            if os.stat(self.filename):
+                f=open(self.filename, "r")
+                self.idMap=pickle.load(f)
+                f.close()
+        except:
+            pass
+
+    def save(self):
+        f=open(self.filename+".tmp","w")
+        pickle.dump(self.idMap,f)
+        f.close()
+        os.rename(self.filename+".tmp", self.filename)
+
+class ChangesetClosed(Exception): pass
+
+class Changeset:
+    id = None
+    tags = {}
+    currentDiffSet = None
+    opened = False
+    closed = False
+    
+    itemcount = 0
+
+    def __init__(self,tags={},idMap=None, httpObj=None):
+        self.id = None
+        self.tags = tags
+        self.idMap = idMap
+        self.httpObj = httpObj
+        
+        self.createDiffSet()
+
+    def open(self):
+        createReq = ET.Element('osm', version="0.6")
+        change = ET.SubElement(createReq, 'changeset')
+        for tag in self.tags:
+            ET.SubElement(change, 'tag', k=tag, v=self.tags[tag])
+        
+        xml = ET.tostring(createReq)
+        resp,content = self.httpObj.request(api_host +
+            '/api/0.6/changeset/create','PUT',xml,headers=headers)
         if resp.status != 200:
-            print 'Error creating changeset:' + str(resp.status)
-            exit(-1)
-        self.changesetid=content
-    def addItem(self,item):
-        item.attrib['changeset']=self.changesetid
-        self.addElem.append(item)
-    def deleteItem(self,item):
-        item.attrib['changeset']=self.changesetid
-        self.deleteElem.append(item)
-    def modifyItem(self,item):
-        item.attrib['changeset']=self.changesetid
-        self.modifyElem.append(item)
+            raise APIError('Error creating changeset:' + str(resp.status))
+        self.id = content
+        print "Created changeset: %s" % self.id
+        self.opened = True
+
+    def close(self):
+        if not self.opened:
+            return
+        self.currentDiffSet.upload()
+        
+        resp,content = self.httpObj.request(api_host +
+            '/api/0.6/changeset/' +
+            self.id + '/close','PUT',headers=headers)
+        if resp.status != 200:
+            print "Error closing changeset " + str(self.id) + ":" + str(resp.status)
+        print "Closed changeset: %s" % self.id
+        self.closed = True
+
+    def createDiffSet(self):
+        self.currentDiffSet = DiffSet(self, self.idMap, self.httpObj)
+
+    def addChange(self,action,item):
+        if not self.opened:
+            self.open() # So that a changeset is only opened when required.
+        if self.closed:
+            raise ChangesetClosed
+        item.attrib['changeset']=self.id
+        try:
+            self.currentDiffSet.addChange(action,item)
+        except DiffSetClosed:
+            self.createDiffSet()
+            self.currentDiffSet.addChange(action,item)
+        
+        self.itemcount += 1
+        if self.itemcount >= self.getItemLimit():
+            self.currentDiffSet.upload()
+            self.close()
+
+    def getItemLimit(self):
+        # This is actually dictated by the API's capabilities call
+        return 50000
+
+class DiffSetClosed(Exception): pass
+
+class DiffSet:
+    itemcount = 0
+    closed = False
+    
+    def __init__(self, changeset, idMap, httpObj):
+        self.elems = {
+            'create': ET.Element('create'),
+            'modify': ET.Element('modify'),
+            'delete': ET.Element('delete')
+        }
+        self.changeset = changeset
+        self.idMap = idMap
+        self.httpObj = httpObj
+        print "new diffset: %s" % self.itemcount
+
+    def __getitem__(self, item):
+        return self.elems[item]
+
+    def addChange(self,action,item):
+        if self.closed:
+            raise DiffSetClosed
+        self[action].append(item)
+
+        self.itemcount += 1
+        if self.itemcount >= self.getItemLimit():
+            print self.itemcount
+            print self.getItemLimit()
+            self.upload()
+
     def upload(self):
+        if not self.itemcount:
+            return False
+    
         xml = ET.Element('osmChange')
-        xml.append(self.addElem)
-        xml.append(self.modifyElem)
-        xml.append(self.deleteElem)
-        print "Uploading to changeset:" + self.changesetid
+        for elem in self.elems.values():
+            xml.append(elem)
+        print "Uploading to changeset %s" % self.changeset.id
 
         xmlstr = ET.tostring(xml)
-        #f = open("/tmp/%s.osc" % self.changesetid, 'a')
+        #f = open("/tmp/%s.osc" % self.changeset.id, 'a')
         #f.write(xmlstr)
         #f.write("\n\n")
         #f.close()
 
-        resp,content = self.httpCon.request(api_host +
-                                            '/api/0.6/changeset/'+self.changesetid+
+        resp,content = self.httpObj.request(api_host +
+                                            '/api/0.6/changeset/'+self.changeset.id+
                                             '/upload',
                                             'POST', xmlstr,headers=headers)
         if resp.status != 200:
             print "Error uploading changeset:" + str(resp.status)
             print content
+            print self.idMap.idMap
             exit(-1)
         else:
             self.processResult(content)
-    def closeSet(self):
-        resp,content=self.httpCon.request(api_host +
-                                          '/api/0.6/changeset/' +
-                                          self.changesetid + '/close','PUT',headers=headers)
-        if resp.status != 200:
-            print "Error closing changeset " + str(self.changesetid) + ":" + str(resp.status)
-    #
-    # Uploading a change set returns a <diffResult> containing elements
+            self.idMap.save()
+            self.closed = True
+
+    # Uploading a diffset returns a <diffResult> containing elements
     # that map the old id to the new id
     # Process them.
     def processResult(self,content):
@@ -128,116 +290,48 @@ class ImportProcessor:
             old_id=child.attrib['old_id']
             if child.attrib.has_key('new_id'):
                 new_id=child.attrib['new_id']
-                idMap[id_type][old_id]=new_id
+                self.idMap[id_type][old_id]=new_id
             else:
                 # (Object deleted)
-                idMap[id_type][old_id]=old_id
+                self.idMap[id_type][old_id]=old_id
 
-    def getDiffUploadLimit(self):
+    def getItemLimit(self):
+        # This is an arbitrary self-imposed limit (that must be below the changeset limit)
+        # so to limit upload times to sensible chunks.
         return 1000
 
-    def getChangesetLimit(self):
-        return 50000
 
-# Allow enforcing of required arguements
-# code from http://www.python.org/doc/2.3/lib/optparse-extending-examples.html
-class OptionParser (optparse.OptionParser):
+if __name__ == "__main__":
+    import optparse
+    # Allow enforcing of required arguements
+    # code from http://www.python.org/doc/2.3/lib/optparse-extending-examples.html
+    class OptionParser (optparse.OptionParser):
 
-    def check_required (self, opt):
-      option = self.get_option(opt)
+        def check_required (self, opt):
+            option = self.get_option(opt)
 
-      # Assumes the option's 'default' is set to None!
-      if getattr(self.values, option.dest) is None:
-          self.error("%s option not supplied" % option)
-    
+            # Assumes the option's 'default' is set to None!
+            if getattr(self.values, option.dest) is None:
+                self.error("%s option not supplied" % option)
 
+    usage = "usage: %prog -i input.osm -u user -p password"
 
-usage = "usage: %prog -i input.osm -u user -p password"
+    parser = OptionParser(usage)
+    parser.add_option("-i", "--input", dest="infile", help="read data from input.osm")
+    parser.add_option("-u", "--user", dest="user", help="username")
+    parser.add_option("-p", "--password", dest="password", help="password")
+    parser.add_option("-c", "--comment", dest="comment", help="ChangeSet Comment")
+    (options, args) = parser.parse_args()
 
-parser = OptionParser(usage)
-parser.add_option("-i", "--input", dest="infile", help="read data from input.osm")
-parser.add_option("-u", "--user", dest="user", help="username")
-parser.add_option("-p", "--password", dest="password", help="password")
-parser.add_option("-c", "--comment", dest="comment", help="ChangeSet Comment")
-(options, args) = parser.parse_args()
- 
-parser.check_required("-i")
-parser.check_required("-u")
-parser.check_required("-p") 
-parser.check_required("-c")
+    parser.check_required("-i")
+    parser.check_required("-u")
+    parser.check_required("-p")
+    parser.check_required("-c")
 
-osmData=ET.parse(options.infile)
-osmRoot = osmData.getroot()
-if osmRoot.tag != "osm":
-    raise XMLException("Input file must be a .osm XML file (JOSM-style)")
-
-httpObj = httplib2.Http()
-httpObj.add_credentials(options.user,options.password)
-idMap={'node': {}, 'way': {}, 'relation': {}}
-dcnt = 0
-ccnt = 0
-try:
-    if os.stat(options.infile + ".db"):
-        hasCache=True
-except:
-    hasCache=False
-if hasCache:
-    f=open(options.infile + ".db","r")
-    idMap=pickle.load(f)
-    f.close()
-
-importProcessor=ImportProcessor(httpObj,options.comment,idMap)
-for type in ('node','way','relation'):
-    for elem in osmRoot.getiterator(type):
-        # If elem.id is already mapped we can skip this object
-        #
-        id=elem.attrib['id']
-        if idMap[type].has_key(id):
-                continue
-        #
-        # If elem contains nodes, ways or relations as a child
-        # then the ids need to be remapped.
-        if elem.tag=='way':
-            for child in elem.getiterator('nd'):
-                if child.attrib.has_key('ref'):
-                    old_id=child.attrib['ref']
-                    if idMap['node'].has_key(old_id):
-                        child.attrib['ref'] = idMap['node'][old_id]
-        elif elem.tag=='relation':
-            for child in elem.getiterator('member'):
-                if child.attrib.has_key('ref'):
-                    old_id=child.attrib['ref']
-                    old_id_type = child.attrib['type']
-                    if idMap[old_id_type].has_key(old_id):
-                        child.attrib['ref'] = idMap[old_id_type][old_id]
-        if elem.attrib.has_key('action') and elem.attrib['action']=='delete':
-            importProcessor.deleteItem(elem)
-        elif elem.attrib.has_key('action') and elem.attrib['action']=='modify':
-            importProcessor.modifyItem(elem)
-        else:
-            importProcessor.addItem(elem)
-
-        dcnt += 1
-        ccnt += 1
-        if (dcnt >= importProcessor.getDiffUploadLimit()
-            or ccnt >= importProcessor.getChangesetLimit()):
-            importProcessor.upload()
-            f=open(options.infile+".db.tmp","w")
-            pickle.dump(idMap,f)
-            f.close()
-            os.rename(options.infile+".db.tmp", options.infile+".db")
-            dcnt = 0
-            if ccnt >= importProcessor.getChangesetLimit():
-                importProcessor.closeSet()
-                importProcessor=ImportProcessor(httpObj,options.comment,idMap)
-                ccnt=0
-            else:
-                changesetid = importProcessor.changesetid
-                importProcessor = ImportProcessor(httpObj,options.comment,idMap,changesetid)
-    #for
-importProcessor.upload()
-f=open(options.infile+".db.tmp","w")
-pickle.dump(idMap,f)
-f.close()
-os.rename(options.infile+".db.tmp", options.infile+".db")
-importProcessor.closeSet()
+    idMap = IdMap(options.infile + ".db")
+    tags = {
+        'created_by': 'bulk_upload.py',
+        'comment': options.comment
+    }
+    importProcessor = ImportProcessor(options.user,options.password,idMap,tags)
+    importProcessor.parse(options.infile)

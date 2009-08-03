c302baac057f4ed0a1f9d588719f6c06c924a58e
Jochen Topf <jochen@remote.org>
2009-08-03 08:36:55 +0000

    Added Heilbronn

--- sites/www.openstreetmap.de/var/www/www.openstreetmap.de/js/markers.js
+++ sites/www.openstreetmap.de/var/www/www.openstreetmap.de/js/markers.js
@@ -2,59 +2,59 @@
 
 function createMarkers() {
     /* news */
-    addMarker(layer_events, -8.5534518, 52.6688727, 
-        '<p><b><a href="http://www.stateofthemap.org/">State of the Map 2008</a></b><br/>Jährliche OpenStreetMap-Konferenz<br/>12.-13. Juli 2008<br/>Limerick, Irland</p>'
-    );
 
     /* events */
 
     /* local groups */
     addMarker(layer_local, 9.146, 49.974,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/Aschaffenburg">Aschaffenburg</a></b></p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/Aschaffenburg">Aschaffenburg</a></b></p>'
     );
     addMarker(layer_local, 13.3172, 52.5138,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/Berlin">Berlin</a></b></p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/Berlin">Berlin</a></b></p>'
     );
     addMarker(layer_local, 10.556111, 52.319167,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/Braunschweig">Braunschweig</a></b><br/>Treffen jeden 1. Dienstag im Monat<br/><a href="https://mail.atekon.de/mailman/listinfo/osm-bs">Mailingliste</a></p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/Braunschweig">Braunschweig</a></b><br/>Treffen jeden 1. Dienstag im Monat<br/><a href="https://mail.atekon.de/mailman/listinfo/osm-bs">Mailingliste</a></p>'
     );
     addMarker(layer_local, 13.71299, 51.02516,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/DresdnerOSMStammtisch">Dresden</a></b><br/>Treffen jeden 1. Donnerstag im Monat</p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/DresdnerOSMStammtisch">Dresden</a></b><br/>Treffen jeden 1. Donnerstag im Monat</p>'
     );
     addMarker(layer_local, 7.0123, 51.4552, 
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/Essen">Essen</a></b><br/>Unregelmäßige Treffen</p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/Essen">Essen</a></b><br/>Unregelmäßige Treffen</p>'
     );
     addMarker(layer_local, 8.666, 50.5833,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/Stammtisch_Mittelhessen">Gießen</a> (Mittelhessen)</b><br/>Unregelmäßige Treffen</p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/Stammtisch_Mittelhessen">Gießen</a> (Mittelhessen)</b><br/>Unregelmäßige Treffen</p>'
     );
     addMarker(layer_local, 9.99, 53.57,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/Hamburger_Mappertreffen">Hamburg</a></b><br/>Monatliche Treffen</p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/Hamburger_Mappertreffen">Hamburg</a></b><br/>Monatliche Treffen</p>'
     );
     addMarker(layer_local, 9.739, 52.375,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/Stammtisch_Hannover">Hannover</a></b><br/>Monatliche Treffen</p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/Stammtisch_Hannover">Hannover</a></b><br/>Monatliche Treffen</p>'
+    );
+    addMarker(layer_local, 9.20677, 49.13925
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/OSM-Treff_Heilbronn">Heilbronn</a></b><br/>Monatliche Treffen</p>'
     );
     addMarker(layer_local, 8.403978, 49.009192,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/Karlsruhe">Karlsruhe</a></b><br/>Unregelmäßige Treffen<br/><a href="http://www.remote.org/mailman/listinfo/ka-geo/">Mailingliste</a></p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/Karlsruhe">Karlsruhe</a></b><br/>Unregelmäßige Treffen<br/><a href="http://www.remote.org/mailman/listinfo/ka-geo/">Mailingliste</a></p>'
     );
     addMarker(layer_local, 10.414, 53.252,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/Lüneburg">Lüneburg</a></b><br/>Unregelmäßige Treffen<br/><a href="http://lists.openstreetmap.de/cgi-bin/mailman/listinfo/lueneburg">Mailingliste</a></p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/Lüneburg">Lüneburg</a></b><br/>Unregelmäßige Treffen<br/><a href="http://lists.openstreetmap.de/cgi-bin/mailman/listinfo/lueneburg">Mailingliste</a></p>'
     );
     addMarker(layer_local, 11.58843, 48.1227,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/München">München</a></b><br/>Treffen jeden 3. Dienstag im Monat</p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/München">München</a></b><br/>Treffen jeden 3. Dienstag im Monat</p>'
     );
     addMarker(layer_local, 9.5794793, 48.979546,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/Murrhardt">Murrhardt</a></b></p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/Murrhardt">Murrhardt</a></b></p>'
     ); 
     addMarker(layer_local, 10.99603, 49.47368,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/NFE-Treffen">Nürnberg/Fürth/Erlangen</a></b><br/>Unregelmäßige Treffen</p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/NFE-Treffen">Nürnberg/Fürth/Erlangen</a></b><br/>Unregelmäßige Treffen</p>'
     ); 
     addMarker(layer_local, 8.05, 52.27,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/Osnabrück">Osnabrück</a></b></p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/Osnabrück">Osnabrück</a></b></p>'
     ); 
     addMarker(layer_local, 8.38, 51.904,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/OWL-Treffen">Ost-Westfalen/Lippe</a></b><br/>Unregelmäßige Treffen, <a href="http://hydra.gt.owl.de/cgi-bin/mailman/listinfo/osm">Mailingliste</a></p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/OWL-Treffen">Ost-Westfalen/Lippe</a></b><br/>Unregelmäßige Treffen, <a href="http://hydra.gt.owl.de/cgi-bin/mailman/listinfo/osm">Mailingliste</a></p>'
     ); 
     addMarker(layer_local, 9.95, 49.8,
-        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/index.php/Würzburg">Würzburg</a></b><br/>Unregelmäßige Treffen</p>'
+        '<p>Lokale Gruppe in<br/><b><a href="http://wiki.openstreetmap.org/wiki/Würzburg">Würzburg</a></b><br/>Unregelmäßige Treffen</p>'
     ); 
 }

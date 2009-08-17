1c2f0078aa6f9560079fc207dcf5d4bc0fa591e4
Steve Singer <ssinger@svn.openstreetmap.org>
2009-08-17 02:32:26 +0000

    Switching default API host back to the live one

--- applications/utils/import/bulk_upload_06/bulk_upload.py
+++ applications/utils/import/bulk_upload_06/bulk_upload.py
@@ -44,8 +44,8 @@ import httplib2
 import pickle
 import os
 
-#api_host='http://api.openstreetmap.org'
-api_host='http://api06.dev.openstreetmap.org'
+api_host='http://api.openstreetmap.org'
+#api_host='http://api06.dev.openstreetmap.org'
 headers = {
     'User-Agent' : 'bulk_upload.py',
 }

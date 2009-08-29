2e391d4fe45193f21e089926d74765ffa84fe7cc
Frederik Ramm <frederik@remote.org>
2009-08-29 20:22:55 +0000

    small osmgrep.pl update

--- applications/utils/filter/osmgrep/osmgrep.pl
+++ applications/utils/filter/osmgrep/osmgrep.pl
@@ -4,24 +4,27 @@
 #
 # called like this:
 #
-# perl oscgrep.pl -t type regex file file...
+# perl oscgrep.pl [-i] [-l] [-t type] [-r regex] file file...
 #
 # where
 #   "type" is either way, node, or relation;
 #   "regex" is any regex
-#   "file" is an .osc file
+#   "file" is an .osm file
 #
-# lists all elements from the .osm file that match the given type, and
+# lists all elements from the .osm file that match the given type and
 # regex (regex is applied against full XML content of the object). If -t or
-# regex are omitted, dumps all types/all content; if file is omitted,
+# -r are omitted, dumps all types/all content; if file is omitted,
 # reads from stdin.
+#
+# -i causes the regex to be evaluated case-insensitively.
+# -l causes the output to have one line per OSM element only.
 #
 # Written by Frederik Ramm <frederik@remote.org>, public domain.
 
 use Getopt::Std;
 my $options = {};
-getopts("a:t:", $options);
-my $grep = shift(@ARGV);
+getopts("ilr:t:", $options);
+my $grep = $options->{r};
 
 while(<>)
 {
@@ -51,6 +54,17 @@ sub out
 {
     my ($type, $content) = @_;
     return if (defined($options->{"t"}) && $options->{"t"} ne $type);
-    return if (defined($grep) && $content !~ /$grep/s);
+    if (defined($grep))
+    {
+        if ($options->{i})
+        {
+            return if (defined($grep) && $content !~ /$grep/si);
+        }
+        else
+        {
+            return if (defined($grep) && $content !~ /$grep/s);
+        }
+    }
+    $content =~ tr/\n/ / if ($options->{l});
     print "$content\n";
 }

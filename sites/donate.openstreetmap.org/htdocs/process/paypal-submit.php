3fc8e39b0689045a15fd0da9a829271591080966
Grant Slater <git@firefishy.com>
2009-09-04 21:36:54 +0000

    update donate.osm.org: Add base for OpenStreetMapS.org purchase fund. CODE UGLY. Text to follow.

--- sites/donate.openstreetmap.org/htdocs/process/paypal-submit.php
+++ sites/donate.openstreetmap.org/htdocs/process/paypal-submit.php
@@ -4,11 +4,10 @@
 lc = AU, DE FR IT GB ES US
 */
 
-//CONNECT to DB
 ob_start();
-$_DB_H = mysql_connect('localhost','osm_donate','password');
-mysql_select_db('osm_donate', $_DB_H);
-mysql_query('SET NAMES \'utf8\'', $_DB_H);
+
+//CONNECT to DB
+include('db-connect.inc.php');
 
 $data = array();
 $data['comment']		= (get_magic_quotes_gpc() ? stripslashes($_POST['comment'])			: $_POST['comment']			);
@@ -17,11 +16,16 @@ $data['anonymous'] 		= ($data['comment-option'] == 'comment' ? '0' : '1');
 $data['amount']			= (get_magic_quotes_gpc() ? stripslashes($_POST['amountGiven'])		: $_POST['amountGiven']		);
 $data['currency']		= (get_magic_quotes_gpc() ? stripslashes($_POST['currency_code'])	: $_POST['currency_code']	);
 
-$sql_insert =	'INSERT INTO `donations` (`amount`,`currency`,`anonymous`,`comment`) VALUES (\''.
+$data['target']		= (get_magic_quotes_gpc() ? stripslashes($_POST['target'])	: $_POST['target']	);
+if (empty($data['target'])) $data['target'] = 'default';
+
+
+$sql_insert =	'INSERT INTO `donations` (`amount`,`currency`,`anonymous`,`comment`,`target`) VALUES (\''.
 					mysql_real_escape_string($data['amount'],		$_DB_H).'\',\''.
 					mysql_real_escape_string($data['currency'],		$_DB_H).'\',\''.
 					mysql_real_escape_string($data['anonymous'],	$_DB_H).'\',\''.
-					mysql_real_escape_string($data['comment'],		$_DB_H).
+					mysql_real_escape_string($data['comment'],		$_DB_H).'\',\''.
+					mysql_real_escape_string($data['target'],		$_DB_H)
 				'\')';
 $sql_insert_result = mysql_query($sql_insert, $_DB_H) OR error_log('SQL FAIL: '.$sql_insert);
 $sql_insert_id = mysql_insert_id($_DB_H);
@@ -32,11 +36,11 @@ $paypal_fields = array();
 $paypal_fields['on0']			= 'contribution_tracking_id';
 $paypal_fields['os0']			= $sql_insert_id;
 $paypal_fields['business']		= 'treasurer@openstreetmap.org';
-$paypal_fields['item_name']		= 'One-time donation: Hardware Fund';
-$paypal_fields['item_number']	= 'DONATE-HW-200901';
+$paypal_fields['item_name']		= 'One-time donation';
+$paypal_fields['item_number']	= 'DONATE-Domain-2009';
 $paypal_fields['cmd']			= '_xclick';
 $paypal_fields['no_note']		= '0';
-$paypal_fields['notify_url']	= 'http://89.145.67.86/process/paypal-callback.php';
+$paypal_fields['notify_url']	= 'http://donate.openstreetmap.org/process/paypal-callback.php';
 $paypal_fields['return']		= 'http://wiki.openstreetmap.org/wiki/DonationThankYou';
 $paypal_fields['currency_code']	= (empty($data['currency']) ?  'GBP' : $data['currency']);
 $paypal_fields['amount']		= (double) $data['amount'];

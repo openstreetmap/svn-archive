ab0a53a2c0d94526429e87c5b437f7d75d520207
Tom Hughes <tom@compton.nu>
2009-10-06 13:09:10 +0000

    Remove blocking branch.

--- sites/rails_port_branches/blocking/app/models/user_role.rb
+++
@@ -1,8 +0,0 @@
-class UserRole < ActiveRecord::Base
-  belongs_to :user
-
-  ALL_ROLES = ['administrator', 'moderator']
-
-  validates_inclusion_of :role, :in => ALL_ROLES
-  validates_uniqueness_of :role, :scope => :user_id
-end

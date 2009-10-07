092fad4084d3758467eca0ab478d50c29d6deb93
Tom Hughes <tom@compton.nu>
2009-10-07 23:06:06 +0000

    First pass at converter for os1 layer.

---
+++ sites/ooc.openstreetmap.org/scripts/os1.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use File::Path;
+use Imager;
+
+our $SrcDir = "/home/ooc/os1";
+our $DstDir = "/home/ooc/os1";
+our @Tiles;
+
+foreach my $x (30 .. 32)
+{
+    foreach my $y (18 .. 21)
+    {
+        process_tile(6, $x, $y);
+    }
+}
+
+exit 0;
+
+sub process_tile
+{
+    my $z = shift;
+    my $x = shift;
+    my $y = shift;
+
+    if ($z < 15)
+    {
+        $Tiles[$z] = Imager->new(xsize => 256, ysize => 256);
+
+        process_tile($z + 1, $x * 2 + 0, $y * 2 + 0);
+        process_tile($z + 1, $x * 2 + 1, $y * 2 + 0);
+        process_tile($z + 1, $x * 2 + 0, $y * 2 + 1);
+        process_tile($z + 1, $x * 2 + 1, $y * 2 + 1);
+
+        save_tile($Tiles[$z], $z, $x, $y);
+    }
+    elsif (-f "${SrcDir}/${z}/${x}/${y}.jpg")
+    {
+        my $tile = Imager->new;
+
+        $tile->read(file => "${SrcDir}/${z}/${x}/${y}.jpg") || die $tile->errstr;
+
+	for (my $dz = 1; $Tiles[$z - $dz]; $dz++)
+        {
+            my $f = 2 ** $dz;
+            my $s = 256 / $f;
+
+            $Tiles[$z - $dz]->paste(left => $s * ($x % $f),
+                                    top => $s * ($y % $f),
+                                    src => $tile->scale(xpixels => $s)) || die $Tiles[$z - $dz]->errstr;
+        }
+
+        save_tile($tile, $z, $x, $y);
+    }
+
+    return;
+}
+
+sub save_tile
+{
+    my $tile = shift;
+    my $z = shift;
+    my $x = shift;
+    my $y = shift;
+
+    mkpath("${DstDir}/${z}/${x}");
+
+    print "Writing ${DstDir}/${z}/${x}/${y}.jpg\n";
+
+    $tile->write(file => "${DstDir}/${z}/${x}/${y}.jpg", type => "jpeg") || die $tile->errstr;
+
+    return;
+}

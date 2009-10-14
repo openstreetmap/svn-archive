2a6fb0334083b4b0af31453f6886ab8e52c686de
josias <josias@svn.openstreetmap.org>
2009-10-14 21:17:59 +0000

    2.1 beta 2 see changelog

---
+++ applications/utils/osmolt/source/lib/src/org/openstreetmap/gui/jmapviewer/JobDispatcher.java
@@ -0,0 +1,145 @@
+package org.openstreetmap.gui.jmapviewer;
+
+//License: GPL. Copyright 2008 by Jan Peter Stotz
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A generic class that processes a list of {@link Runnable} one-by-one using
+ * one or more {@link Thread}-instances. The number of instances varies between
+ * 1 and {@link #WORKER_THREAD_MAX_COUNT} (default: 8). If an instance is idle
+ * more than {@link #WORKER_THREAD_TIMEOUT} seconds (default: 30), the instance
+ * ends itself.
+ * 
+ * @author Jan Peter Stotz
+ */
+public class JobDispatcher {
+
+	private static JobDispatcher instance;
+
+	/**
+	 * @return the singelton instance of the {@link JobDispatcher}
+	 */
+	public static JobDispatcher getInstance() {
+		// for speed reasons we check if the instance has been created
+		// one time before we enter the synchronized section...
+		if (instance != null)
+			return instance;
+		synchronized (JobDispatcher.class) {
+			// ... and for thread safety reasons one time inside the
+			// synchronized section.
+			if (instance != null)
+				return instance;
+			new JobDispatcher();
+			return instance;
+		}
+	}
+
+	private JobDispatcher() {
+		instance = this;
+		addWorkerThread().firstThread = true;
+	}
+
+	protected BlockingQueue<Runnable> jobQueue = new LinkedBlockingQueue<Runnable>();
+
+	public static int WORKER_THREAD_MAX_COUNT = 8;
+
+	/**
+	 * Specifies the time span in seconds that a worker thread waits for new
+	 * jobs to perform. If the time span has elapsed the worker thread
+	 * terminates itself. Only the first worker thread works differently, it
+	 * ignores the timeout and will never terminate itself.
+	 */
+	public static int WORKER_THREAD_TIMEOUT = 30;
+
+	/**
+	 * Total number of worker threads currently idle or active
+	 */
+	protected int workerThreadCount = 0;
+
+	/**
+	 * Number of worker threads currently idle
+	 */
+	protected int workerThreadIdleCount = 0;
+
+	/**
+	 * Just an id for identifying an worker thread instance
+	 */
+	protected int workerThreadId = 0;
+
+	/**
+	 * Removes all jobs from the queue that are currently not being processed.
+	 */
+	public void cancelOutstandingJobs() {
+		jobQueue.clear();
+	}
+
+	public void addJob(Runnable job) {
+		try {
+			jobQueue.put(job);
+			if (workerThreadIdleCount == 0 && workerThreadCount < WORKER_THREAD_MAX_COUNT)
+				addWorkerThread();
+		} catch (InterruptedException e) {
+		}
+	}
+
+	protected JobThread addWorkerThread() {
+		JobThread jobThread = new JobThread(++workerThreadId);
+		synchronized (this) {
+			workerThreadCount++;
+		}
+		return jobThread;
+	}
+
+	protected class JobThread extends Thread {
+
+		Runnable job;
+		boolean firstThread = false;
+
+		public JobThread(int threadId) {
+			super("OSMJobThread " + threadId);
+			setDaemon(true);
+			job = null;
+			start();
+		}
+
+		@Override
+		public void run() {
+			executeJobs();
+			synchronized (instance) {
+				workerThreadCount--;
+			}
+		}
+
+		protected void executeJobs() {
+			while (!isInterrupted()) {
+				try {
+					synchronized (instance) {
+						workerThreadIdleCount++;
+					}
+					if (firstThread)
+						job = jobQueue.take();
+					else
+						job = jobQueue.poll(WORKER_THREAD_TIMEOUT, TimeUnit.SECONDS);
+				} catch (InterruptedException e1) {
+					return;
+				} finally {
+					synchronized (instance) {
+						workerThreadIdleCount--;
+					}
+				}
+				if (job == null)
+					return;
+				try {
+					job.run();
+					job = null;
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
---
+++ applications/utils/osmolt/source/lib/src/org/openstreetmap/gui/jmapviewer/OsmTileLoader.java
@@ -0,0 +1,91 @@
+package org.openstreetmap.gui.jmapviewer;
+
+//License: GPL. Copyright 2008 by Jan Peter Stotz
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.openstreetmap.gui.jmapviewer.interfaces.TileCache;
+import org.openstreetmap.gui.jmapviewer.interfaces.TileLoader;
+import org.openstreetmap.gui.jmapviewer.interfaces.TileLoaderListener;
+import org.openstreetmap.gui.jmapviewer.interfaces.TileSource;
+
+/**
+ * A {@link TileLoader} implementation that loads tiles from OSM via HTTP.
+ * 
+ * @author Jan Peter Stotz
+ */
+public class OsmTileLoader implements TileLoader {
+
+    /**
+     * Holds the used user agent used for HTTP requests. If this field is 
+     * <code>null</code>, the default Java user agent is used.
+     */
+    public static String USER_AGENT = null;
+    public static String ACCEPT = "text/html, image/png, image/jpeg, image/gif, */*";
+
+    protected TileLoaderListener listener;
+
+    public OsmTileLoader(TileLoaderListener listener) {
+        this.listener = listener;
+    }
+
+    public Runnable createTileLoaderJob(final TileSource source, final int tilex, final int tiley, final int zoom) {
+        return new Runnable() {
+
+            InputStream input = null;
+
+            public void run() {
+                TileCache cache = listener.getTileCache();
+                Tile tile;
+                synchronized (cache) {
+                    tile = cache.getTile(source, tilex, tiley, zoom);
+                    if (tile == null || tile.isLoaded() || tile.loading)
+                        return;
+                    tile.loading = true;
+                }
+                try {
+                    // Thread.sleep(500);
+                    input = loadTileFromOsm(tile).getInputStream();
+                    tile.loadImage(input);
+                    tile.setLoaded(true);
+                    listener.tileLoadingFinished(tile, true);
+                    input.close();
+                    input = null;
+                } catch (Exception e) {
+                    tile.setImage(Tile.ERROR_IMAGE);
+                    listener.tileLoadingFinished(tile, false);
+                    if (input == null)
+                        System.err.println("failed loading " + zoom + "/" + tilex + "/" + tiley + " " + e.getMessage());
+                } finally {
+                    tile.loading = false;
+                    tile.setLoaded(true);
+                }
+            }
+
+        };
+    }
+
+    protected HttpURLConnection loadTileFromOsm(Tile tile) throws IOException {
+        URL url;
+        url = new URL(tile.getUrl());
+        HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+        prepareHttpUrlConnection(urlConn);
+        urlConn.setReadTimeout(30000); // 30 seconds read timeout
+        return urlConn;
+    }
+
+    protected void prepareHttpUrlConnection(HttpURLConnection urlConn) {
+        if (USER_AGENT != null)
+            urlConn.setRequestProperty("User-agent", USER_AGENT);
+        urlConn.setRequestProperty("Accept", ACCEPT);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName();
+    }
+
+}

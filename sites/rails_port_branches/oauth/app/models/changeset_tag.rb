fb26f2bb32c44a9a75143e3b683a4b358f713b6f
Tom Hughes <tom@compton.nu>
2009-10-23 07:47:21 +0000

    Remove oauth branch.

--- sites/rails_port_branches/oauth/app/models/changeset_tag.rb
+++
@@ -1,8 +0,0 @@
-class ChangesetTag < ActiveRecord::Base
-  belongs_to :changeset, :foreign_key => 'id'
-
-  validates_presence_of :id
-  validates_length_of :k, :v, :maximum => 255, :allow_blank => true
-  validates_uniqueness_of :id, :scope => :k
-  validates_numericality_of :id, :only_integer => true
-end
--- sites/rails_port_branches/oauth/config/initializers/i18n.rb
+++
@@ -1,4 +0,0 @@
-require 'globalize/i18n/missing_translations_log_handler'
-
-I18n.missing_translations_logger = Logger.new("#{RAILS_ROOT}/log/missing_translations.log")
-I18n.exception_handler = :missing_translations_log_handler

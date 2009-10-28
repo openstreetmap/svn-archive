fbbb996bbfa48bfa3fd1c9f9b6e6234a516c78f5
Milo van der Linden <milo@dogodigi.net>
2009-10-28 09:44:47 +0000

    Message strings and Dutch translation FINISHED

--- applications/utils/export/poiexport/include/en.php
+++ applications/utils/export/poiexport/include/en.php
@@ -5,7 +5,6 @@
  * <your language 2lettercode>.php
  * And translate *ONLY* the strings after the => sign
  *
- * TODO: Complete this file to reflect all the <? msg('xx'); ?> blocks from index.php
  */
 $messages = array (
         'OpenStreetMap POI export' => 'OpenStreetMap POI export',
@@ -16,6 +15,54 @@ $messages = array (
         'Select the device or file type you want to download to' => 'Select the device or file type you want to download to',
         '3: POI file' => '3: POI file',
         'Download POI file' => 'Download POI file',
-        'Select the type of POI you would like to download. Currently only one type can be selected.' => 'Select the type of POI you would like to download. Currently only one type can be selected.'
+        'Select the type of POI you would like to download. Currently only one type can be selected.' => 'Select the type of POI you would like to download. Currently only one type can be selected.',
+        'Fuel' => 'Fuel',
+        'ATM' => 'ATM',
+        'Speed camera' => 'Speed camera',
+        'Bus stop' => 'Bus stop',
+        'Parking' => 'Parking',
+        'Bicycle parking' => 'Bicycle parking',
+        'Place of worship' => 'Place of worship',
+        'Hospitail' => 'Hospital',
+        'Supermarket' => 'Supermarket',
+        'Theatre' => 'Theatre',
+        'Police'=> 'Police',
+        'Fire station' => 'Fire station',
+        'Post box' => 'Post box',
+        'Post office' => 'Post office',
+        'Recycling' => 'Recycling',
+        'Fast food' => 'Fast food',
+        'Toilets' => 'Toilets',
+        'Pub' => 'Pub',
+        'Waste basket' => 'Waste basket',
+        'Camp site' => 'Camp site',
+        'Hotel' => 'Hotel',
+        'Museum' => 'Museum',
+        'Zoo' => 'Zoo',
+        'Castle' => 'Castle',
+        'Windmill' => 'Windmill',
+        'Lighthouse' => 'Lighthouse',
+        'Watermill' => 'Watermill',
+        'Water tower' => 'Water tower',
+        'Nightclub' => 'Nightclub',
+        'Stripclub' => 'Stripclub',
+        'Next' => 'Next',
+        'Previous' => 'Previous',
+        'Select the device or file type you want to download the POI in' => 'Select the device or file type you want to download the POI in',
+        'TomTom overlay (ov2)' => 'TomTom overlay (ov2)',
+        'Garmin (csv)' => 'Garmin (csv)',
+        'GPS Exchange format (gpx)' => 'GPS Exchange format (gpx)',
+        'Google Earth (kml)' => 'Google Earth (kml)',
+        'The file is ready for download.' => 'The file is ready for download.',
+        'Poi type' => 'Poi type',
+        'Device type' => 'Device type',
+        'Download' => 'Download',
+        'Poi Export v0.2' => 'Poi Export v0.2',
+        'Data' => 'Data',
+        'OpenStreetMap' => 'OpenStreetMap',
+        'community' => 'community',
+        'Created by' => 'Created by',
+        'and' => 'and'
+
         );
 ?>

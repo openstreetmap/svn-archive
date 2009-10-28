6cfd6b86e6996223c4f04fac8e1f3434c44b375b
Milo van der Linden <milo@dogodigi.net>
2009-10-28 09:44:47 +0000

    Message strings and Dutch translation FINISHED

--- applications/utils/export/poiexport/include/nl.php
+++ applications/utils/export/poiexport/include/nl.php
@@ -1,14 +1,69 @@
 <?php
 /*
+ * DUTCH
  * the messages array is a from => to replacement array used by localization
  * copy this file and create a file named:
  * <your language 2lettercode>.php
  * And translate *ONLY* the strings after the => sign
  *
- * TODO: Finish Dutch translation
  */
 $messages = array (
-		'OpenStreetMap POI export' => 'OpenStreetMap POI export',
-		'download free point of interest' => 'download free point of interest'
-                );
+        'OpenStreetMap POI export' => 'OpenStreetMap POI export',
+        'download free point of interest' => 'download interessante lokaties',
+        '1: POI type' => '1: POI type',
+        'Select the POI type' => 'Selecteer het POI type',
+        '2: Device' => '2: Apparaat',
+        'Select the device or file type you want to download to' => 'Selecteer het apparaat of bestandstype dat je wilt downloaden',
+        '3: POI file' => '3: POI bestand',
+        'Download POI file' => 'Download POI bestand',
+        'Select the type of POI you would like to download. Currently only one type can be selected.' => 'Selecteer het type POI dat je wilt downloaden. Momenteel kan slechts een type worden geselecteerd.',
+        'Fuel' => 'Tankstation',
+        'ATM' => 'Geldautomaat',
+        'Speed camera' => 'Snelheidscamera',
+        'Bus stop' => 'Bushalte',
+        'Parking' => 'Parkeerplaats',
+        'Bicycle parking' => 'Fietsparkeerplaats',
+        'Place of worship' => 'Kerk, moskee of tempel',
+        'Hospitail' => 'Ziekenhuis',
+        'Supermarket' => 'Supermarkt',
+        'Theatre' => 'Theater',
+        'Police'=> 'Politie',
+        'Fire station' => 'Brandweer',
+        'Post box' => 'Brievenbus',
+        'Post office' => 'Postkantoor',
+        'Recycling' => 'Recycling',
+        'Fast food' => 'Fast food',
+        'Toilets' => 'Toilet',
+        'Pub' => 'Pub',
+        'Waste basket' => 'Afvalbak',
+        'Camp site' => 'Camping',
+        'Hotel' => 'Hotel',
+        'Museum' => 'Museum',
+        'Zoo' => 'Dierentuin',
+        'Castle' => 'Kasteel',
+        'Windmill' => 'Windmolen',
+        'Lighthouse' => 'Vuurtoren',
+        'Watermill' => 'Watermolen',
+        'Water tower' => 'Watertoren',
+        'Nightclub' => 'Nachtclub',
+        'Stripclub' => 'Stripclub',
+        'Next' => 'Volgende',
+        'Previous' => 'Vorige',
+        'Select the device or file type you want to download the POI in' => 'Selecteer het apparaat of bestandsformaat',
+        'TomTom overlay (ov2)' => 'TomTom overlay (ov2)',
+        'Garmin (csv)' => 'Garmin (csv)',
+        'GPS Exchange format (gpx)' => 'GPS Exchange format (gpx)',
+        'Google Earth (kml)' => 'Google Earth (kml)',
+        'The file is ready for download.' => 'Het bestand is gereed voor download.',
+        'Poi type' => 'Poi type',
+        'Device type' => 'Apparaat type',
+        'Download' => 'Download',
+        'Poi Export v0.2' => 'Poi Export v0.2',
+        'Data' => 'Data',
+        'OpenStreetMap' => 'OpenStreetMap',
+        'community' => 'gemeenschap',
+        'Created by' => 'Gemaakt door',
+        'and' => 'en'
+        );
 ?>
+

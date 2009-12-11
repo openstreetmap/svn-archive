8e1e56ac593b1c9b114a0f5a11b56f8e97c71a9e
Frank Steggink <steggink@steggink.org>
2009-12-11 02:30:57 +0000

    more advanced db connection, parsing improvements

--- applications/utils/osm-extract/polygons/polybuffer.py
+++ applications/utils/osm-extract/polygons/polybuffer.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python
 # polybuffer.py
-# version 0.1
+# version 0.2
 
 # History
 # - 12/01/2009 0.1   First release
+# - 12/10/2009 0.2   More advanced DB connection, improvements in polygon parsing
 
 
 # Usage: ./polybuffer.py -h
-# Example use with stdin and stdout: cat quebec.poly | ./polybuffer.py >out.txt
+# Example use with stdin and stdout: cat poly/quebec2pts.txt | ./polybuffer.py >out.txt
 
 # Requirements:
 # * Recent Python version (tested with 2.6)
 # * PostGIS (tested with 1.3)
+# * PyGreSQL (http://www.pygresql.org/)
 
 # Notes:
-# * Only tested on Ubuntu, but should return everywhere
+# * Only tested on Ubuntu, but should run everywhere
 # * In the input file, holes in polygons are not (yet?) recognized, but they are generated in the output file
 # * Poly file is assumed to be in WGS84
 # * TODO Use a different SRS in PostGIS, eventually make it an option. For now, WGS84 is sufficient
 
 
+import getpass
 import random
 import re
 import sys
@@ -28,7 +31,7 @@ from optparse import OptionParser
 
 
 # Do not change any of the following constants
-VERSION = "0.1"
+VERSION = "0.2"
 
 
 # Read a polygon from file
@@ -57,8 +60,8 @@ def read_polygon(f):
 		
 		ords = line.split()
 		coords.append("%f %f" % (float(ords[0]), float(ords[1])))
-		
-	if len(coords) == 0:
+	
+	if len(coords) < 3:
 		return None
 	
 	polygon = "((" + ",".join(coords) + "))"
@@ -87,36 +90,53 @@ def read_multipolygon(f):
 	return wkt
 
 
+# Write a polygon to the file
+def write_polygon(f, wkt, p):
+
+	match = re.search("^\(\((?P<pdata>.*)\)\)$", wkt)
+	pdata = match.group("pdata")
+	rings = re.split("\),\(", pdata)
+	
+	first_ring = True
+	for ring in rings:
+		coords = re.split(",", ring)			
+	
+		p = p + 1
+		if first_ring:
+			f.write(str(p) + "\n")
+			first_ring = False
+		else:
+			f.write("!" + str(p) + "\n")
+		
+		for coord in coords:
+			ords = coord.split()
+			f.write("\t%E\t%E\n" % (float(ords[0]), float(ords[1])))
+		
+		f.write("END\n")
+	
+	return p
+
+
 # Write a multipolygon to the file
 def write_multipolygon(f, wkt):
 
 	match = re.search("^MULTIPOLYGON\((?P<mpdata>.*)\)$", wkt)
-	mpdata = match.group("mpdata")
-	polygons = re.split("(?<=\)\)),(?=\(\()", mpdata)
 	
-	p = 0
-	for polygon in polygons:
-		match = re.search("^\(\((?P<pdata>.*)\)\)$", polygon)
+	if match:
+		mpdata = match.group("mpdata")
+		polygons = re.split("(?<=\)\)),(?=\(\()", mpdata)
+	
+		p = 0
+		for polygon in polygons:
+			p = write_polygon(f, polygon, p)
+		
+		return
+		
+	match = re.search("^POLYGON(?P<pdata>.*)$", wkt)
+	if match:
 		pdata = match.group("pdata")
-		rings = re.split("\),\(", pdata)
-		
-		first_ring = True
-		for ring in rings:
-			coords = re.split(",", ring)			
-		
-			p = p + 1
-			if first_ring:
-				f.write(str(p) + "\n")
-				first_ring = False
-			else:
-				f.write("!" + str(p) + "\n")
-			
-			for coord in coords:
-				ords = coord.split()
-				f.write("\t%E\t%E\n" % (float(ords[0]), float(ords[1])))
-			
-			f.write("END\n")
-
+		write_polygon(f, pdata, 0)
+	
 
 # Calculate a buffer around the polygon, using PostGIS
 def buffer_polygon(wkt, db, buffer_distance):
@@ -129,7 +149,7 @@ def buffer_polygon(wkt, db, buffer_distance):
 	# Create a table in the DB
 	sql = "CREATE TABLE %s ( id integer );" % (table_name)
 	db.query(sql)
-	sql = "SELECT AddGeometryColumn('', '%s', 'the_geom', '-1', 'MULTIPOLYGON' ,2);" % (table_name)
+	sql = "SELECT AddGeometryColumn('', '%s', 'the_geom', '-1', 'GEOMETRY' ,2);" % (table_name)
 	db.query(sql)
 
 	# Load data into it
@@ -165,7 +185,7 @@ def buffer_polygon(wkt, db, buffer_distance):
 
 
 # Perform all the conversion steps
-def convert_poly (input_file, output_file, buffer_distance, database, dbhost):
+def convert_poly (input_file, output_file, buffer_distance, db):
 
 	# Steps:
 	# 1. Convert poly to WKT
@@ -189,7 +209,6 @@ def convert_poly (input_file, output_file, buffer_distance, database, dbhost):
 	f.close()
 
 	# Buffer the polygon
-	db = _pg.connect(database, dbhost)
 	wkt = buffer_polygon(wkt, db, buffer_distance)
 
 	# Convert WKT to polygon, and write output file
@@ -209,19 +228,34 @@ def convert_poly (input_file, output_file, buffer_distance, database, dbhost):
 
 def main():
 
+	default_user = getpass.getuser()
+
 	# Parse arguments
-	arg_list = "-i <input> -o <output> -d <distance>"
+	arg_list = "-i <input> -o <output> [-b <distance>] [-d <db> ...]"
 	usage = "Usage: %prog " + arg_list
 	version = "%prog " + VERSION
 	parser = OptionParser(usage, version=version)
 	parser.add_option("-i", "--input", dest="input", help="input file (default stdin)")
 	parser.add_option("-o", "--output", dest="output", help="output file (default stdout)")
-	parser.add_option("-b", "--buffer", dest="buffer", help="buffer distance in m (default 1000)", default=1000)
-	parser.add_option("-d", "--db", dest="database", help="database")
-	parser.add_option("--host", dest="dbhost", help="host (default localhost)", default="localhost")
+	parser.add_option("-b", "--buffer", type="float", dest="buffer", help="buffer distance in m (default '1000')", default=1000)
+	parser.add_option("-d", "--dbname", dest="dbname", help="database")
+	parser.add_option("--host", dest="host", help="host (default 'localhost')", default="localhost")
+	parser.add_option("--port", type="int", dest="port", help="port (default '5432')", default="5432")
+	parser.add_option("-u", "--user", dest="user", help="user name (default '%s')" % (default_user), default=default_user)
+	parser.add_option("-w", "--password", action="store_true", dest="passwd", help="password", default=False)
 	(options, args) = parser.parse_args()
-
-	convert_poly(options.input, options.output, options.buffer, options.database, options.dbhost)
+	
+	passwd = ""
+	if options.passwd:
+		passwd = getpass.getpass("Please enter your password: ")
+	
+	db = _pg.connect(dbname=options.dbname,
+					 host=options.host,
+					 port=options.port, 
+					 user=options.user, 
+					 passwd=passwd)
+					 
+	convert_poly(options.input, options.output, options.buffer, db)
 
 
 if __name__ == "__main__":

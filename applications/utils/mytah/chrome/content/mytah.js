2d83f4fcfc3e0cd23c97675b964a106f2dc8403c
Mario Ferraro <fadinlight@gmail.com>
2010-02-18 19:34:02 +0000

    MyTaH: initial commit for version 0.3: new panel for changelog in about, compatibility with all firefox versions 3.*, style fixes

--- applications/utils/mytah/chrome/content/mytah.js
+++ applications/utils/mytah/chrome/content/mytah.js
@@ -22,151 +22,151 @@ var tiles_to_better_rank="updating...";
 var interval_id=null;
 
 function mytah_init() {
-	current_rank="updating...";
-	number_of_tiles="updating...";
-	number_of_kb="updating...";
-	idle_time="updating...";
-	tiles_to_better_rank="updating...";
-	document.getElementById("mytah_sbmi").label="MyTah: "+eval(display);
-	document.getElementById("mytah_currentrank").value=current_rank;
-	document.getElementById("mytah_numberoftiles").value=number_of_tiles;
-	document.getElementById("mytah_numberofkb").value=number_of_kb;
-	document.getElementById("mytah_idletime").value=idle_time;
-	document.getElementById("mytah_tilestobetterrank").value=tiles_to_better_rank;
+  current_rank="updating...";
+  number_of_tiles="updating...";
+  number_of_kb="updating...";
+  idle_time="updating...";
+  tiles_to_better_rank="updating...";
+  document.getElementById("mytah_sbmi").label="MyTah: "+eval(display);
+  document.getElementById("mytah_currentrank").value=current_rank;
+  document.getElementById("mytah_numberoftiles").value=number_of_tiles;
+  document.getElementById("mytah_numberofkb").value=number_of_kb;
+  document.getElementById("mytah_idletime").value=idle_time;
+  document.getElementById("mytah_tilestobetterrank").value=tiles_to_better_rank;
 
-	this.prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-	if (this.prefs.getPrefType("mytah.user_type") == this.prefs.PREF_INT) {
-		user_type = this.prefs.getIntPref("mytah.user_type");
-	} else {
-		this.prefs.setIntPref("mytah.user_type", USER_BY_NAME);
-		user_type = USER_BY_NAME;
-	}
-	if (this.prefs.getPrefType("mytah.user") == this.prefs.PREF_STRING) {
-		user = this.prefs.getCharPref("mytah.user");
-	} else {
-		user = "";
-		this.prefs.setCharPref("mytah.user", user);
-	}
-	if (this.prefs.getPrefType("mytah.timeout") == this.prefs.PREF_INT) {
-		timeout = this.prefs.getIntPref("mytah.timeout");
-	}
-	else {
-		timeout = 60;
-		this.prefs.setIntPref("mytah.timeout", timeout);
-	}
-	if (this.prefs.getPrefType("mytah.display") == this.prefs.PREF_STRING) {
-		display = this.prefs.getCharPref("mytah.display");
-	} else {
-		display = "current_rank";
-		this.prefs.setCharPref("mytah.display", display);
-	}
-	if (interval_id!=null) {
-		clearInterval(interval_id);
-	}
-	interval_id=setInterval("mytah_init()",(timeout*60*1000));
-	if (user=="") {
-		document.getElementById("mytah_sbmi").label="MyTah: No name";
-	}
-	else {
-		mytah_update_text();
-	}
+  this.prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+
+  if (this.prefs.getPrefType("mytah.user_type") == this.prefs.PREF_INT) {
+    user_type = this.prefs.getIntPref("mytah.user_type");
+  } else {
+    this.prefs.setIntPref("mytah.user_type", USER_BY_NAME);
+    user_type = USER_BY_NAME;
+  }
+  if (this.prefs.getPrefType("mytah.user") == this.prefs.PREF_STRING) {
+    user = this.prefs.getCharPref("mytah.user");
+  } else {
+    user = "";
+    this.prefs.setCharPref("mytah.user", user);
+  }
+  if (this.prefs.getPrefType("mytah.timeout") == this.prefs.PREF_INT) {
+    timeout = this.prefs.getIntPref("mytah.timeout");
+  }
+  else {
+    timeout = 60;
+    this.prefs.setIntPref("mytah.timeout", timeout);
+  }
+  if (this.prefs.getPrefType("mytah.display") == this.prefs.PREF_STRING) {
+    display = this.prefs.getCharPref("mytah.display");
+  } else {
+    display = "current_rank";
+    this.prefs.setCharPref("mytah.display", display);
+  }
+  if (interval_id!=null) {
+    clearInterval(interval_id);
+  }
+  interval_id=setInterval("mytah_init()",(timeout*60*1000));
+  if (user=="") {
+    document.getElementById("mytah_sbmi").label="MyTah: No name";
+  }
+  else {
+    mytah_update_text();
+  }
 }
 
 function mytah_update_text() {
-	var req = new XMLHttpRequest();
-		req.open('GET', 'http://server.tah.openstreetmap.org/User/show/?order=tiles', true);
-		req.onreadystatechange = function (aEvt) {
-		  if (req.readyState == 4) {
-		     if(req.status == 200) {
-		     	  current_text=req.responseText;
-			      mytah_getRanking();
-			}
-		     else {
-		      dump("Error loading page\n");
-		      }
-		  }
-		};
-	req.send(null); 
+  var req = new XMLHttpRequest();
+  req.open('GET', 'http://server.tah.openstreetmap.org/User/show/?order=tiles', true);
+  req.onreadystatechange = function (aEvt) {
+    if (req.readyState == 4) {
+      if(req.status == 200) {
+        current_text=req.responseText;
+        mytah_getRanking();
+      }
+      else {
+        dump("Error loading page\n");
+      }
+    }
+  };
+  req.send(null); 
 }
 
 function mytah_getRanking() {
-	var regexs = {
-		by_id: {
-			user_id: 'variable:user',
-			current_rank: '(?:(<tr><td>))(.*?)(?=(<\/td><td><a href="\/User\/show\/byid\/'+user+'\/">))',
-			number_of_tiles: '(?:(\/byid\/'+user+'\/">.*?<\/a><\/td><td>))(.*)(?=(<\/td><td>.*?<\/td><td>.*?<\/td><\/tr>))',
-			number_of_kb: '(?:(\/byid\/'+user+'\/">.*?<\/a><\/td><td>.*?<\/td><td>))(.*)(?=(<\/td><td>))',
-			idle_time: '(?:(\/byid\/'+user+'\/">.*?<\/a><\/td><td>.*?<\/td><td>.*?<\/td><td>))(.*)(?=(<\/td><\/tr>))',
-			tiles_to_better_rank: '(?:(<\/a><\/td><td>))(.*?)(?=(<\/td><td>.*?<\/td><td>.*?<\/td><\/tr>\\s*<tr><td>.*?<\/td><td><a href="\/User\/show\/byid\/'+user+'\/">))'
-		},
-		by_name: {
-			user_id: '(?:(<tr><td>.*?<\/td><td><a href="\/User\/show\/byid\/))(.*?)(?=(/">'+user+'))',
-			current_rank: '(?:(<tr><td>))(.*?)(?=(<\/td><td><a href="\/User\/show\/byid\/\\d*\/">'+user+'))',
-			number_of_tiles: '(?:('+user+'<\/a><\/td><td>))(.*?)(?=(<\/td><td>))',
-			number_of_kb: '(?:('+user+'<\/a><\/td><td>.*?<\/td><td>)(.*?)(?=(<\/td><td>)))',
-			idle_time: '(?:('+user+'<\/a><\/td><td>.*?<\/td><td>.*?<\/td><td>)(.*?)(?=(<\/td><\/tr>)))',
-			tiles_to_better_rank: '(?:(<\/a><\/td><td>))(.*?)(?=(<\/td><td>.*?<\/td><td>.*?<\/td><\/tr>\\s*<tr><td>.*?<\/td><td><a href="\/User\/show\/byid\/\\d*\/">'+user+'<\/a>))'
-		}
-	};
-	var looper;
-	if (user_type==USER_BY_ID) looper=regexs.by_id; else looper=regexs.by_name;
-	for (var a in looper) {
-		if (looper[a].indexOf("variable:")==-1) {
-			var m = new RegExp(looper[a]).exec(current_text);
-			eval(a+"='"+m[2]+"'");
-		}
-		else {
-			m = looper[a].substring((looper[a].indexOf(":")+1),looper[a].length);
-			eval(a+"='"+eval(m)+"'");
-		}
-	}
-	tiles_to_better_rank="-"+(tiles_to_better_rank-number_of_tiles);
-	document.getElementById("mytah_sbmi").label="MyTaH: "+eval(display);
-	document.getElementById("mytah_currentrank").value=current_rank;
-	document.getElementById("mytah_numberoftiles").value=number_of_tiles;
-	document.getElementById("mytah_numberofkb").value=number_of_kb;
-	document.getElementById("mytah_idletime").value=idle_time;
-	document.getElementById("mytah_tilestobetterrank").value=tiles_to_better_rank;
+  var regexs = {
+    by_id: {
+      user_id: 'variable:user',
+      current_rank: '(?:(<tr><td>))(.*?)(?=(<\/td><td><a href="\/User\/show\/byid\/'+user+'\/">))',
+      number_of_tiles: '(?:(\/byid\/'+user+'\/">.*?<\/a><\/td><td>))(.*)(?=(<\/td><td>.*?<\/td><td>.*?<\/td><\/tr>))',
+      number_of_kb: '(?:(\/byid\/'+user+'\/">.*?<\/a><\/td><td>.*?<\/td><td>))(.*)(?=(<\/td><td>))',
+      idle_time: '(?:(\/byid\/'+user+'\/">.*?<\/a><\/td><td>.*?<\/td><td>.*?<\/td><td>))(.*)(?=(<\/td><\/tr>))',
+      tiles_to_better_rank: '(?:(<\/a><\/td><td>))(.*?)(?=(<\/td><td>.*?<\/td><td>.*?<\/td><\/tr>\\s*<tr><td>.*?<\/td><td><a href="\/User\/show\/byid\/'+user+'\/">))'
+    },
+    by_name: {
+      user_id: '(?:(<tr><td>.*?<\/td><td><a href="\/User\/show\/byid\/))(.*?)(?=(/">'+user+'))',
+      current_rank: '(?:(<tr><td>))(.*?)(?=(<\/td><td><a href="\/User\/show\/byid\/\\d*\/">'+user+'))',
+      number_of_tiles: '(?:('+user+'<\/a><\/td><td>))(.*?)(?=(<\/td><td>))',
+      number_of_kb: '(?:('+user+'<\/a><\/td><td>.*?<\/td><td>)(.*?)(?=(<\/td><td>)))',
+      idle_time: '(?:('+user+'<\/a><\/td><td>.*?<\/td><td>.*?<\/td><td>)(.*?)(?=(<\/td><\/tr>)))',
+      tiles_to_better_rank: '(?:(<\/a><\/td><td>))(.*?)(?=(<\/td><td>.*?<\/td><td>.*?<\/td><\/tr>\\s*<tr><td>.*?<\/td><td><a href="\/User\/show\/byid\/\\d*\/">'+user+'<\/a>))'
+    }
+  };
+  var looper;
+  if (user_type==USER_BY_ID) looper=regexs.by_id; else looper=regexs.by_name;
+  for (var a in looper) {
+    if (looper[a].indexOf("variable:")==-1) {
+      var m = new RegExp(looper[a]).exec(current_text);
+      eval(a+"='"+m[2]+"'");
+    }
+    else {
+      m = looper[a].substring((looper[a].indexOf(":")+1),looper[a].length);
+      eval(a+"='"+eval(m)+"'");
+    }
+  }
+  tiles_to_better_rank="-"+(tiles_to_better_rank-number_of_tiles);
+  document.getElementById("mytah_sbmi").label="MyTaH: "+eval(display);
+  document.getElementById("mytah_currentrank").value=current_rank;
+  document.getElementById("mytah_numberoftiles").value=number_of_tiles;
+  document.getElementById("mytah_numberofkb").value=number_of_kb;
+  document.getElementById("mytah_idletime").value=idle_time;
+  document.getElementById("mytah_tilestobetterrank").value=tiles_to_better_rank;
 }
 
 function mytah_showPreferences() {
-	var params={out:null};
-	window.openDialog("chrome://MyTaH/content/preferences.xul","","chrome,dialog,modal",params).focus();
-	if (params.out) {
-		mytah_updateNow();
-	}
-	else {
-	}
+  var params={out:null};
+  window.openDialog("chrome://MyTaH/content/preferences.xul","","chrome,dialog,modal",params).focus();
+  if (params.out) {
+    mytah_updateNow();
+  }
+  else {
+  }
 }
 
 function mytah_updateNow() {
-		mytah_init();
+  mytah_init();
 }
 
 function mytah_showAbout() {
-	window.openDialog("chrome://MyTaH/content/about.xul","","chrome,dialog").focus();
+  window.openDialog("chrome://MyTaH/content/about.xul","","chrome,dialog").focus();
 }
 
 function mytah_openPage(whichpage) {
-	switch (whichpage) {
-		case 'order_tiles' : {
-			gBrowser.selectedTab = gBrowser.addTab("http://server.tah.openstreetmap.org/User/show/?order=tiles");
-			break;
-		}
-		case 'order_upload': {
-			gBrowser.selectedTab = gBrowser.addTab("http://server.tah.openstreetmap.org/User/show/?order=upload");
-			break;
-		}
-		case 'order_lastactivity': {
-			gBrowser.selectedTab = gBrowser.addTab("http://server.tah.openstreetmap.org/User/show/?order=last");
-			break;
-		}
-		case 'personal' : {
-			if (user!="" && user_id!=null) {
-				gBrowser.selectedTab = gBrowser.addTab("http://tah.openstreetmap.org/User/show/byid/"+user_id);
-			}
-			break;
-		}
-		
-	}
+  switch (whichpage) {
+    case 'order_tiles' : {
+      gBrowser.selectedTab = gBrowser.addTab("http://server.tah.openstreetmap.org/User/show/?order=tiles");
+      break;
+    }
+    case 'order_upload': {
+      gBrowser.selectedTab = gBrowser.addTab("http://server.tah.openstreetmap.org/User/show/?order=upload");
+      break;
+    }
+    case 'order_lastactivity': {
+      gBrowser.selectedTab = gBrowser.addTab("http://server.tah.openstreetmap.org/User/show/?order=last");
+      break;
+    }
+    case 'personal' : {
+      if (user!="" && user_id!=null) {
+        gBrowser.selectedTab = gBrowser.addTab("http://tah.openstreetmap.org/User/show/byid/"+user_id);
+      }
+      break;
+    }
+  }
 }

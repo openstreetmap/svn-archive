d80a287d760eca329af80fd8a7237f4f3bb82aea
Koying Bing <koying@semperpax.com>
2010-02-28 15:15:14 +0000

    0.15 release

---
+++ applications/editors/merkaartor-tags/merkaartor-0.15/src/Docks/MDockAncestor.h
@@ -0,0 +1,44 @@
+#ifndef MDOCKANCESTOR_H
+#define MDOCKANCESTOR_H
+
+
+#ifndef _MOBILE
+
+#include <QDockWidget>
+
+class MDockAncestor : public QDockWidget
+{
+	public:
+        MDockAncestor(QWidget *parent = 0) : QDockWidget(parent) {}
+        void setWidget ( QWidget * widget ) { QDockWidget::setWidget(widget); }
+		QWidget* getWidget();
+        void setAllowedAreas ( Qt::DockWidgetAreas areas )  { QDockWidget::setAllowedAreas(areas); }
+
+	protected:
+		QWidget* mainWidget;
+		virtual void retranslateUi() = 0;
+};
+
+#else
+
+#include <QDialog>
+
+class QVBoxLayout;
+
+class MDockAncestor : public QDialog
+{
+	public:
+		MDockAncestor(QWidget *parent = 0);
+		void setWidget ( QWidget * widget );
+		QWidget* getWidget();
+		void setAllowedAreas ( Qt::DockWidgetAreas  )  {  };
+	
+	protected:
+		QVBoxLayout* theLayout;
+		QWidget* mainWidget;
+		virtual void retranslateUi() = 0;
+};
+
+#endif
+
+#endif //MDOCKANCESTOR_H

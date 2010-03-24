708b54ce634d4d4fdad6c81990bd75835cce21f0
Nick Whitelegg <nick_whitelegg@yahoo.co.uk>
2010-03-24 18:52:32 +0000

    removed a couple of unnecessary files

--- sites/free-map.org.uk/otv/connect.php
+++ sites/free-map.org.uk/otv/connect.php
@@ -1,5 +1,9 @@
 <?php
-include ('../lib/defines.php');
+
+// define DB_USERNAME, DB_PASSWORD and DB_DBASE in this file
+// Not in SVN for obvious reasons
+require_once('/var/www-data/private/defines.php');
+
 $conn=mysql_connect("localhost",DB_USER,DB_PASSWORD);
 mysql_select_db(DB_DBASE);
 ?>

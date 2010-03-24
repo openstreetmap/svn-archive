03c274631e7eb40631e9665deee74d713ed8e5e6
Nick Whitelegg <nick_whitelegg@yahoo.co.uk>
2010-03-24 19:54:36 +0000

    corrected typo in include path

--- sites/free-map.org.uk/otv/connect.php
+++ sites/free-map.org.uk/otv/connect.php
@@ -2,7 +2,7 @@
 
 // define DB_USERNAME, DB_PASSWORD and DB_DBASE in this file
 // Not in SVN for obvious reasons
-require_once('/var/www-data/private/defines.php');
+require_once('/home/www-data/private/defines.php');
 
 $conn=mysql_connect("localhost",DB_USER,DB_PASSWORD);
 mysql_select_db(DB_DBASE);

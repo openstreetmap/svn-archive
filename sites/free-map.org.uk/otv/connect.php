db2c58494303a2abac493f6142e4cdd167f1bdaf
Nick Whitelegg <nick_whitelegg@yahoo.co.uk>
2010-03-29 19:28:45 +0000

    silly typo in connect.php

--- sites/free-map.org.uk/otv/connect.php
+++ sites/free-map.org.uk/otv/connect.php
@@ -1,6 +1,6 @@
 <?php
-require_once("/home/www-data/private/defines.php")
+require_once("/home/www-data/private/defines.php");
 global $conn;
-$conn=mysql_connect("localhost",DB_USER,DB_PASSWORD);
+$conn=mysql_connect("localhost",DB_USERNAME,DB_PASSWORD);
 mysql_select_db("otv");
 ?>

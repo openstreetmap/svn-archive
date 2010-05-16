55e605918df6587ae6c95c8984979d0d6188f0c7
Kay Drangmeister <kay@drangmeister.net>
2010-05-16 18:51:05 +0000

    kayg

--- applications/rendering/parking/mapnik/kayg-toolserver.sh
+++ applications/rendering/parking/mapnik/kayg-toolserver.sh
@@ -1,4 +1,4 @@
-./generate_xml.py osm-parking-src.xml osm-parking.xml --host localhost --user `whoami` --host sql-mapnik --dbname osm_mapnik --inc ./parking-inc --symbols ./parking-symbols/ --world_boundaries /home/project/o/s/m/osm/data/world_boundaries/ --port 5432 --password 'kay' 
+./generate_xml.py osm-parking-src.xml osm-parking.xml --user `whoami` --host sql-mapnik --dbname osm_mapnik --inc ./parking-inc --symbols ./parking-symbols/ --world_boundaries /home/project/o/s/m/osm/data/world_boundaries/ --port 5432 --password 'kay' 
 #./generate_xml.py osm-parking-bw-src.xml osm-parking-bw.xml --host localhost --user `whoami` --dbname gis --symbols ./symbols/ --world_boundaries ./world_boundaries/ --port 5432 --password 'kay' 
 #./generate_xml.py osm-parktrans-src.xml osm-parktrans.xml --host localhost --user `whoami` --dbname gis --symbols ./symbols/ --world_boundaries ./world_boundaries/ --port 5432 --password 'kay' 
 #./generate_xml.py osm-parkerr-src.xml osm-parkerr.xml --host localhost --user `whoami` --dbname gis --symbols ./symbols/ --world_boundaries ./world_boundaries/ --port 5432 --password 'kay' 

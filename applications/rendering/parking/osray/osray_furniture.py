c8d67af19c591bc66cf7441509eee9fdb171e20a
Kay Drangmeister <kay@drangmeister.net>
2010-07-15 14:56:22 +0000

    added tree type=conifer

--- applications/rendering/parking/osray/osray_furniture.py
+++ applications/rendering/parking/osray/osray_furniture.py
@@ -8,20 +8,20 @@ Radiosity = True
 
 declarations = """
 #macro tree_broad_leafed (xp,yp,height)
-#local wipfel = sphere {
-  <0,1,0>,0.5
+#local wipfel = cone {
+  <0,0.666,0>,0.333
   texture {
     pigment {
       color rgb <0.6,1.0,0.6>
     }
     finish {
-      diffuse 0.8
+      diffuse 0.7
       ambient 0
     }
   }   
 } // end of sphere
 #local stamm = cylinder {
-  <0,0,0>,<0,1,0>,0.18
+  <0,0,0>,<0,0.4,0>,0.12
   texture {
     pigment {
       color rgb <0.4,0.3,0.1>
@@ -38,6 +38,40 @@ union{
   object{stamm scale <height,height,height> translate <xp,0,yp>}
  } // end of tree
 #end
+
+#macro tree_conifer (xp,yp,height)
+#local wipfel = cone {
+  <0,0.3,0>,0.5
+  <0,1.0,0>,0.0
+  texture {
+    pigment {
+      color rgb <0.5,0.8,0.6>
+    }
+    finish {
+      diffuse 0.7
+      ambient 0
+    }
+  }   
+} // end of sphere
+#local stamm = cylinder {
+  <0,0,0>,<0,0.31,0>,0.12
+  texture {
+    pigment {
+      color rgb <0.4,0.3,0.1>
+    }
+    finish {
+      diffuse 0.8
+      ambient 0
+    }
+  }   
+} // end of cylinder
+
+union{
+  object{wipfel scale <height,height,height> translate <xp,0,yp>}
+  object{stamm scale <height,height,height> translate <xp,0,yp>}
+ } // end of tree
+#end
+
 """
 def pov_tree(f,tree):
     treetype = tree['type']
@@ -48,7 +82,10 @@ def pov_tree(f,tree):
     else:
         height = parse_length_in_meters(heightstring,0.01)
     f.write("/* tree osm_id={0} */\n".format(tree['osm_id']))
-    f.write("object {{ tree_broad_leafed ({x},{y},{height}) }}\n".format(x=x,y=y,height=height))
+    if treetype=='conifer':
+        f.write("object {{ tree_conifer ({x},{y},{height}) }}\n".format(x=x,y=y,height=height))
+    else:
+        f.write("object {{ tree_broad_leafed ({x},{y},{height}) }}\n".format(x=x,y=y,height=height))
     
 def render_furniture(f,osraydb,options):
     Radiosity = options['radiosity']

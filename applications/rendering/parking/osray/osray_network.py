04ce95e68f330c3ecae7e739bef720725fafe967
Kay Drangmeister <kay@drangmeister.net>
2010-07-19 08:04:56 +0000

    removed debug output

--- applications/rendering/parking/osray/osray_network.py
+++ applications/rendering/parking/osray/osray_network.py
@@ -14,12 +14,12 @@ class osrayNetwork():
         lastpoint = None
         for point in highway_way:
             self.G.add_node(point)
-            print "adding node:",point
+            #print "adding node:",point
             if(lastpoint!=None):
                 self.G.add_edge(lastpoint,point,layer=layer)
-                print "adding edge:",(lastpoint,point,layer)
+                #print "adding edge:",(lastpoint,point,layer)
             lastpoint=point
-        print "G=",self.G.number_of_nodes(),self.G.number_of_edges()
+        #print "G=",self.G.number_of_nodes(),self.G.number_of_edges()
 
     def calculate_height_on_nodes(self):
         for n in self.G.nodes():
@@ -27,14 +27,11 @@ class osrayNetwork():
             sum_height = 0.0
             for m in neighbors:
                 edge = self.G[n][m]
-                print "Edge(n,m)=",n,m,edge
+                #print "Edge(n,m)=",n,m,edge
                 sum_height += edge['layer']*5.0
             self.G.node[n]['height'] = sum_height/len(neighbors)
 
     def get_height(self,n):
         return self.G.node[n]['height']
-
-    def printG(self):
-        print "G=",self.G.edges()
 
            

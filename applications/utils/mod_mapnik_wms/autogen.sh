4f30c56fa8d244a93bad8c0500f2d3088c60c4a3
Dane Springmeyer <dane@dbsgeo.com>
2010-07-24 00:57:28 +0000

    fixup logic around autogen support for mac osx

--- applications/utils/mod_mapnik_wms/autogen.sh
+++ applications/utils/mod_mapnik_wms/autogen.sh
@@ -5,12 +5,12 @@ set -e
 
 ## on macosx glibtoolize, others have libtool
 if test x$LIBTOOLIZE = x; then
-  if test \! x`which glibtoolize` = x; then
+  if test \! x`which libtoolize` = x; then
+    LIBTOOLIZE=libtoolize
+  elif test \! x`which glibtoolize` = x; then
     LIBTOOLIZE=glibtoolize
   elif test \! x`which libtoolize-1.5` = x; then
     LIBTOOLIZE=libtoolize-1.5
-  elif test \! x`which libtoolize` = x; then
-    LIBTOOLIZE=libtoolize
   fi
 fi
 

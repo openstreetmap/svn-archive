50d9e47f5794fef55f9a9c59ede68a9a6843f693
Dane Springmeyer <dane@dbsgeo.com>
2010-07-24 00:47:33 +0000

    touch up build for mac osx

--- applications/utils/mod_mapnik_wms/autogen.sh
+++ applications/utils/mod_mapnik_wms/autogen.sh
@@ -1,2 +1,18 @@
 #! /bin/sh
-libtoolize -f -c && aclocal -I ./acinclude.d -I /usr/share/aclocal && autoheader && automake -ac && autoconf && ./configure "$@"
+
+## jump out if one of the programs returns 'false'
+set -e
+
+## on macosx glibtoolize, others have libtool
+if test x$LIBTOOLIZE = x; then
+  if test \! x`which glibtoolize` = x; then
+    LIBTOOLIZE=glibtoolize
+  elif test \! x`which libtoolize-1.5` = x; then
+    LIBTOOLIZE=libtoolize-1.5
+  elif test \! x`which libtoolize` = x; then
+    LIBTOOLIZE=libtoolize
+  fi
+fi
+
+
+$LIBTOOLIZE -f -c && aclocal -I ./acinclude.d -I /usr/share/aclocal && autoheader && automake -ac && autoconf && ./configure "$@"

62382811b0322910659f1af029465cda17e2adfa
Dirk Stöcker <openstreetmap@dstoecker.de>
2010-08-02 20:16:13 +0000

    fix typos

--- applications/editors/josm/i18n/specialmessages.java
+++ applications/editors/josm/i18n/specialmessages.java
@@ -2,7 +2,7 @@
   This file can be used to add some special messages to the translation,
   which are not part of the programm texts, but should be translated nevertheless.
 
-  The tex before is for translators
+  The text before is for translators
 */
 
 /* OSM server message */ tr("You requested too many nodes (limit is 50,000). Either request a smaller area, or use planet.osm");
@@ -10,7 +10,7 @@
 /* OSM server message */ tr("The maximum bbox size is 0.25, and your request was too large. Either request a smaller area, or use planet.osm");
 /* Java message loading audio data */ tr("could not get audio input stream from input URL");
 /* Java message loading audio data */ tr("Audio Device Unavailable");
-/* use has not made edits public */ tr("You must make your edits public to upload new data");
+/* OSM server message */ tr("You must make your edits public to upload new data");
 /* Nominatim search place type */ tr("town");
 /* Landuse type used in multipolygons */ tr("forest");
 /* Landuse type used in multipolygons */ tr("farmland");

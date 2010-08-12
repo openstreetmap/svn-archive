64e862194298a88f1e598f769bc88052a3deb154
godofglow <thomas@open-map.org>
2010-08-12 18:39:16 +0000

    erster Schuss

---
+++ applications/rendering/mtb/test.sh
@@ -0,0 +1,6 @@
+ # Stammheim
+/home/project/o/s/m/osm/bin/render --bbox 8.7138,48.68405,8.75478,48.66614 --style /home/godofglow/osm/mtb.xml --size 10000x10000
+
+# Deutschland
+#/home/project/o/s/m/osm/bin/render --bbox 5.56,55.04,15.35,47.15 --style /home/godofglow/osm/mtb.xml --size 10000x10000
+

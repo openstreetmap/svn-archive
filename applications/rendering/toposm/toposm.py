108c9301ca1187da0832063438584c369e82aa1a
Lars Ahlzen Lars Ahlzen <lars@ahlzen.com>
2010-09-11 22:38:54 +0000

    Work on highway shields. Minor style fixes.

--- applications/rendering/toposm/toposm.py
+++ applications/rendering/toposm/toposm.py
@@ -35,7 +35,7 @@ OSM_SRS = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 ' + 
 # A too low number is inefficient. A too high number uses
 # large amounts of memory and sometimes breaks the gdal tools.
 NTILES = {2:1, 3:1, 4:1, 5:1, 6:1, 7:1, 8:1, 9:1, 10:1,
-    11:1, 12:2, 13:4, 14:8, 15:10, 16:12, 17:12, 18:12 }
+    11:2, 12:4, 13:6, 14:8, 15:10, 16:12, 17:12, 18:12 }
 
 # side length (in degrees) for slicing up NED files into more
 # manageable pieces during preprocessing

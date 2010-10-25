d4926baf4f8d6bf7821fe51350a05093d6ee75ba
Nic Roets <nroets@gmail.com>
2010-10-25 08:18:06 +0000

    Rebuild: Allow for larger planet

--- applications/rendering/gosmore/libgosm.cpp
+++ applications/rendering/gosmore/libgosm.cpp
@@ -853,7 +853,7 @@ void GosmLoadAltStyle(const char* elemstylefile, const char* iconscsvfile) {
 #define MAX_BUCKETS (1<<26)
 #define IDXGROUPS 676
 #define NGROUPS 60
-#define MAX_NODES 16000000 /* Max in a group */
+#define MAX_NODES 17000000 /* Max in a group */
 #define S2GROUPS 129 // Last group is reserved for lowzoom halfSegs
 #define NGROUP(x)  ((x) / MAX_NODES % NGROUPS + IDXGROUPS)
 #define S1GROUPS NGROUPS

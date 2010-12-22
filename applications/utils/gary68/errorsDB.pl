fffdbe7cf6d3da776fc7e9912841982e4dfdd336
Gerhard Schwanz <gary@gary68.de>
2010-12-22 12:41:17 +0000

    DB functions

---
+++ applications/utils/gary68/errorsDB.pl
@@ -0,0 +1,190 @@
+
+
+
+# errorsDB.pl
+
+
+use strict ;
+use warnings ;
+
+use OSM::osm ;
+use OSM::osmDB ;
+
+my $version = 1.0 ;
+
+my @allowedREF = qw (motorway motorway_link trunk trunk_link primary primary_link secondary secondary_link tertiary unclassified) ;
+my %allowedREFHash = () ;
+foreach my $a (@allowedREF) { $allowedREFHash{$a} = 1 ; }
+
+my %users  ;
+
+
+#$users{""} = 1 ;
+
+
+# read data from file
+my $wayId ;
+my $wayUser ;
+my @wayNodes ;
+my @wayTags ;
+my %wayProperties ;
+my $nodeId ;
+my $nodeUser ;
+my $nodeLat ;
+my $nodeLon ;
+my @nodeTags ;
+my $aRef0 ;
+my $aRef1 ;
+my $aRef2 ;
+my $relationId ;
+my $relationUser ;
+my @relationTags ;
+my @relationMembers ;
+
+my $txtFile ;
+
+my $line = 0 ;
+my $problems = 0 ;
+
+my $txtFileName = shift ;
+
+my $html ;
+my $htmlName ;
+
+
+open ($txtFile, ">", $txtFileName) or die ("ERROR: couldn't open outfile $txtFileName\n") ;
+
+$htmlName = $txtFileName ;
+$htmlName =~ s/\.txt/\.htm/i ;
+
+open ($html, ">", $htmlName)  or die ("ERROR: couldn't open outfile $htmlName\n");
+printHTMLiFrameHeader ($html, "errors by Gary68") ;
+
+print $html "<H1>Errors by Gary68</H1>\n" ;
+print $html "<p>Version ", $version, "</p>\n" ;
+print $html "<H2>Data</H2>\n" ;
+print $html "<table border=\"1\">\n";
+print $html "<tr>\n" ;
+print $html "<th>Line</th>\n" ;
+print $html "<th>Object</th>\n" ;
+print $html "<th>OSM link</th>\n" ;
+print $html "<th>History link</th>\n" ;
+print $html "<th>User</th>\n" ;
+print $html "<th>Comment</th>\n" ;
+print $html "</tr>\n" ;
+
+
+
+
+dbConnect() ;
+
+loopInitWays ("ref", undef) ;
+$wayId = loopGetNextWay() ;
+
+while (defined $wayId) {
+
+	($aRef0, $aRef1, $aRef2) = getDBWay ($wayId) ;
+	@wayTags = @$aRef2 ;
+	%wayProperties = %$aRef0 ;
+
+	if (! defined $wayProperties{'user'}) {
+		print "user undefined\n" ;
+		$wayUser = "unknown" ;
+	}
+	else {
+		$wayUser = $wayProperties{'user'} ;
+	}
+
+	if ($wayUser eq "") {
+		print "user empty\n" ;
+		$wayUser = "unknown" ;
+	}
+
+#	if (defined $users{$wayUser}) {
+
+		my $ref ; my $refPresent = 0 ; my $refUsed = "" ; my $highway = 0 ; my $invalidRefPresent = 0 ;
+		foreach my $t (@wayTags) {
+			if ( ($t->[0] eq "highway") and (defined $allowedREFHash{$t->[1]}) ) { $highway = 1 ; }
+			if ($t->[0] eq "ref") {
+				$refPresent = 1 ;
+				$refUsed = $t->[1] ;
+				$refUsed =~ s/neu//ig ;
+				$refUsed =~ s/alt//ig ;
+				my @refs ;
+				if (grep /;/, $refUsed) { 
+					@refs = split /;/, $refUsed ; 
+				}
+				else {
+					if (grep /,/, $refUsed) { @refs = split /,/, $refUsed ; }
+					else {
+						if (grep /\//, $refUsed) { @refs = split /\//, $refUsed ; }
+						else {
+							@refs = ($refUsed) ;
+						}
+
+					}
+				}
+				foreach my $r (@refs) {
+					($ref) = ( $r =~ /^(\s*[a-zäöüÄÖÜ]+\s*[0-9]+\s*[a-z]?\s*)$/i ) ;
+					if (!defined $ref) { $invalidRefPresent = 1 ; }
+				}
+			}
+		}
+		if ($refPresent and ($invalidRefPresent) and $highway) {
+			$problems++ ;
+			writeFiles ("way", $wayId, $wayUser, "UNCOMMON REF: $refUsed", $problems) ;
+		}
+
+#	}
+
+	$wayId = loopGetNextWay() ;
+}
+
+dbDisconnect() ;
+
+print $html "</table>\n" ;
+
+
+print "problems found: $problems\n" ;
+
+print $html "<P>problems found: $problems</P>\n" ;
+
+
+printHTMLFoot ($html) ;
+
+
+close ($txtFile) ;
+close ($html) ;
+
+
+# -----------------------------------------------------
+
+sub writeFiles {
+	my ($object, $id, $user, $comment, $line) = @_ ;
+	# print $txtFile "$object,$id,$user,$comment,$line\n" ;
+
+	my $text = sprintf "%s,%d,%s," , $object,$id,$user ;
+	# printf $txtFile "%s,%d,%s, %-80s %-5d\n", $object,$id,$user,$comment,$line ;
+	printf $txtFile "%-50s %-80s %-5d\n", $text, $comment,$line ;
+
+	print $html "<tr>\n" ;
+	print $html "<td>$line</td>\n" ;
+	print $html "<td>$object</td>\n" ;
+	print $html "<td>", objectLink($object, $id) , "</td>\n" ;
+	print $html "<td>", historyLink ($object, $id), "</td>\n" ;
+	print $html "<td>$user</td>\n" ;
+	print $html "<td>$comment</td>\n" ;
+	print $html "</tr>\n" ;
+
+}
+
+# ----------------------------------------------------------------
+
+sub objectLink {
+	my $obj = shift ;
+	my $id = shift ;
+
+	my $link = "<A HREF=\"http://www.openstreetmap.org/?$obj=$id\">$id</A>" ;
+
+	return $link ;
+}
---
+++ applications/utils/gary68/populateDB.pl
@@ -0,0 +1,172 @@
+#
+#
+#
+#
+#
+# Copyright (C) 2010, Gerhard Schwanz
+#
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the 
+# Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>
+#
+
+
+
+use strict ;
+use warnings ;
+
+use OSM::osm ;
+use OSM::osmdb ;
+
+use DBI ;
+
+my $program = "populateDB.pl" ;
+my $version = "1.0 BETA" ;
+my $usage = $program . " file.osm" ;
+
+my $wayId ;
+my $wayId2 ;
+my $wayUser ;
+my @wayNodes ;
+my @wayTags ;
+my $nodeId ;
+my $nodeUser ;
+my $nodeLat ;
+my $nodeLon ;
+my @nodeTags ;
+my $aRef1 ;
+my $aRef2 ;
+my $relationId ;
+my $relationUser ;
+my @relationTags ;
+my @relationMembers ;
+
+
+my $wayCount = 0 ;
+my $nodeCount = 0 ;
+my $relationCount = 0 ;
+
+my $time0 = time() ; my $time1 ; my $time2 ;
+
+my $osmName ;
+
+my $maxK = 0 ;
+my $maxV = 0 ;
+
+###############
+# get parameter
+###############
+$osmName = shift||'';
+if (!$osmName)
+{
+	die (print $usage, "\n");
+}
+
+print "\n$program $version for file $osmName\n\n" ;
+print "\n\n" ;
+
+
+
+
+
+dbConnect() ;
+print "DB connected\n" ;
+
+initTableNodes() ;
+initTableWays() ;
+initTableRelations() ;
+
+
+
+######################
+# get node information
+######################
+print "\nget node information...\n" ;
+openOsmFile ($osmName) ;
+
+
+($nodeId, $nodeLon, $nodeLat, $nodeUser, $aRef1) = getNode2 () ;
+while ($nodeId != -1) {
+	$nodeCount++ ;
+	if ($nodeCount % 10000 == 0) { print "node $nodeCount\n" ; }
+
+	storeDBNode ($nodeId, $nodeLon, $nodeLat, $nodeUser, $aRef1) ;
+
+	# next
+	($nodeId, $nodeLon, $nodeLat, $nodeUser, $aRef1) = getNode2 () ;
+}
+
+
+$time1 = time () ;
+print "\nINFO: nodes finished after ", stringTimeSpent ($time1-$time0), "\n" ;
+
+my $nps = $nodeCount / ($time1-$time0) ;
+my $nph = int ($nps * 3600) ;
+printf "INFO: %10d nodes/h\n", $nph ;
+
+
+print "\nget way information...\n" ;
+
+($wayId, $wayUser, $aRef1, $aRef2) = getWay2 () ;
+while ($wayId != -1) {	
+	$wayCount++ ;
+
+	if ($wayCount % 10000 == 0) { print "way $wayCount\n" ; }
+
+	storeDBWay ($wayId, $wayUser, $aRef1, $aRef2) ;
+
+	# next way
+	($wayId, $wayUser, $aRef1, $aRef2) = getWay2 () ;
+}
+
+
+$time2 = time () ;
+print "\nINFO: ways finished after ", stringTimeSpent ($time2-$time1), "\n" ;
+
+my $wps = $wayCount / ($time2-$time1) ;
+my $wph = int ($wps * 3600) ;
+printf "INFO: %10d nodes/h\n", $nph ;
+printf "INFO: %10d ways/h\n", $wph ;
+
+print "\nget relation information...\n" ;
+
+
+($relationId, $relationUser, $aRef1, $aRef2) = getRelation () ;
+
+while ($relationId != -1) {
+	$relationCount++ ;	
+
+	if ($relationCount % 10000 == 0) { print "rel: $relationCount\n" ; }
+
+	storeDBRelation ($relationId, $relationUser, $aRef1, $aRef2) ;
+
+	#next
+	($relationId, $relationUser, $aRef1, $aRef2) = getRelation () ;
+}
+
+closeOsmFile () ;
+
+
+
+dbDisconnect() ;
+print "DB disconnected\n" ;
+
+my $time3 = time () ;
+print "\nINFO: relations finished after ", stringTimeSpent ($time3-$time2), "\n" ;
+
+my $rps = $relationCount / ($time3-$time2) ;
+my $rph = int ($rps * 3600) ;
+printf "INFO: %10d nodes/h\n", $nph ;
+printf "INFO: %10d ways/h\n", $wph ;printf "INFO: %10d relations/h\n", $rph ;
+
+
+printMaxValues() ;
+
+
+print "\nINFO: finished after ", stringTimeSpent ($time3-$time0), "\n\n" ;
+
+

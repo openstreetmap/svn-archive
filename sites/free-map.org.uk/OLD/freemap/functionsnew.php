b661326bf1efef43e44af8d30a56cd1300863f66
Nick Whitelegg <nick_whitelegg@yahoo.co.uk>
2011-01-28 23:49:35 +0000

    Moved code to Freemap's own SVN server

--- sites/free-map.org.uk/OLD/freemap/functionsnew.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once('../lib/functionsnew.php');
-
-?>
--- sites/free-map.org.uk/OLD/freemap/render/tomerc.h
+++
@@ -1,9 +0,0 @@
-#include "Map.h"
-
-EarthPoint lltomerc(const EarthPoint& ep);
-double lonToMerc(double lon);
-double latToMerc(double lat);
-double mercToLat(double merc_y);
-double phi2(double ts, double e);
-double mercToLon(double merc_x);
-EarthPoint merctoll(const EarthPoint& ep);

ccc5d7fa01b6a63231fe90cb814ca9e9180e7450
Michael Vigovsky <xvmv@mail.ru>
2011-03-24 20:06:37 +0000

    add support for OsGeo tiling scheme in TemplatedTMSTileSource

--- applications/viewer/jmapviewer/src/org/openstreetmap/gui/jmapviewer/tilesources/TemplatedTMSTileSource.java
+++ applications/viewer/jmapviewer/src/org/openstreetmap/gui/jmapviewer/tilesources/TemplatedTMSTileSource.java
@@ -3,18 +3,20 @@ package org.openstreetmap.gui.jmapviewer.tilesources;
 
 public class TemplatedTMSTileSource extends AbstractOsmTileSource {
     private int maxZoom;
-    
+
     public TemplatedTMSTileSource(String name, String url, int maxZoom) {
         super(name, url);
         this.maxZoom = maxZoom;
     }
 
+    @Override
     public String getTileUrl(int zoom, int tilex, int tiley) {
         return this.baseUrl
         .replaceAll("\\{zoom\\}", Integer.toString(zoom))
         .replaceAll("\\{x\\}", Integer.toString(tilex))
-        .replaceAll("\\{y\\}", Integer.toString(tiley));
-        
+        .replaceAll("\\{y\\}", Integer.toString(tiley))
+        .replaceAll("\\{!y\\}", Integer.toString((int)Math.pow(2, zoom)-1-tiley));
+
     }
 
     @Override

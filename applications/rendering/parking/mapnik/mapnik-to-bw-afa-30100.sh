275a83c6d013109472972acd7c07fbf450760206
Kay Drangmeister <kay@drangmeister.net>
2011-06-29 22:20:23 +0000

    original-mapnik no longer needed

--- applications/rendering/parking/mapnik/mapnik-to-bw-afa-30100.sh
+++ applications/rendering/parking/mapnik/mapnik-to-bw-afa-30100.sh
@@ -1,1 +1,1 @@
-python mapnik-to-bw.py -s /cygdrive/c/Works/kay/eclipse-workspace-kay/Parking/mapnik/original-mapnik -f osm.xml -d /cygdrive/c/temp/osmmapnik/deploy
+python mapnik_to_bw.py -s /cygdrive/c/Works/kay/eclipse-workspace-kay/Parking/mapnik/original-mapnik -f osm.xml -d /cygdrive/c/temp/osmmapnik/deploy

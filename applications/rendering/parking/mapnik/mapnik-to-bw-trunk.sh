a7169183e228feb0e4448424c12df93be5404c3c
Kay Drangmeister <kay@drangmeister.net>
2011-06-29 22:18:37 +0000

    settings as patch files

--- applications/rendering/parking/mapnik/mapnik-to-bw-trunk.sh
+++ applications/rendering/parking/mapnik/mapnik-to-bw-trunk.sh
@@ -1,1 +1,20 @@
-python mapnik-to-bw.py -s /home/kayd/workspace/Parking/mapnik/original-mapnik -f osm.xml -d /tmp/deploy
+#! /bin/bash
+# run in the current workspace, assuming that "Parking" and "mapnik" are checked out projects.
+MAPNIKSRC=../../mapnik
+DESTDIR=/tmp/deploy
+TMPDIR=/tmp/parking-mapnik
+#-------------------------------------------------------
+
+MAPNIKPATCHED=$TMPDIR/mapnik
+# cleanup
+rm -rf $DESTDIR
+rm -rf $TMPDIR
+# copy mapnik to temp
+mkdir -p $MAPNIKPATCHED
+cp -r $MAPNIKSRC/* $MAPNIKPATCHED
+# patch mapnik to have local settings
+cp ./mapnik-patch/datasource-settings.xml.inc.trunk $MAPNIKPATCHED/inc/datasource-settings.xml.inc
+cp ./mapnik-patch/fontset-settings.xml.inc.trunk $MAPNIKPATCHED/inc/fontset-settings.xml.inc
+cp ./mapnik-patch/settings.xml.inc.trunk $MAPNIKPATCHED/inc/settings.xml.inc
+# call converter
+python mapnik_to_bw.py -s $MAPNIKPATCHED -f osm.xml -d $DESTDIR

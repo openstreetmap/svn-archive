717613572869d6c345b741890c17af207c61dcca
Kay Drangmeister <kay@drangmeister.net>
2011-07-13 20:31:19 +0000

    change deployment

--- applications/rendering/parking/mapnik/setup_target_style.py
+++ applications/rendering/parking/mapnik/setup_target_style.py
@@ -6,7 +6,7 @@ from optparse import OptionParser
 #from xml.dom.minidom import parse, parseString
 import pxdom
 #import colorsys
-import mapnik_to_bw
+import mapnik_to_bw,generate_parking_layer_xml
 
 def add_license_files(dirname):
     f = open(os.path.join(dirname,"CONTACT"), 'w')
@@ -32,9 +32,9 @@ def setup_deploy_directory(deploy_base_dir,style_name):
 def copy_files(src,dest,files):
     for f in files:
         if type(f) is tuple:
-            print "copy ({a},{b})".format(a=os.path.join(src,f[0]),b=os.path.join(dest,f[1]))
+            shutil.copy2(os.path.join(src,f[0]),os.path.join(dest,f[1]))
         else:
-            print "copy ({a},{b})".format(a=os.path.join(src,f),b=os.path.join(dest,f))
+            shutil.copy2(os.path.join(src,f),os.path.join(dest,f))
 
 def copy_settings_files(settings_dir,target_inc_dir):
     copy_files(settings_dir,target_inc_dir,[
@@ -73,15 +73,23 @@ def main(options):
     #print "shutil.copytree(original_mapnik_dir={o},patched_mapnik_dir={p})".format(o=original_mapnik_dir,p=patched_mapnik_dir)
     shutil.copy2(os.path.join(original_parking_dir,"colorents.xml.inc"),os.path.join(parking_dir,"colorents.xml.inc"))
     shutil.copy2(os.path.join(original_parking_dir,"colorents-bw.xml.inc"),os.path.join(parking_dir,"colorents-bw.xml.inc"))
-    # TODO ^^^^ remove this
+    # TODO: ^^^^ remove this
     shutil.copy2(os.path.join(original_parking_dir,"osm-parktrans-src.xml"),os.path.join(parking_dir,"osm-parktrans-src.xml"))
+    shutil.copy2(os.path.join(original_parking_dir,"osm-parking-src.xml"),os.path.join(parking_dir,"osm-parking-src.xml"))
+    # TODO: ^^^ weg mit dem
     shutil.copytree(os.path.join(patched_mapnik_dir,"inc"),parking_inc_dir)
     original_parking_inc_dir = os.path.join(original_parking_dir,"parking-inc-src")
     #shutil.copytree(original_parking_inc_dir,parking_inc_dir)
     ### files einzeln kopieren...
-    copy_files(original_parking_inc_dir,parking_inc_dir,["layer-parking-area-semi.xml","layer-parking-area.xml","layer-parking-lane.xml"])
+    copy_files(original_parking_inc_dir,parking_inc_dir,["layer-parking-area-semi.xml.inc","layer-parking-area.xml.inc","layer-parking-lane.xml.inc"])
+    shutil.copytree(os.path.join(original_mapnik_dir,"symbols"),os.path.join(parking_dir,"symbols"))
+    shutil.copytree(os.path.join(original_parking_dir,"parking-symbols-src"),os.path.join(parking_dir,"parking-symbols-src"))
 
-
+    # TODO: kludge to copy bw icons to parking/symbols dir
+    # this is ugly because it relies on knowledge of mapnik_to_bw.main() i.e. how the dirs are named.
+    shutil.copytree(os.path.join(os.path.join(deploy_dir,"bw-noicons"),"symbols"),os.path.join(os.path.join(deploy_dir,"parking"),"symbols"))
+    
+    generate_parking_layer_xml.main({'sourcedir':parking_dir, 'sourcefile':'osm-parktrans-src.xml', 'destdir':deploy_dir})
 
 if __name__ == '__main__':
     parser = OptionParser()

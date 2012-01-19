d52c8218c131325244f83ff4d535e66ef815fcf4
Kay Drangmeister <kay@drangmeister.net>
2012-01-19 22:17:29 +0000

    deploy-crite initial file

--- applications/rendering/parking/mapnik/deploy-crite.sh
+++ applications/rendering/parking/mapnik/deploy-crite.sh
@@ -7,3 +7,8 @@ DESTDIR=/tmp/deploy-neu-$TARGETSRV
 TMPDIR=/tmp/tmp-kay-mapnik
 
 python setup_target_style.py -m $MAPNIKSRC -t $TMPDIR -d $DESTDIR -v $TARGETSRV
+
+# convert to mapnik2 (using /usr/local/bin/upgrade_map_xml.py)
+
+upgrade_map_xml.py $TMPDIR/parking/osm-parking.xml $TMPDIR/parking/osm-parking2.xml
+upgrade_map_xml.py $TMPDIR/wifi/osm-wifi.xml $TMPDIR/wifi/osm-wifi2.xml

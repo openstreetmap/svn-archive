9b1f53ba72276b890ccb82abaea9518e0091220c
Kay Drangmeister <kay@drangmeister.net>
2012-01-22 14:39:25 +0000

    new render-samples commands for crite

---
+++ applications/rendering/parking/mapnik/render-samples-crite-to-deploy.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# style directory
+STYLES=/home/osm/styles-deploy
+# image target directory
+IMAGES=/home/osm/wuerzburg-render-film
+
+# Mozart
+# BBOX=9.944475,49.796291,9.948402,49.799359
+
+# Würzburg komplett bis IKEA
+#BBOX=9.911,49.774,9.9929,49.8282
+BBOX=9.9315,49.78324,9.97245,49.81036
+
+# Size = Full HD
+SIZE=1920x1080
+
+# Timestamp
+NOW=`date +%FT%H-%M-%S`
+
+# Render command
+RENDER=/home/osm/bin/render
+
+#$RENDER --bbox $BBOX --size $SIZE --style $STYLES/bw-mapnik/osm-bw.xml --file $IMAGES/map-bw-$NOW
+#$RENDER --bbox $BBOX --size $SIZE --style $STYLES/bw-noicons/osm-bw-noicons.xml --file $IMAGES/map-bw-noicons-$NOW
+#$RENDER --bbox $BBOX --size $SIZE --style $STYLES/parktrans/osm-parktrans.xml --file $IMAGES/map-parktrans-$NOW
+$RENDER --bbox $BBOX --size $SIZE --style $STYLES/parking/osm-parking.xml --file $IMAGES/map-parking-$NOW
+$RENDER --bbox $BBOX --size $SIZE --style $STYLES/wifi/osm-wifi.xml --file $IMAGES/map-wifi-$NOW

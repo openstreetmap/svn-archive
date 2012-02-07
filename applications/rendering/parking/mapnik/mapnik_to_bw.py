5a75797eb946d1f8f43b3c346f6e916bfb404d3c
Kay Drangmeister <kay@drangmeister.net>
2012-02-07 00:38:36 +0000

    buffer_size -> buffer-size changed to be mapnik2 conform

--- applications/rendering/parking/mapnik/mapnik_to_bw.py
+++ applications/rendering/parking/mapnik/mapnik_to_bw.py
@@ -375,7 +375,7 @@ def strip_doctype(f):
     p = subprocess.Popen(['sed','-i','2,5 d',f]) # -i means 'in place'
     p.wait()
     ### FIXME: the following line does not respect the prior attributes but rather sets its own. It should only set srs, buffer_size and maximum-extent.
-    p = subprocess.Popen(['sed','-i','s/[<]Map .*[>]/\<Map background-color="#c9c9c9" srs="+init=epsg;3857" minimum-version="2.0.0" buffer_size="512" maximum-extent="-20037508.342789244,-20037508.342780735,20037508.342789244,20037508.342780709"\>/',f]) # -i means 'in place'
+    p = subprocess.Popen(['sed','-i','s/[<]Map .*[>]/\<Map background-color="#c9c9c9" srs="+init=epsg;3857" minimum-version="2.0.0" buffer-size="512" maximum-extent="-20037508.342789244,-20037508.342780735,20037508.342789244,20037508.342780709"\>/',f]) # -i means 'in place'
     p.wait()
 
 def convert_icons_to_bw(source_symbols_dir,dest_symbols_dir):

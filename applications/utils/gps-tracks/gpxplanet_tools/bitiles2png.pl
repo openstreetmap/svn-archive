a9f71b876b3ca59cc78155afd2c53e036273a306
Ilya Zverev <ilya@zverev.info>
2012-08-17 12:21:13 +0000

    planet.gpx tools, copied from my private repository

---
+++ applications/utils/gps-tracks/gpxplanet_tools/bitiles2png.pl
@@ -0,0 +1,269 @@
+#!/usr/bin/perl
+
+# Create tiles out of processed bitiles.
+# Written by Ilya Zverev, licensed WTFPL.
+
+use strict;
+use GD;
+use Bit::Vector;
+use File::Path 2.07 qw(make_path);
+use File::Basename;
+use Getopt::Long;
+use Fcntl qw( O_RDONLY O_RDWR O_CREAT O_BINARY );
+
+my $source_dir;
+my $dest_dir;
+my $delete_original;
+my $zoom;
+my $colour_str = '0,0,0';
+my $uptozoom;
+my $make_empty_tiles;
+my $help;
+my $verbose;
+
+GetOptions('h|help' => \$help,
+           'v|verbose' => \$verbose,
+           'i|input=s' => \$source_dir,
+           'o|output=s' => \$dest_dir,
+           'z|zoom=i' => \$zoom,
+           'u|zoom0=i' => \$uptozoom,
+           'd|delorig' => \$delete_original,
+           'e|empty' => \$make_empty_tiles,
+           'c|colour=s' => \$colour_str,
+           ) || usage();
+
+if( $help ) {
+    usage();
+}
+
+usage("Please specify input directory with -i") unless defined($source_dir);
+die "Source directory not found" unless -d $source_dir;
+usage("Please specify source zoom level with -z") unless defined($zoom);
+
+$dest_dir = $source_dir unless defined($dest_dir);
+$uptozoom = $zoom unless defined($uptozoom) && $uptozoom <= $zoom;
+
+my @colour = split(",", $colour_str);
+die "Colour should be three comma-separated numbers: r,g,b" unless $#colour == 2;
+
+my $xmin;
+my $ymin;
+my $xmax;
+my $ymax;
+
+process_zoom($zoom);
+
+if( $uptozoom < $zoom && $xmin <= $xmax && $ymin <= $ymax ) {
+    for my $zoomd (1 .. $zoom-$uptozoom) {
+        my $newzoom = $zoom - $zoomd;
+        generate_lowzoom($newzoom);
+        clean_bitiles($newzoom + 1) if $delete_original || $newzoom + 1 < $zoom;
+        process_zoom($newzoom);
+    }
+}
+clean_bitiles($uptozoom) if $delete_original || $uptozoom < $zoom;
+
+sub generate_lowzoom {
+    my $zoom = shift;
+    my $z = $zoom + 1;
+
+    print STDERR "Generating zoom $zoom" if $verbose;
+    for( my $x = $xmin - $xmin%2; $x <= $xmax; $x += 2 ) {
+        print STDERR '.' if $verbose;
+        make_path("$source_dir/$zoom/".($x/2));
+        for( my $y = $ymin - $ymin%2; $y <= $ymax; $y += 2 ) {
+            quadtile(
+                "$source_dir/$z/$x/$y.bitile",
+                "$source_dir/$z/".($x+1)."/$y.bitile",
+                "$source_dir/$z/$x/".($y+1).".bitile",
+                "$source_dir/$z/".($x+1)."/".($y+1).".bitile",
+                "$source_dir/$zoom/".($x/2)."/".($y/2).".bitile"
+            );
+        }
+    }
+    print STDERR "Done\n" if $verbose;
+}
+
+sub quadtile {
+    # (x,y) (x+1,y) (x,y+1) (x+1,y+1) (x/2,y/2)
+    my ($b1file, $b2file, $b3file, $b4file, $result) = @_;
+    return if !-r $b1file && !-r $b2file && !-r $b3file && !-r $b4file;
+
+    my @btiles = (read_bit_vector($b1file), read_bit_vector($b2file),
+        read_bit_vector($b3file), read_bit_vector($b4file));
+    my $res = Bit::Vector->new(65536);
+
+    for my $y (0..255) {
+        for my $x (0..255) {
+            my $b = ($y >> 7) * 2 + ($x >> 7);
+            my $bx = ($x * 2) % 256;
+            my $by = ($y * 2) % 256;
+            $res->Bit_On($y * 256 + $x )
+                if $btiles[$b]->bit_test($by * 256 + $bx)
+                || $btiles[$b]->bit_test($by * 256 + $bx + 1)
+                || $btiles[$b]->bit_test(($by + 1) * 256 + $bx)
+                || $btiles[$b]->bit_test(($by + 1) * 256 + $bx + 1);
+        }
+    }
+
+    sysopen(BITILE, $result, O_RDWR | O_CREAT | O_BINARY);
+    syswrite(BITILE, $res->Block_Read());
+    close BITILE;
+}
+
+sub process_zoom {
+    my $zoom = shift;
+    print STDERR "Generating PNG images for zoom $zoom" if $verbose;
+
+    $xmin = 10**6;
+    $ymin = 10**6;
+    $xmax = -10**6;
+    $ymax = -10**6;
+
+    my $dh;
+    if( !opendir($dh, "$source_dir/$zoom") ) {
+        print STDERR "Fail: cannot open $source_dir/$zoom\n";
+        return;
+    }
+    my @xlist = grep { /^\d+$/ && -d "$source_dir/$zoom/$_" } readdir($dh);
+    closedir($dh);
+    if( $#xlist < 0 ) {
+        print STDERR "No tiles there\n";
+        return;
+    }
+
+    for my $x (@xlist) {
+        my $folder = "$source_dir/$zoom/$x";
+        opendir($dh, $folder) || next;
+        my @ylist = grep { /^\d+\.bitile$/ && -r "$folder/$_" } readdir($dh);
+        closedir($dh);
+
+        print STDERR "." if $verbose;
+        make_path("$dest_dir/$zoom/$x");
+
+        for my $y (@ylist) {
+            $y =~ /^(\d+)/;
+            my $yt = $1;
+
+            my $vec = read_bit_vector("$folder/$y");
+            my $tile = vector2png($vec);
+            open PIC, ">$dest_dir/$zoom/$x/$yt.png";
+            binmode PIC;
+            print PIC $tile->png();
+            close PIC;
+
+            $xmin = $x if $x < $xmin;
+            $xmax = $x if $x > $xmax;
+            $ymin = $yt if $yt < $ymin;
+            $ymax = $yt if $yt > $ymax;
+        }
+    }
+    if( $make_empty_tiles ) {
+        print STDERR "Empty tiles..." if $verbose;
+        generate_empty_tiles($zoom);
+    }
+    print STDERR "Done\n" if $verbose;
+}
+
+sub read_bit_vector {
+    my $filename = shift;
+    my $vec = Bit::Vector->new(65536);
+    if( sysopen(BITILE, $filename, O_RDONLY | O_BINARY) ) {
+        if( sysread(BITILE, my $read, 8192) == 8192 ) {
+            $vec->Block_Store($read);
+        }
+        close BITILE;
+    }
+    return $vec;
+}
+
+sub vector2png {
+    my $vec = shift;
+    my $tile = GD::Image->new(256, 256);
+    my $transp = $tile->colorAllocate(253,254,255);
+    $tile->transparent($transp);
+    $tile->filledRectangle(0,0,255,255,$transp);
+    my $col = $tile->colorAllocate($colour[0], $colour[1], $colour[2]);
+
+    for my $yy (0..255) {
+        for my $xx (0..255) {
+            if( $vec->bit_test($yy * 256 + $xx) ) {
+                $tile->setPixel($xx, $yy, $col);
+            }
+        }
+    }
+    return $tile;
+}
+
+sub generate_empty_tiles {
+    my $z = shift;
+
+    my $img = GD::Image->new(256, 256);
+    my $white = $img->colorAllocate(255, 255, 255);
+    $img->transparent($white);
+    $img->filledRectangle(0,0,255,255,$white);
+    my $empty_tile = $img->png();
+
+    for my $x ($xmin .. $xmax) {
+        for my $y ($ymin .. $ymax) {
+            my $filename = "$dest_dir/$z/$x/$y.png";
+            if( !-f $filename ) {
+                open PIC, ">$filename";
+                binmode PIC;
+                print PIC $empty_tile;
+                close PIC;
+            }
+        }
+    }
+}
+
+sub clean_bitiles {
+    my $zoom = shift;
+
+    print STDERR "Removing z$zoom bitiles\n" if $verbose;
+    opendir(my $dh, "$source_dir/$zoom") || return;
+    my @xlist = grep { /^\d+$/ && -d "$source_dir/$zoom/$_" } readdir($dh);
+    closedir($dh);
+    return if $#xlist < 0;
+
+    for my $x (@xlist) {
+        my $folder = "$source_dir/$zoom/$x";
+        opendir($dh, $folder) || next;
+        my @ylist = grep { /^\d+\.bitile$/ && -r "$folder/$_" } readdir($dh);
+        closedir($dh);
+
+        for my $y (@ylist) {
+            $y =~ /^(\d+)/;
+            my $yt = $1;
+            unlink "$folder/$y" if -f "$dest_dir/$zoom/$x/$yt.png";
+        }
+        rmdir $folder;
+    }
+    rmdir "$source_dir/$zoom";
+}
+
+sub usage {
+    my ($msg) = @_;
+    print STDERR "$msg\n\n" if defined($msg);
+
+    my $prog = basename($0);
+    print STDERR << "EOF";
+This script traverses input directory and creates PNG tiles from all
+bitiles found (created with gpx2bitiles.pl).
+
+usage: $prog [-h] [-v] -i source [-o target] -z zoom [-u zoom] [-d] [-e] [-c colour]
+
+ -h        : print ths help message and exit.
+ -i source : directory with bitiles.
+ -o target : directory to store PNG tiles (by default equal to source).
+ -z zoom   : bitiles zoom level.
+ -u zoom   : generate tiles up to that zoom level (e.g. 0).
+ -d        : delete processed bitiles.
+ -e        : generate empty tiles where there's no bitiles.
+ -c colour : colour of GPS points (three comma-separated numbers: r,g,b
+             default is black).
+ -v        : display progress.
+
+EOF
+    exit;
+}
---
+++ applications/utils/gps-tracks/gpxplanet_tools/gpx2bitiles.pl
@@ -0,0 +1,153 @@
+#!/usr/bin/perl
+
+# This script receives CSV file of "lat,lon" and makes a lot of bitiles.
+# A bitile is a 8k bit array. Those should be converted to PNG images with bitiles2png.pl
+# Written by Ilya Zverev, licensed WTFPL.
+
+use strict;
+use Math::Trig;
+use File::Path 2.07 qw(make_path);
+use File::Basename;
+use Bit::Vector;
+use Fcntl qw( SEEK_SET O_RDWR O_BINARY O_CREAT );
+use Getopt::Long;
+
+my $factor = 10**7; # number by which all coordinates in source CSV are multiplied
+
+my $tile_folder;
+my $zoom;
+my $bbox_str = '-180,-85,180,85';
+#my $bbox_str = '27.4,50,62,63'; # Russia z11..z14 http://www.openstreetmap.org/?box=yes&bbox=27.4,50,62,63
+
+my $help;
+my $verbose;
+my $infile = '-';
+my $thread_str;
+my $state_file = 'gpx2bitiles.state';
+
+GetOptions('h|help' => \$help,
+           'o|output=s' => \$tile_folder,
+           'z|zoom=i' => \$zoom,
+           'v|verbose' => \$verbose,
+           'i|input=s' => \$infile,
+           'b|bbox=s' => \$bbox_str,
+           't|thread=s' => \$thread_str,
+           ) || usage();
+
+if( $help ) {
+  usage();
+}
+
+usage("Please specify output directory with -o") unless defined($tile_folder);
+usage("Please specify zoom level with -z") unless defined($zoom);
+
+my @bbox = split(",", $bbox_str);
+die("badly formed bounding box - use four comma-separated values for ".
+   "bottom left latitude, bottom left longitude, top right latitude, ".
+   "top right longitude") unless $#bbox == 3;
+die("max longitude is less than min longitude") if ($bbox[2] < $bbox[0]);
+die("max latitude is less than min latitude") if ($bbox[3] < $bbox[1]);
+
+my $zoom2 = 2**$zoom;
+my $xmintile = 0;
+my $xmaxtile = 2**18;
+my $thread = '';
+
+if( defined($thread_str) ) {
+  die("Badly formed thread string: it must be THREAD,TOTAL") unless $thread_str =~ /^(\d+),(\d+)$/;
+  $thread = $1;
+  my $total = $2;
+  die("Total number of threads must be no more than 16") if $total <1 || $total > 16;
+  die("Thread number is higher than number of threads") if $thread > $total || $thread < 1;
+
+  my $eps = 10**-8;
+  my $bxmin = int(($bbox[0]+$eps+180)/360 * $zoom2);
+  my $bxmax = int(($bbox[2]-$eps+180)/360 * $zoom2);
+  my $bxwidth = $bxmax - $bxmin + 1;
+  die("Total number of threads is higher than horizontal number of tiles") if $total > $bxwidth;
+  my $width = int(($bxwidth + $total - 1) / $total);
+  $xmintile = ($thread-1) * $width;
+  $xmaxtile = $xmintile + $width - 1;
+  # print STDERR "Thread $thread: width=$width (of $bxwidth, $bxmin..$bxmax), $xmintile..$xmaxtile\n"; exit;
+}
+
+my $count = 0;
+if( open(STATE, "<$state_file$thread") ) {
+  my $line = <STATE>;
+  close STATE;
+  $count = -$1 if $line =~ /^\d+/;
+}
+
+open CSV, "<$infile" or die "Cannot open $infile: $!\n";
+while(<CSV>) {
+  $count++;
+  next if $count <= 0;
+  if( $count % 10000 == 0 && open(STATE, ">$state_file$thread") ) {
+    print STDERR '.' if $verbose;
+    print STATE $count-1;
+    close STATE;
+  }
+
+  next if !/^(-?\d+(?:\.\d+)?),\s*(-?\d+(?:\.\d+)?)/;
+  my $lat = $1 / $factor;
+  my $lon = $2 / $factor;
+  next if $lat <= $bbox[1] || $lat >= $bbox[3] || $lon <= $bbox[0] || $lon >= $bbox[2];
+
+  my $x = ($lon+180)/360 * $zoom2;
+  my $y = (1 - log(tan(deg2rad($lat)) + sec(deg2rad($lat)))/pi)/2 * $zoom2;
+  my $xtile = int($x);
+  my $ytile = int($y);
+  next if $xtile < $xmintile || $xtile > $xmaxtile;
+  my $xpix = int(256 * ($x - $xtile));
+  my $ypix = int(256 * ($y - $ytile));
+  my $folder = "$tile_folder/$zoom/$xtile";
+  make_path($folder);
+  set_pixel("$folder/$ytile.bitile", $xpix, $ypix);
+}
+close CSV;
+
+sub set_pixel {
+  my($filename, $x, $y) = @_;
+  my $vec = Bit::Vector->new(65536);
+  my $read;
+
+  print STDERR "Error opening tile $filename\n" unless sysopen(BITILE, $filename, O_RDWR | O_CREAT | O_BINARY);
+  if( sysread(BITILE, $read, 8192) == 8192 ) {
+      $vec->Block_Store($read);
+  }
+
+  if( !$vec->bit_test($y * 256 + $x) ) {
+    $vec->Bit_On($y * 256 + $x);
+    sysseek(BITILE, 0, SEEK_SET);
+    syswrite(BITILE, $vec->Block_Read());
+  }
+  close BITILE;
+}
+
+sub usage {
+    my ($msg) = @_;
+    print STDERR "$msg\n\n" if defined($msg);
+
+    my $prog = basename($0);
+    print STDERR << "EOF";
+This script receives CSV file of "lat,lon" and makes a lot of bitiles.
+A bitile is a 8k bit array, which can be converted to regular PNG tile
+with bitiles2png.pl
+
+usage: $prog [-h] [-v] [-i file] -o target -z zoom [-b bbox]
+
+ -h        : print ths help message and exit.
+ -i file   : CSV points file to process (default is STDIN).
+ -o target : directory to store bitiles.
+ -z zoom   : generated tiles zoom level.
+ -b bbox   : limit points by bounding box (four comma-separated
+             numbers: minlon,minlat,maxlon,maxlat).
+ -t n,t    : process Nth part of T jobs.
+ -v        : print a dot every 10000 lines.
+
+All coordinates in source CSV file should be multiplied by $factor
+(you can change this number in the code).
+
+EOF
+    exit;
+}

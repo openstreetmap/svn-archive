fdeee23f895a25efb58f6fa4d6b5843f423dcce8
Vincent Privat <vincent.privat@gmail.com>
2013-12-15 16:40:33 +0000

    [josm_terracer] fix #josm9431, #josm9432, #josm9436 - null parameter

--- applications/editors/josm/plugins/terracer/src/terracer/TerracerAction.java
+++ applications/editors/josm/plugins/terracer/src/terracer/TerracerAction.java
@@ -463,7 +463,7 @@ public final class TerracerAction extends JosmAction {
             @Override
             public boolean executeCommand() {
                 boolean result = super.executeCommand();
-                if (result) {
+                if (result && tagsInConflict != null) {
                     try {
                         // Build conflicts commands only after all primitives have been added to dataset to fix #8942
                         List<Command> conflictCommands = CombinePrimitiveResolverDialog.launchIfNecessary(

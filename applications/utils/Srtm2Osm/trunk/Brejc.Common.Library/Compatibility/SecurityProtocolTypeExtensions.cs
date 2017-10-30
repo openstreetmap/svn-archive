e843ea862f607047b1bc54cac680fee2116b7acb
Michael Bemmerl <mail@mx-server.de>
2017-10-30 23:53:16 +0000

    TLS > 1.0 support for .NET < 4.5 through the extension classes.

---
+++ applications/utils/Srtm2Osm/trunk/Brejc.Common.Library/Compatibility/SecurityProtocolTypeExtensions.cs
@@ -0,0 +1,12 @@
+﻿// 1:1 copy from Microsoft's KB3154518
+
+namespace System.Net
+{
+	using System.Security.Authentication;
+	public static class SecurityProtocolTypeExtensions
+	{
+		public const SecurityProtocolType Tls12 = (SecurityProtocolType)SslProtocolsExtensions.Tls12;
+		public const SecurityProtocolType Tls11 = (SecurityProtocolType)SslProtocolsExtensions.Tls11;
+		public const SecurityProtocolType SystemDefault = (SecurityProtocolType)0;
+	}
+}
---
+++ applications/utils/Srtm2Osm/trunk/Brejc.Common.Library/Compatibility/SslProtocolsExtensions.cs
@@ -0,0 +1,10 @@
+﻿// 1:1 copy from Microsoft's KB3154518
+
+namespace System.Security.Authentication
+{
+	public static class SslProtocolsExtensions
+	{
+		public const SslProtocols Tls12 = (SslProtocols)0x00000C00;
+		public const SslProtocols Tls11 = (SslProtocols)0x00000300;
+	}
+}

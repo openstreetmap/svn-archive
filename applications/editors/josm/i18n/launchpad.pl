b2bc6b122f39ac1299faf2f925dd1401ce61e771
Dirk Stöcker <openstreetmap@dstoecker.de>
2020-03-04 10:38:40 +0000

    see #josm18856

--- applications/editors/josm/i18n/launchpad.pl
+++ applications/editors/josm/i18n/launchpad.pl
@@ -16,7 +16,7 @@ $pwd = '';
 
 # list of supported languages
 my %lang = map {$_ => 1} (
-"ast", "bg", "be", "ca", "ca\@valencia", "cs", "da", "de", "el", "en_AU", "en_GB",
+"ar", "ast", "bg", "be", "ca", "ca\@valencia", "cs", "da", "de", "el", "en_AU", "en_GB",
 "es", "et", "fi", "fr", "gl", "hu", "id",
 "it", "ja", "ko", "km", "lt", "mr", "nb", "nl", "pl", "pt", "pt_BR", "ru", "sk",
 "sv", "uk", "vi", "zh_CN", "zh_TW"
